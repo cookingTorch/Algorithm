6e9f5025e59dff71ae19dba7f011a29c92f7bc74
cookingTorch <jhw7181@naver.com>
2023-04-04 20:51:17 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	if (a > b) {
+		printf(">");
+	}
+	if (a < b) {
+		printf("<");
+	}
+	if (a == b) {
+		printf("==");
+	}
+	return 0;
+}
