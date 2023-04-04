57adda42c57afda90e52fbf94c60c1306786110b
cookingTorch <jhw7181@naver.com>
2023-04-04 20:58:07 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 684 ms, Memory: 8928 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmp(const void* a, const void* b) {
+	return *(int*)a - *(int*)b;
+}
+
+int main(void) {
+	int n, i;
+	int* p;
+	p = (int*)malloc(1000000 * sizeof(int));
+
+	scanf("%d", &n);
+
+	for (i = 0; i < n; i++) {
+		scanf("%d", &p[i]);
+	}
+
+	qsort(p, n, sizeof(int), cmp);
+
+	for (i = 0; i < n; i++) {
+		printf("%d\n", p[i]);
+	}
+
+	free(p);
+	return 0;
+}
