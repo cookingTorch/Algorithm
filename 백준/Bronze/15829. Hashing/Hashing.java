629d4e6fa621b391cc1ffe688fde86b9dc119155
cookingTorch <jhw7181@naver.com>
2023-04-08 16:50:59 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -12,15 +12,19 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		String str;
 		
-		int n, i;
-		long ans = 0;
+		int n, i, j;
+		long ri, ans = 0;
 		
 		str = br.readLine();
 		n = Integer.parseInt(str);
 		
 		str = br.readLine();
 		for (i = 0; i < n; i++) {
-			ans += ((((int) str.charAt(i)) - ((int) 'a') + 1) * Math.pow(31, i)) % 1234567891;
+			ri = 1;
+			for (j = 1; j <= i; j++) {
+				ri = (ri * 31) % 1234567891;
+			}
+			ans += ((((int) str.charAt(i)) - ((int) 'a') + 1) * ri) % 1234567891;
 			ans = ans % 1234567891;
 		}
 		
