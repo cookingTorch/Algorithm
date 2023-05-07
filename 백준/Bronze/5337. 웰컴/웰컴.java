9222dacfe8661b6fb90612e74e7422782e32a0aa
cookingTorch <jhw7181@naver.com>
2023-05-07 14:59:25 +0900

    [Bronze V] Title: 웰컴, Time: 76 ms, Memory: 11300 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,22 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(".  .   .").append("\n");
+		sb.append("|  | _ | _. _ ._ _  _").append("\n");
+		sb.append("|/\\|(/.|(_.(_)[ | )(/.").append("\n");
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+
+	}
+
+}
