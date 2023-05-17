496cb6452fc2b00888f0ffb16ec7ee791a572cd0
cookingTorch <jhw7181@naver.com>
2023-05-17 21:35:06 +0900

    [Platinum III] Title: 아이돌, Time: 432 ms, Memory: 57524 KB -BaekjoonHub

--- 백준/Platinum/3648. 아이돌/아이돌.java
+++ 백준/Platinum/3648. 아이돌/아이돌.java
@@ -8,40 +8,41 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static int n, m, sccCnt, vertexCnt;
+	private static int n, vertexCnt, sccCnt;
     private static int[] sccNum, order;
     private static boolean[] finished;
     private static Stack<Integer> stack;
     private static ArrayList<ArrayList<Integer>> adj;
     
     // 찬반 전환
-    private static int opp(int num) {
+    private static int opposite(int num) {
         return num > n ? num - n : num + n;
     }
     
     // DFS
-    private static int dfs(int now) {
-    	int minOrder, t;
-        order[now] = ++vertexCnt;
-        stack.push(now);
-        minOrder = order[now];
-        for (int next : adj.get(now)) {
+    private static int findSCC(int node) {
+    	int minOrder, stackNum;
+        order[node] = ++vertexCnt;
+        stack.push(node);
+        minOrder = order[node];
+        
+        // 연결된 노드 탐색
+        for (int next : adj.get(node)) {
             if (order[next] == 0) {
-            	minOrder = Math.min(minOrder, dfs(next));
+            	minOrder = Math.min(minOrder, findSCC(next));
             }
             else if (!finished[next]) {
             	minOrder = Math.min(minOrder, order[next]);
             }
         }
-        if (minOrder == order[now]) {
-            while (true) {
-                t = stack.pop();
-                finished[t] = true;
-                sccNum[t] = sccCnt;
-                if (t == now) {
-                	break;
-                }
-            }
+        
+        // 루트 노드일 경우 새로운 SCC 형성
+        if (minOrder == order[node]) {
+            do {
+                stackNum = stack.pop();
+                finished[stackNum] = true;
+                sccNum[stackNum] = sccCnt;
+            } while (stackNum != node);
             sccCnt++;
         }
         return minOrder;
@@ -53,7 +54,8 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         String str;
 
-        int i;
+        int m, a, b, i;
+        boolean valid;
         
         // 테스트 케이스
         while (true) {
@@ -65,6 +67,23 @@ public class Main {
             n = Integer.parseInt(st.nextToken());
             m = Integer.parseInt(st.nextToken());
 
+            // 간선 그리기
+            adj = new ArrayList<>();
+            for (i = 0; i < 2 * n + 1; i++) {
+            	adj.add(new ArrayList<>());
+            }
+            adj.get(opposite(1)).add(1);
+            for (i = 0; i < m; i++) {
+            	str = br.readLine();
+                st = new StringTokenizer(str);
+                a = Integer.parseInt(st.nextToken());
+                b = Integer.parseInt(st.nextToken());
+                if (a < 0) a = n - a;
+                if (b < 0) b = n - b;
+                adj.get(opposite(a)).add(b);
+                adj.get(opposite(b)).add(a);
+            }
+            
             // 초기화
             sccCnt = 0;
             vertexCnt = 0;
@@ -72,37 +91,24 @@ public class Main {
             order = new int[2 * n + 1];
             finished = new boolean[2 * n + 1];
             stack = new Stack<>();
-            adj = new ArrayList<>();
-            for (i = 0; i < 2 * n + 1; i++) {
-            	adj.add(new ArrayList<>());
-            }
-
-            // 간선 그리기
-            adj.get(opp(1)).add(1);
-            for (i = 0; i < m; i++) {
-            	str = br.readLine();
-                st = new StringTokenizer(str);
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                if (a < 0) a = n - a;
-                if (b < 0) b = n - b;
-                adj.get(opp(a)).add(b);
-                adj.get(opp(b)).add(a);
-            }
 
             // SCC 탐색
-            for (i = 1; i <= 2 * n; i++)
-                if (order[i] == 0)
-                    dfs(i);
+            for (i = 1; i <= 2 * n; i++) {
+            	if (order[i] == 0) {
+            		findSCC(i);
+            	}
+            }
 
             // 같은 SCC인지 확인
-            boolean valid = true;
+            valid = true;
             for (i = 1; i <= n; i++) {
-                if (sccNum[i] == sccNum[opp(i)]) {
+                if (sccNum[i] == sccNum[opposite(i)]) {
                     valid = false;
                     break;
                 }
             }
+            
+            // 출력
             if (valid) {
             	sb.append("yes\n");
             }
@@ -111,7 +117,6 @@ public class Main {
             }
         }
         
-        // 출력
         bw.write(sb.toString());
         bw.flush();
         bw.close();
