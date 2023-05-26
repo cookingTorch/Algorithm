65c39e2e4135de718e7295c1a5276de75c82d94d
cookingTorch <jhw7181@naver.com>
2023-05-26 13:42:08 +0900

    [Bronze V] Title: Hook, Time: 72 ms, Memory: 11368 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,21 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("#  # #### #### #  #\n");
+		sb.append("#### #  # #  # # #\n");
+		sb.append("#### #  # #  # # #\n");
+		sb.append("#  # #### #### #  #\n");
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+
+}
