54bf6e539fbff1503d0bd0ba77d307de05b5497b
cookingTorch <jhw7181@naver.com>
2023-06-03 19:39:31 +0900

    [Platinum V] Title: 찾기, Time: 516 ms, Memory: 91988 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.java
+++ 백준/Platinum/1786. 찾기/찾기.java
@@ -6,72 +6,73 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 public class Main {
+	
+	private static ArrayList<Integer> ans = new ArrayList<>();
+	
+	// LPS 생성
+	private static int[] findLps(String pat) {
+		int len = 0, i = 1;
+		int[] lps = new int[pat.length()];
+		while (i < pat.length()) {
+			if (pat.charAt(i) == pat.charAt(len)) {
+				len++;
+				lps[i] = len;
+				i++;
+			}
+			else if (len == 0) {
+				i++;
+			}
+			else {
+				len = lps[len - 1];
+			}
+		}
+		return lps;
+	}
+	
+	// KMP
+	private static void kmp(String txt, String pat) {
+		int i = 0, j = 0;
+		int[] lps;
+		lps = findLps(pat);
+		while (i < txt.length()) {
+			if (txt.charAt(i) == pat.charAt(j)) {
+				i++;
+				j++;
+				if (j == pat.length()) {
+					ans.add(i - j + 1);
+					j = lps[j - 1];
+				}
+			}
+			else if (j == 0) {
+				i++;
+			}
+			else {
+				j = lps[j - 1];
+			}
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		
+		int i;
 		String t, p;
 		
-		int i, j, tLength, pLength, cnt, next, flag, num;
-		int[] k;
-		ArrayList<Integer> ans = new ArrayList<>();
+		t = br.readLine();
+		p = br.readLine();
 		
-		t = "0" + br.readLine();
-		p = "0" + br.readLine();
-		tLength = t.length() - 1;
-		pLength = p.length() - 1;
-		k = new int[p.length() + 1];
-		cnt = 0;
-		next = 0;
-		flag = 0;
-		num = 0;
+		kmp(t, p);
 		
-		j = 1;
-		for (i = 2; i <= pLength; i++) {
-			while (j > 1 && p.charAt(i) != p.charAt(j)) {
-				j = k[j] + 1;
-			}
-			if (p.charAt(i) == p.charAt(j)) {
-				j++;
-				k[i + 1] = j - 1;
-			}
-		}
-		
-		for (i = 1; i <= tLength; i++) {
-			for (j = next + 1; j <= pLength; j++) {
-				if (j <= tLength - i + 1 && t.charAt(i + j - 1) != p.charAt(j)) {
-					if (flag == 1) {
-						next = k[j];
-						i = i - 1 + j - next - 1;
-					}
-					break;
-				}
-				else {
-					flag = 1;
-				}
-			}
-			if (j == pLength + 1 && i <= tLength - pLength + 1) {
-				ans.add(i);
-				num++;
-				next = k[j];
-				i = i - 1 + j - next - 1;
-			}
-			if (flag == 0) {
-				next = 0;
-			}
-			flag = 0;
-		}
-		
-		bw.write(Integer.toString(num));
-		bw.newLine();
+		sb.append(ans.size()).append("\n");
 		for (i = 0; i < ans.size(); i++) {
-			bw.write(Integer.toString(ans.get(i)) + " ");
+			sb.append(ans.get(i)).append(" ");
 		}
 		
+		bw.write(sb.toString());
 		bw.flush();
 		bw.close();
-		
 	}
 
 }
