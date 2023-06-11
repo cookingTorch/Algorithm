d515b62f18c83256e307329d1da1079427f5b6c6
cookingTorch <jhw7181@naver.com>
2023-06-11 18:55:17 +0900

    [Bronze V] Title: 나는 누구인가, Time: 76 ms, Memory: 11292 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.java
@@ -0,0 +1,18 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("I'm Sexy");
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+
+}
