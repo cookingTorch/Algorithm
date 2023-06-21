682002c993d8bee50055fc3e3489bca9071b89a9
cookingTorch <jhw7181@naver.com>
2023-06-21 20:54:57 +0900

    [Bronze V] Title: 새로운 시작, Time: 68 ms, Memory: 11308 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.java
@@ -0,0 +1,18 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("파이팅!!");
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+
+}
