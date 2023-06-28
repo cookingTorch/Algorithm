61d19f38ac9d71280d1979135c7c481249bca870
cookingTorch <jhw7181@naver.com>
2023-06-28 20:40:49 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 80 ms, Memory: 11632 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n, i, j;
+
+        n = Integer.parseInt(br.readLine());
+        for (i = n; i > 0; i--) {
+            for (j = 0; j < i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
