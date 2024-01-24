1669eafac542ee531a92efff7afdfc35cca5b2c1
cookingTorch <jhw7181@naver.com>
2024-01-24 12:14:16 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 116 ms, Memory: 15352 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,24 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
 	private static final int INF = Integer.MAX_VALUE;
 	
 	private static int[] distance;
-	private static boolean[] inQueue;
-	private static Queue<Integer> q;
+	private static Deque<Integer> dq;
 	
 	private static void update(int curr, int next, int time) {
 		if (distance[curr] + time < distance[next]) {
 			distance[next] = distance[curr] + time;
-			if (!inQueue[next]) {
-				q.add(next);
-				inQueue[next] = true;
+			if (time == 0) {
+				dq.addFirst(next);
+			} else {
+				dq.add(next);
 			}
 		}
 	}
@@ -26,24 +26,24 @@ public class Main {
 	private static int dist(int n, int k) {
 		int curr;
 		
-		q = new LinkedList<>();
-		inQueue = new boolean[2 * k];
 		distance = new int[2 * k];
 		Arrays.fill(distance, INF);
 		distance[n] = 0;
-		q.add(n);
-		inQueue[n] = true;
-		while (!q.isEmpty()) {
-			curr = q.poll();
-			inQueue[curr] = false;
+		dq = new ArrayDeque<>();
+		dq.add(n);
+		while (!dq.isEmpty()) {
+			curr = dq.poll();
+			if (curr == k) {
+				break;
+			}
+			if (curr * 2 < 2 * k) {
+				update(curr, curr * 2, 0);
+			}
 			if (curr - 1 >= 0) {
 				update(curr, curr - 1, 1);
 			}
 			if (curr + 1 < 2 * k) {
 				update(curr, curr + 1, 1);
-			}
-			if (curr * 2 < 2 * k) {
-				update(curr, curr * 2, 0);
 			}
 		}
 		return distance[k];
