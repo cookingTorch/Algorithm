055df5cf3dca36d51598b8548e5856cf18c4da34
cookingTorch <jhw7181@naver.com>
2024-01-29 20:16:13 +0900

    [Gold V] Title: 평범한 배낭, Time: 156 ms, Memory: 51224 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -5,29 +5,13 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static int[] weight, value;
-	private static int[][] dp;
-	
-	private static int getDp(int i, int j) {
-		if (i < 0 || j < 0) {
-			return 0;
-		}
-		if (dp[i][j] != -1) {
-			return dp[i][j];
-		}
-		if (j < weight[i]) {
-			dp[i][j] = getDp(i - 1, j);
-		} else {
-			dp[i][j] = Math.max(getDp(i - 1, j), getDp(i - 1, j - weight[i]) + value[i]);
-		}
-		return dp[i][j];
-	}
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
-		int n, k, i;
+		int n, k, i, j;
+		int[] weight, value;
+		int[][] dp;
 		
 		st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
@@ -41,9 +25,13 @@ public class Main {
 		}
 		dp = new int[n + 1][k + 1];
 		for (i = 1; i <= n; i++) {
-			Arrays.fill(dp[i], -1);
-			dp[i][0] = 0;
+			for (j = 1; j <= k; j++) {
+				dp[i][j] = dp[i - 1][j];
+				if (j >= weight[i]) {
+					dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - weight[i]] + value[i]);
+				}
+			}
 		}
-		System.out.print(getDp(n, k));
+		System.out.print(dp[n][k]);
 	}
 }
