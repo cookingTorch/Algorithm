c97f154356fa00c21bf35ebb2e2a9a5d5aa801ac
cookingTorch <jhw7181@naver.com>
2024-01-30 10:07:02 +0900

    [Gold V] Title: 상어 초등학교, Time: 112 ms, Memory: 14520 KB -BaekjoonHub

--- 백준/Gold/21608. 상어 초등학교/상어 초등학교.java
+++ 백준/Gold/21608. 상어 초등학교/상어 초등학교.java
@@ -2,119 +2,88 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static int n;
-	private static int[][] seat;
-	private static ArrayList<ArrayList<Integer>> adj;
-	
-	private static int like(int num, int i, int j) {
-		if (i < 0 || j < 0 || i >= n || j >= n) {
-			return 0;
-		}
-		if (adj.get(num).contains(seat[i][j])) {
-			return 1;
-		}
-		return 0;
-	}
-	
-	private static int zero(int i, int j) {
-		if (i < 0 || j < 0 || i >= n || j >= n) {
-			return 0;
-		}
-		if (seat[i][j] == 0) {
-			return 1;
-		}
-		return 0;
-	}
-	
-	private static int getSatisfaction(int num, int i, int j) {
-		int cnt;
-		
-		cnt = like(num, i - 1, j)
-				+ like(num, i + 1, j)
-				+ like(num, i, j - 1)
-				+ like(num, i, j + 1);
-		if (cnt == 0) {
-			return 0;
-		}
-		return (int) Math.pow(10, cnt - 1);
-	}
-	
-	private static int getEmpty(int i, int j) {
-		return zero(i - 1, j)
-				+ zero(i + 1, j)
-				+ zero(i, j - 1)
-				+ zero(i, j + 1);
-	}
-	
-	private static void arrange(int num) {
-		int satisfaction, empty, maxS, maxE, i, j, x, y;
-		
-		x = 0;
-		y = 0;
-		maxS = -1;
-		maxE = -1;
-		for (i = 0; i < n; i++) {
-			for (j = 0; j < n; j++) {
-				if (seat[i][j] != 0) {
-					continue;
-				}
-				satisfaction = getSatisfaction(num, i, j);
-				if (satisfaction > maxS) {
-					x = i;
-					y = j;
-					maxS = satisfaction;
-					maxE = getEmpty(i, j);
-				} else if (satisfaction == maxS && (empty = getEmpty(i, j)) > maxE) {
-					x = i;
-					y = j;
-					maxE = empty;
-				}
-			}
-		}
-		seat[x][y] = num;
-	}
-	
-	private static int getMax() {
-		int ans, i, j;
-		
-		ans = 0;
-		for (i = 0; i < n; i++) {
-			for (j = 0; j < n; j++) {
-				ans += getSatisfaction(seat[i][j], i, j);
-			}
-		}
-		return ans;
-	}
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
-		int size, student, i, j;
+		int n, size, max, temp, ans, i, j, x, y;
 		int[] order;
+		int[][] seat, empty;
+		int[][][] satisfaction;
+		ArrayList<ArrayList<Integer>> adj;
 		
 		n = Integer.parseInt(br.readLine());
 		size = n * n;
-		order = new int[size];
 		adj = new ArrayList<>();
 		for (i = 0; i <= size; i++) {
 			adj.add(new ArrayList<>());
 		}
+		order = new int[size];
 		for (i = 0; i < size; i++) {
 			st = new StringTokenizer(br.readLine());
-			student = Integer.parseInt(st.nextToken());
-			order[i] = student;
+			order[i] = Integer.parseInt(st.nextToken());
 			for (j = 0; j < 4; j++) {
-				adj.get(student).add(Integer.parseInt(st.nextToken()));
+				adj.get(Integer.parseInt(st.nextToken())).add(order[i]);
 			}
 		}
-		seat = new int[n][n];
-		for (int num : order) {
-			arrange(num);
+		satisfaction = new int[size + 1][n + 2][n + 2];
+		empty = new int[n + 2][n + 2];
+		Arrays.fill(empty[1], 3);
+		empty[1][1] = 2;
+		empty[1][n] = 2;
+		for (i = 2; i < n; i++) {
+			Arrays.fill(empty[i], 4);
+			empty[i][1] = 3;
+			empty[i][n] = 3;
 		}
-		System.out.println(getMax());
+		Arrays.fill(empty[n], 3);
+		empty[n][1] = 2;
+		empty[n][n] = 2;
+		seat = new int[n + 2][n + 2];
+		for (int num : order) {
+			max = -1;
+			temp = -1;
+			x = 0;
+			y = 0;
+			for (i = 1; i <= n; i++) {
+				for (j = 1; j <= n; j++) {
+					if (seat[i][j] != 0) {
+						continue;
+					}
+					if (satisfaction[num][i][j] > max) {
+						x = i;
+						y = j;
+						max = satisfaction[num][i][j];
+						temp = empty[i][j];
+					} else if (satisfaction[num][i][j] == max && empty[i][j] > temp) {
+						x = i;
+						y = j;
+						temp = empty[i][j];
+					}
+				}
+			}
+			seat[x][y] = num;
+			for (int other : adj.get(num)) {
+				satisfaction[other][x - 1][y]++;
+				satisfaction[other][x + 1][y]++;
+				satisfaction[other][x][y - 1]++;
+				satisfaction[other][x][y + 1]++;
+			}
+			empty[x - 1][y]--;
+			empty[x + 1][y]--;
+			empty[x][y - 1]--;
+			empty[x][y + 1]--;
+		}
+		ans = 0;
+		for (i = 1; i <= n; i++) {
+			for (j = 1; j <= n; j++) {
+				ans += (int) Math.pow(10, satisfaction[seat[i][j]][i][j] - 1);
+			}
+		}
+		System.out.print(ans);
 	}
 }
