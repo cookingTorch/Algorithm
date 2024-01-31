62e9979ea26ca128eb8aa165c0719ad8343e9d10
cookingTorch <jhw7181@naver.com>
2024-01-31 11:16:56 +0900

    [Gold V] Title: 리모컨, Time: 304 ms, Memory: 74148 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.java
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -1,35 +1,25 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String str;
 		StringTokenizer st;
 		
-		int n, m, i, j, flag = 0, small, large, current, ans, close;
+		int n, m, i, j, flag, small, large, current, ans, close;
 		String button, number, goTo;
 		Character[] broken;
 		
-		str = br.readLine();
-		n = Integer.parseInt(str);
-		str = br.readLine();
-		m = Integer.parseInt(str);
-		
+		n = Integer.parseInt(br.readLine());
+		m = Integer.parseInt(br.readLine());
 		broken = new Character[m];
-		
 		if (m < 10) {
+			flag = 0;
 			if (m > 0) {
-				str = br.readLine();
-				st = new StringTokenizer(str, " ");
+				st = new StringTokenizer(br.readLine());
 				for (i = 0; i < m; i++) {
 					button = st.nextToken();
 					broken[i] = button.charAt(0);
@@ -40,7 +30,6 @@ public class Main {
 					broken[i] = 10; 
 				}
 			}
-			
 			for (i = n; i >= 0; i--) {
 				number = Integer.toString(i);
 				flag = 1;
@@ -56,7 +45,6 @@ public class Main {
 			}
 			if (flag == 1) {
 				small = n - i;
-				
 				for (i = n + 1; i - n < small; i++) {
 					number = Integer.toString(i);
 					flag = 1;
@@ -71,7 +59,6 @@ public class Main {
 					}
 				}
 				large = i - n;
-				
 				if (small <= large) {
 					current = n - small;
 					goTo = Integer.toString(current);
@@ -103,19 +90,12 @@ public class Main {
 				goTo = Integer.toString(current);
 				ans = goTo.length() + large;
 			}
-			
 			close = Math.abs(n - 100);
 		}
 		else {
 			close = Math.abs(n - 100);
 			ans = close + 1;
 		}
-		
-		bw.write(Integer.toString(Math.min(ans, close)));
-		
-		bw.flush();
-		bw.close();
-
+		System.out.print(Math.min(ans, close));
 	}
-
 }
