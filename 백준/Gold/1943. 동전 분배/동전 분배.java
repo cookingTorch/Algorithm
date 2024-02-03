552bc29fc9f75d85af086de95ec64fb8a63f740d
cookingTorch <jhw7181@naver.com>
2024-02-03 22:03:54 +0900

    [Gold III] Title: 동전 분배, Time: 296 ms, Memory: 17196 KB -BaekjoonHub

---
+++ 백준/Gold/1943. 동전 분배/동전 분배.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static int solution(BufferedReader br, StringTokenizer st) throws IOException {
+		int n, sum, coin, num, size, i, j;
+		int[] dp;
+		ArrayList<Integer> coins;
+		
+		n = Integer.parseInt(br.readLine());
+		sum = 0;
+		coins = new ArrayList<>();
+		coins.add(0);
+		for (i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			coin = Integer.parseInt(st.nextToken());
+			num = Integer.parseInt(st.nextToken());
+			sum += coin * num;
+			for (j = 1; j < num; num -= j, j *= 2) {
+				coins.add(coin * j);
+			}
+			coins.add(coin * num);
+		}
+		if (sum % 2 != 0) {
+			return 0;
+		}
+		sum /= 2;
+		size = coins.size() - 1;
+		dp = new int[sum + 1];
+		for (i = 1; i <= size; i++) {
+			for (j = sum; j >= 1; j--) {
+				if (j >= coins.get(i)) {
+					dp[j] = Math.max(dp[j], dp[j - coins.get(i)] + coins.get(i));
+				}
+			}
+		}
+		if (dp[sum] == sum) {
+			return 1;
+		}
+		return 0;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+		
+		int testCase;
+		
+		for (testCase = 0; testCase < 3; testCase++) {
+			sb.append(solution(br, st)).append('\n');
+		}
+		System.out.print(sb);
+	}
+}
