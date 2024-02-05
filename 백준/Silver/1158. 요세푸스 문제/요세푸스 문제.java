637ff1ff4f42c16cffbf7c45111bf16242505ca6
cookingTorch <jhw7181@naver.com>
2024-02-05 17:25:57 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 88 ms, Memory: 11980 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -36,7 +34,6 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
@@ -45,10 +42,8 @@ public class Main {
 		st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		k = Integer.parseInt(st.nextToken());
-		
 		tree = new int[4 * n];
 		init(1, 1, n);
-		
 		idx = k;
 		sb.append('<');
 		for (i = 1; i <= n; i++) {
@@ -59,9 +54,6 @@ public class Main {
 			}
 		}
 		sb.append('>');
-		
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
+		System.out.print(sb.toString());
 	}
 }
