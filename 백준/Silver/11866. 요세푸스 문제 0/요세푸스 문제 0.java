b1fa225f255ca581890dd7af09c8027154445ddb
cookingTorch <jhw7181@naver.com>
2024-02-05 17:33:02 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 76 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,69 +1,59 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Deque;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
-
+	private static int[] tree;
+	
+	private static void init(int node, int start, int end) {
+		int mid;
+		
+		tree[node] = end - start + 1;
+		if (start < end) {
+			mid = (start + end) / 2;
+			init(node * 2, start, mid);
+			init(node * 2 + 1, mid + 1, end);
+		}
+	}
+	
+	private static int delete(int node, int start, int end, int idx) {
+		int mid;
+		
+		tree[node]--;
+		if (start < end) {
+			mid = (start + end) / 2;
+			if (idx <= tree[node * 2]) {
+				return delete(node * 2, start, mid, idx);
+			} else {
+				return delete(node * 2 + 1, mid + 1, end, idx - tree[node * 2]);
+			}
+		}
+		return start;
+	}
+	
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String str;
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
-		int n, k, i, j, num = 0;
-		Deque<Integer> circle = new LinkedList<>();
-		Deque<Integer> temp = new LinkedList<>();
+		int n, k, idx, i;
 		
-		str = br.readLine();
-		st = new StringTokenizer(str, " ");
+		st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		k = Integer.parseInt(st.nextToken());
-		
-		for (i = 0; i < n; i++) {
-			circle.addLast(i + 1);
+		tree = new int[4 * n];
+		init(1, 1, n);
+		idx = k;
+		sb.append('<');
+		for (i = 1; i <= n; i++) {
+			sb.append(delete(1, 1, n, idx));
+			if (i < n) {
+				idx = ((idx + k - 2) % (n - i)) + 1;
+				sb.append(", ");
+			}
 		}
-		
-		bw.write("<");
-		
-		for (i = 0; i < n; i++) {
-			
-			for (j = 0; j < k - 1; j++) {
-				num++;
-				if (num == circle.size()) {
-					num = 0;
-				}
-			}
-			
-			for (j = 0; j < num; j++) {
-				temp.addLast(circle.pollFirst());
-			}
-			
-			bw.write(Integer.toString(circle.pollFirst()));
-			
-			while (temp.size() > 0) {
-				circle.addFirst(temp.pollLast());
-			}
-			
-			if (circle.size() > 0) {
-				bw.write(", ");
-			}
-			
-			if (num == circle.size()) {
-				num = 0;
-			}
-			
-		}
-		
-		bw.write(">");
-		
-		bw.flush();
-		bw.close();
+		sb.append('>');
+		System.out.print(sb.toString());
 	}
-
 }
