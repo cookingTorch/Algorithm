b4a1f5f8b619ef448e4499a046717d435eaa57ed
cookingTorch <jhw7181@naver.com>
2024-02-05 17:17:57 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 96 ms, Memory: 12080 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,40 +1,67 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
+	private static int[] tree;
+	
+	private static void init(int node, int start, int end) {
+		int mid;
+		
+		tree[node] = end - start + 1;
+		if (start < end) {
+			mid = (start + end) / 2;
+			init(node * 2, start, mid);
+			init(node * 2 + 1, mid + 1, end);
+		}
+	}
+	
+	private static int delete(int node, int start, int end, int idx) {
+		int mid;
+		
+		tree[node]--;
+		if (start < end) {
+			mid = (start + end) / 2;
+			if (idx <= tree[node * 2]) {
+				return delete(node * 2, start, mid, idx);
+			} else {
+				return delete(node * 2 + 1, mid + 1, end, idx - tree[node * 2]);
+			}
+		}
+		return start;
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
-		int n, k, curr, i;
-		Queue<Integer> q, ans;
+		int n, k, idx, i;
 		
 		st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		k = Integer.parseInt(st.nextToken());
-		q = new ArrayDeque<>();
+		
+		tree = new int[4 * n];
+		init(1, 1, n);
+		
+		idx = k;
+		sb.append('<');
 		for (i = 1; i <= n; i++) {
-			q.add(i);
-		}
-		ans = new ArrayDeque<>();
-		while (!q.isEmpty()) {
-			for (i = 0; i < k - 1; i++) {
-				curr = q.poll();
-				q.add(curr);
+			sb.append(delete(1, 1, n, idx));
+			if (i < n) {
+				idx = ((idx + k - 2) % (n - i)) + 1;
+				sb.append(", ");
 			}
-			curr = q.poll();
-			ans.add(curr);
-		}
-		sb.append('<').append(ans.poll());
-		for (i = 1; i < n; i++) {
-			sb.append(", ").append(ans.poll());
 		}
 		sb.append('>');
-		System.out.print(sb);
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
 	}
 }
