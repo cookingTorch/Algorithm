d0e72e0dcf15eeb381cac85bd9ad36c2fbd3637a
cookingTorch <jhw7181@naver.com>
2024-02-05 15:26:14 +0900

    [Gold V] Title: 탑, Time: 592 ms, Memory: 105764 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,43 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
+	private static final int INF = Integer.MAX_VALUE;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
-		int n, height, temp, idx, i;
+		int n, curr, i;
 		int[] tower;
-		boolean flag;
-		Stack<int[]> towers;
+		Deque<int[]> towers;
 		
 		n = Integer.parseInt(br.readLine());
-		towers = new Stack<>();
-		idx = 0;
-		tower = new int[2];
+		towers = new ArrayDeque<>();
+		tower = new int[] {0, INF};
+		towers.push(tower);
 		st = new StringTokenizer(br.readLine());
-		for (i = 0; i < n; i++) {
-			height = Integer.parseInt(st.nextToken());
-			temp = -1;
-			flag = true;
-			while (temp < height) {
-				if (towers.isEmpty()) {
-					sb.append("0 ");
-					flag = false;
-					break;
-				}
-				tower = towers.pop();
-				temp = tower[0];
+		for (i = 1; i <= n; i++) {
+			curr = Integer.parseInt(st.nextToken());
+			while (tower[1] < curr) {
+				towers.pop();
+				tower = towers.peek();
 			}
-			if (flag) {
-				sb.append(tower[1]).append(' ');
-				towers.add(tower);
-			}
-			towers.add(new int[] {height, ++idx});
+			sb.append(tower[0]).append(' ');
+			tower = new int[] {i, curr};
+			towers.push(tower);
 		}
 		System.out.print(sb);
 	}
