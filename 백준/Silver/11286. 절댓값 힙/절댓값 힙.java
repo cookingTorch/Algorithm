27eee0122470465fb2f80827e4fbcd37dea45417
cookingTorch <jhw7181@naver.com>
2024-02-07 14:22:57 +0900

    [Silver I] Title: 절댓값 힙, Time: 248 ms, Memory: 21544 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,36 +1,85 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.PriorityQueue;
 
 public class Main {
+	private static class AbsPQ {
+		private int size;
+		private int[] arr;
+		
+		AbsPQ() {
+			size = 0;
+			arr = new int[100001];
+		}
+		
+		void add(int val) {
+			int idx, parentIdx, parentVal;
+			
+			idx = ++size;
+			arr[idx] = val;
+			while (idx > 1) {
+				parentIdx = idx / 2;
+				parentVal = arr[parentIdx];
+				if (compare(val, parentVal) >= 0) {
+					break;
+				}
+				arr[idx] = parentVal;
+				idx = parentIdx;
+			}
+			arr[idx] = val;
+		}
+		
+		int poll() {
+			int val, last, parent, child, right;
+			
+			if (size == 0) {
+				return 0;
+			}
+			val = arr[1];
+			last = arr[size];
+			arr[size--] = 0;
+			parent = 1;
+			while ((child = parent * 2) <= size) {
+				right = parent * 2 + 1;
+				if (right <= size && compare(arr[child], arr[right]) > 0) {
+					child = right;
+				}
+				if (compare(last, arr[child]) <= 0) {
+					break;
+				}
+				arr[parent] = arr[child];
+				parent = child;
+			}
+			arr[parent] = last;
+			return val;
+		}
+		
+		private int compare(int a, int b) {
+			if (Math.abs(a) == Math.abs(b)) {
+				return a - b;
+			}
+			if (Math.abs(a) < Math.abs(b)) {
+				return -1;
+			}
+			return 1;
+		}
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
-		int n, x, i;
-		PriorityQueue<Integer> pq;
+		int n, num, i;
+		AbsPQ pq = new AbsPQ();
 		
-		pq = new PriorityQueue<>(new Comparator<Integer>() {
-			@Override
-			public int compare(Integer o1, Integer o2) {
-				return Math.abs(o1) == Math.abs(o2) ? o1 - o2 : Math.abs(o1) - Math.abs(o2);
-			}
-		});
 		n = Integer.parseInt(br.readLine());
 		for (i = 0; i < n; i++) {
-			x = Integer.parseInt(br.readLine());
-			if (x == 0) {
-				if (pq.isEmpty()) {
-					sb.append('0').append('\n');
-				} else {
-					sb.append(pq.poll()).append('\n');
-				}
+			if ((num = Integer.parseInt(br.readLine())) == 0) {
+				sb.append(pq.poll()).append('\n');
 			} else {
-				pq.add(x);
+				pq.add(num);
 			}
 		}
-		System.out.println(sb);
+		System.out.print(sb);
 	}
 }
