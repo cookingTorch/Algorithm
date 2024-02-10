3e3352d71426620608a18ef46e6e940785ff503c
cookingTorch <jhw7181@naver.com>
2024-02-10 14:55:33 +0900

    [Gold IV] Title: DSLR, Time: 1680 ms, Memory: 298496 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -1,83 +1,70 @@
-import java.io.*;
-import java.util.LinkedList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static class pair {
-        String c;
-        int n;
-
-        pair(String c, int n) {
-            this.c = c;
-            this.n = n;
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-
-        int t, testCase, a, b, i;
-        boolean[] visited = new boolean[10000];
-        Queue<pair> q = new LinkedList<>();
-        pair node, next;
-
-        testCase = Integer.parseInt(br.readLine());
-        for (t = 0; t < testCase; t++) {
-            st = new StringTokenizer(br.readLine());
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
-            for (i = 0; i < 10000; i++)
-                visited[i] = false;
-            visited[a] = true;
-            q.add(new pair("", a));
-            while (!q.isEmpty()) {
-                node = q.poll();
-                next = new pair(node.c + "D", (2 * node.n) % 10000);
-                if (next.n == b) {
-                    sb.append(next.c).append("\n");
-                    break;
-                }
-                if (!visited[next.n]) {
-                    visited[next.n] = true;
-                    q.add(next);
-                }
-                next = new pair(node.c + "S", (node.n + 9999) % 10000);
-                if (next.n == b) {
-                    sb.append(next.c).append("\n");
-                    break;
-                }
-                if (!visited[next.n]) {
-                    visited[next.n] = true;
-                    q.add(next);
-                }
-                next = new pair(node.c + "L", ((10 * node.n) + (node.n / 1000)) % 10000);
-                if (next.n == b) {
-                    sb.append(next.c).append("\n");
-                    break;
-                }
-                if (!visited[next.n]) {
-                    visited[next.n] = true;
-                    q.add(next);
-                }
-                next = new pair(node.c + "R", ((node.n + ((node.n % 10) * 10000)) / 10) % 10000);
-                if (next.n == b) {
-                    sb.append(next.c).append("\n");
-                    break;
-                }
-                if (!visited[next.n]) {
-                    visited[next.n] = true;
-                    q.add(next);
-                }
-            }
-            q.clear();
-        }
-
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-    }
+	private static int a, b, curr;
+	private static int[] prev;
+	private static char[] command;
+	private static Queue<Integer> q;
+	
+	private static boolean add(char dslr, int next) {
+		if (command[next] == '\0') {
+			command[next] = dslr;
+			prev[next] = curr;
+			if (next == b) {
+				return true;
+			}
+			q.add(next);
+		}
+		return false;
+	}
+	
+	private static void bfs() {
+		command = new char[10000];
+		q = new ArrayDeque<>();
+		q.add(a);
+		while (!q.isEmpty()) {
+			curr = q.poll();
+			if (add('D', curr * 2 % 10000)
+					|| add('S', curr == 0 ? 9999 : curr - 1)
+					|| add('L', curr % 1000 * 10 + curr / 1000)
+					|| add('R', curr / 10 + curr % 10 * 1000)) {
+				break;
+			}
+		}
+	}
+	
+	private static StringBuilder ans(int curr) {
+		if (curr == a) {
+			return new StringBuilder();
+		}
+		return ans(prev[curr]).append(command[curr]);
+	}
+	
+	private static StringBuilder solution(BufferedReader br, StringTokenizer st) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		a = Integer.parseInt(st.nextToken());
+		b = Integer.parseInt(st.nextToken());
+		bfs();
+		return ans(b);
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+		
+		int t, testCase;
+		
+		prev = new int[10000];
+		t = Integer.parseInt(br.readLine());
+		for (testCase = 0; testCase < t; testCase++) {
+			sb.append(solution(br, st)).append('\n');
+		}
+		System.out.print(sb);
+	}
 }
