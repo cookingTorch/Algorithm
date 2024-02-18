23ad31681a601d05b5cad01c1e7bdbfa9014320d
cookingTorch <jhw7181@naver.com>
2024-02-19 08:36:31 +0900

    [Gold II] Title: 선분 교차 2, Time: 76 ms, Memory: 11528 KB -BaekjoonHub

--- 백준/Gold/17387. 선분 교차 2/선분 교차 2.java
+++ 백준/Gold/17387. 선분 교차 2/선분 교차 2.java
@@ -17,11 +17,27 @@ public class Main {
 		return Long.compare((p2.x - p1.x) * (p3.y - p2.y) - (p3.x - p2.x) * (p2.y - p1.y), 0);
 	}
 	
+	private static int cross(Point a, Point b, Point c, Point d) {
+		long c1, c2, c3, c4;
+		
+		if ((c1 = ccw(a, b, c)) * (c2 = ccw(a, b, d)) <= 0 && (c3 = ccw(c, d, a)) * (c4 = ccw(c, d, b)) <= 0) {
+			if (c1 == 0 && c2 == 0 && c3 == 0 && c4 == 0) {
+				if (Math.max(a.x, b.x) < Math.min(c.x, d.x) || Math.max(c.x, d.x) < Math.min(a.x, b.x)) {
+					return 0;
+				} else if (Math.max(a.y, b.y) < Math.min(c.y, d.y) || Math.max(c.y, d.y) < Math.min(a.y, b.y)) {
+					return 0;
+				}
+			}
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
-		long c1, c2, c3, c4;
 		Point a, b, c, d;
 		
 		st = new StringTokenizer(br.readLine());
@@ -30,19 +46,6 @@ public class Main {
 		st = new StringTokenizer(br.readLine());
 		c = new Point(Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken()));
 		d = new Point(Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken()));
-		if ((c1 = ccw(a, b, c)) * (c2 = ccw(a, b, d)) <= 0 && (c3 = ccw(c, d, a)) * (c4 = ccw(c, d, b)) <= 0) {
-			if (c1 == 0 && c2 == 0 && c3 == 0 && c4 == 0) {
-				if (Math.max(a.x, b.x) < Math.min(c.x, d.x) || Math.max(c.x, d.x) < Math.min(a.x, b.x)) {
-					System.out.print('0');
-					return;
-				} else if (Math.max(a.y, b.y) < Math.min(c.y, d.y) || Math.max(c.y, d.y) < Math.min(a.y, b.y)) {
-					System.out.print('0');
-					return;
-				}
-			}
-			System.out.print('1');
-		} else {
-			System.out.print('0');
-		}
+		System.out.print(cross(a, b, c, d));
 	}
 }
