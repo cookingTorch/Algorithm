61300ae8dd913f5e27a23192ba68211dbdbbdc78
cookingTorch <jhw7181@naver.com>
2024-02-21 17:23:46 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 140 ms, Memory: 29,504 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -7,20 +7,20 @@ import java.util.StringTokenizer;
 
 class Solution {
 	private static final int INF = Integer.MAX_VALUE;
-	private static final int SIZE = 14;
+	private static final int NUM = 12;
+	private static final int SIZE = NUM + 2;
 	private static final char POWER = '\0';
 	private static final char CORE = '1';
 	private static final char EMPTY = '0';
 	
-	private static int max, ans;
-	private static int[] len;
+	private static int max, ans, coresSize, lenSize;
+	private static int[] cores, temp, len;
 	private static char[] map;
 	private static boolean[] visited;
-	private static LinkedList<Integer> cores, temp;
 	private static ArrayList<LinkedList<Character>> lines, cross;
 	
 	private static void select(int core, int cnt, int sum) {
-		if (core == cores.size()) {
+		if (core == coresSize) {
 			if (cnt > max) {
 				max = cnt;
 				ans = sum;
@@ -44,48 +44,49 @@ class Solution {
 	}
 	
 	private static void connect() {
-		int curr, size, num, i;
+		int curr, core, num, i, j;
 		char idx;
 		LinkedList<Character> list;
 		
-		size = cores.size() * 4 + 1;
+		lenSize = coresSize * 4 + 1;
 		lines = new ArrayList<>();
-		len = new int[size];
 		idx = 0;
-		for (int core : cores) {
+		for (i = 0; i < coresSize; i++) {
+			core = cores[i];
 			list = new LinkedList<>();
 			idx++;
 			curr = core - 1;
-			for (i = 0; map[curr] != CORE && map[curr] != POWER; i++) {
+			for (j = 0; map[curr] != CORE && map[curr] != POWER; j++) {
 				map[curr] = idx;
 				curr--;
 			}
 			if (map[curr] == POWER) {
-				len[idx] = i;
+				len[idx] = j;
 				list.add(idx);
 			}
 			idx++;
 			curr = core + 1;
-			for (i = 0; map[curr] != CORE && map[curr] != POWER; i++) {
+			for (j = 0; map[curr] != CORE && map[curr] != POWER; j++) {
 				map[curr] = idx;
 				curr++;
 			}
 			if (map[curr] == POWER) {
-				len[idx] = i;
+				len[idx] = j;
 				list.add(idx);
 			}
 			lines.add(list);
 		}
 		cross = new ArrayList<>();
-		for (i = 0; i < size; i++) {
+		for (j = 0; j < lenSize; j++) {
 			cross.add(new LinkedList<>());
 		}
 		num = 0;
-		for (int core : cores) {
+		for (i = 0; i < coresSize; i++) {
+			core = cores[i];
 			list = lines.get(num++);
 			idx++;
 			curr = core - SIZE;
-			for (i = 0; map[curr] != CORE && map[curr] != POWER; i++) {
+			for (j = 0; map[curr] != CORE && map[curr] != POWER; j++) {
 				if (map[curr] != EMPTY) {
 					cross.get(map[curr]).add(idx);
 					cross.get(idx).add(map[curr]);
@@ -93,12 +94,12 @@ class Solution {
 				curr -= SIZE;
 			}
 			if (map[curr] == POWER) {
-				len[idx] = i;
+				len[idx] = j;
 				list.add(idx);
 			}
 			idx++;
 			curr = core + SIZE;
-			for (i = 0; map[curr] != CORE && map[curr] != POWER; i++) {
+			for (j = 0; map[curr] != CORE && map[curr] != POWER; j++) {
 				if (map[curr] != EMPTY) {
 					cross.get(map[curr]).add(idx);
 					cross.get(idx).add(map[curr]);
@@ -106,23 +107,22 @@ class Solution {
 				curr += SIZE;
 			}
 			if (map[curr] == POWER) {
-				len[idx] = i;
+				len[idx] = j;
 				list.add(idx);
 			}
 		}
-		visited = new boolean[size];
 	}
 	
 	private static int solution(BufferedReader br, StringTokenizer st) throws IOException {
-		int n, pos, add, x, y, i, j;
+		int n, pos, add, x, y, tempSize, core, i, j;
 		
 		n = Integer.parseInt(br.readLine());
 		for (i = 1; i <= n; i++) {
 			map[(n + 1) * SIZE + i] = POWER;
 			map[i * SIZE + n + 1] = POWER;
 		}
-		cores = new LinkedList<>();
-		temp = new LinkedList<>();
+		coresSize = 0;
+		tempSize = 0;
 		for (i = 1; i <= n; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (j = 1; j <= n; j++) {
@@ -130,21 +130,22 @@ class Solution {
 				map[pos] = st.nextToken().charAt(0);
 				if (map[pos] == CORE && i != 1 && i != n && j != 1 && j != n) {
 					if (i == 2 || i == n - 1 || j == 2 || j == n - 1) {
-						temp.add(pos);
+						temp[tempSize++] = pos;
 					} else {
-						cores.add(pos);
+						cores[coresSize++] = pos;
 					}
 				}
 			}
 		}
 		add = 0;
-		for (int core : temp) {
+		for (i = 0; i < tempSize; i++) {
+			core = temp[i];
 			x = core / SIZE;
 			y = core % SIZE;
 			if ((x == 2 && map[core - SIZE] == EMPTY) || (x == n - 1 && map[core + SIZE] == EMPTY) || (y == 2 && map[core - 1] == EMPTY) || (y == n - 1 && map[core + 1] == EMPTY)) {
 				add++;
 			} else {
-				cores.add(core);
+				cores[coresSize++] = core;
 			}
 		}
 		connect();
@@ -162,6 +163,10 @@ class Solution {
 		int t, testCase;
 		
 		map = new char[SIZE * SIZE];
+		cores = new int[NUM];
+		temp = new int[NUM];
+		len = new int[NUM * 4 + 1];
+		visited = new boolean[NUM * 4 + 1];
 		t = Integer.parseInt(br.readLine());
 		for (testCase = 1; testCase <= t; testCase++) {
 			sb.append('#').append(testCase).append(' ').append(solution(br, st)).append('\n');
