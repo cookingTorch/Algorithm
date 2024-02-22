92bf133f36f6f76a13e79bb00d9ed60a8192dbf1
cookingTorch <jhw7181@naver.com>
2024-02-22 09:55:28 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1132 ms, Memory: 324640 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -1,11 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
+	private static class Edge implements Comparable<Edge> {
+		int u, v, weight;
+		
+		Edge(int u, int v, int weight) {
+			this.u = u;
+			this.v = v;
+			this.weight = weight;
+		}
+		
+		@Override
+		public int compareTo(Edge o) {
+			return Integer.compare(this.weight, o.weight);
+		}
+	}
+	
 	private static int[] root;
 	
 	private static boolean union(int u, int v) {
@@ -37,28 +51,23 @@ public class Main {
 		StringTokenizer st;
 		
 		int n, m, ans, i;
-		int[] curr;
-		PriorityQueue<int[]> pq;
+		Edge edge;
+		PriorityQueue<Edge> pq;
 		
 		st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
-		pq = new PriorityQueue<>(new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return Integer.compare(o1[2], o2[2]);
-			}
-		});
+		pq = new PriorityQueue<>();
 		for (i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
-			pq.add(new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
+			pq.add(new Edge(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
 		}
 		root = new int[n + 1];
 		ans = 0;
 		for (i = 0; i < n - 2;) {
-			curr = pq.poll();
-			if (union(curr[0], curr[1])) {
-				ans += curr[2];
+			edge = pq.poll();
+			if (union(edge.u, edge.v)) {
+				ans += edge.weight;
 				i++;
 			}
 		}
