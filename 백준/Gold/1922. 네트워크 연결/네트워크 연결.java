8a9e53f0fe9db1fa134c8a82781c840ab6f296ee
cookingTorch <jhw7181@naver.com>
2024-02-22 09:58:55 +0900

    [Gold IV] Title: 네트워크 연결, Time: 348 ms, Memory: 44532 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -1,28 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static int[] parent;
-	
-	private static boolean union(int node1, int node2) {
-		int parent1, parent2;
+	private static class Edge implements Comparable<Edge> {
+		int u, v, weight;
 		
-		if ((parent1 = find(node1)) == (parent2 = find(node2))) {
+		Edge(int u, int v, int weight) {
+			this.u = u;
+			this.v = v;
+			this.weight = weight;
+		}
+		
+		@Override
+		public int compareTo(Edge o) {
+			return Integer.compare(this.weight, o.weight);
+		}
+	}
+	
+	private static int[] root;
+	
+	private static boolean union(int u, int v) {
+		int ru, rv;
+		
+		if ((ru = find(u)) == (rv = find(v))) {
 			return false;
 		}
-		parent[parent2] = parent1;
+		if (root[ru] > root[rv]) {
+			root[ru] = rv;
+		} else {
+			if (root[ru] == root[rv]) {
+				root[ru]--;
+			}
+			root[rv] = ru;
+		}
 		return true;
 	}
 	
-	private static int find(int node) {
-		if (parent[node] == node) {
-			return node;
+	private static int find(int u) {
+		if (root[u] <= 0) {
+			return u;
 		}
-		return parent[node] = find(parent[node]);
+		return root[u] = find(root[u]);
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -30,31 +51,23 @@ public class Main {
 		StringTokenizer st;
 		
 		int n, m, ans, i;
-		int[][] edge;
+		Edge edge;
+		PriorityQueue<Edge> pq;
 		
 		n = Integer.parseInt(br.readLine());
 		m = Integer.parseInt(br.readLine());
-		edge = new int[m][3];
+		pq = new PriorityQueue<>();
 		for (i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
-			edge[i][0] = Integer.parseInt(st.nextToken()) - 1;
-			edge[i][1] = Integer.parseInt(st.nextToken()) - 1;
-			edge[i][2] = Integer.parseInt(st.nextToken());
+			pq.add(new Edge(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
 		}
-		Arrays.sort(edge, new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return Integer.compare(o1[2], o2[2]);
-			}
-		});
-		parent = new int[n];
-		for (i = 0; i < n; i++) {
-			parent[i] = i;
-		}
+		root = new int[n + 1];
 		ans = 0;
-		for (i = 0; i < m; i++) {
-			if (union(edge[i][0], edge[i][1])) {
-				ans += edge[i][2];
+		for (i = 0; i < n - 1;) {
+			edge = pq.poll();
+			if (union(edge.u, edge.v)) {
+				ans += edge.weight;
+				i++;
 			}
 		}
 		System.out.print(ans);
