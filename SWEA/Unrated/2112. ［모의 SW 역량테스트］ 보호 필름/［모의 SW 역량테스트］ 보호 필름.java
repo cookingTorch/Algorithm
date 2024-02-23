78033d33217d4a9fdc4ef5fe4655f0bd604f3a7f
cookingTorch <jhw7181@naver.com>
2024-02-23 20:40:20 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 212 ms, Memory: 23,952 KB -BaekjoonHub

--- SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -5,15 +5,15 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 class Solution {
-	private static final char A = '0';
-	private static final char B = '1';
 	private static final int MAX_D = 13;
 	private static final int MAX_W = 20;
+	private static final char A = '0';
+	private static final char B = '1';
 	private static final char[] CHEM_A = new char[MAX_W];
 	private static final char[] CHEM_B = new char[MAX_W];
 	
 	private static int d, w, k;
-	private static char[][] films;
+	private static char[][] films, copy;
 	
 	private static boolean test() {
 		int temp, i, j;
@@ -40,31 +40,28 @@ class Solution {
 	
 	private static boolean dfs(int num, int start, int cnt) {
 		int i;
-		char[] temp;
 		
 		if (cnt == num) {
 			return test();
 		}
 		for (i = start; i < d; i++) {
 			if (films[i] != CHEM_A && films[i] != CHEM_B) {
-				temp = films[i];
 				films[i] = CHEM_A;
 				if (dfs(num, i + 1, cnt + 1)) {
-					films[i] = temp;
+					films[i] = copy[i];
 					return true;
 				}
-				films[i] = temp;
+				films[i] = copy[i];
 			}
 		}
 		for (i = start; i < d; i++) {
 			if (films[i] != CHEM_A && films[i] != CHEM_B) {
-				temp = films[i];
 				films[i] = CHEM_B;
 				if (dfs(num, i + 1, cnt + 1)) {
-					films[i] = temp;
+					films[i] = copy[i];
 					return true;
 				}
-				films[i] = temp;
+				films[i] = copy[i];
 			}
 		}
 		return false;
@@ -82,6 +79,7 @@ class Solution {
 			for (j = 0; j < w; j++) {
 				films[i][j] = st.nextToken().charAt(0);
 			}
+			copy[i] = films[i];
 		}
 		if (k == 1) {
 			return 0;
@@ -107,6 +105,7 @@ class Solution {
 		int t, testCase;
 		
 		films = new char[MAX_D][MAX_W];
+		copy = new char[MAX_D][];
 		Arrays.fill(CHEM_A, A);
 		Arrays.fill(CHEM_B, B);
 		t = Integer.parseInt(br.readLine());
