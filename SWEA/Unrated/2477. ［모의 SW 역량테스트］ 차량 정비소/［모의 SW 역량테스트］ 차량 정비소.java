0ff50d08ec364d146a4bf5c11d8f38e2a5955d2d
cookingTorch <jhw7181@naver.com>
2024-02-24 16:18:46 +0900

    [Unrated] Title: [모의 SW 역량테스트] 차량 정비소, Time: 148 ms, Memory: 29,812 KB -BaekjoonHub

---
+++ SWEA/Unrated/2477. ［모의 SW 역량테스트］ 차량 정비소/［모의 SW 역량테스트］ 차량 정비소.java
@@ -0,0 +1,138 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Solution {
+	private static final int MAX_N = 9;
+	private static final int MAX_M = 9;
+	private static final int MAX_TK = 1001;
+	private static final Customer EMPTY = null;
+	
+	private static class Customer implements Comparable<Customer> {
+		int idx, time, a, b;
+		
+		Customer(int idx) {
+			this.idx = idx;
+		}
+		
+		Customer setTime(int time) {
+			this.time = time;
+			return this;
+		}
+		
+		Customer setA(int a) {
+			this.a = a;
+			return this;
+		}
+		
+		Customer setB(int b) {
+			this.b = b;
+			return this;
+		}
+
+		@Override
+		public int compareTo(Customer o) {
+			return Integer.compare(this.idx, o.idx);
+		}
+	}
+	
+	private static int n, m, k, a, b;
+	private static int[] ai, bi, tk;
+	private static Customer[] reception, repair;
+	private static Queue<Customer> q;
+	private static PriorityQueue<Customer> pq;
+	
+	private static int repairShop() {
+		int time, finish, num, idx, ans, i;
+		Customer customer;
+		
+		idx = 1;
+		ans = 0;
+		for (finish = 0, time = 0;; time++) {
+			if (time < MAX_TK) {
+				num = tk[time];
+				for (i = 0; i < num; i++) {
+					pq.add(new Customer(idx++));
+				}
+			}
+			for (i = 0; i < n; i++) {
+				customer = reception[i];
+				if (customer != EMPTY && customer.time == time) {
+					q.add(customer);
+					reception[i] = EMPTY;
+				}
+			}
+			for (i = 0; i < m; i++) {
+				customer = repair[i];
+				if (customer != EMPTY && customer.time == time) {
+					if (customer.a == a && customer.b == b) {
+						ans += customer.idx;
+					}
+					repair[i] = EMPTY;
+					if (++finish == k) {
+						return ans == 0 ? -1 : ans;
+					}
+				}
+			}
+			for (i = 0; !pq.isEmpty() && i < n; i++) {
+				if (reception[i] == EMPTY) {
+					reception[i] = pq.poll().setA(i).setTime(time + ai[i]);
+				}
+			}
+			for (i = 0; !q.isEmpty() && i < m; i++) {
+				if (repair[i] == EMPTY) {
+					repair[i] = q.poll().setB(i).setTime(time + bi[i]);
+				}
+			}
+		}
+	}
+	
+	private static int solution(BufferedReader br, StringTokenizer st) throws IOException {
+		int i;
+		
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		a = Integer.parseInt(st.nextToken()) - 1;
+		b = Integer.parseInt(st.nextToken()) - 1;
+		st = new StringTokenizer(br.readLine());
+		for (i = 0; i < n; i++) {
+			ai[i] = Integer.parseInt(st.nextToken());
+		}
+		st = new StringTokenizer(br.readLine());
+		for (i = 0; i < m; i++) {
+			bi[i] = Integer.parseInt(st.nextToken());
+		}
+		tk = new int[MAX_TK];
+		st = new StringTokenizer(br.readLine());
+		for (i = 0; i < k; i++) {
+			tk[Integer.parseInt(st.nextToken())]++;
+		}
+		return repairShop();
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+		
+		int t, testCase;
+		
+		reception = new Customer[MAX_N];
+		repair = new Customer[MAX_M];
+		ai = new int[MAX_N];
+		bi = new int[MAX_M];
+		q = new ArrayDeque<>();
+		pq = new PriorityQueue<>();
+		t = Integer.parseInt(br.readLine());
+		for (testCase = 1; testCase <= t; testCase++) {
+			sb.append('#').append(testCase).append(' ').append(solution(br, st)).append('\n');
+		}
+		System.out.print(sb);
+	}
+}
