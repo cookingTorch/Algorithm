5bd879462e6339e5e801cb23c21c395425725bc8
cookingTorch <jhw7181@naver.com>
2024-02-27 19:07:27 +0900

    [Gold IV] Title: 최단경로, Time: 712 ms, Memory: 118664 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,75 +1,85 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static class Edge {
-        int to, weight;
+	private static final int INF = Integer.MAX_VALUE;
+	
+	private static class Edge {
+		int to, weight;
+		
+		Edge(int to, int weight) {
+			this.to = to;
+			this.weight = weight;
+		}
+	}
+	
+	private static int[] distance;
+	private static boolean[] inQueue;
+	private static ArrayList<LinkedList<Edge>> adj;
+	
+	private static void dijkstra(int start) {
+		int curr;
+		PriorityQueue<Integer> pq;
 
-        Edge(int to, int weight) {
-            this.to = to;
-            this.weight = weight;
-        }
-    }
-
-    private static class Node implements Comparable<Node> {
-        int index, distance;
-
-        Node(int index, int distance) {
-            this.index = index;
-            this.distance = distance;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return this.distance - o.distance;
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-
-        int v, e, k, i, curr;
-        int[] distance;
-        Node node;
-        ArrayList<ArrayList<Edge>> adj = new ArrayList<>();
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-
-        st = new StringTokenizer(br.readLine());
-        v = Integer.parseInt(st.nextToken());
-        e = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(br.readLine());
-        distance = new int[v + 1];
-        for (i = 0; i <= v; i++)
-            adj.add(new ArrayList<>());
-        for (i = 0; i < e; i++) {
-            st = new StringTokenizer(br.readLine());
-            adj.get(Integer.parseInt(st.nextToken())).add(new Edge(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
-        }
-        pq.add(new Node(k, 0));
-        while (!pq.isEmpty()) {
-            node = pq.poll();
-            curr = node.index;
-            for (Edge edge : adj.get(curr)) {
-                if ((edge.to != k && distance[edge.to] == 0) || distance[curr] + edge.weight < distance[edge.to]) {
-                    distance[edge.to] = distance[curr] + edge.weight;
-                    pq.add(new Node(edge.to, distance[edge.to]));
-                }
-            }
-        }
-        for (i = 1; i <= v; i++) {
-            if (i != k && distance[i] == 0)
-                sb.append("INF").append("\n");
-            else
-                sb.append(distance[i]).append("\n");
-        }
-
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-    }
+		Arrays.fill(distance, INF);
+		distance[start] = 0;
+		pq = new PriorityQueue<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return Integer.compare(distance[o1], distance[o2]);
+			}
+		});
+		pq.add(start);
+		while (!pq.isEmpty()) {
+			curr = pq.poll();
+			inQueue[curr] = false;
+			for (Edge edge : adj.get(curr)) {
+				if (distance[curr] + edge.weight < distance[edge.to]) {
+					distance[edge.to] = distance[curr] + edge.weight;
+					if (!inQueue[edge.to]) {
+						pq.add(edge.to);
+						inQueue[edge.to] = true;
+					}
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int num, e, k, u, v, w, i;
+		
+		st = new StringTokenizer(br.readLine());
+		num = Integer.parseInt(st.nextToken());
+		e = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(br.readLine()) - 1;
+		adj = new ArrayList<>();
+		for (i = 0; i < num; i++) {
+			adj.add(new LinkedList<>());
+		}
+		for (i = 0; i < e; i++) {
+			st = new StringTokenizer(br.readLine());
+			u = Integer.parseInt(st.nextToken()) - 1;
+			v = Integer.parseInt(st.nextToken()) - 1;
+			w = Integer.parseInt(st.nextToken());
+			adj.get(u).add(new Edge(v, w));
+		}
+		distance = new int[num];
+		inQueue = new boolean[num];
+		dijkstra(k);
+		for (i = 0; i < num; i++) {
+			sb.append(distance[i] == INF ? "INF" : distance[i]).append('\n');
+		}
+		System.out.print(sb);
+	}
 }
