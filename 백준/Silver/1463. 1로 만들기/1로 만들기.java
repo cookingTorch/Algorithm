3421f82724c58228565b0bbcbae09a73f1df1b5c
cookingTorch <jhw7181@naver.com>
2024-02-27 11:26:30 +0900

    [Silver III] Title: 1로 만들기, Time: 104 ms, Memory: 15464 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,24 +1,18 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
-	
-	private static int[] dp;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
+
+		int x;
+		int[] dp;
 		
-		int n = Integer.parseInt(br.readLine());
-		
-		dp = new int[n + 1];
+		x = Integer.parseInt(br.readLine());
+		dp = new int[x + 1];
 		dp[1] = 0;
-		
-		for (int i = 2; i <= n; i++) {
+		for (int i = 2; i <= x; i++) {
 			dp[i] = dp[i - 1] + 1;
 			if (i % 2 == 0) {
 				dp[i] = Math.min(dp[i], dp[i / 2] + 1);
@@ -27,11 +21,6 @@ public class Main {
 				dp[i] = Math.min(dp[i], dp[i / 3] + 1);
 			}
 		}
-		
-		sb.append(dp[n]);
-		
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
+		System.out.print(dp[x]);
 	}
 }
