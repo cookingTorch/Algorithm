fc85bebf9947be4d89af3182fc524fab6ab9d362
cookingTorch <jhw7181@naver.com>
2024-02-27 10:52:57 +0900

    [Platinum II] Title: 고속도로, Time: 2336 ms, Memory: 309960 KB -BaekjoonHub

--- 백준/Platinum/10254. 고속도로/고속도로.java
+++ 백준/Platinum/10254. 고속도로/고속도로.java
@@ -7,6 +7,8 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
+	private static final long INF = Long.MAX_VALUE;
+	
 	private static class Point {
 	    long x, y;
 
@@ -15,17 +17,17 @@ public class Main {
 	        this.y = y;
 	    }
 	}
+	
+	private static Point base;
+	private static Point[] result;
+	private static ArrayList<Point> points;
 
     private static Point[] rotatingCalipers(ArrayList<Point> hull) {
-    	int ni, nj;
-    	long ix, iy, jx, jy;
-        double max;
-        Point[] result;
+    	int i, j, ni, nj;
+    	long max, distance, ix, iy, jx, jy;
 
-        result = new Point[2];
         max = 0;
-        int j = 1;
-        for (int i = 0; i < hull.size(); i++) {
+        for (i = 0, j = 1; i < hull.size(); i++) {
             ni = (i + 1) % hull.size();
             for (;;) {
                 nj = (j + 1) % hull.size();
@@ -40,8 +42,9 @@ public class Main {
                     break;
                 }
             }
-            if (dist(hull.get(i), hull.get(j)) > max) {
-                max = dist(hull.get(i), hull.get(j));
+            distance = dist(hull.get(i), hull.get(j));
+            if (distance > max) {
+                max = distance;
                 result[0] = hull.get(i);
                 result[1] = hull.get(j);
             }
@@ -51,20 +54,18 @@ public class Main {
 
     private static ArrayList<Point> convexHull(ArrayList<Point> input) {
     	int i;
-        Point root, base;
         Stack<Point> stack;
 
-        root = new Point(Long.MAX_VALUE, Long.MAX_VALUE);
+        base = new Point(INF, INF);
         for (i = 0; i < input.size(); i++) {
-            if (input.get(i).x < root.x) {
-                root = input.get(i);
-            } else if (input.get(i).x == root.x) {
-                if (input.get(i).y < root.y) {
-                    root = input.get(i);
+            if (input.get(i).x < base.x) {
+                base = input.get(i);
+            } else if (input.get(i).x == base.x) {
+                if (input.get(i).y < base.y) {
+                    base = input.get(i);
                 }
             }
         }
-        base = root;
         input.sort(new Comparator<Point>() {
             @Override
             public int compare(Point o1, Point o2) {
@@ -75,12 +76,12 @@ public class Main {
                 } else if (ccw < 0) {
                     return 1;
                 } else {
-                    return Double.compare(dist(base, o1), dist(base, o2));
+                    return Long.compare(dist(base, o1), dist(base, o2));
                 }
             }
         });
         stack = new Stack<>();
-        stack.add(root);
+        stack.add(base);
         for (i = 1; i < input.size(); i++) {
             while (stack.size() > 1 && (ccw(stack.get(stack.size() - 2), stack.get(stack.size() - 1), input.get(i)) <= 0)) {
                 stack.pop();
@@ -94,30 +95,37 @@ public class Main {
         return (p1.x * p2.y + p2.x * p3.y + p3.x * p1.y) - (p1.y * p2.x + p2.y * p3.x + p3.y * p1.x);
     }
 
-    private static double dist(Point p1, Point p2) {
-        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+    private static long dist(Point p1, Point p2) {
+        return (p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y);
+    }
+    
+    private static void solution(BufferedReader br, StringBuilder sb, StringTokenizer st) throws IOException {
+    	int n, i;
+    	Point[] result;
+    	
+    	st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        points = new ArrayList<>();
+        for (i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            points.add(new Point(Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken())));
+        }
+        result = rotatingCalipers(convexHull(points));
+        sb.append(result[0].x).append(' ').append(result[0].y).append(' ').append(result[1].x).append(' ').append(result[1].y).append('\n');
     }
 	
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = null;
 
-        int n, t, testCase, i;
-        ArrayList<Point> points;
+        int t, testCase;
 
-        t = Integer.parseInt(st.nextToken());
+        result = new Point[2];
+        t = Integer.parseInt(br.readLine());
         for (testCase = 0; testCase < t; testCase++) {
-            st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
-            points = new ArrayList<>();
-            for (i = 0; i < n; i++) {
-                st = new StringTokenizer(br.readLine());
-                points.add(new Point(Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken())));
-            }
-            ArrayList<Point> list = convexHull(points);
-            Point[] result = rotatingCalipers(list);
-            sb.append(result[0].x).append(' ').append(result[0].y).append(' ').append(result[1].x).append(' ').append(result[1].y).append('\n');
+        	solution(br, sb, st);
+        	sb.append('\n');
         }
         System.out.print(sb);
     }
