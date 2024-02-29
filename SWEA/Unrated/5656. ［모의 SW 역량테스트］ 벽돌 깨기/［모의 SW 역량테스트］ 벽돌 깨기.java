d8de1f017428bd95cd2f28d1833ba599bae520b3
cookingTorch <jhw7181@naver.com>
2024-02-29 14:58:25 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벽돌 깨기, Time: 197 ms, Memory: 43,956 KB -BaekjoonHub

--- SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
+++ SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
@@ -6,16 +6,6 @@ import java.util.Deque;
 import java.util.StringTokenizer;
 
 class Solution {
-	
-	/**
-	 * 293 ms
-	 * 맵 클론 생성
-	 * 구슬 발사 DFS
-	 * 벽돌 폭발 4방 DFS
-	 * 벽돌 낙하: 세로줄 벽돌 stack에 다 집어 넣음
-	 * 다시 꺼내면서 아래쪽부터 채우기
-	 */
-	
 	private static final int MAX_W = 12;
 	private static final int MAX_H = 15;
 	private static final int EMPTY = 0;
@@ -24,12 +14,12 @@ class Solution {
 	private static int[][] matrix;
 	private static Deque<Integer> stack;
 	
-	private static int[][] getClone(int[][] map) { // 맵 클론 생성
+	private static int[][] getClone(int[][] map) {
 		int i, j;
 		int[][] clone;
 		
 		clone = new int[h][w];
-		for(i = 0; i < h; i++) { // 깊은 복사
+		for(i = 0; i < h; i++) {
 			for (j = 0; j < w; j++) {
 				clone[i][j] = map[i][j];
 			}
@@ -37,69 +27,71 @@ class Solution {
 		return clone;
 	}
 	
-	private static void explode(int[][] map, int x, int y) { // 벽돌 폭발
+	private static void explode(int[][] map, int x, int y) {
 		int range, i;
 		
-		range = map[x][y]; // 폭발 범위
-		map[x][y] = EMPTY; // 벽돌 삭제
+		range = map[x][y];
+		map[x][y] = EMPTY;
 		if (range <= 1) {
 			return;
 		}
-		for (i = 1; i < range && x - i >= 0; i++) { // 위쪽 폭발 범위
-			explode(map, x - i, y); // 벽돌 폭발
+		for (i = 1; i < range && x - i >= 0; i++) {
+			explode(map, x - i, y);
 		}
-		for (i = 1; i < range && y - i >= 0; i++) { // 왼쪽 폭발 범위
-			explode(map, x, y - i); // 벽돌 폭발
+		for (i = 1; i < range && y - i >= 0; i++) {
+			explode(map, x, y - i);
 		}
-		for (i = 1; i < range && x + i < h; i++) { // 아래쪽 폭발 범위
-			explode(map, x + i, y); // 벽돌 폭발
+		for (i = 1; i < range && x + i < h; i++) {
+			explode(map, x + i, y);
 		}
-		for (i = 1; i < range && y + i < w; i++) { // 오른쪽 폭발 범위
-			explode(map, x, y + i); // 벽돌 폭발
+		for (i = 1; i < range && y + i < w; i++) {
+			explode(map, x, y + i);
 		}
 	}
 	
-	private static int count(int[][] map) { // 벽돌 낙하
+	private static int count(int[][] map) {
 		int cnt, size, i, j;
 		
 		cnt = 0;
 		for (i = 0; i < w; i++) {
 			size = 0;
-			for (j = 0; j < h; j++) { // 세로 줄 탐색
-				if (map[j][i] != EMPTY) { // 벽돌 존재
-					stack.push(map[j][i]); // stack에 삽입
-					map[j][i] = EMPTY; // 맵 비우기
-					size++; // stack 사이즈
+			for (j = 0; j < h; j++) {
+				if (map[j][i] != EMPTY) {
+					stack.push(map[j][i]);
+					map[j][i] = EMPTY;
+					size++;
 				}
 			}
-			for (j = 1; j <= size; j++) { // 가장 아래 칸부터
-				map[h - j][i] = stack.pop(); // stack에서 꺼내서 채우기
-				cnt++; // 벽돌 개수 카운트
+			for (j = 1; j <= size; j++) {
+				map[h - j][i] = stack.pop();
+				cnt++;
 			}
 		}
 		return cnt;
 	}
 	
-	private static boolean drop(int[][] map, int y, int cnt, int depth) { // 구슬 발사
+	private static boolean drop(int[][] map, int y, int cnt, int depth) {
 		int i;
 		
-		for (i = 0; i < h; i++) { // 구슬 낙하
-			if (map[i][y] != EMPTY) { // 벽돌 존재
-				explode(map, i, y); // 벽돌 폭발 DFS
-				cnt = count(map); // 벽돌 낙하, 남은 벽돌 카운트
+		for (i = 0; i < h; i++) {
+			if (map[i][y] != EMPTY) {
+				explode(map, i, y);
+				cnt = count(map);
 				break;
 			}
 		}
-		if (cnt == 0) {
-			min = 0;
+		min = Math.min(min, cnt);
+		if (min == 0) {
 			return true;
 		}
-		if (depth == n) { // N 개 구슬 소진
-			min = Math.min(min, cnt); // 남은 벽돌 최소값 업데이트
-			return min == 0;
+		if (depth == n) {
+			return false;
 		}
 		for (i = 0; i < w; i++) {
-			if (drop(getClone(map), i, cnt, depth + 1)) { // 맵 클론 생성 후 다음 구슬 발사
+			if (map[h - 1][i] == EMPTY) {
+				continue;
+			}
+			if (drop(getClone(map), i, cnt, depth + 1)) {
 				return true;
 			}
 		}
@@ -114,21 +106,27 @@ class Solution {
 		w = Integer.parseInt(st.nextToken());
 		h = Integer.parseInt(st.nextToken());
 		min = 0;
-		for (i = 0; i < h; i++) { // 초기 벽돌 정보 입력
+		for (i = 0; i < h; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (j = 0; j < w; j++) {
 				matrix[i][j] = Integer.parseInt(st.nextToken());
 				if (matrix[i][j] != EMPTY) {
-					min++; // 초기 벽돌 개수 카운트
+					min++;
 				}
 			}
 		}
+		if (min == 0) {
+			return 0;
+		}
 		for (i = 0; i < w; i++) {
-			if (drop(getClone(matrix), i, min, 1)) { // 맵 클론 생성 후 구슬 발사 DFS
+			if (matrix[h - 1][i] == EMPTY) {
+				continue;
+			}
+			if (drop(getClone(matrix), i, min, 1)) {
 				break;
 			}
 		}
-		return min; // 남은 벽돌 최소값 반환
+		return min;
 	}
 	
 	public static void main(String[] args) throws IOException {
