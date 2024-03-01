7a1d678c4d2fa9dad25ee7f92efdf76bd243a290
cookingTorch <jhw7181@naver.com>
2024-03-01 22:40:24 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 676 ms, Memory: 181508 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
@@ -1,71 +1,61 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
+	private static int[] arr;
+	
+	public static int binarySearch(int target, int right) {
+		int left, mid;
+		
+		left = 0;
+		while (left < right) {
+			mid = (left + right) / 2;
+			if (arr[mid] < target) {
+				left = mid + 1;
+			} else {
+				right = mid;
+			}
+		}
+		return right;
+	}
 
-    public static int binarySearch(int[] arr, int target, int end) {
-        int left = 0;
-        int right = end;
-        int mid;
-        while (left < right) {
-            mid = (left + right) >> 1;
-            if (arr[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        return right;
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int n, cnt, size, i;
+		int[] nums, idx, ans;
 
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int n = Integer.parseInt(br.readLine()), i, idx;
-        int[] nums = new int[n];
-        int[] temp = new int[n];
-        int[] numIndex = new int[n];
-        int[] ans = new int[n];
-
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for (i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int size = 0;
-        for (i = 0; i < n; i++) {
-            idx = binarySearch(temp, nums[i], size);
-            temp[idx] = nums[i];
-            numIndex[i] = idx;
-            if (idx == size) {
-                size++;
-            }
-        }
-
-        int cnt = size - 1;
-        for (i = n - 1; i >= 0; i--) {
-            if (numIndex[i] == cnt) {
-                ans[cnt--] = nums[i];
-            }
-            if (cnt < 0) {
-                break;
-            }
-        }
-
-        StringBuilder sb = new StringBuilder();
-        sb.append(size).append("\n");
-        for (i = 0; i < size; i++) {
-            sb.append(ans[i]).append(" ");
-        }
-
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-    }
+		n = Integer.parseInt(br.readLine());
+		nums = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (i = 0; i < n; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		idx = new int[n];
+		arr = new int[n];
+		size = 0;
+		for (i = 0; i < n; i++) {
+			idx[i] = binarySearch(nums[i], size);
+			arr[idx[i]] = nums[i];
+			if (idx[i] == size) {
+				size++;
+			}
+		}
+		ans = new int[size];
+		sb.append(size).append('\n');
+		cnt = size - 1;
+		for (i = n - 1; cnt >= 0; i--) {
+			if (idx[i] == cnt) {
+				ans[cnt--] = nums[i];
+			}
+		}
+		for (i = 0; i < size; i++) {
+			sb.append(ans[i]).append(' ');
+		}
+		System.out.print(sb);
+	}
 }
