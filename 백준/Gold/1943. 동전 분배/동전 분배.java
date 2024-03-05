b9acb986e1d78a195394a078cf4d7f213abaf5f0
cookingTorch <jhw7181@naver.com>
2024-03-05 20:25:57 +0900

    [Gold III] Title: 동전 분배, Time: 260 ms, Memory: 16696 KB -BaekjoonHub

--- 백준/Gold/1943. 동전 분배/동전 분배.java
+++ 백준/Gold/1943. 동전 분배/동전 분배.java
@@ -19,15 +19,15 @@ public class Main {
 			coin = Integer.parseInt(st.nextToken());
 			num = Integer.parseInt(st.nextToken());
 			sum += coin * num;
-			for (j = 1; j < num; num -= j, j *= 2) {
+			for (j = 1; j < num; num -= j, j <<= 1) {
 				coins.add(coin * j);
 			}
 			coins.add(coin * num);
 		}
-		if (sum % 2 != 0) {
+		if ((sum & 1) == 1) {
 			return 0;
 		}
-		sum /= 2;
+		sum >>= 1;
 		size = coins.size() - 1;
 		dp = new int[sum + 1];
 		dp[0] = 1;
