eab147fa9ebe358331324033f2d841b681248c9a
cookingTorch <jhw7181@naver.com>
2024-03-05 20:37:24 +0900

    [Gold III] Title: 동전 분배, Time: 220 ms, Memory: 15576 KB -BaekjoonHub

--- 백준/Gold/1943. 동전 분배/동전 분배.java
+++ 백준/Gold/1943. 동전 분배/동전 분배.java
@@ -1,40 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static int solution(BufferedReader br, StringTokenizer st) throws IOException {
+	private static final int MAX_SIZE = 1000;
+	
+	private static int solution(BufferedReader br, StringTokenizer st, int[] coins) throws IOException {
 		int n, sum, coin, num, size, i, j;
 		int[] dp;
-		ArrayList<Integer> coins;
 		
 		n = Integer.parseInt(br.readLine());
 		sum = 0;
-		coins = new ArrayList<>();
-		coins.add(0);
+		size = 0;
 		for (i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			coin = Integer.parseInt(st.nextToken());
 			num = Integer.parseInt(st.nextToken());
 			sum += coin * num;
 			for (j = 1; j < num; num -= j, j <<= 1) {
-				coins.add(coin * j);
+				coins[++size] = coin * j;
 			}
-			coins.add(coin * num);
+			coins[++size] = coin * num;
 		}
 		if ((sum & 1) == 1) {
 			return 0;
 		}
 		sum >>= 1;
-		size = coins.size() - 1;
 		dp = new int[sum + 1];
 		dp[0] = 1;
 		for (i = 1; i <= size; i++) {
 			for (j = sum; j >= 1; j--) {
-				if (j >= coins.get(i)) {
-					dp[j] |= dp[j - coins.get(i)];
+				if (j >= coins[i]) {
+					dp[j] |= dp[j - coins[i]];
 				}
 			}
 		}
@@ -47,9 +45,11 @@ public class Main {
 		StringTokenizer st = null;
 		
 		int testCase;
-		
+		int[] coins;
+
+		coins = new int[MAX_SIZE];
 		for (testCase = 0; testCase < 3; testCase++) {
-			sb.append(solution(br, st)).append('\n');
+			sb.append(solution(br, st, coins)).append('\n');
 		}
 		System.out.print(sb);
 	}
