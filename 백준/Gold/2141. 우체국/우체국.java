ab3c559ff5179f974e55ede720124a0682d9bf2a
cookingTorch <jhw7181@naver.com>
2024-03-06 10:38:21 +0900

    [Gold IV] Title: 우체국, Time: 428 ms, Memory: 44856 KB -BaekjoonHub

--- 백준/Gold/2141. 우체국/우체국.java
+++ 백준/Gold/2141. 우체국/우체국.java
@@ -1,40 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
+	private static class Town implements Comparable<Town> {
+		int x, a;
+		
+		Town(int x, int a) {
+			this.x = x;
+			this.a = a;
+		}
+
+		@Override
+		public int compareTo(Town o) {
+			return Integer.compare(this.x, o.x);
+		}
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
-		int n, i;
-		int[][] xa;
+		int n, x, a, i;
 		long sum, prefix;
+		Town town;
+		PriorityQueue<Town> pq;
 		
 		n = Integer.parseInt(br.readLine());
-		xa = new int[n][2];
+		pq = new PriorityQueue<>();
 		sum = 0;
 		for (i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
-			xa[i][0] = Integer.parseInt(st.nextToken());
-			xa[i][1] = Integer.parseInt(st.nextToken());
-			sum += xa[i][1];
+			x = Integer.parseInt(st.nextToken());
+			a = Integer.parseInt(st.nextToken());
+			pq.add(new Town(x, a));
+			sum += a;
 		}
-		Arrays.sort(xa, new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return Integer.compare(o1[0], o2[0]);
-			}
-		});
 		sum = (sum + 1) / 2;
 		prefix = 0;
 		for (i = 0; i < n; i++) {
-			prefix += xa[i][1];
+			town = pq.poll();
+			prefix += town.a;
 			if (prefix >= sum) {
-				System.out.print(xa[i][0]);
+				System.out.print(town.x);
 				return;
 			}
 		}
