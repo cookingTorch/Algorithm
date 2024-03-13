c3aba9798f8a9bb082828dfb5193102b726d9002
cookingTorch <jhw7181@naver.com>
2024-03-13 09:22:56 +0900

    [Gold III] Title: 동전 분배, Time: 160 ms, Memory: 15352 KB -BaekjoonHub

--- 백준/Gold/1943. 동전 분배/동전 분배.java
+++ 백준/Gold/1943. 동전 분배/동전 분배.java
@@ -1,45 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+	private static final int INF = Integer.MAX_VALUE;
 	private static final int T = 3;
 	private static final int MAX_SIZE = 500;
 	
-	private static int solution(BufferedReader br, StringTokenizer st, int[] coins) throws IOException {
-		int n, sum, coin, num, size, weight, i, j;
-		boolean[] dp;
+	private static int solution(BufferedReader br, StringTokenizer st, int[] coins, int[] nums) throws IOException {
+		int n, sum, coin, num, size, prev, i, j;
+		int[] dp;
 		
 		n = Integer.parseInt(br.readLine());
 		sum = 0;
-		size = 0;
+		size = 1;
 		for (i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			coin = Integer.parseInt(st.nextToken());
 			num = Integer.parseInt(st.nextToken());
 			sum += coin * num;
-			for (j = 1; j < num; num -= j, j <<= 1) {
-				coins[++size] = coin * j;
-			}
-			coins[++size] = coin * num;
+			coins[size] = coin;
+			nums[size++] = num;
 		}
 		if ((sum & 1) == 1) {
 			return 0;
 		}
 		sum >>= 1;
-		dp = new boolean[sum + 1];
-		dp[0] = true;
-		for (i = 1; i <= size; i++) {
-			weight = coins[i];
-			if (!dp[sum] && sum >= weight && (dp[sum] = dp[sum - weight])) {
-				return 1;
-			}
-			for (j = sum - 1; j >= weight; j--) {
-				if (!dp[j]) {
-					dp[j] = dp[j - weight];
+		dp = new int[sum + 1];
+		Arrays.fill(dp, INF);
+		dp[0] = 0;
+		prev = 0;
+		for (i = 1; i < size; i++) {
+			coin = coins[i];
+			num = nums[i];
+			for (j = coin; j < sum; j++) {
+				if (dp[j - coin] < num && dp[j] == INF) {
+					dp[j] = dp[j - coin] + 1;
+					dp[j - coin] = 0;
+					prev = j;
 				}
 			}
+			if (sum >= coin && dp[sum - coin] < num) {
+				return 1;
+			}
+			dp[prev] = 0;
 		}
 		return 0;
 	}
@@ -50,11 +56,12 @@ public class Main {
 		StringTokenizer st = null;
 		
 		int testCase;
-		int[] coins;
+		int[] coins, nums;
 
 		coins = new int[MAX_SIZE];
+		nums = new int[MAX_SIZE];
 		for (testCase = 0; testCase < T; testCase++) {
-			sb.append(solution(br, st, coins)).append('\n');
+			sb.append(solution(br, st, coins, nums)).append('\n');
 		}
 		System.out.print(sb);
 	}
