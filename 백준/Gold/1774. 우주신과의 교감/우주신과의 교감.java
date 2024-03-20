3134a3d506ef9daf2a9692b68f44bd579c341cfd
cookingTorch <jhw7181@naver.com>
2024-03-20 23:38:18 +0900

    [Gold III] Title: 우주신과의 교감, Time: 216 ms, Memory: 35316 KB -BaekjoonHub

--- 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.java
+++ 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.java
@@ -7,9 +7,9 @@ import java.util.StringTokenizer;
 public class Main {
 	private static class Edge implements Comparable<Edge> {
 		int u, v;
-		double weight;
+		long weight;
 		
-		Edge(int u, int v, double weight) {
+		Edge(int u, int v, long weight) {
 			this.u = u;
 			this.v = v;
 			this.weight = weight;
@@ -17,14 +17,14 @@ public class Main {
 		
 		@Override
 		public int compareTo(Edge o) {
-			return Double.compare(this.weight, o.weight);
+			return Long.compare(this.weight, o.weight);
 		}
 	}
 	
 	private static int[] root;
 	
-	private static double dist(double[] u, double[] v) {
-		return Math.sqrt((u[0] - v[0]) * (u[0] - v[0]) + (u[1] - v[1]) * (u[1] - v[1]));
+	private static long dist(long[] u, long[] v) {
+		return (u[0] - v[0]) * (u[0] - v[0]) + (u[1] - v[1]) * (u[1] - v[1]);
 	}
 	
 	private static boolean union(int u, int v) {
@@ -56,19 +56,19 @@ public class Main {
 		StringTokenizer st;
 		
 		int n, m, u, v, size, i;
+		long[][] gods;
 		double ans;
-		double[][] gods;
 		Edge edge;
 		PriorityQueue<Edge> pq;
 		
 		st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
-		gods = new double[n + 1][2];
+		gods = new long[n + 1][2];
 		for (i = 1; i <= n; i++) {
 			st = new StringTokenizer(br.readLine());
-			gods[i][0] = Double.parseDouble(st.nextToken());
-			gods[i][1] = Double.parseDouble(st.nextToken());
+			gods[i][0] = Long.parseLong(st.nextToken());
+			gods[i][1] = Long.parseLong(st.nextToken());
 		}
 		size = n - 1;
 		root = new int[n + 1];
@@ -88,10 +88,10 @@ public class Main {
 		for (i = 0; i < size;) {
 			edge = pq.poll();
 			if (union(edge.u, edge.v)) {
-				ans += edge.weight;
+				ans += Math.sqrt(edge.weight);
 				i++;
 			}
 		}
-        System.out.print(String.format("%.2f", ans));
+        System.out.printf("%.2f", ans);
 	}
 }
