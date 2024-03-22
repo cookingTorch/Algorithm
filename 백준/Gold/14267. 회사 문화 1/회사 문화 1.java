af493cde1516e86eb2e483e365aa92a3e9923753
cookingTorch <jhw7181@naver.com>
2024-03-22 22:27:24 +0900

    [Gold IV] Title: 회사 문화 1, Time: 624 ms, Memory: 87420 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
@@ -6,7 +6,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 	private static int idx;
-	private static int[] tree, lazy, rangeS, rangeE;
+	private static int[] lazy, rangeS, rangeE, ans;
 	private static ArrayList<ArrayList<Integer>> adj;
 	
 	private static int euler(int node) {
@@ -20,7 +20,6 @@ public class Main {
 	
 	private static void propagate(int node, int start, int end) {
 		if (lazy[node] != 0) {
-			tree[node] += (end - start + 1) * lazy[node];
 			if (start < end) {
 				lazy[node * 2] += lazy[node];
 				lazy[node * 2 + 1] += lazy[node];
@@ -32,30 +31,28 @@ public class Main {
 	private static void update(int node, int start, int end, int left, int right, int diff) {
 		int mid;
 		
-		propagate(node, start, end);
 		if (left <= start && end <= right) {
 			lazy[node] += diff;
-			propagate(node, start, end);
 		} else if (!(end < left || right < start)) {
+            propagate(node, start, end);
 			mid = (start + end) / 2;
 			update(node * 2, start, mid, left, right, diff);
 			update(node * 2 + 1, mid + 1, end, left, right, diff);
-			tree[node] = tree[node * 2] + tree[node * 2 + 1];
 		}
 	}
 	
-	private static int sum(int node, int start, int end, int left, int right) {
+	private static void makeAns(int node, int start, int end) {
 		int mid;
 		
-		propagate(node, start, end);
-		if (left <= start && end <= right) {
-			return tree[node];
-		} else if (!(end < left || right < start)) {
-			mid = (start + end) / 2;
-			return sum(node * 2, start, mid, left, right) + sum(node * 2 + 1, mid + 1, end, left, right);
-		} else {
-			return 0;
+		if (start == end) {
+			ans[start] = lazy[node];
+			return;
 		}
+		lazy[node * 2] += lazy[node];
+		lazy[node * 2 + 1] += lazy[node];
+		mid = (start + end) / 2;
+        makeAns(node * 2, start, mid);
+        makeAns(node * 2 + 1, mid + 1, end);
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -81,15 +78,16 @@ public class Main {
 		rangeS = new int[n + 1];
 		rangeE = new int[n + 1];
 		euler(1);
-		tree = new int[4 * n];
 		lazy = new int[4 * n];
 		for (i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
 			idx = Integer.parseInt(st.nextToken());
 			update(1, 1, n, rangeS[idx], rangeE[idx], Integer.parseInt(st.nextToken()));
 		}
+		ans = new int[n + 1];
+		makeAns(1, 1, n);
 		for (i = 1; i <= n; i++) {
-			sb.append(sum(1, 1, n, rangeS[i], rangeS[i])).append(' ');
+			sb.append(ans[rangeS[i]]).append(' ');
 		}
 		System.out.print(sb);
 	}
