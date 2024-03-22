c86ae552f96c48936339bdc80fa0da893ae87fc2
cookingTorch <jhw7181@naver.com>
2024-03-22 22:54:25 +0900

    [Gold IV] Title: 회사 문화 1, Time: 420 ms, Memory: 53252 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
@@ -1,19 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
 	private static int[] ans;
-	private static ArrayList<ArrayList<Integer>> adj;
-	
-	private static void prop(int node) {
-		for (int child : adj.get(node)) {
-			ans[child] += ans[node];
-			prop(child);
-		}
-	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,27 +12,22 @@ public class Main {
 		StringTokenizer st;
 		
 		int n, m, i;
+		String str;
 		
 		st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine());
-		st.nextToken();
-		adj = new ArrayList<>(n + 1);
-		adj.add(null);
-		adj.add(new ArrayList<>());
-		for (i = 2; i <= n; i++) {
-			adj.add(new ArrayList<>());
-			adj.get(Integer.parseInt(st.nextToken())).add(i);
-		}
+		str = br.readLine();
 		ans = new int[n + 1];
 		for (i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
 			ans[Integer.parseInt(st.nextToken())] += Integer.parseInt(st.nextToken());
 		}
-		prop(1);
-		for (i = 1; i <= n; i++) {
-			sb.append(ans[i]).append(' ');
+		st = new StringTokenizer(str);
+		st.nextToken();
+		sb.append(ans[1]).append(' ');
+		for (i = 2; i <= n; i++) {
+			sb.append(ans[i] += ans[Integer.parseInt(st.nextToken())]).append(' ');
 		}
 		System.out.print(sb);
 	}
