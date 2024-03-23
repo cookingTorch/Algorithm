a68f78c36cbfa2107c7e3cedec41b6853a8eee1d
cookingTorch <jhw7181@naver.com>
2024-03-23 17:41:07 +0900

    [Gold IV] Title: 줄세우기, Time: 80 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	private static int[] arr;
+	
+	public static int binarySearch(int num, int right) {
+		int left, mid;
+		
+		left = 0;
+		while (left < right) {
+			mid = (left + right) / 2;
+			if (arr[mid] < num) {
+				left = mid + 1;
+			} else {
+				right = mid;
+			}
+		}
+		return right;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n, size, idx, num, i;
+		
+		n = Integer.parseInt(br.readLine());
+		arr = new int[n];
+		size = 0;
+		for (i = 0; i < n; i++) {
+			num = Integer.parseInt(br.readLine());
+			idx = binarySearch(num, size);
+			arr[idx] = num;
+			if (idx == size) {
+				size++;
+			}
+		}
+		System.out.print(n - size);
+	}
+}
