180d9cf6b2e7d183716aa4fc1da521be4a0f1b5e
cookingTorch <jhw7181@naver.com>
2024-04-02 11:30:42 +0900

    [Gold II] Title: 피보나치 수 6, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.java
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.java
@@ -1,40 +1,39 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    private static long[][] multi(long[][] a, long[][] b) {
-        long[][] c = new long[2][2];
+	private static final int P = 1000000007;
+	private static final long[][] FIRST = {{1L, 1L}, {1L, 0L}};
+	
+    private static long[][] multiply(long[][] a, long[][] b) {
+        long[][] c;
 
-        c[0][0] = ((a[0][0] * b[0][0]) + (a[0][1] * b[1][0])) % 1000000007;
-        c[0][1] = ((a[0][0] * b[0][1]) + (a[0][1] * b[1][1])) % 1000000007;
-        c[1][0] = ((a[1][0] * b[0][0]) + (a[1][1] * b[1][0])) % 1000000007;
-        c[1][1] = ((a[1][0] * b[0][1]) + (a[1][1] * b[1][1])) % 1000000007;
+        c = new long[2][2];
+        c[0][0] = ((a[0][0] * b[0][0]) + (a[0][1] * b[1][0])) % P;
+        c[0][1] = ((a[0][0] * b[0][1]) + (a[0][1] * b[1][1])) % P;
+        c[1][0] = ((a[1][0] * b[0][0]) + (a[1][1] * b[1][0])) % P;
+        c[1][1] = ((a[1][0] * b[0][1]) + (a[1][1] * b[1][1])) % P;
         return c;
     }
 
-    private static long[][] pow(long[][] i, long n) {
+    private static long[][] power(long[][] matrix, long n) {
+    	long[][] sqrt;
+    	
         if (n == 0)
             return new long[2][2];
         if (n == 1)
-            return i;
+            return matrix;
         if (n % 2 == 0) {
-            long[][] sqrt = pow(i, n / 2);
-
-            return multi(sqrt, sqrt);
+            sqrt = power(matrix, n / 2);
+            return multiply(sqrt, sqrt);
         } else
-            return multi(pow(i, n - 1), i);
+            return multiply(power(matrix, n - 1), matrix);
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
 
-        long[][] i = new long[][] {{1, 1}, {1, 0}};
-
-        sb.append(pow(i, Long.parseLong(br.readLine()))[0][1]);
-
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        System.out.print(power(FIRST, Long.parseLong(br.readLine()))[0][1]);
     }
 }
