58e89b43d399af0dfdbacd6f86f8589e52365595
cookingTorch <jhw7181@naver.com>
2024-04-02 11:35:25 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 76 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -3,18 +3,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+	private static final long[][] FIRST = {{1L, 1L}, {1L, 0L}};
+	
+    private static long[][] multiply(long[][] a, long[][] b) {
+        long[][] c;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str1 = br.readLine();
-		int n = Integer.parseInt(str1);
-		long[] fibo = new long[91];
-		fibo[0] = 0;
-		fibo[1] = 1;
-		for(int i = 2; i <= n; i++) {
-			fibo[i] = fibo[i - 1] + fibo[i - 2]; 
-		}
-		System.out.println(fibo[n]);
-	}
+        c = new long[2][2];
+        c[0][0] = ((a[0][0] * b[0][0]) + (a[0][1] * b[1][0]));
+        c[0][1] = ((a[0][0] * b[0][1]) + (a[0][1] * b[1][1]));
+        c[1][0] = ((a[1][0] * b[0][0]) + (a[1][1] * b[1][0]));
+        c[1][1] = ((a[1][0] * b[0][1]) + (a[1][1] * b[1][1]));
+        return c;
+    }
 
-} 
+    private static long[][] power(long[][] matrix, long n) {
+    	long[][] sqrt;
+    	
+        if (n == 0)
+            return new long[2][2];
+        if (n == 1)
+            return matrix;
+        if (n % 2 == 0) {
+            sqrt = power(matrix, n / 2);
+            return multiply(sqrt, sqrt);
+        } else
+            return multiply(power(matrix, n - 1), matrix);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print(power(FIRST, Long.parseLong(br.readLine()))[0][1]);
+    }
+}
