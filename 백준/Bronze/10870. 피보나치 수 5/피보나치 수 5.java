c323991970d6a5d79838828d5a86d9000b597f40
cookingTorch <jhw7181@naver.com>
2024-04-02 11:39:14 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 80 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	private static final long[][] FIRST = {{1L, 1L}, {1L, 0L}};
+	
+    private static long[][] multiply(long[][] a, long[][] b) {
+        long[][] c;
+
+        
+        c = new long[2][2];
+        c[0][0] = ((a[0][0] * b[0][0]) + (a[0][1] * b[1][0]));
+        c[0][1] = ((a[0][0] * b[0][1]) + (a[0][1] * b[1][1]));
+        c[1][0] = ((a[1][0] * b[0][0]) + (a[1][1] * b[1][0]));
+        c[1][1] = ((a[1][0] * b[0][1]) + (a[1][1] * b[1][1]));
+        return c;
+    }
+
+    private static long[][] power(long[][] matrix, long n) {
+    	long[][] sqrt;
+    	
+        if (n == 0)
+            return new long[2][2];
+        if (n == 1)
+            return matrix;
+        if (n % 2 == 0) {
+            sqrt = power(matrix, n / 2);
+            return multiply(sqrt, sqrt);
+        } else
+            return multiply(power(matrix, n - 1), matrix);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print(power(FIRST, Long.parseLong(br.readLine()))[0][1]);
+    }
+}
