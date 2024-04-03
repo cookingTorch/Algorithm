e44655cc27506f40ba58a96306caf3423bf06bae
cookingTorch <jhw7181@naver.com>
2024-04-03 13:45:25 +0900

    [Gold II] Title: 가운데를 말해요, Time: 416 ms, Memory: 34156 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -1,38 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.PriorityQueue;
+import java.util.TreeSet;
 
 public class Main {
+	private static final class Node implements Comparable<Node> {
+		int idx, num;
+		
+		Node(int idx, int num) {
+			this.idx = idx;
+			this.num = num;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			if (this.num == o.num) {
+				return Integer.compare(this.idx, o.idx);
+			}
+			return Integer.compare(this.num, o.num);
+		}
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
-		int n, i;
-		PriorityQueue<Integer> left, right;
+		int n, num, i;
+		Node mid;
+		TreeSet<Node> set;
 		
 		n = Integer.parseInt(br.readLine());
-		left = new PriorityQueue<>(Collections.reverseOrder());
-		right = new PriorityQueue<>();
-		left.add(Integer.parseInt(br.readLine()));
-		sb.append(left.peek()).append('\n');
+		set = new TreeSet<>();
+		mid = new Node(0, Integer.parseInt(br.readLine()));
+		set.add(mid);
+		sb.append(mid.num).append('\n');
 		for (i = 1; i < n; i++) {
-			right.add(Integer.parseInt(br.readLine()));
-			if (left.peek() > right.peek()) {
-				left.add(right.poll());
-				right.add(left.poll());
+			num = Integer.parseInt(br.readLine());
+			set.add(new Node(i, num));
+			if (num < mid.num) {
+				mid = set.lower(mid);
 			}
-			sb.append(left.peek()).append('\n');
+			sb.append(mid.num).append('\n');
 			if (++i == n) {
 				break;
 			}
-			left.add(Integer.parseInt(br.readLine()));
-			if (left.peek() > right.peek()) {
-				left.add(right.poll());
-				right.add(left.poll());
+			num = Integer.parseInt(br.readLine());
+			set.add(new Node(i, num));
+			if (num >= mid.num) {
+				mid = set.higher(mid);
 			}
-			sb.append(left.peek()).append('\n');
+			sb.append(mid.num).append('\n');
 		}
 		System.out.print(sb);
 	}
