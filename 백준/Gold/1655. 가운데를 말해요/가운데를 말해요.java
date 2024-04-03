27a1149c554d6c29b3059abbdfc3391e9d9fab3c
cookingTorch <jhw7181@naver.com>
2024-04-03 13:06:58 +0900

    [Gold II] Title: 가운데를 말해요, Time: 416 ms, Memory: 34860 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -1,51 +1,39 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String str;
+		StringBuilder sb = new StringBuilder();
 		
-		int n, i, j, number, left, right, mid;
-		int[] arr = new int[20001];
+		int n, i;
+		PriorityQueue<Integer> left, right;
 		
-		str = br.readLine();
-		n = Integer.parseInt(str);
-		
-		for (i = 1; i <= n; i++) {
-			
-			str = br.readLine();
-			number = Integer.parseInt(str);
-			
-			for (j = number; j <= 10000; j++) {
-				arr[j + 10000] += 1;
+		n = Integer.parseInt(br.readLine());
+		left = new PriorityQueue<>(Collections.reverseOrder());
+		right = new PriorityQueue<>();
+		left.add(Integer.parseInt(br.readLine()));
+		sb.append(left.peek()).append('\n');
+		for (i = 1; i < n; i++) {
+			right.add(Integer.parseInt(br.readLine()));
+			if (left.peek() > right.peek()) {
+				left.add(right.poll());
+				right.add(left.poll());
 			}
-			
-			left = 0;
-			right = 20001;
-			while (left < right) {
-				mid = (left + right) / 2;
-				if (arr[mid] < (i + 1) / 2) {
-					left = mid + 1;
-				}
-				else {
-					right = mid;
-				}
+			sb.append(left.peek()).append('\n');
+			if (++i == n) {
+				break;
 			}
-			bw.write(Integer.toString(right - 10000));
-			bw.newLine();
-			
+			left.add(Integer.parseInt(br.readLine()));
+			if (left.peek() > right.peek()) {
+				left.add(right.poll());
+				right.add(left.poll());
+			}
+			sb.append(left.peek()).append('\n');
 		}
-		
-		bw.flush();
-		bw.close();
-
+		System.out.print(sb);
 	}
-
 }
