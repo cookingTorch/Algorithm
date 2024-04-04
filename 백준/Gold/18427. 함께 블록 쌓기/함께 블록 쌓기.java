76f953edb71e48e4a0e6f3c99a94edb8fb597210
cookingTorch <jhw7181@naver.com>
2024-04-04 09:13:45 +0900

    [Gold IV] Title: 함께 블록 쌓기, Time: 104 ms, Memory: 12336 KB -BaekjoonHub

--- 백준/Gold/18427. 함께 블록 쌓기/함께 블록 쌓기.java
+++ 백준/Gold/18427. 함께 블록 쌓기/함께 블록 쌓기.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,31 +10,28 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
-		int n, h, i, j;
+		int n, m, h, i, j;
 		int[] dp;
-		ArrayList<Integer> blocks;
-		ArrayList<ArrayList<Integer>> students;
+		int[][] students;
 		
 		st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
-		Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
 		h = Integer.parseInt(st.nextToken());
-		students = new ArrayList<>(n);
+		students = new int[n][m];
 		for (i = 0; i < n; i++) {
-			blocks = new ArrayList<>();
 			st = new StringTokenizer(br.readLine());
-			while (st.hasMoreTokens()) {
-				blocks.add(Integer.parseInt(st.nextToken()));
+			for (j = 0; st.hasMoreTokens(); j++) {
+				students[i][j] = Integer.parseInt(st.nextToken());
 			}
-			students.add(blocks);
 		}
 		dp = new int[h + 1];
 		dp[0] = 1;
-		for (ArrayList<Integer> student : students) {
-			for (j = h; j >= 0; j--) {
+		for (int[] student : students) {
+			for (i = h; i >= 0; i--) {
 				for (int block : student) {
-					if (j >= block) {
-						dp[j] = (dp[j] + dp[j - block]) % P;
+					if (block > 0 && i >= block) {
+						dp[i] = (dp[i] + dp[i - block]) % P;
 					}
 				}
 			}
