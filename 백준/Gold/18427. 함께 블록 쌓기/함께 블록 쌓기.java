b419bdcb5da8a0051f0398dcba141dd7cd4f2c7d
cookingTorch <jhw7181@naver.com>
2024-04-04 09:35:35 +0900

    [Gold IV] Title: 함께 블록 쌓기, Time: 104 ms, Memory: 11880 KB -BaekjoonHub

--- 백준/Gold/18427. 함께 블록 쌓기/함께 블록 쌓기.java
+++ 백준/Gold/18427. 함께 블록 쌓기/함께 블록 쌓기.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.NoSuchElementException;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,7 +11,7 @@ public class Main {
 		StringTokenizer st;
 		
 		int n, m, h, i, j;
-		int[] dp, blocks;
+		int[] dp;
 		int[][] students;
 		
 		st = new StringTokenizer(br.readLine());
@@ -22,28 +21,19 @@ public class Main {
 		students = new int[n][m];
 		for (i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
-			try {
-				for (j = 0;; j++) {
-					students[i][j] = Integer.parseInt(st.nextToken());
-				}
-			} catch (NoSuchElementException e) {}
+			for (j = 0; st.hasMoreTokens(); j++) {
+				students[i][j] = Integer.parseInt(st.nextToken());
+			}
 		}
 		dp = new int[h + 1];
 		dp[0] = 1;
-		n--;
-		for (i = 0; i < n; i++) {
-			blocks = students[i];
-			for (j = h; j >= 0; j--) {
-				for (int block : blocks) {
-					if (block > 0 && j >= block) {
-						dp[j] = (dp[j] + dp[j - block]) % P;
+		for (int[] student : students) {
+			for (i = h; i >= 0; i--) {
+				for (int block : student) {
+					if (block > 0 && i >= block) {
+						dp[i] = (dp[i] + dp[i - block]) % P;
 					}
 				}
-			}
-		}
-		for (int block : students[i]) {
-			if (block > 0 && h >= block) {
-				dp[h] = (dp[h] + dp[h - block]) % P;
 			}
 		}
 		System.out.print(dp[h]);
