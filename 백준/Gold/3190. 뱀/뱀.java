b69807412a60805c67e88e8cf8c17d5f80c531d2
cookingTorch <jhw7181@naver.com>
2024-04-08 17:46:41 +0900

    [Gold IV] Title: 뱀, Time: 84 ms, Memory: 11824 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -1,160 +1,80 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-
+	private static final int INF = Integer.MAX_VALUE;
+	private static final int WALL = -1;
+	private static final int APPLE = 1;
+	private static final int EMPTY = 0;
+	private static final char LEFT = 'L';
+	
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String str;
 		StringTokenizer st;
 		
-		int n, k, l, i, appleX, appleY, changeNumber, live, time, size, flag;
-		int[] head = new int[2], temp = new int[2];
-		String way, turn;
-		Deque<int[]> snake = new LinkedList<>();
-		Deque<int[]> apple = new LinkedList<>();
-		Deque<Integer> changeTime = new LinkedList<>();
-		Deque<String> changeWay = new LinkedList<>();
+		int n, k, x, l, size, max, pos, next, dir, time, diff, cnt, i;
+		int[] map, d;
+		Queue<Integer> q;
 		
-		str = br.readLine();
-		n = Integer.parseInt(str);
-		str = br.readLine();
-		k = Integer.parseInt(str);
-		
+		n = Integer.parseInt(br.readLine());
+		k = Integer.parseInt(br.readLine());
+		size = n + 2;
+		max = size * size;
+		d = new int[] {-size, 1, size, -1};
+		map = new int[max];
+		for (i = 1; i <= n; i++) {
+			map[i] = WALL;
+			map[(n + 1) * size + i] = WALL;
+			map[i * size] = WALL;
+			map[i * size + n + 1] = WALL;
+		}
 		for (i = 0; i < k; i++) {
-			str = br.readLine();
-			st = new StringTokenizer(str, " ");
-			appleX = Integer.parseInt(st.nextToken());
-			appleY = Integer.parseInt(st.nextToken());
-			apple.add(new int[] {appleX, appleY});
+			st = new StringTokenizer(br.readLine());
+			map[Integer.parseInt(st.nextToken()) * size + Integer.parseInt(st.nextToken())] = APPLE;
 		}
-		
-		str = br.readLine();
-		l = Integer.parseInt(str);
-		
-		for (i = 0; i < l; i++) {
-			str = br.readLine();
-			st = new StringTokenizer(str, " ");
-			changeNumber = Integer.parseInt(st.nextToken());
-			way = st.nextToken();
-			changeTime.addLast(changeNumber);
-			changeWay.addLast(way);
+		dir = 1;
+		l = Integer.parseInt(br.readLine());
+		cnt = 1;
+		st = new StringTokenizer(br.readLine());
+		x = Integer.parseInt(st.nextToken());
+		if (st.nextToken().charAt(0) == LEFT) {
+			diff = 3;
+		} else {
+			diff = 1;
 		}
-		
-		way = "right";
-		live = 1;
-		time = 0;
-		snake.add(new int[] {1, 1});
-		while (live == 1) {
-			
-			time++;
-			
-			head = Arrays.copyOf(snake.getFirst(), 2);
-			
-			if (way.equals("right")) {
-				head[1]++;
+		q = new ArrayDeque<>();
+		pos = 1 * size + 1;
+		q.add(pos);
+		for (time = 1;; time++, pos = next) {
+			next = pos + d[dir];
+			switch (map[next]) {
+			case WALL:
+				System.out.print(time);
+				return;
+			case EMPTY:
+				map[q.poll()] = EMPTY;
+			case APPLE:
+				map[next] = WALL;
+				q.add(next);
 			}
-			else if (way.equals("left")) {
-				head[1]--;
-			}
-			else if (way.equals("up")) {
-				head[0]--;
-			}
-			else if (way.equals("down")) {
-				head[0]++;
-			}
-			
-			if (head[0] < 1 || head[1] < 1 || head[0] > n || head[1] > n) {
-				live = 0;
-				break;
-			}
-			
-			size = snake.size();
-			flag = 0;
-			for (i = 0; i < size; i++) {
-				temp = snake.pollFirst();
-				if (Arrays.equals(head, temp)) {
-					flag = 1;
-					break;
-				}
-				else {
-					snake.addLast(temp);
-				}
-			}
-			if (flag == 1) {
-				live = 0;
-				break;
-			}
-			
-			snake.addFirst(head);
-			size = apple.size();
-			flag = 0;
-			for (i = 0; i < size; i++) {
-				temp = apple.pollFirst();
-				if (Arrays.equals(head, temp)) {
-					flag = 1;
-				}
-				else {
-					apple.addLast(temp);
-				}
-			}
-			if (flag == 0) {
-				snake.removeLast();
-			}
-			
-			if (changeTime.size() > 0 && time == changeTime.getFirst()) {
-				turn = changeWay.pollFirst();
-				if (way.equals("right")) {
-					if (turn.equals("L")) {
-						way = "up";
-					}
-					else {
-						way = "down";
+			if (time == x) {
+				dir = (dir + diff) % 4;
+				if (cnt++ == l) {
+					x = INF;
+				} else {
+					st = new StringTokenizer(br.readLine());
+					x = Integer.parseInt(st.nextToken());
+					if (st.nextToken().charAt(0) == LEFT) {
+						diff = 3;
+					} else {
+						diff = 1;
 					}
 				}
-				else if (way.equals("left")) {
-					if (turn.equals("L")) {
-						way = "down";
-					}
-					else {
-						way = "up";
-					}
-				}
-				else if (way.equals("up")) {
-					if (turn.equals("L")) {
-						way = "left";
-					}
-					else {
-						way = "right";
-					}
-				}
-				else if (way.equals("down")) {
-					if (turn.equals("L")) {
-						way = "right";
-					}
-					else {
-						way = "left";
-					}
-				}
-				changeTime.removeFirst();
 			}
-			
 		}
-		
-		bw.write(Integer.toString(time));
-		
-		bw.flush();
-		bw.close();
-
 	}
-
 }
