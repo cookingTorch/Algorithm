7ca58fdbc87d621b5406b09d996527dcb600d44b
cookingTorch <jhw7181@naver.com>
2024-04-10 20:12:11 +0900

    [Gold IV] Title: 화장실의 규칙, Time: 332 ms, Memory: 44256 KB -BaekjoonHub

--- 백준/Gold/19640. 화장실의 규칙/화장실의 규칙.java
+++ 백준/Gold/19640. 화장실의 규칙/화장실의 규칙.java
@@ -12,6 +12,7 @@ public class Main {
 		
 		int n, m, k, cnt, size, row, col, ans, i, j;
 		int[] min;
+		int[][] line;
 		int[][][] map;
 		
 		st = new StringTokenizer(br.readLine());
@@ -19,44 +20,46 @@ public class Main {
 		m = Integer.parseInt(st.nextToken());
 		k = Integer.parseInt(st.nextToken());
 		size = (n - 1) / m + 1;
-		map = new int[size][m][];
+		map = new int[m][size][];
 		cnt = 0;
 		for (i = 0; i < size; i++) {
 			for (j = 0; j < m; j++) {
 				if (cnt++ < n) {
 					st = new StringTokenizer(br.readLine());
-					map[i][j] = new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+					map[j][i] = new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
 				} else {
-					map[i][j] = MIN;
+					map[j][i] = MIN;
 				}
 			}
 		}
-		row = k / m;
-		col = k % m;
-		min = map[0][col];
-		for (i = 1; i <= row; i++) {
-			if (map[i][col][0] < min[0] || (map[i][col][0] == min[0] && map[i][col][1] < min[1])) {
-				min = map[i][col];
+		row = k % m;
+		col = k / m;
+		line = map[row];
+		min = line[0];
+		for (i = 1; i <= col; i++) {
+			if (line[i][0] < min[0] || (line[i][0] == min[0] && line[i][1] < min[1])) {
+				min = line[i];
 			}
 		}
 		ans = 0;
-		for (i = 0; i < col; i++) {
+		for (i = 0; i < row; i++) {
+			line = map[i];
 			for (j = 0; j < size; j++) {
-				if (map[j][i][0] < min[0] || (map[j][i][0] == min[0] && map[j][i][1] < min[1])) {
+				if (line[j][0] < min[0] || (line[j][0] == min[0] && line[j][1] < min[1])) {
 					break;
 				}
 			}
 			ans += j;
 		}
 		for (++i; i < m; i++) {
+			line = map[i];
 			for (j = 0; j < size; j++) {
-				if (map[j][i][0] < min[0] || (map[j][i][0] == min[0] && map[j][i][1] <= min[1])) {
+				if (line[j][0] < min[0] || (line[j][0] == min[0] && line[j][1] <= min[1])) {
 					break;
 				}
 			}
 			ans += j;
 		}
-		ans += row;
-		System.out.print(ans);
+		System.out.print(ans + col);
 	}
 }
