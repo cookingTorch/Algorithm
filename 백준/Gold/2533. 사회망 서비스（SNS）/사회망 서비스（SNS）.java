7c7f2309060413cff607c08c832c75eb0ff5d633
cookingTorch <jhw7181@naver.com>
2024-04-13 22:13:13 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 1884 ms, Memory: 403660 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
@@ -1,66 +1,49 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
+	private static int cnt;
+	private static ArrayList<ArrayList<Integer>> adj;
 	
-	private static ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-	
-	private static int[] findMin(int from, int num) {
-		if (from > 0 && adj.get(num).size() == 1) {
-			return new int[] {0, 1};
-		}
-		else {
-			int i, next, min = 0, min1 = 0;
-			int[] temp;
-			for (i = 0; i < adj.get(num).size(); i++) {
-				next = adj.get(num).get(i);
-				if (next == from) {
-					continue;
-				}
-				temp = findMin(num, next);
-				min += Math.min(temp[0], temp[1]);
-				min1 += temp[1];
+	private static boolean isEarly(int node, int parent) {
+		boolean early;
+		
+		early = false;
+		for (int next : adj.get(node)) {
+			if (next != parent && !isEarly(next, node)) {
+				early = true;
 			}
-			return new int[] {min1, min + 1};
 		}
+		if (early) {
+			cnt++;
+		}
+		return early;
 	}
-
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		String str;
 		StringTokenizer st;
 		
-		int n, i, u, v;
-		int[] ans;
+		int n, u, v, i;
 		
-		str = br.readLine();
-		n = Integer.parseInt(str);
-		
-		for (i = 0; i < n + 1; i++) {
+		n = Integer.parseInt(br.readLine());
+		adj = new ArrayList<>(n + 1);
+		adj.add(null);
+		for (i = 1; i <= n; i++) {
 			adj.add(new ArrayList<>());
 		}
-		for (i = 0; i < n - 1; i++) {
-			str = br.readLine();
-			st = new StringTokenizer(str);
+		for (i = 1; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
 			u = Integer.parseInt(st.nextToken());
 			v = Integer.parseInt(st.nextToken());
 			adj.get(u).add(v);
 			adj.get(v).add(u);
 		}
-		
-		ans = findMin(0, 1);
-		sb.append(Math.min(ans[0], ans[1]));
-		
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
+		cnt = 0;
+		isEarly(1, 0);
+		System.out.print(cnt);
 	}
-
 }
