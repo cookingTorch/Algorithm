f03ccf58fabb9d09cdb04da300f051bf9b0f5392
cookingTorch <jhw7181@naver.com>
2024-04-13 20:25:47 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 124 ms, Memory: 48864 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
@@ -1,52 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 	private static final int MIN = 3;
-	private static final char STAR = '*';
-	private static final char EMPTY = ' ';
-	private static final char LINE_BREAK = '\n';
+	private static final byte STAR = '*';
+	private static final byte EMPTY = ' ';
+	private static final byte LINE_BREAK = '\n';
 	
-	private static char[][] ans;
+	private static int size;
+	private static byte[] ans;
 	
-	private static void draw(int x, int y, int size) {
-		if (size == MIN) {
-			ans[x][y] = STAR;
-			ans[x + 1][y - 1] = STAR;
-			ans[x + 1][y + 1] = STAR;
-			ans[x + 2][y - 2] = STAR;
-			ans[x + 2][y - 1] = STAR;
-			ans[x + 2][y] = STAR;
-			ans[x + 2][y + 1] = STAR;
-			ans[x + 2][y + 2] = STAR;
+	private static void draw(int x, int y, int scale) {
+		if (scale == MIN) {
+			ans[x * size + y] = STAR;
+			ans[(x + 1) * size + y - 1] = STAR;
+			ans[(x + 1) * size + y + 1] = STAR;
+			ans[(x + 2) * size + y - 2] = STAR;
+			ans[(x + 2) * size + y - 1] = STAR;
+			ans[(x + 2) * size + y] = STAR;
+			ans[(x + 2) * size + y + 1] = STAR;
+			ans[(x + 2) * size + y + 2] = STAR;
 			return;
 		}
-		size >>= 1;
-		draw(x, y, size);
-		draw(x + size, y - size, size);
-		draw(x + size, y + size, size);
+		scale >>= 1;
+		draw(x, y, scale);
+		draw(x + scale, y - scale, scale);
+		draw(x + scale, y + scale, scale);
 	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		
-		int n, size, i;
+		int n, i;
 		
 		n = Integer.parseInt(br.readLine());
 		size = 2 * n + 1;
-		ans = new char[n][size];
-		Arrays.fill(ans[0], EMPTY);
-		ans[0][size - 1] = LINE_BREAK;
+		ans = new byte[n * size];
+		for (i = 0; i < size; i++) {
+			ans[i] = EMPTY;
+		}
+		ans[size - 1] = LINE_BREAK;
 		for (i = 1; i < n; i++) {
-			System.arraycopy(ans[0], 0, ans[i], 0, size);
+			System.arraycopy(ans, 0, ans, i * size, size);
 		}
 		draw(0, n - 1, n);
-		for (i = 0; i < n; i++) {
-			sb.append(ans[i]);
-		}
-		System.out.print(sb);
+		System.out.write(ans);
 	}
 }
