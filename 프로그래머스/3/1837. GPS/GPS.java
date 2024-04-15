776c739d8862471765c9b9331939d933f2ede341
cookingTorch <jhw7181@naver.com>
2024-04-15 12:11:49 +0900

    [level 3] Title: GPS, Time: 66.81 ms, Memory: 94.4 MB -BaekjoonHub

--- 프로그래머스/3/1837. GPS/GPS.java
+++ 프로그래머스/3/1837. GPS/GPS.java
@@ -3,57 +3,51 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 class Solution {
-    private static final int MIN = Integer.MIN_VALUE;
+    private static final int INF = Integer.MAX_VALUE;
     private static ArrayList<ArrayList<Integer>> adj;
     
     private static int dijkstra(int n, int k, int[] gpsLog) {
-        int dest, last, node, cnt, min, i;
+        int node, cnt, i, j;
         int[] curr;
         int[][] distance;
         PriorityQueue<int[]> pq;
         
-        last = k - 2;
-        dest = gpsLog[k - 1];
-        distance = new int[n + 1][k];
-        distance[gpsLog[0]][0] = MIN;
+        distance = new int[n + 1][k + 1];
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= k; j++) {
+                distance[i][j] = INF;
+            }
+        }
+        distance[gpsLog[0]][1] = 0;
         pq = new PriorityQueue<>(new Comparator<int[]>() {
 			@Override
 			public int compare(int[] o1, int[] o2) {
 				return Integer.compare(distance[o1[0]][o1[1]], distance[o2[0]][o2[1]]);
 			}
 		});
-        pq.offer(new int[] {gpsLog[0], 0});
+        pq.offer(new int[] {gpsLog[0], 1});
         while (!pq.isEmpty()) {
             curr = pq.poll();
             node = curr[0];
             cnt = curr[1];
+            if (cnt == k) {
+                continue;
+            }
             for (int next : adj.get(node)) {
-                if (next == gpsLog[cnt + 1]) {
+                if (next == gpsLog[cnt]) {
                     if (distance[node][cnt] < distance[next][cnt + 1]) {
                         distance[next][cnt + 1] = distance[node][cnt];
-                        if (cnt < last) {
-                        	pq.offer(new int[] {next, cnt + 1});
-                        }
+                        pq.offer(new int[] {next, cnt + 1});
                     }
                 } else {
                     if (distance[node][cnt] + 1 < distance[next][cnt + 1]) {
                         distance[next][cnt + 1] = distance[node][cnt] + 1;
-                        if (cnt < last) {
-                        	pq.offer(new int[] {next, cnt + 1});
-                        }
+                        pq.offer(new int[] {next, cnt + 1});
                     }
                 }
             }
         }
-        min = 0;
-        for (i = 1; i <= n; i++) {
-        	if (adj.get(i).contains(dest)) {
-        		if (distance[i][last] < min) {
-        			min = distance[i][last];
-        		}
-        	}
-        }
-        return min == 0 ? -1 : min - MIN;
+        return distance[gpsLog[k - 1]][k] == INF ? -1 : distance[gpsLog[k - 1]][k];
     }
     
     public int solution(int n, int m, int[][] edge_list, int k, int[] gps_log) {
