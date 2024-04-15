03a48bfe1f3abc73f4a8e541eb804a998f11bc52
cookingTorch <jhw7181@naver.com>
2024-04-15 20:23:18 +0900

    [level 3] Title: GPS, Time: 19.43 ms, Memory: 94.4 MB -BaekjoonHub

--- 프로그래머스/3/1837. GPS/GPS.java
+++ 프로그래머스/3/1837. GPS/GPS.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 import java.util.ArrayDeque;
 
 class Solution {
+    private static final int MAX_N = 200;
     private static final int INF = Integer.MAX_VALUE;
     
     private static final class Node {
@@ -14,18 +15,20 @@ class Solution {
     	}
     }
     
-    private static int[] level;
+    private static int n;
+    private static int[] level = new int[MAX_N + 1];
+    private static ArrayDeque<Integer> q = new ArrayDeque<>();
     private static Node[] adj;
     
-    private static void bfs(int start) {
-    	int curr;
-    	ArrayDeque<Integer> q;
+    private static void bfs(int end) {
+    	int curr, i;
     	Node next;
     	
-    	Arrays.fill(level, INF);
-    	level[start] = 0;
-    	q = new ArrayDeque<>();
-    	q.addLast(start);
+        for (i = 1; i <= n; i++) {
+            level[i] = INF;
+        }
+    	level[end] = 0;
+    	q.addLast(end);
     	while (!q.isEmpty()) {
     		curr = q.pollFirst();
     		for (next = adj[curr]; next != null; next = next.next) {
@@ -37,12 +40,12 @@ class Solution {
     	}
     }
     
-    private static int bfs01(int n, int k, int[] gpsLog) {
-        int idx, cnt, dist;
+    private static int bfs01(int k, int[] gpsLog) {
+        int idx, next, cnt, dist;
         int[] curr;
         boolean[][] visited;
         ArrayDeque<int[]> dq;
-        Node next;
+        Node node;
         
         visited = new boolean[n + 1][k + 1];
         dq = new ArrayDeque<>();
@@ -55,30 +58,31 @@ class Solution {
             if (cnt == k) {
                 return dist;
             }
-            for (next = adj[idx]; next != null; next = next.next) {
-            	if (level[next.idx] < k - cnt && !visited[next.idx][cnt + 1]) {
-            		if (next.idx == gpsLog[cnt]) {
-            			dq.addFirst(new int[] {next.idx, cnt + 1, dist});
+            for (node = adj[idx]; node != null; node = node.next) {
+                next = node.idx;
+            	if (level[next] < k - cnt && !visited[next][cnt + 1]) {
+            		if (next == gpsLog[cnt]) {
+            			dq.addFirst(new int[] {next, cnt + 1, dist});
                     } else {
-                        dq.addLast(new int[] {next.idx, cnt + 1, dist + 1});
+                        dq.addLast(new int[] {next, cnt + 1, dist + 1});
                     }
+                    visited[next][cnt + 1] = true;
             	}
-            	visited[next.idx][cnt + 1] = true;
             }
         }
     }
     
     public int solution(int n, int m, int[][] edge_list, int k, int[] gps_log) {
+        this.n = n;
         adj = new Node[n + 1];
         for (int[] edge : edge_list) {
         	adj[edge[0]] = new Node(edge[1], adj[edge[0]]);
         	adj[edge[1]] = new Node(edge[0], adj[edge[1]]);
         }
-        level = new int[n + 1];
         bfs(gps_log[k - 1]);
         if (level[gps_log[0]] > k - 1) {
         	return -1;
         }
-        return bfs01(n, k, gps_log);
+        return bfs01(k, gps_log);
     }
 }
