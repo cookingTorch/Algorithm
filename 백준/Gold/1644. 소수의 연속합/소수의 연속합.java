0d80a8d3349785bd1949fda04d4cecd4ac5f4d4f
cookingTorch <jhw7181@naver.com>
2024-04-15 15:18:49 +0900

    [Gold III] Title: 소수의 연속합, Time: 148 ms, Memory: 31732 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -1,38 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n, idx, left, right, mid, cnt, i, j;
+		long[] primes;
 		boolean[] notPrime;
-		ArrayList<Long> primes;
 		
 		n = Integer.parseInt(br.readLine());
-		primes = new ArrayList<>(n >> 1);
-		primes.add(0L);
+		primes = new long[(n >> 1) + 2];
+		primes[0] = 0L;
 		idx = 0;
 		notPrime = new boolean[n + 1];
 		for (i = 2; i <= n; i++) {
 			if (!notPrime[i]) {
-				primes.add(primes.get(idx++) + i);
+				primes[++idx] = primes[idx - 1] + i;
 				for (j = i + i; j <= n; j += i) {
 					notPrime[j] = true;
 				}
 			}
 		}
 		cnt = 0;
-		for (i = 0; i < primes.size() - 1; i++) {
+		for (i = 0; i < idx; i++) {
 			left = i + 1;
-			right = primes.size();
+			right = idx + 1;
 			while (left < right) {
 				mid = (left + right) >> 1;
-				if (primes.get(mid) - primes.get(i) < n) {
+				if (primes[mid] - primes[i] < n) {
 					left = mid + 1;
-				} else if (primes.get(mid) - primes.get(i) > n) {
+				} else if (primes[mid] - primes[i] > n) {
 					right = mid;
 				} else {
 					cnt++;
