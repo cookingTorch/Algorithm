a5ee783619d76d01b452bf3d8c9022136d2b5217
cookingTorch <jhw7181@naver.com>
2024-04-16 15:07:47 +0900

    [Platinum V] Title: 격자점 컨벡스헐, Time: 76 ms, Memory: 11668 KB -BaekjoonHub

---
+++ 백준/Platinum/2699. 격자점 컨벡스헐/격자점 컨벡스헐.java
@@ -0,0 +1,114 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static final int LINE = 5;
+	private static final long INF = Long.MAX_VALUE;
+	private static final Point BASE = new Point(INF, INF);
+	
+	private static class Point implements Comparable<Point> {
+		long x, y;
+		
+		Point(long x, long y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		@Override
+		public int compareTo(Point o) {
+			long val;
+			
+			val = ccw(base, this, o);
+			if (val == 0) {
+				return Long.compare(dist(base, this), dist(base, o));
+			}
+			return val > 0 ? -1 : 1;
+		}
+	}
+	
+	private static Point base;
+	
+	private static long ccw(Point p1, Point p2, Point p3) {
+		return (p2.x - p1.x) * (p3.y - p2.y) - (p3.x - p2.x) * (p2.y - p1.y);
+	}
+	
+	private static long dist(Point p1, Point p2) {
+		return (p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y);
+	}
+	
+	private static ArrayList<Point> convexHull(ArrayList<Point> points) {
+		int i;
+		Stack<Point> stack;
+		
+		Collections.sort(points);
+		if (points.size() < 3) {
+			return new ArrayList<>(points);
+		} else if (ccw(points.get(points.size() - 1), points.get(0), points.get(1)) == 0) {
+			stack = new Stack<>();
+			stack.add(points.get(0));
+			stack.add(points.get(points.size() - 1));
+			return new ArrayList<>(stack);
+		}
+		stack = new Stack<>();
+		stack.add(points.get(0));
+		for (i = 0; i < points.size(); i++) {
+			while (stack.size() > 1 && ccw(stack.get(stack.size() - 2), stack.get(stack.size() - 1), points.get(i)) <= 0) {
+				stack.pop();
+			}
+			stack.add(points.get(i));
+		}
+		return new ArrayList<>(stack);
+	}
+	
+	private static void solution(BufferedReader br, StringBuilder sb, StringTokenizer st) throws IOException {
+		int n, start, i;
+		Point point;
+		ArrayList<Point> points, hull;
+		
+		n = Integer.parseInt(br.readLine());
+		base = BASE;
+		points = new ArrayList<>(n);
+		for (i = 0; i < n; i++) {
+			if (i % LINE == 0) {
+				st = new StringTokenizer(br.readLine());
+			}
+			point = new Point(Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken()));
+			points.add(point);
+			if (point.y < base.y || (point.y == base.y && point.x < base.x)) {
+				base = point;
+			}
+		}
+		hull = convexHull(points);
+		sb.append(hull.size()).append('\n');
+		start = 0;
+		for (i = 0; i < hull.size(); i++) {
+			point = hull.get(i);
+			if (point.y > base.y || (point.y == base.y && point.x < base.x)) {
+				base = point;
+				start = i;
+			}
+		}
+		for (i = start + hull.size(); i > start; i--) {
+			sb.append(hull.get(i % hull.size()).x).append(' ').append(hull.get(i % hull.size()).y).append('\n');
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+		
+		int t, testCase;
+		
+		t = Integer.parseInt(br.readLine());
+		for (testCase = 0; testCase < t; testCase++) {
+			solution(br, sb, st);
+		}
+		System.out.print(sb);
+	}
+}
