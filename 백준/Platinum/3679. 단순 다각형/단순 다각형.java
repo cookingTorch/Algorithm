af96680f17d0387b4b3c85dc5a7592ae3830957f
cookingTorch <jhw7181@naver.com>
2024-04-16 11:52:57 +0900

    [Platinum IV] Title: 단순 다각형, Time: 296 ms, Memory: 23372 KB -BaekjoonHub

---
+++ 백준/Platinum/3679. 단순 다각형/단순 다각형.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static final long INF = Long.MAX_VALUE;
+	
+	private static class Point implements Comparable<Point> {
+		int idx;
+		long x, y;
+		
+		Point(int idx, long x, long y) {
+			this.idx = idx;
+			this.x = x;
+			this.y = y;
+		}
+		
+		@Override
+		public int compareTo(Point o) {
+			long val;
+			
+			val = ccw(base, this, o);
+			if (val == 0) {
+				return Long.compare(dist(base, this), dist(base, o));
+			}
+			return val > 0 ? -1 : 1;
+		}
+	}
+	
+	private static int n;
+	private static Point base;
+	
+	private static long ccw(Point p1, Point p2, Point p3) {
+		return (p2.x - p1.x) * (p3.y - p2.y) - (p3.x - p2.x) * (p2.y - p1.y);
+	}
+	
+	private static long dist(Point p1, Point p2) {
+		return (p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y);
+	}
+	
+	private static void solution(BufferedReader br, StringBuilder sb, StringTokenizer st) throws IOException {
+		int end, i;
+		Point point;
+		ArrayList<Point> points;
+		
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		base = new Point(0, INF, INF);
+		points = new ArrayList<>();
+		for (i = 0; i < n; i++) {
+			point = new Point(i, Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken()));
+			points.add(point);
+			if (point.y < base.y || (point.y == base.y && point.x < base.x)) {
+				base = point;
+			}
+		}
+		Collections.sort(points);
+		for (end = n - 2; ccw(base, points.get(n - 1), points.get(end)) == 0; end--);
+		for (i = 0; i <= end; i++) {
+			sb.append(points.get(i).idx).append(' ');
+		}
+		for (i = n - 1; i > end; i--) {
+			sb.append(points.get(i).idx).append(' ');
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+		
+		int t, testCase;
+		
+		t = Integer.parseInt(br.readLine());
+		for (testCase = 0; testCase < t; testCase++) {
+			solution(br, sb, st);
+			sb.append('\n');
+		}
+		System.out.print(sb);
+	}
+}
