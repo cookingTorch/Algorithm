825b3a6546bcb735e490c01568e38d9be2cdb998
cookingTorch <jhw7181@naver.com>
2024-04-23 20:09:17 +0900

    [Gold IV] Title: 친구비, Time: 168 ms, Memory: 16968 KB -BaekjoonHub

--- 백준/Gold/16562. 친구비/친구비.java
+++ 백준/Gold/16562. 친구비/친구비.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+	private static int sum;
 	private static int[] costs, roots;
 	private static String IMPOSSIBLE = "Oh no";
 	
@@ -24,15 +25,15 @@ public class Main {
 		}
 		if (roots[ru] > roots[rv]) {
 			roots[ru] = rv;
+			sum -= Math.max(costs[ru], costs[rv]);
 			costs[rv] = Math.min(costs[ru], costs[rv]);
-			costs[ru] = 0;
 		} else {
 			if (roots[ru] == roots[rv]) {
 				roots[ru]--;
 			}
 			roots[rv] = ru;
+			sum -= Math.max(costs[ru], costs[rv]);
 			costs[ru] = Math.min(costs[ru], costs[rv]);
-			costs[rv] = 0;
 		}
 	}
 	
@@ -40,25 +41,23 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
-		int n, m, k, sum, i;
+		int n, m, k, i;
 		
 		st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		k = Integer.parseInt(st.nextToken());
 		costs = new int[n + 1];
+		sum = 0;
 		st = new StringTokenizer(br.readLine());
 		for (i = 1; i <= n; i++) {
 			costs[i] = Integer.parseInt(st.nextToken());
+			sum += costs[i];
 		}
 		roots = new int[n + 1];
 		for (i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
 			union(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-		}
-		sum = 0;
-		for (i = 1; i <= n; i++) {
-			sum += costs[i];
 		}
 		if (k < sum) {
 			System.out.print(IMPOSSIBLE);
