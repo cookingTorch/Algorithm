5f42115787fa7746e1ac5cf46202ebf9e7cc880b
cookingTorch <jhw7181@naver.com>
2024-04-27 23:30:34 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 204 ms, Memory: 129436 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -3,28 +3,47 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	private static int cnt;
-	private static StringBuilder sb;
+	private static final int SIZE = 4;
 	
-	private static void move(int size, int from, int to, int mid) {
+	private static StringBuilder[][] move;
+	private static StringBuilder[][][] dp;
+	
+	private static int getHanoi(int size) {
 		if (size == 1) {
-			cnt++;
-			sb.append(from).append(' ').append(to).append('\n');
-			return;
+			return 1;
 		}
-		move(size - 1, from, mid, to);
-		cnt++;
-		sb.append(from).append(' ').append(to).append('\n');
-		move(size - 1, mid, to, from);
+		return 2 * getHanoi(size - 1) + 1;
+	}
+	
+	private static StringBuilder getDp(int size, int from, int to, int mid) {
+		if (dp[size][from][to] != null) {
+			return dp[size][from][to];
+		}
+		if (size == 1) {
+			return dp[size][from][to] = move[from][to];
+		}
+		return dp[size][from][to] = new StringBuilder()
+				.append(getDp(size - 1, from, mid, to))
+				.append(move[from][to])
+				.append(getDp(size - 1, mid, to, from));
 	}
 
 	public static void main(String[] args) throws IOException {
 		int n;
+		int i;
+		int j;
 		
 		n = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
-		cnt = 0;
-		sb = new StringBuilder();
-		move(n, 1, 3, 2);
-		System.out.print(new StringBuilder().append(cnt).append('\n').append(sb));
+		move = new StringBuilder[SIZE][SIZE];
+		for (i = 1; i < SIZE; i++) {
+			for (j = 1; j < SIZE; j++) {
+				if (i == j) {
+					continue;
+				}
+				move[i][j] = new StringBuilder().append(i).append(' ').append(j).append('\n');
+			}
+		}
+		dp = new StringBuilder[n + 1][SIZE][SIZE];
+		System.out.print(new StringBuilder().append(getHanoi(n)).append('\n').append(getDp(n, 1, 3, 2)));
 	}
 }
