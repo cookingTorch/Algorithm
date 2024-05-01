9da5d405496cab5c6f661b5bb24ee79dbb4c299a
cookingTorch <jhw7181@naver.com>
2024-05-01 20:24:56 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 2.72 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -2,14 +2,8 @@ class Solution {
     private static final int SIZE = 1000001;
     
     public int[] solution(int[][] edges) {
-        int u;
-        int v;
-        int idx;
+        int i;
         int max;
-        int curr;
-        int start;
-        int[] next;
-        int[] firsts;
         int[] result;
         int[] outDegree;
         boolean[] in;
@@ -22,48 +16,19 @@ class Solution {
             in[edge[1]] = true;
             max = Math.max(max, Math.max(edge[0], edge[1]));
         }
-        idx = 0;
-        start = 0;
-        next = new int[max + 1];
-        firsts = null;
         result = new int[4];
-        for (int[] edge : edges) {
-            u = edge[0];
-            v = edge[1];
-            if (start == 0 && !in[u] && outDegree[u] > 1) {
-                start = u;
-                result[0] = start;
-                firsts = new int[outDegree[start]];
-            }
-            if (u == start) {
-                firsts[idx++] = v;
-            }
-            next[u] = v;
-        }
-        for (int first : firsts) {
-            if (next[first] == 0) {
-                result[2]++;
-                continue;
-            }
-            if (outDegree[first] == 2) {
-                result[3]++;
-                continue;
-            }
-            for (curr = next[first];; curr = next[curr]) {
-                if (curr == first) {
-                    result[1]++;
-                    break;
-                }
-                if (next[curr] == 0) {
-                    result[2]++;
-                    break;
-                }
-                if (outDegree[curr] == 2) {
+        for (i = 1; i <= max; i++) {
+            if (outDegree[i] > 1) {
+                if (in[i]) {
                     result[3]++;
-                    break;
+                } else {
+                    result[0] = i;
                 }
+            } else if (in[i] && outDegree[i] == 0) {
+                result[2]++;
             }
         }
+        result[1] = outDegree[result[0]] - result[2] - result[3];
         return result;
     }
 }
