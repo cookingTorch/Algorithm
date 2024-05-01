d5d01561feb8f879dd13aa2514848af4d215980b
cookingTorch <jhw7181@naver.com>
2024-05-01 19:04:50 +0900

    [level 4] Title: [3차] 자동완성, Time: 139.90 ms, Memory: 259 MB -BaekjoonHub

---
+++ 프로그래머스/4/17685. ［3차］ 자동완성/［3차］ 자동완성.java
@@ -0,0 +1,56 @@
+class Solution {
+    private static final int SIZE = 26;
+    private static final int DIFF = 'a';
+    
+    private static final class Trie {
+        int cnt;
+        Trie[] next;
+        
+        Trie() {
+            next = new Trie[SIZE];
+        }
+        
+        final void insert(String str) {
+            int i;
+            int len;
+            int idx;
+            Trie curr;
+            
+            len = str.length();
+            for (curr = this, i = 0; i < len; curr = curr.next[idx], i++) {
+                curr.cnt++;
+                idx = str.charAt(i) - DIFF;
+                if (curr.next[idx] == null) {
+                    curr.next[idx] = new Trie();
+                }
+            }
+            curr.cnt++;
+        }
+        
+        final int search(String str) {
+            int i;
+            int len;
+            Trie curr;
+            
+            len = str.length();
+            for (curr = this, i = 0; i < len && curr.cnt != 1; curr = curr.next[str.charAt(i++) - DIFF]);
+            return i;
+        }
+    }
+    
+    private static Trie root;
+    
+    public int solution(String[] words) {
+        int cnt;
+        
+        root = new Trie();
+        for (String word : words) {
+            root.insert(word);
+        }
+        cnt = 0;
+        for (String word : words) {
+            cnt += root.search(word);
+        }
+        return cnt;
+    }
+}
