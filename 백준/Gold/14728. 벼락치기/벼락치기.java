cb49cc3b6025f789ad71ff46ce5357915701a305
cookingTorch <jhw7181@naver.com>
2024-05-02 01:51:07 +0900

    [Gold V] Title: 벼락치기, Time: 88 ms, Memory: 11668 KB -BaekjoonHub

--- 백준/Gold/14728. 벼락치기/벼락치기.java
+++ 백준/Gold/14728. 벼락치기/벼락치기.java
@@ -1,17 +1,64 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		int n;
-		int t;
-		int k;
-		int s;
+	private static final class Item implements Comparable<Item> {
+		int weight;
+		int value;
+		double ratio;
+		
+		Item(int weight, int value) {
+			this.weight = weight;
+			this.value = value;
+			this.ratio = (double) value / weight;
+		}
+		
+		@Override
+		public int compareTo(Item o) {
+			return Double.compare(o.ratio, this.ratio);
+		}
+	}
+	
+	private static int n;
+	private static int t;
+	private static int maxProfit;
+	private static Item[] items;
+	
+	private static boolean promising(int depth, int weight, int profit) {
 		int i;
-		int j;
-		int[] dp;
+		int bound;
+		int totalWeight;
+		
+		if (weight >= t) {
+			return false;
+		}
+		bound = profit;
+		totalWeight = weight;
+		for (i = depth + 1; i < n && totalWeight + items[i].weight <= t; i++) {
+			bound += items[i].value;
+			totalWeight += items[i].weight;
+		}
+		if (i < n) {
+			bound += (t - totalWeight) * items[i].ratio;
+		}
+		return bound > maxProfit;
+	}
+	
+	private static void knapsack(int depth, int weight, int profit) {
+		if (weight <= t && profit > maxProfit) {
+			maxProfit = profit;
+		}
+		if (promising(depth, weight, profit)) {
+			knapsack(depth + 1, weight + items[depth + 1].weight, profit + items[depth + 1].value);
+			knapsack(depth + 1, weight, profit);
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		int i;
 		BufferedReader br;
 		StringTokenizer st;
 		
@@ -19,15 +66,14 @@ public class Main {
 		st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		t = Integer.parseInt(st.nextToken());
-		dp = new int[t + 1];
+		items = new Item[n];
 		for (i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
-			k = Integer.parseInt(st.nextToken());
-			s = Integer.parseInt(st.nextToken());
-			for (j = t; j >= k; j--) {
-				dp[j] = Math.max(dp[j], dp[j - k] + s);
-			}
+			items[i] = new Item(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
 		}
-		System.out.print(dp[t]);
+		Arrays.sort(items);
+		maxProfit = 0;
+		knapsack(-1, 0, 0);
+		System.out.print(maxProfit);
 	}
 }
