baeb3ad9e8a602d90b980cc967104e0d5be1cf11
cookingTorch <jhw7181@naver.com>
2024-05-03 15:32:49 +0900

    [level 3] Title: 표 편집, Time: 248.87 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -2,90 +2,102 @@ import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 class Solution {
-	private static final char U = 'U';
-	private static final char D = 'D';
-	private static final char Z = 'Z';
-	private static final char C = 'C';
-	private static final char[] XO = new char[] {'X', 'O'};
-	
-	private static int block;
-	private static int[] arr;
-	private static int[] bucket;
-	private static char[] ans;
-	
-	private static int delete(int prefix) {
-		int i;
-		int idx;
-		int sum;
-		
-		for (i = -1, sum = 0; sum < prefix; sum += bucket[++i]);
-		sum -= bucket[i]--;
-		for (idx = block * i - 1; sum < prefix; sum += arr[++idx]);
-		arr[idx]--;
-		return idx;
-	}
-	
-	private static int undo(int idx) {
-		int i;
-		int num;
-		int prefix;
-		
-		num = idx / block;
-		arr[idx]++;
-		bucket[num]++;
-		for (i = 0, prefix = 0; i < num; prefix += bucket[i++]);
-		for (i = block * num - 1; i < idx; prefix += arr[++i]);
-		return prefix;
-	}
-	
-	public String solution(int n, int k, String[] cmd) {
-		int i;
-		int idx;
-		int size;
-		StringTokenizer st;
-		ArrayDeque<Integer> stack;
-		
-		arr = new int[n];
-		for (i = 0; i < n; i++) {
-			arr[i] = 1;
-		}
-		block = (int) Math.sqrt(n);
-		bucket = new int[(n - 1) / block + 1];
-		for (i = 0; i < n / block; i++) {
-			bucket[i] = block;
-		}
-		if (i < bucket.length) {
-			bucket[i] = n % block;
-		}
-		idx = k + 1;
-		size = n;
-		stack = new ArrayDeque<>();
-		for (String str : cmd) {
-			st = new StringTokenizer(str);
-			switch (st.nextToken().charAt(0)) {
-			case U:
-				idx -= Integer.parseInt(st.nextToken());
-				break;
-			case D:
-				idx += Integer.parseInt(st.nextToken());
-				break;
-			case C:
-				stack.addFirst(delete(idx));
-				if (idx == size--) {
-					idx--;
-				}
-				break;
-			case Z:
-				if (undo(stack.removeFirst()) <= idx) {
-					idx++;
-				}
-				size++;
-			}
-		}
-		ans = new char[n];
-		for (i = 0; i < n; i++) {
-			ans[i] = XO[arr[i]];
-		}
-		return new String(ans);
-	}
+    private static final char U = 'U';
+    private static final char D = 'D';
+    private static final char Z = 'Z';
+    private static final char C = 'C';
+    private static final char O = 'O';
+    private static final char X = 'X';
+    
+    private static int[] tree;
+    private static char[] ans;
+    
+    private static void init(int node, int start, int end) {
+        int mid;
+        
+        if (start == end) {
+        	tree[node] = 1;
+            return;
+        }
+        tree[node] = end - start + 1;
+        mid = start + end >> 1;
+        init(node << 1, start, mid);
+        init(node << 1 | 1, mid + 1, end);
+    }
+    
+    private static int delete(int node, int start, int end, int prefix) {
+        int mid;
+        
+        tree[node]--;
+        if (start == end) {
+            ans[start] = X;
+            return start;
+        }
+        mid = start + end >> 1;
+        if (tree[node << 1] < prefix) {
+            return delete(node << 1 | 1, mid + 1, end, prefix - tree[node << 1]);
+        } else {
+            return delete(node << 1, start, mid, prefix);
+        }
+    }
+    
+    private static int undo(int node, int start, int end, int idx) {
+        int mid;
+        
+        tree[node]++;
+        if (start == end) {
+            ans[start] = O;
+            return 1;
+        }
+        mid = (start + end) >> 1;
+        if (idx <= mid) {
+            return undo(node << 1, start, mid, idx);
+        }
+        return tree[node << 1] + undo(node << 1 | 1, mid + 1, end, idx);
+    }
+    
+    public String solution(int n, int k, String[] cmd) {
+        int i;
+        int len;
+        int end;
+        int size;
+        int prefix;
+        StringTokenizer st;
+        ArrayDeque<Integer> stack;
+        
+        ans = new char[n];
+        for (i = 0; i < n; i++) {
+            ans[i] = O;
+        }
+        end = n - 1;
+        tree = new int[n << 2];
+        init(1, 0, end);
+        size = n;
+        prefix = k + 1;
+        stack = new ArrayDeque<>();
+        len = cmd.length;
+        for (i = 0; i < len; i++) {
+            st = new StringTokenizer(cmd[i]);
+            switch (st.nextToken().charAt(0)) {
+            case U:
+                prefix -= Integer.parseInt(st.nextToken());
+                break;
+            case D:
+                prefix += Integer.parseInt(st.nextToken());
+                break;
+            case C:
+                stack.addFirst(delete(1, 0, end, prefix));
+                if (prefix == size--) {
+                    prefix--;
+                }
+                break;
+            case Z:
+                if (undo(1, 0, end, stack.removeFirst()) <= prefix) {
+                    prefix++;
+                }
+                size++;
+            }
+        }
+        return new String(ans);
+    }
 }
