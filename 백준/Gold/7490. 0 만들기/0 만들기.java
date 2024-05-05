9db5d74b48389ee5f555bda58610046bd3d7548b
cookingTorch <jhw7181@naver.com>
2024-05-05 21:28:20 +0900

    [Gold V] Title: 0 만들기, Time: 76 ms, Memory: 11496 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.java
+++ 백준/Gold/7490. 0 만들기/0 만들기.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
-	private static final int SIZE = 10;
+	private static final int MAX = 10;
+	private static final int SIZE = 500;
+	private static final int DIFF = '0';
 	private static final char PLUS = '+';
 	private static final char MINUS = '-';
 	private static final char SPACE = ' ';
@@ -13,14 +15,14 @@ public class Main {
 	private static int max;
 	private static int[] num;
 	private static int[] idx;
-	private static char[] expression;
-	private static ArrayList<ArrayList<char[]>> strs;
+	private static byte[] expression;
+	private static ArrayList<ArrayList<byte[]>> list;
 	
 	private static final void dfs(int sum, int temp, int depth) {
 		int next;
 		
 		if (num[depth] >= 0 && sum + temp == 0) {
-			strs.get(num[depth]).add(Arrays.copyOf(expression, idx[depth] + 2));
+			list.get(num[depth]).add(Arrays.copyOf(expression, idx[depth] + 2));
 		}
 		if (depth == max) {
 			return;
@@ -38,37 +40,41 @@ public class Main {
 		int n;
 		int t;
 		int i;
-		char j;
-		StringBuilder sb;
+		byte j;
+		byte[] ans;
 		BufferedReader br;
 		
-		num = new int[SIZE];
+		num = new int[MAX];
 		Arrays.fill(num, -1);
-		idx = new int[SIZE];
-		for (i = 1; i < SIZE; i++) {
+		idx = new int[MAX];
+		for (i = 1; i < MAX; i++) {
 			idx[i] = ((i - 1) << 1) - 1;
 		}
-		strs = new ArrayList<>();
+		list = new ArrayList<>();
 		br = new BufferedReader(new InputStreamReader(System.in));
-		t = Integer.parseInt(br.readLine());
+		t = br.read() - DIFF;
 		for (i = 0; i < t; i++) {
-			n = Integer.parseInt(br.readLine());
+			br.read();
+			n = br.read() - DIFF;
 			num[n] = i;
-			strs.add(new ArrayList<>());
+			list.add(new ArrayList<>());
 			max = Math.max(max, n);
 		}
-		expression = new char[(max << 1) + 1];
+		expression = new byte[(max << 1) + 1];
 		for (i = 1, j = '1'; i <= max; i++, j++) {
 			expression[idx[i] + 1] = j;
 		}
 		dfs(0, 1, 1);
-		sb = new StringBuilder();
-		for (ArrayList<char[]> ans : strs) {
-			for (char[] str : ans) {
-				sb.append(str).append('\n');
+		ans = new byte[SIZE];
+		i = 0;
+		for (ArrayList<byte[]> strs : list) {
+			for (byte[] str : strs) {
+				System.arraycopy(str, 0, ans, i, str.length);
+				i += str.length;
+				ans[i++] = '\n';
 			}
-			sb.append('\n');
+			ans[i++] = '\n';
 		}
-		System.out.print(sb);
+		System.out.write(ans, 0, i);
 	}
 }
