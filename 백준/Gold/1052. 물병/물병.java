b2adbdbc613299c61315915f44ec2fd3679f9683
cookingTorch <jhw7181@naver.com>
2024-05-16 08:50:50 +0900

    [Gold V] Title: 물병, Time: 92 ms, Memory: 11556 KB -BaekjoonHub

---
+++ 백준/Gold/1052. 물병/물병.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		int n;
+		int k;
+		int ans;
+		BufferedReader br;
+		StringTokenizer st;
+		
+		br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		for (ans = 0; Integer.bitCount(n) > k; ans++, n++);
+		System.out.print(ans);
+	}
+}
