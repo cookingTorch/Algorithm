4f4b4f48ccb57d943e07a5bd311ceb178581bdf3
cookingTorch <jhw7181@naver.com>
2024-06-01 22:39:25 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 88 ms, Memory: 11804 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static int[] arr;
+	
+	public static int binarySearch(int num, int right) {
+		int left, mid;
+		
+		left = 0;
+		while (left < right) {
+			mid = (left + right) / 2;
+			if (arr[mid] < num) {
+				left = mid + 1;
+			} else {
+				right = mid;
+			}
+		}
+		return right;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int n, cnt, size, i;
+		int[] nums, idx, ans;
+		
+		n = Integer.parseInt(br.readLine());
+		nums = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (i = 0; i < n; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		idx = new int[n];
+		arr = new int[n];
+		size = 0;
+		for (i = 0; i < n; i++) {
+			idx[i] = binarySearch(nums[i], size);
+			arr[idx[i]] = nums[i];
+			if (idx[i] == size) {
+				size++;
+			}
+		}
+		ans = new int[size];
+		sb.append(size).append('\n');
+		cnt = size - 1;
+		for (i = n - 1; cnt >= 0; i--) {
+			if (idx[i] == cnt) {
+				ans[cnt--] = nums[i];
+			}
+		}
+		for (i = 0; i < size; i++) {
+			sb.append(ans[i]).append(' ');
+		}
+		System.out.print(sb);
+	}
+}
