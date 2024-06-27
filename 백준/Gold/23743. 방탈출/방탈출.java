3e87386516e121731079606f9ec32b91e4db09c6
cookingTorch <jhw7181@naver.com>
2024-06-28 06:02:02 +0900

    [Gold II] Title: 방탈출, Time: 668 ms, Memory: 74624 KB -BaekjoonHub

---
+++ 백준/Gold/23743. 방탈출/방탈출.java
@@ -0,0 +1,90 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static final class Edge implements Comparable<Edge> {
+		int u;
+		int v;
+		int weight;
+		
+		Edge(int u, int v, int weight) {
+			this.u = u;
+			this.v = v;
+			this.weight = weight;
+		}
+		
+		@Override
+		public int compareTo(Edge o) {
+			return Integer.compare(this.weight, o.weight);
+		}
+	}
+	
+	private static int[] roots;
+	
+	private static final int find(int v) {
+		if (roots[v] <= 0) {
+			return v;
+		}
+		return roots[v] = find(roots[v]);
+	}
+	
+	private static final boolean union(int u, int v) {
+		int ru;
+		int rv;
+		
+		ru = find(u);
+		rv = find(v);
+		if (ru == rv) {
+			return false;
+		}
+		if (roots[ru] > roots[rv]) {
+			roots[ru] = rv;
+		} else {
+			if (roots[ru] == roots[rv]) {
+				roots[ru]--;
+			}
+			roots[rv] = ru;
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		int n;
+		int m;
+		int i;
+		int out;
+		int ans;
+		Edge edge;
+		BufferedReader br;
+		StringTokenizer st;
+		PriorityQueue<Edge> pq;
+		
+		br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		pq = new PriorityQueue<>();
+		for (i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			pq.offer(new Edge(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+		}
+		out = n + 1;
+		st = new StringTokenizer(br.readLine());
+		for (i = 1; i <= n; i++) {
+			pq.offer(new Edge(out, i, Integer.parseInt(st.nextToken())));
+		}
+		ans = 0;
+		roots = new int[n + 2];
+		for (i = 0; i < n;) {
+			edge = pq.poll();
+			if (union(edge.u, edge.v)) {
+				ans += edge.weight;
+				i++;
+			}
+		}
+		System.out.print(ans);
+	}
+}
