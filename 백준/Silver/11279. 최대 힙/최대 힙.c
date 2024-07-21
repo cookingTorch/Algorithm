ec88bc21a06ecf8b2648f0bdfa88e86d731bf963
cookingTorch <jhw7181@naver.com>
2024-07-21 20:09:52 +0900

    [Silver II] Title: 최대 힙, Time: 20 ms, Memory: 1388 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.c
+++ 백준/Silver/11279. 최대 힙/최대 힙.c
@@ -4,7 +4,7 @@
 typedef struct priority_queue_s priority_queue_t;
 struct priority_queue_s {
 	int size;
-	int arr[MAX_PQ_SIZE];
+	int heap[MAX_PQ_SIZE];
 	int (*cmp)(int, int);
 };
 
@@ -20,22 +20,22 @@ priority_queue_offer(priority_queue_t *pq, int val)
 	int idx;
 	int parent_idx;
 	int parent_val;
-	int *arr;
+	int *heap;
 	int (*cmp)(int, int);
 
-	arr = pq->arr;
 	cmp = pq->cmp;
+	heap = pq->heap;
 	idx = ++pq->size;
-	pq->arr[idx] = val;
+	heap[idx] = val;
 	while (idx > 1) {
 		parent_idx = idx >> 1;
-		parent_val = pq->arr[parent_idx];
+		parent_val = pq->heap[parent_idx];
 		if (cmp(val, parent_val) >= 0)
 			break;
-		pq->arr[idx] = parent_val;
+		heap[idx] = parent_val;
 		idx = parent_idx;
 	}
-	pq->arr[idx] = val;
+	heap[idx] = val;
 }
 
 int
@@ -45,34 +45,31 @@ priority_queue_poll(priority_queue_t *pq)
 	int size;
 	int last;
 	int child;
-	int right;
 	int parent;
-	int *arr;
+	int *heap;
 	int (*cmp)(int, int);
 
 	if (!pq->size) {
 		return 0;
 	}
-	arr = pq->arr;
 	cmp = pq->cmp;
-	val = arr[1];
-	last = pq->arr[pq->size];
-	pq->arr[pq->size] = 0;
-	pq->size--;
-	size = pq->size;
+	heap = pq->heap;
+	val = heap[1];
+	last = heap[pq->size];
+	heap[pq->size] = 0;
+	size = --pq->size;
 	parent = 1;
 	child = parent << 1;
 	while (child <= size) {
-		right = child + 1;
-		if (right <= size && cmp(arr[child], arr[right]) > 0)
-			child = right;
-		if (cmp(last, arr[child]) <= 0)
+		if (child < size && cmp(heap[child], heap[child + 1]) > 0)
+			child++;
+		if (cmp(last, heap[child]) <= 0)
 			break;
-		arr[parent] = arr[child];
+		heap[parent] = heap[child];
 		parent = child;
 		child = parent << 1;
 	}
-	arr[parent] = last;
+	heap[parent] = last;
 	return val;
 }
 
