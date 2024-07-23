abdeefff9a0c60af078d34b67bb2dffdd39be4b2
cookingTorch <jhw7181@naver.com>
2024-07-23 16:48:05 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.c
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.c
@@ -1,89 +1,47 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#define INT_MAX          2147483647
-#define ARRAY_DEQUE_SIZE 20000
-
-typedef struct array_deque_s array_deque_t;
-struct array_deque_s {
-    int head;
-    int tail;
-    int elements[ARRAY_DEQUE_SIZE];
-};
-
-void
-array_deque_init(array_deque_t *dq)
-{
-    dq->head = 0;
-    dq->tail = 0;
-}
-
-void
-array_deque_add_last(array_deque_t *dq, int val)
-{
-    dq->tail = (dq->tail + 1) % ARRAY_DEQUE_SIZE;
-    dq->elements[dq->tail] = val;
-}
-
-void
-array_deque_add_first(array_deque_t *dq, int val)
-{
-    dq->elements[dq->head] = val;
-    dq->head = (dq->head + ARRAY_DEQUE_SIZE - 1) % ARRAY_DEQUE_SIZE;
-}
+int _n;
+int _min;
 
 int
-array_deque_poll_first(array_deque_t *dq)
+hide_and_seek_min(int val1, int val2)
 {
-    return dq->elements[dq->head = (dq->head + 1) % ARRAY_DEQUE_SIZE];
+    return val1 < val2 ? val1 : val2;
 }
 
-int
-hide_and_seek_bfs_0_1(int n, int k)
+void
+hide_and_seek_dfs(int k, int dist)
 {
-    int i;
-    int size;
-    int curr;
-    int *dist;
-    array_deque_t dq;
-    
-    if (n >= k)
-        return n - k;
-    size = k + 2;
-    dist = (int *) malloc(size * (sizeof(int)));
-    for (i = 0; i <= size; i++)
-        dist[i] = INT_MAX;
-    dist[n] = 0;
-    array_deque_init(&dq);
-    array_deque_add_last(&dq, n);
-    for (;;) {
-        curr = array_deque_poll_first(&dq);
-        if (curr == k)
-            break;
-        if (curr != 0 && (curr << 1) < size && dist[curr] < dist[curr << 1]) {
-            dist[curr << 1] = dist[curr];
-            array_deque_add_first(&dq, curr << 1);
-        }
-        if (curr > 0 && dist[curr] + 1 < dist[curr - 1]) {
-            dist[curr - 1] = dist[curr] + 1;
-            array_deque_add_last(&dq, curr - 1);
-        }
-        if (curr <= k && dist[curr] + 1 < dist[curr + 1]) {
-            dist[curr + 1] = dist[curr] + 1;
-            array_deque_add_last(&dq, curr + 1);
-    
-        }
+    if (_n >= k) {
+        _min = hide_and_seek_min(_min, dist + _n - k);
+        return;
     }
-    return dist[k];
+    _min = hide_and_seek_min(_min, dist + k - _n);
+    if (k & 1) {
+        hide_and_seek_dfs(k + 1, dist + 1);
+        hide_and_seek_dfs(k - 1, dist + 1);
+    } else
+        hide_and_seek_dfs(k >> 1, dist);
 }
 
 int
 main()
 {
-    int n;
     int k;
 
-    scanf("%d %d", &n, &k);
-    printf("%d", hide_and_seek_bfs_0_1(n, k));
+    scanf("%d %d", &_n, &k);
+    if (_n >= k) {
+        printf("%d", _n - k);
+        return 0;
+    }
+    _min = k - _n;
+    if (_n) {
+        hide_and_seek_dfs(k, 0);
+    }
+    else {
+        _n++;
+        hide_and_seek_dfs(k, 1);
+    }
+    printf("%d", _min);
     return 0;
 }
