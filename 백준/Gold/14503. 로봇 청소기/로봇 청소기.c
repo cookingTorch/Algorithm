0ea2531266efb2837ae4757647024dd3970f5c91
cookingTorch <jhw7181@naver.com>
2024-07-23 21:24:46 +0900

    [Gold V] Title: 로봇 청소기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.c
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.c
@@ -4,79 +4,42 @@
 #define ROBOT_WALL  '1'
 #define ROBOT_EMPTY '0'
 
-int _m;
-int _d;
-int _pos;
-int _cnt;
-int _delta[4];
-char *_map;
-
-void
-robot_clean()
-{
-    if (_map[_pos] == ROBOT_EMPTY) {
-        _map[_pos]--;
-        _cnt++;
-    }
-}
-
 int
-robot_all_cleaned()
+robot_clean(int m, int pos, int d, char *map)
 {
-    int i;
+	const int delta[] = {-m, 2, m, -2};
 
-    for (i = 0; i < 4; i++) {
-        if (_map[_pos + _delta[_d = (_d + 3) % 4]] == ROBOT_EMPTY)
-            break;
+	int i;
+	int rc;
+
+    for (rc = 0;;) {
+        if (map[pos] == ROBOT_EMPTY) {
+			map[pos]--;
+			rc++;
+		}
+		for (i = 4; i > 0 && map[pos + delta[d = (d + 3) % 4]] != ROBOT_EMPTY; i--);
+		if (i)
+			pos += delta[d];
+		else if (map[pos += delta[(d + 2) % 4]] == ROBOT_WALL)
+			break;
     }
-    return i == 4;
-}
-
-int
-robot_reverse()
-{
-    return _map[_pos += _delta[(_d + 2) % 4]] != ROBOT_WALL;
-}
-
-void
-robot_forward()
-{
-    _pos += _delta[_d];
-}
-
-void
-robot_start()
-{
-    for (;;) {
-        robot_clean();
-        if (robot_all_cleaned()) {
-            if (!robot_reverse())
-                break;
-        }
-        else
-            robot_forward();
-    }
+	return rc;
 }
 
 int
 main()
 {
     int n;
+	int m;
     int r;
     int c;
-    int i;
+	int d;
+	char *map;
 
-    scanf("%d %d\n%d %d %d", &n, &_m, &r, &c, &_d);
-    _map = (char *) malloc((n * (_m <<= 1) + 1) * sizeof(char));
-    scanf(" %[^2]", _map);
-    _pos = r * _m + (c << 1);
-    _delta[0] = -_m;
-    _delta[1] = 2;
-    _delta[2] = _m;
-    _delta[3] = -2;
-    _cnt = 0;
-    robot_start();
-    free(_map);
-    printf("%d", _cnt);
+    scanf("%d %d\n%d %d %d", &n, &m, &r, &c, &d);
+    map = (char *) malloc((n * (m <<= 1) + 1) * sizeof(char));
+    scanf(" %[^2]", map);
+    printf("%d", robot_clean(m, r * m + (c << 1), d, map));
+    free(map);
     return 0;
 }
