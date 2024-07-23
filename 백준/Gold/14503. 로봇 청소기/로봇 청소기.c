063d50b3bd11f7c036809002154d4489830a0266
cookingTorch <jhw7181@naver.com>
2024-07-23 12:55:12 +0900

    [Gold V] Title: 로봇 청소기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.c
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.c
@@ -1,54 +1,80 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define ROBOT_WALL      '1'
-#define ROBOT_EMPTY     '0'
-#define ROBOT_MAX_ROW   50
-#define ROBOT_MAX_COL   100
+#define ROBOT_WALL  '1'
+#define ROBOT_EMPTY '0'
 
 const int _dr[4] = {-1, 0, 1, 0};
 const int _dc[4] = {0, 2, 0, -2};
 
-char _map[ROBOT_MAX_ROW][ROBOT_MAX_COL];
+int _r;
+int _c;
+int _d;
+int _cnt;
+char **_map;
+
+void
+robot_clean() {
+    if (_map[_r][_c] == ROBOT_EMPTY) {
+        _map[_r][_c]--;
+        _cnt++;
+    }
+}
+
+int
+robot_all_cleaned() {
+    int i;
+
+    for (i = 0; i < 4; i++) {
+        _d = (_d + 3) % 4;
+        if (_map[_r + _dr[_d]][_c + _dc[_d]] == ROBOT_EMPTY)
+            break;
+    }
+    return i == 4;
+}
+
+int
+robot_reverse() {
+    return _map[_r += _dr[(_d + 2) % 4]][_c += _dc[(_d + 2) % 4]] != ROBOT_WALL;
+}
+
+void
+robot_forward() {
+    _r += _dr[_d];
+    _c += _dc[_d];
+}
 
 int
 main()
 {
     int n;
-    int r;
-    int c;
-    int d;
+    int m;
     int i;
-    int cnt;
 
     scanf("%d", &n);
-    scanf("%*s");
-    scanf("%d", &r);
-    scanf("%d", &c);
-    scanf("%d", &d);
+    scanf("%d", &m);
+    scanf("%d", &_r);
+    scanf("%d", &_c);
+    scanf("%d", &_d);
+    m <<= 1;
+    _map = (char **) malloc(n * sizeof(char *));
     for (i = 0; i < n; i++) {
+        _map[i] = (char *) malloc(m * sizeof(char));
         scanf(" %[^\n]", _map[i]);
     }
-    c <<= 1;
-    cnt = 0;
+    _c <<= 1;
+    _cnt = 0;
     for (;;) {
-        if (_map[r][c] == ROBOT_EMPTY) {
-            _map[r][c]--;
-            cnt++;
-        }
-        for (i = 0; i < 4; i++) {
-            d = (d + 3) % 4;
-            if (_map[r + _dr[d]][c + _dc[d]] == ROBOT_EMPTY)
+        robot_clean();
+        if (robot_all_cleaned()) {
+            if (!robot_reverse())
                 break;
         }
-        if (i == 4) {
-            r += _dr[(d + 2) % 4];
-            c += _dc[(d + 2) % 4];
-            if (_map[r][c] == ROBOT_WALL)
-                break;
-        } else {
-            r += _dr[d];
-            c += _dc[d];
-        }
+        else
+            robot_forward();
     }
-    printf("%d", cnt);
+    while (n-- > 0)
+        free(_map[n]);
+    free(_map);
+    printf("%d", _cnt);
 }
