4d545c497d9e43172136bb536bea559c393ee02e
cookingTorch <jhw7181@naver.com>
2024-07-23 21:39:38 +0900

    [Gold V] Title: 로봇 청소기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.c
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.c
@@ -12,8 +12,8 @@ robot_clean(int m, int pos, int d, char *map)
 	int i;
 	int rc;
 
-    for (rc = 0;;) {
-        if (map[pos] == ROBOT_EMPTY) {
+	for (rc = 0;;) {
+		if (map[pos] == ROBOT_EMPTY) {
 			map[pos]--;
 			rc++;
 		}
@@ -22,24 +22,24 @@ robot_clean(int m, int pos, int d, char *map)
 			pos += delta[d];
 		else if (map[pos += delta[(d + 2) % 4]] == ROBOT_WALL)
 			break;
-    }
+	}
 	return rc;
 }
 
 int
 main()
 {
-    int n;
+	int n;
 	int m;
-    int r;
-    int c;
+	int r;
+	int c;
 	int d;
 	char *map;
 
-    scanf("%d %d\n%d %d %d", &n, &m, &r, &c, &d);
-    map = (char *) malloc((n * (m <<= 1) + 1) * sizeof(char));
-    scanf(" %[^2]", map);
-    printf("%d", robot_clean(m, r * m + (c << 1), d, map));
-    free(map);
-    return 0;
+	scanf("%d %d\n%d %d %d", &n, &m, &r, &c, &d);
+	map = (char *) malloc((n * (m <<= 1) + 1) * sizeof(char));
+	scanf(" %[^2]", map);
+	printf("%d", robot_clean(m, r * m + (c << 1), d, map));
+	free(map);
+	return 0;
 }
