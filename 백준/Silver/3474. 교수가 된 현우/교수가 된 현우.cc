3ead346e53fb6ab4e33c9b22c5205ee5e429a745
cookingTorch <jhw7181@naver.com>
2024-08-23 11:07:39 +0900

    [Silver III] Title: 교수가 된 현우, Time: 12 ms, Memory: 3916 KB -BaekjoonHub

--- 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
@@ -6,28 +6,23 @@ using namespace std;
 constexpr char kLineBreak = '\n';
 constexpr int kFive = 5;
 
-int solution() {
+int main() {
+    int t;
     int n;
     int i;
     int cnt;
-
-    cin >> n;
-    cnt = 0;
-    for (i = kFive; i <= n; i *= kFive) {
-        cnt += n / i;
-    }
-    return cnt;
-}
-
-int main() {
-    int t;
     ostringstream os;
 
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> t;
     while (t--) {
-        os << solution() << kLineBreak;
+        cin >> n;
+        cnt = 0;
+        for (i = kFive; i <= n; i *= kFive) {
+            cnt += n / i;
+        }
+        os << cnt << kLineBreak;
     }
     cout << os.str();
 }
