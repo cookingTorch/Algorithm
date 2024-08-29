36e1d241c2a8058eb92bfe5dfcbcc60a9e5cd398
cookingTorch <jhw7181@naver.com>
2024-08-29 15:56:30 +0900

    [Gold III] Title: 최소 편집, Time: 100 ms, Memory: 12420 KB -BaekjoonHub

--- 백준/Gold/15483. 최소 편집/최소 편집.java
+++ 백준/Gold/15483. 최소 편집/최소 편집.java
@@ -3,39 +3,41 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    private static final int EOF = -1;
-    private static final int LINE_BREAK = '\n';
-
     public static void main(String[] args) throws IOException {
         int i;
-        int ch;
-        int len;
+        int j;
+        int len1;
+        int len2;
         int[] prev;
         int[] curr;
         int[] temp;
-        String str;
+        char ch;
+        String str1;
+        String str2;
         BufferedReader br;
 
         br = new BufferedReader(new InputStreamReader(System.in));
-        len = (str = br.readLine()).length();
-        prev = new int[len + 1];
-        curr = new int[len + 1];
-        for (i = 0; i <= len; i++) {
+        len1 = (str1 = br.readLine()).length();
+        len2 = (str2 = br.readLine()).length();
+        prev = new int[len1 + 1];
+        curr = new int[len1 + 1];
+        for (i = 0; i <= len1; i++) {
             prev[i] = i;
         }
-        while ((ch = br.read()) != EOF && ch != LINE_BREAK) {
+        for (i = 0; i < len2; i++) {
+            ch = str2.charAt(i);
             curr[0] = prev[0] + 1;
-            for (i = 1; i <= len; i++) {
-                if (ch == str.codePointAt(i - 1)) {
-                    curr[i] = prev[i - 1];
+            for (j = 1; j <= len1; j++) {
+                if (ch == str1.charAt(j - 1)) {
+                    curr[j] = prev[j - 1];
                 } else {
-                    curr[i] = Math.min(prev[i - 1], Math.min(prev[i], curr[i - 1])) + 1;
+                    curr[j] = Math.min(prev[j - 1], Math.min(prev[j], curr[j - 1])) + 1;
                 }
             }
             temp = prev;
             prev = curr;
             curr = temp;
         }
-        System.out.print(prev[len]);
+        System.out.print(prev[len1]);
     }
 }
