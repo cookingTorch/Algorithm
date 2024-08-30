5fd3c1ef66d55b6b3a9d5bf4d2ab21546c361e03
cookingTorch <jhw7181@naver.com>
2024-08-30 14:58:17 +0900

    [Gold III] Title: 텀 프로젝트, Time: 876 ms, Memory: 301376 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -1,61 +1,65 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static final int MAX_N = 100001;
-	private static final boolean[] VISITED = new boolean[MAX_N];
-	
-	private static int cnt, ans;
-	private static int[] selection;
-	private static boolean[] visited;
-	
-	private static int dfs(int node) {
-		int val;
-		
-		if (visited[node]) {
-			return node;
-		}
-		visited[node] = true;
-		val = dfs(selection[node]);
-		if (val == node) {
-			ans -= ++cnt;
-		}
-		cnt++;
-		return val;
-	}
-	
-	private static int solution(BufferedReader br, StringTokenizer st) throws IOException {
-		int n, i;
-		
-		n = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		for (i = 1; i <= n; i++) {
-			selection[i] = Integer.parseInt(st.nextToken());
-		}
-		visited = Arrays.copyOf(VISITED, n + 1);
-		ans = n;
-		for (i = 1; i <= n; i++) {
-			cnt = 0;
-			dfs(i);
-		}
-		return ans;
-	}
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = null;
-		
-		int t, testCase;
-		
-		selection = new int[MAX_N];
-		t = Integer.parseInt(br.readLine());
-		for (testCase = 0; testCase < t; testCase++) {
-			sb.append(solution(br, st)).append('\n');
-		}
-		System.out.print(sb);
-	}
+    private static final int MAX_N = 100_001;
+    private static final char LINE_BREAK = '\n';
+    private static final boolean[] VISITED = new boolean[MAX_N];
+
+    private static int cnt;
+    private static int ans;
+    private static int[] selection;
+    private static boolean[] visited;
+    private static BufferedReader br;
+
+    private static int dfs(int node) {
+        int val;
+
+        if (visited[node]) {
+            return node;
+        }
+        visited[node] = true;
+        val = dfs(selection[node]);
+        cnt++;
+        if (val == node) {
+            ans -= cnt;
+        }
+        return val;
+    }
+
+    private static int solution() throws IOException {
+        int n;
+        int i;
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (i = 1; i <= n; i++) {
+            selection[i] = Integer.parseInt(st.nextToken());
+        }
+        System.arraycopy(VISITED, 1, visited, 1, n);
+        ans = n;
+        for (i = 1; i <= n; i++) {
+            cnt = 0;
+            dfs(i);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) throws IOException {
+        int t;
+        StringBuilder sb;
+        
+        br = new BufferedReader(new InputStreamReader(System.in));
+        selection = new int[MAX_N];
+        visited = new boolean[MAX_N];
+        sb = new StringBuilder();
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            sb.append(solution()).append(LINE_BREAK);
+        }
+        System.out.print(sb);
+    }
 }
