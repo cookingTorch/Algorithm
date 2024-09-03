0f47e91ca9716aaa0a6d7bfe1a106f39a125d1fb
cookingTorch <jhw7181@naver.com>
2024-09-03 20:34:23 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 376 ms, Memory: 40820 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -4,38 +4,41 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static final int[] dx = {-1, 0, 1, 0};
-	private static final int[] dy = {0, 1, 0, -1};
-	
+	private static final int EMPTY = 0;
+	private static final int[] dx = {0, 0, -1, 1};
+	private static final int[] dy = {1, -1, 0, 0};
+
 	private static int n;
-	private static int[][] map, dp;
-	
+	private static int[][] map;
+	private static int[][] dp;
+
 	private static int getDp(int x, int y) {
-		int nx, ny, i;
-		
-		if (dp[x][y] != 0) {
+		int nx;
+		int ny;
+		int i;
+
+		if (dp[x][y] != EMPTY) {
 			return dp[x][y];
 		}
-		dp[x][y] = 1;
 		for (i = 0; i < 4; i++) {
 			nx = x + dx[i];
 			ny = y + dy[i];
-			if (nx < 0 || nx >= n || ny < 0 || ny >= n) {
+			if (nx < 0 || nx >= n || ny < 0 || ny >= n || map[nx][ny] >= map[x][y]) {
 				continue;
 			}
-			if (map[nx][ny] < map[x][y]) {
-				dp[x][y] = Math.max(dp[x][y], getDp(nx, ny) + 1);
-			}
+			dp[x][y] = Math.max(getDp(nx, ny), dp[x][y]);
 		}
-		return dp[x][y];
+		return ++dp[x][y];
 	}
-	
+
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int i;
+		int j;
+		int max;
+		BufferedReader br;
 		StringTokenizer st;
-		
-		int max, i, j;
-		
+
+		br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
 		map = new int[n][n];
 		for (i = 0; i < n; i++) {
