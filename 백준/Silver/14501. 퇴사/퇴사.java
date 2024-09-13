89b0f0efd25e5164bf256ce1e237c9496d2e3ea6
cookingTorch <jhw7181@naver.com>
2024-09-13 09:32:59 +0900

    [Silver III] Title: 퇴사, Time: 64 ms, Memory: 11556 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -14,15 +14,15 @@ public class Main {
 
         br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        dp = new int[n + 2];
-        for (i = 1; i <= n; i++) {
-            dp[i] = Math.max(dp[i - 1], dp[i]);
+        dp = new int[n + 1];
+        for (i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             t = Integer.parseInt(st.nextToken());
-            if (i + t <= n + 1) {
+            if (i + t <= n) {
                 dp[i + t] = Math.max(dp[i + t], dp[i] + Integer.parseInt(st.nextToken()));
             }
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
         }
-        System.out.print(Math.max(dp[n], dp[n + 1]));
+        System.out.print(dp[n]);
     }
 }
