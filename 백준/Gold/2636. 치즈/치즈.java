019095e734efd33cccd662faa6c21696afaf8929
cookingTorch <jhw7181@naver.com>
2024-09-18 22:27:58 +0900

    [Gold IV] Title: 치즈, Time: 64 ms, Memory: 11692 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -2,72 +2,80 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static int r, c, cnt;
-	private static int[] cheese;
+	private static final char CHEESE = '1';
+	private static final char WALL = '\0';
+	private static final char LINE_BREAK = '\n';
+
+	private static int qSize;
+	private static int[] d;
+	private static char[] map;
 	private static boolean[] visited;
-	private static Queue<Integer> q;
-	
-	private static boolean bfs() {
-		int curr;
-		boolean flag;
-		
-		cnt = 0;
-		flag = false;
-		visited = new boolean[r * c];
-		q = new ArrayDeque<>();
-		q.add(0);
-		visited[0] = true;
-		while (!q.isEmpty()) {
-			curr = q.poll();
-			if (cheese[curr] == 1) {
-				cheese[curr] = 0;
-				cnt++;
-				flag = true;
-			} else {
-				if (curr % c != c - 1 && !visited[curr + 1]) {
-					q.add(curr + 1);
-					visited[curr + 1] = true;
+	private static ArrayDeque<Integer> q;
+	private static ArrayDeque<Integer> q2;
+
+	private static void bfs(int pos) {
+		int i;
+		int npos;
+
+		q2.addLast(pos);
+		while (!q2.isEmpty()) {
+			pos = q2.pollFirst();
+			for (i = 0; i < 4; i++) {
+				npos = pos + d[i];
+				if (visited[npos]) {
+					continue;
 				}
-				if (curr + c < r * c && !visited[curr + c]) {
-					q.add(curr + c);
-					visited[curr + c] = true;
+				visited[npos] = true;
+				if (map[npos] == WALL) {
+					continue;
 				}
-				if (curr % c != 0 && !visited[curr - 1]) {
-					q.add(curr - 1);
-					visited[curr - 1] = true;
-				}
-				if (curr - c >= 0 && !visited[curr - c]) {
-					q.add(curr - c);
-					visited[curr - c] = true;
+				if (map[npos] == CHEESE) {
+					q.addLast(npos);
+				} else {
+					q2.addLast(npos);
 				}
 			}
 		}
-		return flag;
 	}
-	
+
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+		int i;
+		int row;
+		int col;
+		int size;
+		int pos;
+		int time;
+		StringBuilder sb;
+		BufferedReader br;
 		StringTokenizer st;
-		
-		int ans, i, j;
-		
+
+		br = new BufferedReader(new InputStreamReader(System.in));
 		st = new StringTokenizer(br.readLine());
-		r = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
-		cheese = new int[r * c];
-		for (i = 0; i < r; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (j = 0; j < c; j++) {
-				cheese[i * c + j] = Integer.parseInt(st.nextToken());
+		row = Integer.parseInt(st.nextToken());
+		col = (Integer.parseInt(st.nextToken()) << 1) + 3;
+		size = (row + 2) * col;
+		map = new char[size];
+		for (i = 1; i <= row; i++) {
+			br.read(map, i * col + 2, col - 4);
+			br.read();
+		}
+		d = new int[] {2, col, -2, -col};
+		visited = new boolean[size];
+		q = new ArrayDeque<>();
+		q2 = new ArrayDeque<>();
+		visited[1 * col + 2] = true;
+		q.addLast(1 * col + 2);
+		for (time = -1; !q.isEmpty(); time++) {
+			qSize = q.size();
+			for (i = 0; i < qSize; i++) {
+				pos = q.pollFirst();
+				bfs(pos);
 			}
 		}
-		for (i = 0, ans = 0; bfs(); i++, ans = cnt);
-		sb.append(i).append('\n').append(ans);
-		System.out.print(sb);
+		sb = new StringBuilder();
+		System.out.print(sb.append(time).append(LINE_BREAK).append(time == 0 ? 0 : qSize).toString());
 	}
 }
