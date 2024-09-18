6deb72fba6046a3f18801bb41372289b7311ffbd
cookingTorch <jhw7181@naver.com>
2024-09-18 23:42:59 +0900

    [Silver III] Title: 회전하는 큐, Time: 64 ms, Memory: 11592 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -5,42 +5,42 @@ import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        int n;
-        int m;
-        int i;
-        int num;
-        int idx;
-        int cnt;
-        int moves;
-        BufferedReader br;
-        StringTokenizer st;
-        ArrayDeque<Integer> dq;
+	public static void main(String[] args) throws IOException {
+		int n;
+		int m;
+		int i;
+		int num;
+		int idx;
+		int cnt;
+		int moves;
+		BufferedReader br;
+		StringTokenizer st;
+		ArrayDeque<Integer> q;
 
-        br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        dq = new ArrayDeque<>(n);
-        for (i = 1; i <= n; i++) {
-            dq.addLast(i);
-        }
-        cnt = 0;
-        st = new StringTokenizer(br.readLine());
-        while (m-- > 0) {
-            idx = Integer.parseInt(st.nextToken());
-            moves = 0;
-            while ((num = dq.pollFirst()) != idx) {
-                dq.addLast(num);
-                moves++;
-            }
-            if (moves < n - moves) {
-                cnt += moves;
-            } else {
-                cnt += n - moves;
-            }
-            n--;
-        }
-        System.out.print(cnt);
-    }
+		br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		q = new ArrayDeque<>(n);
+		for (i = 1; i <= n; i++) {
+			q.addLast(i);
+		}
+		cnt = 0;
+		st = new StringTokenizer(br.readLine());
+		while (m-- > 0) {
+			idx = Integer.parseInt(st.nextToken());
+			moves = 0;
+			while ((num = q.pollFirst()) != idx) {
+				q.addLast(num);
+				moves++;
+			}
+			if (moves < n - moves) {
+				cnt += moves;
+			} else {
+				cnt += n - moves;
+			}
+			n--;
+		}
+		System.out.print(cnt);
+	}
 }
