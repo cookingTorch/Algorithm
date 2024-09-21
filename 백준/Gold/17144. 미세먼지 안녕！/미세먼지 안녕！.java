215af508c8d9665a591d883d7d196cfcf7c09942
cookingTorch <jhw7181@naver.com>
2024-09-21 22:55:14 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 180 ms, Memory: 15292 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -4,105 +4,127 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static final int AIR_CLEANER = -1;
-	private static final int[] dx = {-1, 0, 1, 0};
-	private static final int[] dy = {0, 1, 0, -1};
-	
-	private static int r, c, x1, y1, x2, y2;
-	private static int[][] map, diff;
-	
-	private static void spread() {
-		int x, y, nx, ny, i;
-		
-		for (x = 0; x < r; x++) {
-			for (y = 0; y < c; y++) {
-				for (i = 0; i < 4; i++) {
-					nx = x + dx[i];
-					ny = y + dy[i];
-					if (0 <= nx && nx < r && 0 <= ny && ny < c && map[nx][ny] != AIR_CLEANER && map[x][y] > 0) {
-						diff[nx][ny] += map[x][y] / 5;
-						diff[x][y] -= map[x][y] / 5;
-					}
-				}
-			}
-		}
-		for (x = 0; x < r; x++) {
-			for (y = 0; y < c; y++) {
-				map[x][y] += diff[x][y];
-				diff[x][y] = 0;
+	private static final int WALL = Integer.MAX_VALUE;
+
+	private static int r;
+	private static int c;
+	private static int ap1;
+	private static int ap2;
+	private static int thr;
+	private static int col;
+	private static int[] d;
+	private static int[] map;
+	private static int[] diff;
+
+	private static void diffuse(int pos) {
+		int i;
+		int val;
+		int npos;
+
+		val = map[pos] / 5;
+		for (i = 0; i < 4; i++) {
+			npos = pos + d[i];
+			if (map[npos] != WALL) {
+				map[pos] -= val;
+				diff[npos] += val;
 			}
 		}
 	}
-	
-	private static void clean() {
-		int x, y, nx, ny, i;
-		
-		x = x1 - 1;
-		y = y1;
-		i = 0;
-		while (x != x1 || y != y1) {
-			nx = x + dx[i];
-			ny = y + dy[i];
-			if (nx < 0 || nx > x1 || ny < 0 || ny == c) {
-				i++;
-				continue;
-			}
-			map[x][y] = map[nx][ny];
-			x = nx;
-			y = ny;
+
+	private static void diffuseAll() {
+		int i;
+		int j;
+
+		for (i = col + 1; i < ap1; i += col) {
+			diffuse(i);
 		}
-		map[x1][y1 + 1] = 0;
-		x = x2 + 1;
-		y = y2;
-		i = 2;
-		while (x != x2 || y != y2) {
-			nx = x + dx[i];
-			ny = y + dy[i];
-			if (nx < x2 || nx == r || ny < 0 || ny == c) {
-				i = (i + 3) % 4;
-				continue;
-			}
-			map[x][y] = map[nx][ny];
-			x = nx;
-			y = ny;
+		for (i = ap2 + col; i < thr; i += col) {
+			diffuse(i);
 		}
-		map[x2][y2 + 1] = 0;
+		for (i = col; i < thr; i += col) {
+			for (j = 2; j <= c; j++) {
+				diffuse(i + j);
+			}
+		}
+		for (i = col; i < thr; i += col) {
+			for (j = i + 1; j <= i + c; j++) {
+				map[j] += diff[j];
+				diff[j] = 0;
+			}
+		}
 	}
-	
+
+	private static void purify() {
+		int i;
+
+		for (i = ap1 - col; i > col + 1; i -= col) {
+			map[i] = map[i - col];
+		}
+		System.arraycopy(map, col + 2, map, col + 1, c - 1);
+		for (i = col + c; i < ap1; i += col) {
+			map[i] = map[i + col];
+		}
+		System.arraycopy(map, ap1 + 1, map, ap1 + 2, c - 2);
+		map[ap1 + 1] = 0;
+		for (i = ap2 + col; i < thr - col; i += col) {
+			map[i] = map[i + col];
+		}
+		System.arraycopy(map, r * col + 2, map, r * col + 1, c - 1);
+		for (i = r * col + c; i > ap2 + col; i -= col) {
+			map[i] = map[i - col];
+		}
+		System.arraycopy(map, ap2 + 1, map, ap2 + 2, c - 2);
+		map[ap2 + 1] = 0;
+	}
+
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t;
+		int i;
+		int j;
+		int cnt;
+		BufferedReader br;
 		StringTokenizer st;
-		
-		int t, sum, i, j;
-		
-		st = new StringTokenizer(br.readLine());
+
+		br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine(), " ", false);
 		r = Integer.parseInt(st.nextToken());
 		c = Integer.parseInt(st.nextToken());
 		t = Integer.parseInt(st.nextToken());
-		map = new int[r][c];
-		for (i = 0; i < r; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (j = 0; j < c; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				if (map[i][j] == AIR_CLEANER) {
-					x2 = i;
-					y2 = j;
-					x1 = x2 - 1;
-					y1 = y2;
-				}
+		col = c + 2;
+		thr = (r + 1) * col;
+		map = new int[(r + 2) * col];
+		for (i = 1; i <= c; i++) {
+			map[i] = WALL;
+		}
+		for (i = col; i < thr; i += col) {
+			map[i] = WALL;
+			st = new StringTokenizer(br.readLine(), " ", false);
+			map[i + 1] = Integer.parseInt(st.nextToken());
+			if (map[i + 1] == -1) {
+				map[i + 1] = WALL;
+				ap2 = i + 1;
+			}
+			for (j = 2; j <= c; j++) {
+				map[i + j] = Integer.parseInt(st.nextToken());
+			}
+			map[i + c + 1] = WALL;
+		}
+		ap1 = ap2 - col;
+		System.arraycopy(map, 1, map, (r + 1) * col + 1, c);
+		diff = new int[(r + 1) * col];
+		d = new int[] {-col, 1, col, -1};
+		while (t-- > 0) {
+			diffuseAll();
+			purify();
+		}
+		map[ap1] = 0;
+		map[ap2] = 0;
+		cnt = 0;
+		for (i = col; i < thr; i += col) {
+			for (j = 1; j <= c; j++) {
+				cnt += map[i + j];
 			}
 		}
-		diff = new int[r][c];
-		for (i = 0; i < t; i++) {
-			spread();
-			clean();
-		}
-		sum = 2;
-		for (i = 0; i < r; i++) {
-			for (j = 0; j < c; j++) {
-				sum += map[i][j];
-			}
-		}
-		System.out.print(sum);
+		System.out.print(cnt);
 	}
 }
