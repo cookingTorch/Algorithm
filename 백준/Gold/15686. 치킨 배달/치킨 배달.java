b313b73a571debe528964e432a3af0f98d08cdca
cookingTorch <jhw7181@naver.com>
2024-09-24 22:37:37 +0900

    [Gold V] Title: 치킨 배달, Time: 76 ms, Memory: 12152 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,78 +1,122 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static final int MAX = 10000;
-	
-	private static int m, num, size, min;
-	private static int[] ans;
-	private static int[][] distance;
-	
-	private static void dfs(int start, int dist, int depth) {
-		int temp, i, j;
-		int[] save;
-		
-		if (depth == m) {
-			min = Math.min(min, dist);
+	private static final int MAX_M = 13;
+	private static final int EMPTY = '0';
+	private static final int HOME = '1';
+	private static final int CHICKEN = '2';
+	private static final int INF = Integer.MAX_VALUE;
+
+	private static final class Dist implements Comparable<Dist> {
+		int idx;
+		int dist;
+
+		Dist(int idx, int dist) {
+			this.idx = idx;
+			this.dist = dist;
 		}
-		for (i = start; i < size; i++) {
-			temp = 0;
-			save = Arrays.copyOf(ans, num);
-			for (j = 0; j < num; j++) {
-				if (distance[j][i] < ans[j]) {
-					temp += ans[j] - distance[j][i];
-					ans[j] = distance[j][i];
-				}
-			}
-			dfs(i + 1, dist - temp, depth + 1);
-			ans = save;
+
+		@Override
+		public int compareTo(Dist o) {
+			return Integer.compare(dist, o.dist);
 		}
 	}
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int n, i, j;
-		int[] home, chicken;
-		ArrayList<int[]> homes, chickens;
-		
-		st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		
-		homes = new ArrayList<>();
-		chickens = new ArrayList<>();
-		for (i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (j = 0; j < n; j++) {
-				switch (Integer.parseInt(st.nextToken())) {
-				case 1:
-					homes.add(new int[] {i, j});
-					break;
-				case 2:
-					chickens.add(new int[] {i, j});
+
+	private static int n;
+	private static int m;
+	private static int sum;
+	private static int min;
+	private static int homeIdx;
+	private static int chickenIdx;
+	private static boolean[] on;
+	private static Dist[][] dists;
+
+	private static int getDist(int pos1, int pos2) {
+		return Math.abs(pos1 / n - pos2 / n) + Math.abs(pos1 % n - pos2 % n);
+	}
+
+	private static void dfs(int start, int depth) {
+		int i;
+
+		if (depth == m) {
+			sum = 0;
+			for (i = 0; i < homeIdx; i++) {
+				for (Dist dist : dists[i]) {
+					if (on[dist.idx]) {
+						sum += dist.dist;
+						break;
+					}
 				}
 			}
+			if (sum < min) {
+				min = sum;
+			}
+			return;
 		}
-		num = homes.size();
-		size = chickens.size();
-		distance = new int[num][size];
-		for (i = 0; i < num; i++) {
-			home = homes.get(i);
-			for (j = 0; j < size; j++) {
-				chicken = chickens.get(j);
-				distance[i][j] = Math.abs(home[0] - chicken[0]) + Math.abs(home[1] - chicken[1]);
+		depth++;
+		for (i = start; i < chickenIdx; i++) {
+			on[i] = true;
+			dfs(i + 1, depth);
+			on[i] = false;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		int i;
+		int j;
+		int size;
+		char[] map;
+		int home;
+		int[] homes;
+		int[] chickens;
+		BufferedReader br;
+		StringTokenizer st;
+
+		br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine(), " ", false);
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		size = n * n;
+		homes = new int[n << 1];
+		chickens = new int[MAX_M];
+		switch (br.read()) {
+			case EMPTY:
+				break;
+			case HOME:
+				homes[homeIdx++] = 0;
+				break;
+			case CHICKEN:
+				chickens[chickenIdx++] = 0;
+				break;
+		}
+		for (i = 1; i < size; i++) {
+			br.read();
+			switch (br.read()) {
+				case EMPTY:
+					break;
+				case HOME:
+					homes[homeIdx++] = i;
+					break;
+				case CHICKEN:
+					chickens[chickenIdx++] = i;
+					break;
 			}
 		}
-		min = MAX * num;
-		ans = new int[num];
-		Arrays.fill(ans, MAX);
-		dfs(0, min, 0);
+		dists = new Dist[homeIdx][chickenIdx];
+		for (i = 0; i < homeIdx; i++) {
+			home = homes[i];
+			for (j = 0; j < chickenIdx; j++) {
+				dists[i][j] = new Dist(j, getDist(home, chickens[j]));
+			}
+			Arrays.sort(dists[i]);
+		}
+		min = INF;
+		on = new boolean[chickenIdx];
+		dfs(0, 0);
 		System.out.print(min);
 	}
 }
