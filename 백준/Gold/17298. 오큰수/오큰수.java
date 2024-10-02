0aaae4962414fc1e996acd1afbfc14630295420a
cookingTorch <jhw7181@naver.com>
2024-10-02 17:02:32 +0900

    [Gold IV] Title: 오큰수, Time: 692 ms, Memory: 195980 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,62 +1,46 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 public class Main {
+	private static final int INF = Integer.MAX_VALUE;
+	private static final int FAIL = -1;
+	private static final char SPACE = ' ';
 
 	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String str;
+		int n;
+		int i;
+		int[] arr;
+		int[] ans;
+		ArrayDeque<Integer> stack;
+		StringBuilder sb;
+		BufferedReader br;
 		StringTokenizer st;
-		
-		int n, i, ai, temp, in;
-		int[] nge;
-		Deque<Integer> index = new LinkedList<>(); 
-		Deque<Integer> num = new LinkedList<>();
-		
-		str = br.readLine();
-		n = Integer.parseInt(str);
-		
-		nge = new int[n];
-		
-		str = br.readLine();
-		st = new StringTokenizer(str, " ");
-		
-		for (i = 0; i < n; i++) {
-			ai = Integer.parseInt(st.nextToken());
-			while (num.size() != 0) {
-				temp = num.pollLast();
-				if (ai > temp) {
-					in = index.pollLast();
-					nge[in] = ai;
-				}
-				else {
-					num.addLast(temp);
-					break;
-				}
-			}
-			index.add(i);
-			num.add(ai);
-		}
-		
-		while (index.size() != 0) {
-			in = index.pollLast();
-			nge[in] = -1;
-		}
-		
-		for (i = 0; i < n; i++) {
-			bw.write(Integer.toString(nge[i]) + " ");
-		}
-		
-		bw.flush();
-		bw.close();
-	}
 
+		br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		stack = new ArrayDeque<>(n + 1);
+		arr = new int[n + 1];
+		st = new StringTokenizer(br.readLine(), " ", false);
+		for (i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		arr[n] = INF;
+		ans = new int[n];
+		stack.addFirst(n);
+		while (n-- > 0) {
+			while (arr[n] >= arr[stack.peekFirst()]) {
+				stack.pollFirst();
+			}
+			ans[n] = arr[stack.peekFirst()];
+			stack.addFirst(n);
+		}
+		sb = new StringBuilder();
+		for (int num : ans) {
+			sb.append(num == INF ? FAIL : num).append(SPACE);
+		}
+		System.out.print(sb.toString());
+	}
 }
