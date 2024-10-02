813351cde83ab40ffe5078da2700e8d7e8f562f5
cookingTorch <jhw7181@naver.com>
2024-10-02 22:40:56 +0900

    [Silver II] Title: DFS와 BFS, Time: 120 ms, Memory: 17152 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -2,32 +2,52 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
 	private static final int MAX_N = 1_000;
 	private static final char SPACE = ' ';
 	private static final char LINE_BREAK = '\n';
+	private static final Edge NIL = new Edge(Integer.MAX_VALUE, null);
 
-	private static StringBuilder sb;
+	private static final class Edge {
+		int to;
+		Edge next;
+
+		Edge(int to, Edge next) {
+			this.to = to;
+			this.next = next;
+		}
+	}
 
 	private static boolean[] visited;
-	private static ArrayList<Integer>[] adj;
+	private static Edge[] adj;
+	private static StringBuilder sb;
+
+	private static final void addEdge(int u, int v) {
+		Edge edge;
+
+		for (edge = adj[u]; edge.next.to < v; edge = edge.next);
+		edge.next = new Edge(v, edge.next);
+	}
 
 	private static final void dfs(int curr) {
-		for (int next : adj[curr]) {
-			if (!visited[next]) {
-				sb.append(next).append(SPACE);
-				visited[next] = true;
-				dfs(next);
+		int to;
+		Edge edge;
+
+		for (edge = adj[curr].next; edge != NIL; edge = edge.next) {
+			if (!visited[to = edge.to]) {
+				sb.append(to).append(SPACE);
+				visited[to] = true;
+				dfs(to);
 			}
 		}
 	}
 
 	private static final void bfs(int start) {
+		int to;
 		int curr;
+		Edge edge;
 		ArrayDeque<Integer> q;
 
 		q = new ArrayDeque<>(MAX_N);
@@ -36,11 +56,11 @@ public class Main {
 		q.addLast(start);
 		while (!q.isEmpty()) {
 			curr = q.pollFirst();
-			for (int next : adj[curr]) {
-				if (!visited[next]) {
-					sb.append(next).append(SPACE);
-					visited[next] = true;
-					q.addLast(next);
+			for (edge = adj[curr].next; edge != NIL; edge = edge.next) {
+				if (!visited[to = edge.to]) {
+					sb.append(to).append(SPACE);
+					visited[to] = true;
+					q.addLast(to);
 				}
 			}
 		}
@@ -61,19 +81,16 @@ public class Main {
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		v = Integer.parseInt(st.nextToken());
-		adj = new ArrayList[n + 1];
+		adj = new Edge[n + 1];
 		for (i = 1; i <= n; i++) {
-			adj[i] = new ArrayList<>();
+			adj[i] = new Edge(0, NIL);
 		}
 		while (m-- > 0) {
 			st = new StringTokenizer(br.readLine(), " ", false);
 			v1 = Integer.parseInt(st.nextToken());
 			v2 = Integer.parseInt(st.nextToken());
-			adj[v1].add(v2);
-			adj[v2].add(v1);
-		}
-		for (i = 1; i <= n; i++) {
-			Collections.sort(adj[i]);
+			addEdge(v1, v2);
+			addEdge(v2, v1);
 		}
 		sb = new StringBuilder();
 		visited = new boolean[n + 1];
