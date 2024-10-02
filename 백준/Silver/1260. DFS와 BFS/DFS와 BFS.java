b6ade992251f11425ca3a00347ba9c5d4e194f1d
cookingTorch <jhw7181@naver.com>
2024-10-02 22:25:45 +0900

    [Silver II] Title: DFS와 BFS, Time: 148 ms, Memory: 17516 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,74 +1,88 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
 public class Main {
-    private static void dfs(ArrayList<ArrayList<Integer>> adj, boolean[] visited, int node, StringBuilder sb) {
-        int i, size, next;
+	private static final int MAX_N = 1_000;
+	private static final char SPACE = ' ';
+	private static final char LINE_BREAK = '\n';
 
-        visited[node] = true;
-        sb.append(node).append(" ");
-        size = adj.get(node).size();
-        Collections.sort(adj.get(node));
-        for (i = 0; i < size; i++) {
-            next = adj.get(node).get(i);
-            if (!visited[next])
-                dfs(adj, visited, next, sb);
-        }
-    }
+	private static StringBuilder sb;
 
-    private static void bfs(ArrayList<ArrayList<Integer>> adj, boolean[] visited, int start, StringBuilder sb) {
-        int i, node, size, next;
-        Queue<Integer> q = new LinkedList<>();
+	private static boolean[] visited;
+	private static ArrayList<Integer>[] adj;
 
-        q.add(start);
-        visited[start] = true;
-        while (!q.isEmpty()) {
-            node = q.poll();
-            sb.append(node).append(" ");
-            Collections.sort(adj.get(node));
-            size = adj.get(node).size();
-            for (i = 0; i < size; i++) {
-                next = adj.get(node).get(i);
-                if (!visited[next]) {
-                    visited[next] = true;
-                    q.add(next);
-                }
-            }
-        }
-    }
+	private static final void dfs(int curr) {
+		for (int next : adj[curr]) {
+			if (!visited[next]) {
+				sb.append(next).append(SPACE);
+				visited[next] = true;
+				dfs(next);
+			}
+		}
+	}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
+	private static final void bfs(int start) {
+		int curr;
+		ArrayDeque<Integer> q;
 
-        int n, m, v, i, node1, node2;
-        boolean[] visited;
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+		q = new ArrayDeque<>(MAX_N);
+		sb.append(start).append(SPACE);
+		visited[start] = true;
+		q.addLast(start);
+		while (!q.isEmpty()) {
+			curr = q.pollFirst();
+			for (int next : adj[curr]) {
+				if (!visited[next]) {
+					sb.append(next).append(SPACE);
+					visited[next] = true;
+					q.addLast(next);
+				}
+			}
+		}
+	}
 
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        v = Integer.parseInt(st.nextToken());
-        for (i = 0; i <= n; i++) {
-            adj.add(new ArrayList<>());
-        }
-        for (i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            node1 = Integer.parseInt(st.nextToken());
-            node2 = Integer.parseInt(st.nextToken());
-            adj.get(node1).add(node2);
-            adj.get(node2).add(node1);
-        }
-        visited = new boolean[n + 1];
-        dfs(adj, visited, v, sb);
-        sb.append("\n");
-        visited = new boolean[n + 1];
-        bfs(adj, visited, v, sb);
+	public static void main(String[] args) throws IOException {
+		int n;
+		int m;
+		int v;
+		int v1;
+		int v2;
+		int i;
+		BufferedReader br;
+		StringTokenizer st;
 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-    }
+		br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine(), " ", false);
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		v = Integer.parseInt(st.nextToken());
+		adj = new ArrayList[n + 1];
+		for (i = 1; i <= n; i++) {
+			adj[i] = new ArrayList<>();
+		}
+		while (m-- > 0) {
+			st = new StringTokenizer(br.readLine(), " ", false);
+			v1 = Integer.parseInt(st.nextToken());
+			v2 = Integer.parseInt(st.nextToken());
+			adj[v1].add(v2);
+			adj[v2].add(v1);
+		}
+		for (i = 1; i <= n; i++) {
+			Collections.sort(adj[i]);
+		}
+		sb = new StringBuilder();
+		visited = new boolean[n + 1];
+		sb.append(v).append(SPACE);
+		visited[v] = true;
+		dfs(v);
+		sb.append(LINE_BREAK);
+		visited = new boolean[n + 1];
+		bfs(v);
+		System.out.print(sb.toString());
+	}
 }
