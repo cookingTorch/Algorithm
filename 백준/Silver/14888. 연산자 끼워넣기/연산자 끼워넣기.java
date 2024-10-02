8437652bc170237b71982bb5f04ac0e57292ca4e
cookingTorch <jhw7181@naver.com>
2024-10-02 22:58:27 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 72 ms, Memory: 12872 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -4,65 +4,57 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static final int MAX = Integer.MAX_VALUE;
+	private static final int INF = Integer.MAX_VALUE;
 	private static final int MIN = Integer.MIN_VALUE;
-	
+	private static final char LINE_BREAK = '\n';
+
 	private static int n;
+	private static int max;
+	private static int min;
 	private static int[] arr;
-	
-	private static int[] maxMin(int add, int sub, int mul, int div, int val, int depth) {
-		int max, min;
-		int[] result;
-		
+
+	private static final void dfs(int depth, int num, int add, int sub, int mul, int div) {
 		if (depth == n) {
-			return new int[] {val, val};
+			if (num > max) {
+				max = num;
+			}
+			if (num < min) {
+				min = num;
+			}
+			return;
 		}
-		max = MIN;
-		min = MAX;
-		if (add > 0) {
-			result = maxMin(add - 1, sub, mul, div, val + arr[depth], depth + 1);
-			max = Math.max(max, result[0]);
-			min = Math.min(min, result[1]);
+		depth++;
+		if (add != 0) {
+			dfs(depth, num + arr[depth], add - 1, sub, mul, div);
 		}
-		if (sub > 0) {
-			result = maxMin(add, sub - 1, mul, div, val - arr[depth], depth + 1);
-			max = Math.max(max, result[0]);
-			min = Math.min(min, result[1]);
+		if (sub != 0) {
+			dfs(depth, num - arr[depth], add, sub - 1, mul, div);
 		}
-		if (mul > 0) {
-			result = maxMin(add, sub, mul - 1, div, val * arr[depth], depth + 1);
-			max = Math.max(max, result[0]);
-			min = Math.min(min, result[1]);
+		if (mul != 0) {
+			dfs(depth, num * arr[depth], add, sub, mul - 1, div);
 		}
-		if (div > 0) {
-			result = maxMin(add, sub, mul, div - 1, val / arr[depth], depth + 1);
-			max = Math.max(max, result[0]);
-			min = Math.min(min, result[1]);
+		if (div != 0) {
+			dfs(depth, num / arr[depth], add, sub, mul, div - 1);
 		}
-		return new int[] {max, min};
 	}
-	
+
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+		int i;
+		BufferedReader br;
 		StringTokenizer st;
-		
-		int add, sub, div, mul, i;
-		int[] ans;
-		
+
+		br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine(), " ", false);
 		arr = new int[n];
-		st = new StringTokenizer(br.readLine());
 		for (i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		st = new StringTokenizer(br.readLine());
-		add = Integer.parseInt(st.nextToken());
-		sub = Integer.parseInt(st.nextToken());
-		mul = Integer.parseInt(st.nextToken());
-		div = Integer.parseInt(st.nextToken());
-		ans = maxMin(add, sub, mul, div, arr[0], 1);
-		sb.append(ans[0]).append('\n').append(ans[1]);
-		System.out.print(sb);
+		n--;
+		max = MIN;
+		min = INF;
+		st = new StringTokenizer(br.readLine(), " ", false);
+		dfs(0, arr[0], Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+		System.out.print(new StringBuilder().append(max).append(LINE_BREAK).append(min));
 	}
 }
