8e8f03fa45b4a3742878c6c738047de75e0875cf
cookingTorch <jhw7181@naver.com>
2024-10-05 15:17:33 +0900

    [Gold IV] Title: 가운데에서 만나기, Time: 272 ms, Memory: 26080 KB -BaekjoonHub

---
+++ 백준/Gold/21940. 가운데에서 만나기/가운데에서 만나기.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static final int INF = Integer.MAX_VALUE >> 1;
+	private static final char SPACE = ' ';
+
+	public static void main(String[] args) throws IOException {
+		int n;
+		int m;
+		int u;
+		int v;
+		int w;
+		int k;
+		int i;
+		int max;
+		int min;
+		int[] cities;
+		int[][] map;
+		ArrayList<Integer> ans;
+		StringBuilder sb;
+		BufferedReader br;
+		StringTokenizer st;
+
+		br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine(), " ", false);
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		map = new int[n + 1][n + 1];
+		for (v = 1; v <= n; v++) {
+			map[1][v] = INF;
+		}
+		for (u = 2; u <= n; u++) {
+			System.arraycopy(map[1], 1, map[u], 1, n);
+		}
+		while (m-- > 0) {
+			st = new StringTokenizer(br.readLine(), " ", false);
+			map[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] = Integer.parseInt(st.nextToken());
+		}
+		for (v = 1; v <= n; v++) {
+			map[v][v] = 0;
+		}
+		for (w = 1; w <= n; w++) {
+			for (u = 1; u <= n; u++) {
+				for (v = 1; v <= n; v++) {
+					map[u][v] = Math.min(map[u][v], map[u][w] + map[w][v]);
+				}
+			}
+		}
+		k = Integer.parseInt(br.readLine());
+		cities = new int[k];
+		st = new StringTokenizer(br.readLine(), " ", false);
+		for (i = 0; i < k; i++) {
+			cities[i] = Integer.parseInt(st.nextToken());
+		}
+		ans = new ArrayList<>();
+		min = INF;
+		for (w = 1; w <= n; w++) {
+			max = 0;
+			for (int city : cities) {
+				max = Math.max(max, map[city][w] + map[w][city]);
+			}
+			if (max < min) {
+				ans.clear();
+				ans.add(w);
+				min = max;
+			} else if (max == min) {
+				ans.add(w);
+			}
+		}
+		sb = new StringBuilder();
+		for (int city : ans) {
+			sb.append(city).append(SPACE);
+		}
+		System.out.print(sb.toString());
+	}
+}
