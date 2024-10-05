df4b5d5a666ec405ab4191eb8e9af2a6462e70f4
cookingTorch <jhw7181@naver.com>
2024-10-05 15:50:19 +0900

    [Silver II] Title: 유기농 배추, Time: 80 ms, Memory: 13168 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,83 +1,67 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+	private static final int COL = 52;
+	private static final int MAX_K = 2_500;
+	private static final int[] d = {-COL, 1, COL, -1};
+	private static final char LINE_BREAK = '\n';
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		
-		int t, testCase, m, n, k, x, y, cnt, i;
-		int[] start, curr;
-		int[][] field;
-		boolean[][] visited;
-		Queue<int[]> q, bfsQ;
-		
-		// 테스트 케이스
-		testCase = Integer.parseInt(br.readLine());
-		for (t = 0; t < testCase; t++) {
-			
-			// 배추 입력
-			st = new StringTokenizer(br.readLine());
-			m = Integer.parseInt(st.nextToken());
-			n = Integer.parseInt(st.nextToken());
-			k = Integer.parseInt(st.nextToken());
-			field = new int[m][n];
-			q = new LinkedList<>();
-			for (i = 0; i < k; i++) {
-				st = new StringTokenizer(br.readLine());
-				x = Integer.parseInt(st.nextToken());
-				y = Integer.parseInt(st.nextToken());
-				field[x][y] = 1;
-				q.add(new int[] {x, y});
+	private static int[] positions;
+	private static boolean[] map;
+	private static BufferedReader br;
+
+	private static final void dfs(int pos) {
+		int i;
+		int npos;
+
+		map[pos] = false;
+		for (i = 0; i < 4; i++) {
+			npos = pos + d[i];
+			if (map[npos]) {
+				dfs(npos);
 			}
-			
-			// 각 배추의 위치부터 BFS
-			cnt = 0;
-			bfsQ = new LinkedList<>();
-			visited = new boolean[m][n];
-			while (!q.isEmpty()) {
-				start = q.poll();
-				if (!visited[start[0]][start[1]]) {
-					cnt++;
-					bfsQ.add(start);
-					while (!bfsQ.isEmpty()) {
-						curr = bfsQ.poll();
-						if (curr[0] > 0 && field[curr[0] - 1][curr[1]] == 1 && !visited[curr[0] - 1][curr[1]]) {
-							visited[curr[0] - 1][curr[1]] = true;
-							bfsQ.add(new int[] {curr[0] - 1, curr[1]});
-						}
-						if (curr[0] < m - 1 && field[curr[0] + 1][curr[1]] == 1 && !visited[curr[0] + 1][curr[1]]) {
-							visited[curr[0] + 1][curr[1]] = true;
-							bfsQ.add(new int[] {curr[0] + 1, curr[1]});
-						}
-						if (curr[1] > 0 && field[curr[0]][curr[1] - 1] == 1 && !visited[curr[0]][curr[1] - 1]) {
-							visited[curr[0]][curr[1] - 1] = true;
-							bfsQ.add(new int[] {curr[0], curr[1] - 1});
-						}
-						if (curr[1] < n - 1 && field[curr[0]][curr[1] + 1] == 1 && !visited[curr[0]][curr[1] + 1]) {
-							visited[curr[0]][curr[1] + 1] = true;
-							bfsQ.add(new int[] {curr[0], curr[1] + 1});
-						}
-					}
-				}
-			}
-			
-			// 출력
-			sb.append(cnt).append("\n");
 		}
-		
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
 	}
 
+	private static final int solution() throws IOException {
+		int k;
+		int i;
+		int cnt;
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine(), " ", false);
+		st.nextToken();
+		st.nextToken();
+		k = Integer.parseInt(st.nextToken());
+		for (i = 0; i < k; i++) {
+			st = new StringTokenizer(br.readLine(), " ", false);
+			map[positions[i] = (Integer.parseInt(st.nextToken()) + 1) * COL + Integer.parseInt(st.nextToken()) + 1] = true;
+		}
+		cnt = 0;
+		while (k-- > 0) {
+			if (map[positions[k]]) {
+				dfs(positions[k]);
+				cnt++;
+			}
+		}
+		return cnt;
+	}
+
+	public static void main(String[] args) throws IOException {
+		int t;
+		StringBuilder sb;
+
+		positions = new int[MAX_K];
+		map = new boolean[COL * COL];
+		br = new BufferedReader(new InputStreamReader(System.in));
+		t = Integer.parseInt(br.readLine());
+		sb = new StringBuilder();
+		while (t-- > 0) {
+			sb.append(solution()).append(LINE_BREAK);
+		}
+		System.out.print(sb.toString());
+	}
 }
