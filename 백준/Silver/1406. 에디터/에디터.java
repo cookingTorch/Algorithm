fc14a150f187210159afb63eafd2dd6eaf54ca49
cookingTorch <jhw7181@naver.com>
2024-10-05 14:48:11 +0900

    [Silver II] Title: 에디터, Time: 256 ms, Memory: 22956 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -1,69 +1,79 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
 
 public class Main {
+	private static final int NIL = 0;
+	private static final int MAX_LEN = 600_001;
+	private static final int LINE_BREAK = '\n';
+	private static final int L = 'L';
+	private static final int D = 'D';
+	private static final int B = 'B';
+	private static final int P = 'P';
 
 	public static void main(String[] args) throws IOException {
+		int m;
+		int idx;
+		int pos;
+		int size;
+		int[] left;
+		int[] right;
+		char[] str;
+		char[] ans;
+		BufferedReader br;
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String str;
-		StringTokenizer st;
-		
-		int n, i, size;
-		String command, temp;
-		Deque<Character> left = new LinkedList<>();
-		Deque<Character> right = new LinkedList<>();
-		
-		str = br.readLine();
-		for (i = 0; i < str.length(); i++) {
-			left.addLast(str.charAt(i));
+		br = new BufferedReader(new InputStreamReader(System.in));
+		str = new char[MAX_LEN];
+		left = new int[MAX_LEN];
+		right = new int[MAX_LEN];
+		pos = NIL;
+		while ((str[++pos] = (char) br.read()) != LINE_BREAK);
+		size = --pos;
+		for (idx = NIL; idx < pos; idx++) {
+			left[idx + 1] = idx;
+			right[idx] = idx + 1;
 		}
-		
-		str = br.readLine();
-		n = Integer.parseInt(str);
-		
-		for (i = 0; i < n; i++) {
-			str = br.readLine();
-			st = new StringTokenizer(str, " ");
-			command = st.nextToken();
-			
-			if (command.equals("L")) {
-				if (left.size() != 0) {
-					right.addFirst(left.pollLast());
-				}
-			} else if (command.equals("D")) {
-				if (right.size() != 0) {
-					left.addLast(right.pollFirst());
-				}
-			} else if (command.equals("B")) {
-				if (left.size() != 0) {
-					left.removeLast();
-				}
-			} else if (command.equals("P")) {
-				temp = st.nextToken();
-				left.addLast(temp.charAt(0));
+		left[NIL] = pos;
+		right[pos] = NIL;
+		m = Integer.parseInt(br.readLine());
+		while (m-- > 0) {
+			switch (br.read()) {
+				case L:
+					if (pos != NIL) {
+						pos = left[pos];
+					}
+					break;
+				case D:
+					if ((pos = right[pos]) == NIL) {
+						pos = left[NIL];
+					}
+					break;
+				case B:
+					if (pos != NIL) {
+						left[right[pos]] = left[pos];
+						right[left[pos]] = right[pos];
+						pos = left[pos];
+						size--;
+					}
+					break;
+				case P:
+					br.read();
+					str[++idx] = (char) br.read();
+					left[right[pos]] = idx;
+					right[idx] = right[pos];
+					left[idx] = pos;
+					right[pos] = idx;
+					pos = idx;
+					size++;
+					break;
 			}
-			
+			br.read();
 		}
-		
-		size = left.size();
-		for (i = 0; i < size; i++) {
-			bw.write(Character.toString(left.poll()));
+		idx = 0;
+		ans = new char[size];
+		for (pos = right[NIL]; pos != NIL; pos = right[pos]) {
+			ans[idx++] = str[pos];
 		}
-		size = right.size();
-		for (i = 0; i < size; i++) {
-			bw.write(Character.toString(right.poll()));
-		}
-
-		bw.flush();
-		bw.close();
+		System.out.print(ans);
 	}
-
 }
