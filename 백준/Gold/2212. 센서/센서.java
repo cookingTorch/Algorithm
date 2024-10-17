0e08335d47b02cc2bc41365bfad13231472a8a2a
cookingTorch <jhw7181@naver.com>
2024-10-17 21:31:09 +0900

    [Gold V] Title: 센서, Time: 108 ms, Memory: 14828 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.java
+++ 백준/Gold/2212. 센서/센서.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.PriorityQueue;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,31 +9,31 @@ public class Main {
 		int n;
 		int k;
 		int i;
-		int num;
+		int thr;
 		int sum;
+		int[] diffs;
+		int[] sensors;
 		BufferedReader br;
 		StringTokenizer st;
-		PriorityQueue<Integer> pq1;
-		PriorityQueue<Integer> pq2;
-		
+
 		br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
-		k = Math.min(n, Integer.parseInt(br.readLine()));
-		pq1 = new PriorityQueue<>();
-		st = new StringTokenizer(br.readLine());
+		k = Integer.parseInt(br.readLine());
+		sensors = new int[n];
+		st = new StringTokenizer(br.readLine(), " ", false);
 		for (i = 0; i < n; i++) {
-			pq1.offer(Integer.parseInt(st.nextToken()));
+			sensors[i] = Integer.parseInt(st.nextToken());
 		}
-		pq2 = new PriorityQueue<>(Collections.reverseOrder());
-		num = pq1.poll();
+		Arrays.sort(sensors);
+		diffs = new int[--n];
+		for (i = 0; i < n; i++) {
+			diffs[i] = sensors[i + 1] - sensors[i];
+		}
+		Arrays.sort(diffs);
 		sum = 0;
-		while (!pq1.isEmpty()) {
-			sum += pq1.peek() - num;
-			pq2.offer(pq1.peek() - num);
-			num = pq1.poll();
-		}
-		for (i = 1; i < k; i++) {
-			sum -= pq2.poll();
+		thr = n - (k - 1);
+		for (i = 0; i < thr; i++) {
+			sum += diffs[i];
 		}
 		System.out.print(sum);
 	}
