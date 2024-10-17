5e722510add67c333ab537039be16cf9f02f1bdb
cookingTorch <jhw7181@naver.com>
2024-10-17 09:53:00 +0900

    [Gold IV] Title: 뱀, Time: 64 ms, Memory: 11696 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -4,77 +4,96 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static final int INF = Integer.MAX_VALUE;
-	private static final int WALL = -1;
-	private static final int APPLE = 1;
-	private static final int EMPTY = 0;
-	private static final int MAX = 10000;
-	private static final char LEFT = 'L';
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int n, k, x, l, size, max, pos, next, dir, time, diff, cnt, head, tail, i;
-		int[] map, snake, d;
-		
-		n = Integer.parseInt(br.readLine());
-		k = Integer.parseInt(br.readLine());
-		size = n + 2;
-		max = size * size;
-		d = new int[] {-size, 1, size, -1};
-		map = new int[max];
-		for (i = 1; i <= n; i++) {
-			map[i] = WALL;
-			map[(n + 1) * size + i] = WALL;
-			map[i * size] = WALL;
-			map[i * size + n + 1] = WALL;
-		}
-		for (i = 0; i < k; i++) {
-			st = new StringTokenizer(br.readLine());
-			map[Integer.parseInt(st.nextToken()) * size + Integer.parseInt(st.nextToken())] = APPLE;
-		}
-		dir = 1;
-		l = Integer.parseInt(br.readLine());
-		cnt = 1;
-		st = new StringTokenizer(br.readLine());
-		x = Integer.parseInt(st.nextToken());
-		if (st.nextToken().charAt(0) == LEFT) {
-			diff = 3;
-		} else {
-			diff = 1;
-		}
-		snake = new int[MAX];
-		head = 0;
-		tail = 0;
-		pos = 1 * size + 1;
-		snake[head] = pos;
-		for (time = 1;; time++, pos = next) {
-			next = pos + d[dir];
-			switch (map[next]) {
-			case WALL:
-				System.out.print(time);
-				return;
-			case EMPTY:
-				map[snake[tail++]] = EMPTY;
-			case APPLE:
-				map[next] = WALL;
-				snake[++head] = next;
-			}
-			if (time == x) {
-				dir = (dir + diff) % 4;
-				if (cnt++ == l) {
-					x = INF;
-				} else {
-					st = new StringTokenizer(br.readLine());
-					x = Integer.parseInt(st.nextToken());
-					if (st.nextToken().charAt(0) == LEFT) {
-						diff = 3;
-					} else {
-						diff = 1;
-					}
-				}
-			}
-		}
-	}
+    private static final int WALL = -1;
+    private static final int APPLE = 1;
+    private static final int EMPTY = 0;
+    private static final int LEFT_TURN = 3;
+    private static final int RIGHT_TURN = 1;
+    private static final int MAX_TIME = 10_100;
+    private static final char LEFT = 'L';
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n;
+        int k;
+        int x;
+        int l;
+        int i;
+        int dir;
+        int max;
+        int cnt;
+        int pos;
+        int col;
+        int end;
+        int next;
+        int time;
+        int head;
+        int tail;
+        int turn;
+        int[] d;
+        int[] map;
+        int[] snake;
+
+        n = Integer.parseInt(br.readLine());
+        k = Integer.parseInt(br.readLine());
+        col = n + 2;
+        end = n + 1;
+        max = col * col;
+        d = new int[] {-col, 1, col, -1};
+        map = new int[max];
+        for (i = 1; i <= n; i++) {
+            map[i] = WALL;
+            map[end * col + i] = WALL;
+            map[i * col] = WALL;
+            map[i * col + end] = WALL;
+        }
+        for (i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            map[Integer.parseInt(st.nextToken()) * col + Integer.parseInt(st.nextToken())] = APPLE;
+        }
+        dir = 1;
+        l = Integer.parseInt(br.readLine());
+        cnt = 1;
+        st = new StringTokenizer(br.readLine());
+        x = Integer.parseInt(st.nextToken());
+        if (st.nextToken().charAt(0) == LEFT) {
+            turn = 3;
+        } else {
+            turn = 1;
+        }
+        snake = new int[MAX_TIME];
+        head = 0;
+        tail = 0;
+        pos = col + 1;
+        snake[head] = pos;
+        for (time = 1;; time++, pos = next) {
+            next = pos + d[dir];
+            switch (map[next]) {
+                case WALL:
+                    System.out.print(time);
+                    return;
+                case EMPTY:
+                    map[snake[tail++]] = EMPTY;
+                case APPLE:
+                    map[next] = WALL;
+                    snake[++head] = next;
+            }
+            if (time == x) {
+                dir = dir + turn & 3;
+                if (cnt++ == l) {
+                    x = MAX_TIME;
+                } else {
+                    st = new StringTokenizer(br.readLine());
+                    x = Integer.parseInt(st.nextToken());
+                    if (st.nextToken().charAt(0) == LEFT) {
+                        turn = LEFT_TURN;
+                    } else {
+                        turn = RIGHT_TURN;
+                    }
+                }
+            }
+        }
+    }
 }
