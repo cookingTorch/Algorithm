1cd5b52fb570fbec4862940cc26a997232970f3d
cookingTorch <jhw7181@naver.com>
2024-10-17 10:51:44 +0900

    [Silver II] Title: 최소 힙, Time: 208 ms, Memory: 24248 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,31 +1,70 @@
-import java.io.*;
-import java.util.PriorityQueue;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+    private static final int SHIFT = 18;
+    private static final long MOD = (1 << SHIFT) - 1;
+    private static final long INF = Long.MAX_VALUE;
+    private static final char LINE_BREAK = '\n';
+    private static final char[] EMPTY = {'0', LINE_BREAK};
+
+    private static int idx;
+    private static long[] tree;
+
+    private static final void init(int n) {
+        int size;
+
+        for (idx = 1; idx < n; idx <<= 1);
+        tree = new long[size = idx << 1];
+        while (size-- > 1) {
+            tree[size] = INF;
+        }
+    }
+
+    private static final void add(long val) {
+        int i;
+
+        i = idx++;
+        tree[i] = val << SHIFT | i;
+        for (i >>= 1; i > 0; i >>= 1) {
+            tree[i] = Math.min(tree[i << 1], tree[i << 1 | 1]);
+        }
+    }
+
+    private static final void remove() {
+        int i;
+
+        i = (int) (tree[1] & MOD);
+        tree[i] = INF;
+        for (i >>= 1; i > 0; i >>= 1) {
+            tree[i] = Math.min(tree[i << 1], tree[i << 1 | 1]);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
+        int n;
+        int num;
+        StringBuilder sb;
+        BufferedReader br;
 
-        int n, x, i;
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-
+        br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        for (i = 0; i < n; i++) {
-            x = Integer.parseInt(br.readLine());
-            if (x == 0) {
-                if (pq.isEmpty()) {
-                    sb.append(0).append("\n");
+        init(n);
+        sb = new StringBuilder();
+        while (n-- > 0) {
+            num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                if (tree[1] == INF) {
+                    sb.append(EMPTY);
                 } else {
-                    sb.append(pq.remove()).append("\n");
+                    sb.append((int) (tree[1] >> SHIFT)).append(LINE_BREAK);
+                    remove();
                 }
             } else {
-                pq.add(x);
+                add(num);
             }
         }
-
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        System.out.print(sb.toString());
     }
 }
