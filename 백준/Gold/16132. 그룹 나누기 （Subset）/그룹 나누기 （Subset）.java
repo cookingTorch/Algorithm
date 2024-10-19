9c61e427c2643207d8aeed448e50e5590cc6d6ec
cookingTorch <jhw7181@naver.com>
2024-10-19 10:49:19 +0900

    [Gold IV] Title: 그룹 나누기 (Subset), Time: 64 ms, Memory: 11484 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Gold/16132. 그룹 나누기 （Subset）/그룹 나누기 （Subset）.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int n;
+        int i;
+        int j;
+        int sum;
+        int dp[];
+        BufferedReader br;
+
+        br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        sum = n * (n + 1) >> 1;
+        if ((sum & 1) == 1) {
+            System.out.print('0');
+            return;
+        }
+        sum >>= 1;
+        dp = new int[sum + 1];
+        dp[0] = 1;
+        for (i = 1; i <= n; i++) {
+            for (j = sum; j >= i; j--) {
+                dp[j] += dp[j - i];
+            }
+        }
+        System.out.print(dp[sum] >> 1);
+    }
+}
