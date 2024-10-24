d9c4067cd193b8e7c66fb43de2cad9683cf6f021
cookingTorch <jhw7181@naver.com>
2024-10-24 19:23:23 +0900

    [Silver III] Title: 스파이, Time: 104 ms, Memory: 12192 KB -BaekjoonHub

--- 백준/Silver/28075. 스파이/스파이.java
+++ 백준/Silver/28075. 스파이/스파이.java
@@ -4,6 +4,10 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+	private static final int PLACE = 2;
+	private static final int WORK = 3;
+	private static final int NONE = -1;
+	
 	private static int n;
 	private static int m;
 	private static int cnt;
@@ -20,8 +24,8 @@ public class Main {
 			return;
 		}
 		depth++;
-		for (i = 0; i < 2; i++) {
-			for (j = 0; j < 3; j++) {
+		for (i = 0; i < PLACE; i++) {
+			for (j = 0; j < WORK; j++) {
 				if (prev == j) {
 					dfs(j, sum + (arr[i][j] >> 1), depth);
 				} else {
@@ -42,14 +46,14 @@ public class Main {
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		cnt = 0;
-		arr = new int[2][3];
-		for (i = 0; i < 2; i++) {
+		arr = new int[PLACE][WORK];
+		for (i = 0; i < PLACE; i++) {
 			st = new StringTokenizer(br.readLine(), " ", false);
-			for (j = 0; j < 3; j++) {
+			for (j = 0; j < WORK; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		dfs(-1, 0, 0);
+		dfs(NONE, 0, 0);
 		System.out.print(cnt);
 	}
 }
