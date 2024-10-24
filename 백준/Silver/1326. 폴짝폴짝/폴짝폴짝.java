32d5c2e2b43f28f823a7512dd07fa803a1f3b82b
cookingTorch <jhw7181@naver.com>
2024-10-24 19:58:59 +0900

    [Silver II] Title: 폴짝폴짝, Time: 92 ms, Memory: 13064 KB -BaekjoonHub

--- 백준/Silver/1326. 폴짝폴짝/폴짝폴짝.java
+++ 백준/Silver/1326. 폴짝폴짝/폴짝폴짝.java
@@ -2,68 +2,74 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static final int INF = Integer.MAX_VALUE;
-	
-	private static int n;
-	private static int[] arr, distance;
-	private static boolean[] inQueue;
-	
-	private static int spfa(int a, int b) {
-		int curr, next;
-		Queue<Integer> q;
-		
-		Arrays.fill(distance, INF);
-		distance[a] = 0;
+	private static final int FAIL = -1;
+
+	private static int bfs(int start, int end, int n, int[] arr) {
+		int cnt;
+		int size;
+		int curr;
+		int dist;
+		int next;
+		boolean[] visited;
+		ArrayDeque<Integer> q;
+
+		if (start == end) {
+			return 0;
+		}
+		visited = new boolean[n];
 		q = new ArrayDeque<>();
-		q.add(a);
-		inQueue[a] = true;
-		while (!q.isEmpty()) {
-			curr = q.poll();
-			inQueue[curr] = false;
-			for (next = curr - arr[curr]; next >= 0; next -= arr[curr]) {
-				if (distance[curr] + 1 < distance[next]) {
-					distance[next] = distance[curr] + 1;
-					if (!inQueue[next]) {
-						q.add(next);
-						inQueue[next] = true;
+		visited[start] = true;
+		q.addLast(start);
+		for (cnt = 1; !q.isEmpty(); cnt++) {
+			size = q.size();
+			while (size-- > 0) {
+				curr = q.pollFirst();
+				dist = arr[curr];
+				for (next = curr - dist; next >= 0; next -= dist) {
+					if (visited[next]) {
+						continue;
 					}
+					if (next == end) {
+						return cnt;
+					}
+					visited[next] = true;
+					q.addLast(next);
 				}
-			}
-			for (next = curr + arr[curr]; next < n; next += arr[curr]) {
-				if (distance[curr] + 1 < distance[next]) {
-					distance[next] = distance[curr] + 1;
-					if (!inQueue[next]) {
-						q.add(next);
-						inQueue[next] = true;
+				for (next = curr + dist; next < n; next += dist) {
+					if (visited[next]) {
+						continue;
 					}
+					if (next == end) {
+						return cnt;
+					}
+					visited[next] = true;
+					q.addLast(next);
 				}
 			}
 		}
-		return distance[b] == INF ? -1 : distance[b];
+		return FAIL;
 	}
-	
+
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n;
+		int a;
+		int b;
+		int i;
+		int[] arr;
+		BufferedReader br;
 		StringTokenizer st;
-		
-		int a, b, i;
-		
+
+		br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
 		arr = new int[n];
-		st = new StringTokenizer(br.readLine());
+		st = new StringTokenizer(br.readLine(), " ", false);
 		for (i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		st = new StringTokenizer(br.readLine());
-		a = Integer.parseInt(st.nextToken()) - 1;
-		b = Integer.parseInt(st.nextToken()) - 1;
-		distance = new int[n];
-		inQueue = new boolean[n];
-		System.out.print(spfa(a, b));
+		st = new StringTokenizer(br.readLine(), " ", false);
+		System.out.print(bfs(Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1, n, arr));
 	}
 }
