339ba6f5d7fa463d8e1fc525395a3d0dfa473586
cookingTorch <jhw7181@naver.com>
2024-10-26 12:41:41 +0900

    [Gold III] Title: 마법사 상어와 토네이도, Time: 336 ms, Memory: 35216 KB -BaekjoonHub

--- 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.java
+++ 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.java
@@ -6,135 +6,136 @@ import java.util.StringTokenizer;
 public class Main {
 	private static final int OUT_OF_BOUNDS = -1;
 
-	private static int x;
-	private static int y;
+	private static int pos;
+	private static int col;
 	private static int cnt;
+	private static int col2;
 	private static int temp;
-	private static int[][] map;
+	private static int[] map;
 
-	private static void add(int x, int y, int val) {
+	private static void add(int pos, int val) {
 		temp += val;
-		if (map[x][y] == OUT_OF_BOUNDS) {
+		if (map[pos] == OUT_OF_BOUNDS) {
 			cnt += val;
 		} else {
-			map[x][y] += val;
+			map[pos] += val;
 		}
 	}
 
 	private static final void left() {
 		int val;
 
-		y--;
-		val = map[x][y];
+		pos--;
+		val = map[pos];
 		temp = 0;
-		add(x - 2, y,
+		add(pos - col2,
 				val * 2 / 100);
-		add(x - 1, y - 1,
+		add(pos - col - 1,
 				val * 10 / 100);
-		add(x - 1, y,
+		add(pos - col,
 				val * 7 / 100);
-		add(x - 1, y + 1,
+		add(pos - col + 1,
 				val / 100);
-		add(x, y - 2,
+		add(pos - 2,
 				val * 5 / 100);
-		add(x + 1, y - 1,
+		add(pos + col - 1,
 				val * 10 / 100);
-		add(x + 1, y,
+		add(pos + col,
 				val * 7 / 100);
-		add(x + 1, y + 1,
+		add(pos + col + 1,
 				val / 100);
-		add(x + 2, y,
+		add(pos + col2,
 				val * 2 / 100);
-		add(x, y - 1,
+		add(pos - 1,
 				val - temp);
-		map[x][y] = 0;
+		map[pos] = 0;
 	}
 
 	private static final void down() {
 		int val;
 
-		x++;
-		val = map[x][y];
+		pos += col;
+		val = map[pos];
 		temp = 0;
-		add(x, y - 2,
+		add(pos - 2,
 				val * 2 / 100);
-		add(x + 1, y - 1,
+		add(pos + col - 1,
 				val * 10 / 100);
-		add(x, y - 1,
+		add(pos - 1,
 				val * 7 / 100);
-		add(x - 1, y - 1,
+		add(pos - col - 1,
 				val / 100);
-		add(x + 2, y,
+		add(pos + col2,
 				val * 5 / 100);
-		add(x + 1, y + 1,
+		add(pos + col + 1,
 				val * 10 / 100);
-		add(x, y + 1,
+		add(pos + 1,
 				val * 7 / 100);
-		add(x - 1, y + 1,
+		add(pos - col + 1,
 				val / 100);
-		add(x, y + 2,
+		add(pos + 2,
 				val * 2 / 100);
-		add(x + 1, y,
+		add(pos + col,
 				val - temp);
-		map[x][y] = 0;
+		map[pos] = 0;
 	}
 
 	private static final void right() {
 		int val;
 
-		y++;
-		val = map[x][y];
+		pos++;
+		val = map[pos];
 		temp = 0;
-		add(x - 2, y,
+		add(pos - col2,
 				val * 2 / 100);
-		add(x - 1, y - 1,
+		add(pos - col - 1,
 				val / 100);
-		add(x - 1, y,
+		add(pos - col,
 				val * 7 / 100);
-		add(x - 1, y + 1,
+		add(pos - col + 1,
 				val * 10 / 100);
-		add(x, y + 2,
+		add(pos + 2,
 				val * 5 / 100);
-		add(x + 1, y - 1,
+		add(pos + col - 1,
 				val / 100);
-		add(x + 1, y,
+		add(pos + col,
 				val * 7 / 100);
-		add(x + 1, y + 1,
+		add(pos + col + 1,
 				val * 10 / 100);
-		add(x + 2, y,
+		add(pos + col2,
 				val * 2 / 100);
-		add(x, y + 1,
+		add(pos + 1,
 				val - temp);
-		map[x][y] = 0;
+		map[pos] = 0;
 	}
 
 	private static final void up() {
 		int val;
 
-		x--;
-		val = map[x][y];
+		pos -= col;
+		val = map[pos];
 		temp = 0;
-		add(x, y - 2,
+		add(pos - 2,
 				val * 2 / 100);
-		add(x + 1, y - 1,
+		add(pos + col - 1,
 				val / 100);
-		add(x, y - 1,
+		add(pos - 1,
 				val * 7 / 100);
-		add(x - 1, y - 1,
+		add(pos - col - 1,
 				val * 10 / 100);
-		add(x - 2, y,
+		add(pos - col2,
 				val * 5 / 100);
-		add(x + 1, y + 1,
+		add(pos + col + 1,
 				val / 100);
-		add(x, y + 1,
+		add(pos + 1,
 				val * 7 / 100);
-		add(x - 1, y + 1,
+		add(pos - col + 1,
 				val * 10 / 100);
-		add(x, y + 2,
+		add(pos + 2,
 				val * 2 / 100);
-		add(x - 1, y,
+		add(pos - col,
 				val - temp);
-		map[x][y] = 0;
+		map[pos] = 0;
 	}
 
 	private static final void tornado(int n) {
@@ -142,7 +143,7 @@ public class Main {
 		int j;
 
 		cnt = 0;
-		x = y = (n >> 1) + 2;
+		pos = ((n >> 1) + 2) * col + ((n >> 1) + 2);
 		for (i = 1; i < n; i++) {
 			for (j = 0; j < i; j++) {
 				left();
@@ -168,32 +169,32 @@ public class Main {
 		int i;
 		int j;
 		int end;
-		int size;
 		BufferedReader br;
 		StringTokenizer st;
 
 		br = new BufferedReader(new InputStreamReader(System.in));
 		st = new StringTokenizer(br.readLine(), " ", false);
 		n = Integer.parseInt(st.nextToken());
-		size = n + 4;
-		map = new int[size][size];
+		col = n + 4;
+		col2 = col << 1;
+		map = new int[col * col];
 		end = n + 1;
-		for (i = 0; i < size; i++) {
-			map[0][i] = OUT_OF_BOUNDS;
+		for (i = 0; i < col; i++) {
+			map[i] = OUT_OF_BOUNDS;
 		}
-		System.arraycopy(map[0], 0, map[1], 0, size);
+		System.arraycopy(map, 0, map, col, col);
 		for (i = 2; i <= end; i++) {
-			map[i][0] = OUT_OF_BOUNDS;
-			map[i][1] = OUT_OF_BOUNDS;
+			map[i * col] = OUT_OF_BOUNDS;
+			map[i * col + 1] = OUT_OF_BOUNDS;
 			st = new StringTokenizer(br.readLine(), " ", false);
 			for (j = 2; j <= end; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+				map[i * col + j] = Integer.parseInt(st.nextToken());
 			}
-			map[i][end + 1] = OUT_OF_BOUNDS;
-			map[i][end + 2] = OUT_OF_BOUNDS;
+			map[i * col + end + 1] = OUT_OF_BOUNDS;
+			map[i * col + end + 2] = OUT_OF_BOUNDS;
 		}
-		System.arraycopy(map[0], 0, map[end + 1], 0, size);
-		System.arraycopy(map[0], 0, map[end + 2], 0, size);
+		System.arraycopy(map, 0, map, (end + 1) * col, col);
+		System.arraycopy(map, 0, map, (end + 2) * col, col);
 		tornado(n);
 		System.out.print(cnt);
 	}
