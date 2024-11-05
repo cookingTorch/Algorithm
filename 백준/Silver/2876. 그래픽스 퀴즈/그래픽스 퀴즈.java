b39e438fee40c5524cdcb26b93ba70095a706743
cookingTorch <jhw7181@naver.com>
2024-11-05 11:26:34 +0900

    [Silver III] Title: 그래픽스 퀴즈, Time: 120 ms, Memory: 14496 KB -BaekjoonHub

--- 백준/Silver/2876. 그래픽스 퀴즈/그래픽스 퀴즈.java
+++ 백준/Silver/2876. 그래픽스 퀴즈/그래픽스 퀴즈.java
@@ -3,66 +3,66 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	private static final char SPACE = ' ';
+    private static final char SPACE = ' ';
 
-	public static void main(String[] args) throws IOException {
-		int n;
-		int max;
-		int lenLeft;
-		int lenRight;
-		int prevLenLeft;
-		int prevLenRight;
-		char grade;
-		char left;
-		char right;
-		char prevLeft;
-		char prevRight;
-		BufferedReader br;
+    public static void main(String[] args) throws IOException {
+        int n;
+        int max;
+        int lenLeft;
+        int lenRight;
+        int prevLenLeft;
+        int prevLenRight;
+        char grade;
+        char left;
+        char right;
+        char prevLeft;
+        char prevRight;
+        BufferedReader br;
 
-		br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		prevLeft = (char) br.read();
-		br.read();
-		prevRight = (char) br.read();
-		grade = prevLeft < prevRight ? prevLeft : prevRight;
-		for (prevLenLeft = prevLenRight = max = 1; --n > 0; prevLenLeft = lenLeft, prevLenRight = lenRight) {
-			br.read();
-			left = (char) br.read();
-			br.read();
-			right = (char) br.read();
-			if (left == prevLeft) {
-				lenLeft = prevLenLeft + 1;
-			} else if (left == prevRight) {
-				lenLeft = prevLenRight + 1;
-			} else {
-				lenLeft = 1;
-			}
-			if (right == prevRight) {
-				lenRight = prevLenRight + 1;
-			} else if (right == prevLeft) {
-				lenRight = prevLenLeft + 1;
-			} else {
-				lenRight = 1;
-			}
-			if (lenLeft > max) {
-				grade = left;
-				max = lenLeft;
-			} else if (lenLeft == max) {
-				if (left < grade) {
-					grade = left;
-				}
-			}
-			if (lenRight > max) {
-				grade = right;
-				max = lenRight;
-			} else if (lenRight == max) {
-				if (right < grade) {
-					grade = right;
-				}
-			}
-			prevLeft = left;
-			prevRight = right;
-		}
-		System.out.print(new StringBuilder().append(max).append(SPACE).append(grade).toString());
-	}
+        br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        prevLeft = prevRight = grade = SPACE;
+        prevLenLeft = prevLenRight = max = 0;
+        while (n-- > 0) {
+            left = (char) br.read();
+            br.read();
+            right = (char) br.read();
+            br.read();
+            if (left == prevLeft) {
+                lenLeft = prevLenLeft + 1;
+            } else if (left == prevRight) {
+                lenLeft = prevLenRight + 1;
+            } else {
+                lenLeft = 1;
+            }
+            if (right == prevRight) {
+                lenRight = prevLenRight + 1;
+            } else if (right == prevLeft) {
+                lenRight = prevLenLeft + 1;
+            } else {
+                lenRight = 1;
+            }
+            if (lenLeft > max) {
+                grade = left;
+                max = lenLeft;
+            } else if (lenLeft == max) {
+                if (left < grade) {
+                    grade = left;
+                }
+            }
+            if (lenRight > max) {
+                grade = right;
+                max = lenRight;
+            } else if (lenRight == max) {
+                if (right < grade) {
+                    grade = right;
+                }
+            }
+            prevLeft = left;
+            prevRight = right;
+            prevLenLeft = lenLeft;
+            prevLenRight = lenRight;
+        }
+        System.out.print(new StringBuilder().append(max).append(SPACE).append(grade).toString());
+    }
 }
