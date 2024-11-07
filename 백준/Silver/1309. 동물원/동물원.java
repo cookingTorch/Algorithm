939d65babd443858872a3195abc64d5385cc5195
cookingTorch <jhw7181@naver.com>
2024-11-07 20:24:15 +0900

    [Silver I] Title: 동물원, Time: 72 ms, Memory: 11488 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.java
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 	private static final int MOD = 9901;
@@ -9,19 +8,20 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		int n;
 		int i;
+		int dp;
 		int sum;
-		int[] dp;
+		int prev;
 		BufferedReader br;
 
 		br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
-		dp = new int[n + 1];
-		sum = dp[0] = 1;
-		dp[1] = 2;
+		sum = 1;
+		dp = prev = 2;
 		for (i = 2; i <= n; i++) {
-			dp[i] = (sum << 1) + dp[i - 1];
-			sum = (sum + dp[i - 1]) % MOD;
+			dp = (sum << 1) + prev;
+			sum = (sum + prev) % MOD;
+			prev = dp;
 		}
-		System.out.print((sum + dp[n]) % MOD);
+		System.out.print((sum + dp) % MOD);
 	}
 }
