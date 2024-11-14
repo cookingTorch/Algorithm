c260fd24f96acc0594bcd8c330d9bfb42af742f0
cookingTorch <jhw7181@naver.com>
2024-11-14 11:43:36 +0900

    [Silver V] Title: 숫자 카드, Time: 592 ms, Memory: 152888 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,36 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static final char[] TRUE = {'1', ' '};
-    private static final char[] FALSE = {'0', ' '};
+    private static final int DIFF = 10_000_000;
+    private static final char TRUE = '1';
+    private static final char FALSE = '0';
+    private static final char SPACE = ' ';
 
     public static void main(String[] args) throws IOException {
         int n;
         int m;
-        HashSet<String> set;
+        boolean[] contains;
         StringBuilder sb;
         BufferedReader br;
         StringTokenizer st;
 
         br = new BufferedReader(new InputStreamReader(System.in));
+        contains = new boolean[(DIFF << 1) + 1];
         n = Integer.parseInt(br.readLine());
-        set = new HashSet<>(n);
         st = new StringTokenizer(br.readLine(), " ", false);
         while (n-- > 0) {
-            set.add(st.nextToken());
+            contains[Integer.parseInt(st.nextToken()) + DIFF] = true;
         }
         m = Integer.parseInt(br.readLine());
         sb = new StringBuilder();
         st = new StringTokenizer(br.readLine(), " ", false);
         while (m-- > 0) {
-            if (set.contains(st.nextToken())) {
-                sb.append(TRUE);
+            if (contains[Integer.parseInt(st.nextToken()) + DIFF]) {
+                sb.append(TRUE).append(SPACE);
             } else {
-                sb.append(FALSE);
+                sb.append(FALSE).append(SPACE);
             }
         }
         System.out.print(sb.toString());
