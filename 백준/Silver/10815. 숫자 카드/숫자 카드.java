04a64a152f382cc1f2ac7d00d0dae194e8978849
cookingTorch <jhw7181@naver.com>
2024-11-14 11:37:59 +0900

    [Silver V] Title: 숫자 카드, Time: 712 ms, Memory: 155112 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -5,36 +5,34 @@ import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static final char ONE = '1';
-	private static final char ZERO = '0';
-	private static final char SPACE = ' ';
+    private static final char[] TRUE = {'1', ' '};
+    private static final char[] FALSE = {'0', ' '};
 
-	public static void main(String[] args) throws IOException {
-		int n;
-		int m;
-		int i;
-		StringBuilder sb;
-		BufferedReader br;
-		StringTokenizer st;
-		HashSet<Integer> set;
+    public static void main(String[] args) throws IOException {
+        int n;
+        int m;
+        HashSet<String> set;
+        StringBuilder sb;
+        BufferedReader br;
+        StringTokenizer st;
 
-		br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		set = new HashSet<>();
-		st = new StringTokenizer(br.readLine());
-		for (i = 0; i < n; i++) {
-			set.add(Integer.parseInt(st.nextToken()));
-		}
-		m = Integer.parseInt(br.readLine());
-		sb = new StringBuilder();
-		st = new StringTokenizer(br.readLine());
-		for (i = 0; i < m; i++) {
-			if (set.contains(Integer.parseInt(st.nextToken()))) {
-				sb.append(ONE).append(SPACE);
-			} else {
-				sb.append(ZERO).append(SPACE);
-			}
-		}
-		System.out.print(sb);
-	}
+        br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        set = new HashSet<>(n);
+        st = new StringTokenizer(br.readLine(), " ", false);
+        while (n-- > 0) {
+            set.add(st.nextToken());
+        }
+        m = Integer.parseInt(br.readLine());
+        sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine(), " ", false);
+        while (m-- > 0) {
+            if (set.contains(st.nextToken())) {
+                sb.append(TRUE);
+            } else {
+                sb.append(FALSE);
+            }
+        }
+        System.out.print(sb.toString());
+    }
 }
