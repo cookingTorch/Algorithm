9a0a4a38c3007362b3323e01d0ca6f48a0d5bce4
cookingTorch <jhw7181@naver.com>
2024-11-14 13:30:34 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 68 ms, Memory: 11632 KB -BaekjoonHub

--- 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.java
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.java
@@ -5,51 +5,57 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int n, m, l, left, right, mid, cnt, prev, i;
-		int[] locs;
-		
-		st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken()) + 2;
-		m = Integer.parseInt(st.nextToken());
-		l = Integer.parseInt(st.nextToken());
-		locs = new int[n];
-		st = new StringTokenizer(br.readLine());
-		for (i = 1; i < n - 1; i++) {
-			locs[i] = Integer.parseInt(st.nextToken());
-		}
-		locs[n - 1] = l;
-		Arrays.sort(locs);
-		right = locs[1];
-		for (i = 2; i < n; i++) {
-			right = Math.max(right, locs[i] - locs[i - 1]);
-		}
-		left = 0;
-		while (left < right) {
-			mid = (left + right) / 2;
-			cnt = m;
-			prev = 0;
-			for (i = 0;;) {
-				if ((prev += mid) >= locs[i]) {
-					prev = locs[i];
-					if (++i == n) {
-						break;
-					}
-				} else {
-					if (--cnt < 0) {
-						break;
-					}
-				}
-			}
-			if (cnt < 0) {
-				left = mid + 1;
-			} else {
-				right = mid;
-			}
-		}
-		System.out.print(right);
-	}
+    public static void main(String[] args) throws IOException {
+        int n;
+        int m;
+        int l;
+        int i;
+        int mid;
+        int cnt;
+        int prev;
+        int left;
+        int right;
+        int[] locs;
+        BufferedReader br;
+        StringTokenizer st;
+
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        l = Integer.parseInt(st.nextToken());
+        locs = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (i = 0; i < n; i++) {
+            locs[i] = Integer.parseInt(st.nextToken());
+        }
+        locs[n] = l;
+        n++;
+        Arrays.sort(locs);
+        right = locs[0];
+        for (i = 1; i < n; i++) {
+            right = Math.max(right, locs[i] - locs[i - 1]);
+        }
+        left = 0;
+        while (left < right) {
+            mid = left + right >> 1;
+            cnt = m;
+            prev = 0;
+            for (i = 0;;) {
+                if ((prev += mid) >= locs[i]) {
+                    prev = locs[i];
+                    if (++i == n) {
+                        right = mid;
+                        break;
+                    }
+                } else {
+                    if (--cnt < 0) {
+                        left = mid + 1;
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.print(right);
+    }
 }
