882067543d96d15d91c3dfd8c427be9e197445e3
cookingTorch <jhw7181@naver.com>
2024-11-19 14:39:56 +0900

    [Silver I] Title: 단축키 지정, Time: 64 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Silver/1283. 단축키 지정/단축키 지정.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static final int SIZE = 100;
+    private static final int ALPHA = 26;
+    private static final char LOWER = 'a';
+    private static final char UPPER = 'A';
+    private static final char OPEN = '[';
+    private static final char CLOSE = ']';
+    private static final char SPACE = ' ';
+    private static final char LINE_BREAK = '\n';
+
+    private static int len;
+    private static char[] str;
+    private static char[] result;
+    private static boolean[] visited;
+    private static StringBuilder sb;
+
+    private static final boolean isVisited(char ch) {
+        if (ch < LOWER) {
+            ch -= UPPER;
+        } else {
+            ch -= LOWER;
+        }
+        if (visited[ch]) {
+            return true;
+        } else {
+            visited[ch] = true;
+            return false;
+        }
+    }
+
+    private static final void prefix(int idx) {
+        if (idx > 0) {
+            System.arraycopy(str, 0, result, 0, idx);
+        }
+        result[idx] = OPEN;
+        result[idx + 1] = str[idx];
+        result[idx + 2] = CLOSE;
+        System.arraycopy(str, idx + 1, result, idx + 3, len - 1 - idx);
+        result[len + 2] = LINE_BREAK;
+        sb.append(result, 0, len + 3);
+    }
+
+    public static void main(String[] args) throws IOException {
+        int n;
+        int i;
+        BufferedReader br;
+
+        br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        visited = new boolean[ALPHA];
+        result = new char[SIZE];
+        sb = new StringBuilder();
+        loop:
+        while (n-- > 0) {
+            str = br.readLine().toCharArray();
+            len = str.length;
+            if (!isVisited(str[0])) {
+                prefix(0);
+                continue;
+            }
+            for (i = 1; i < len; i++) {
+                if (str[i] == SPACE && !isVisited(str[i + 1])) {
+                    prefix(i + 1);
+                    continue loop;
+                }
+            }
+            for (i = 0; i < len; i++) {
+                if (str[i] != SPACE && !isVisited(str[i])) {
+                    prefix(i);
+                    continue loop;
+                }
+            }
+            sb.append(str).append(LINE_BREAK);
+        }
+        System.out.print(sb.toString());
+    }
+}
