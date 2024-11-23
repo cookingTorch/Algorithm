b6d349c6dfa754b9101a799fef442368641f93e3
cookingTorch <jhw7181@naver.com>
2024-11-23 15:15:26 +0900

    [level 4] Title: 가사 검색, Time: 396.52 ms, Memory: 403 MB -BaekjoonHub

---
+++ 프로그래머스/4/60060. 가사 검색/가사 검색.java
@@ -0,0 +1,126 @@
+public class Solution {
+	private static final int FAIL = 0;
+	private static final int DIFF = 'a';
+	private static final int MAX_WORD_LEN = 10_001;
+	private static final int ALPHABET_SIZE = 26;
+	private static final char WILD_CARD = '?';
+
+	private static final class Trie {
+		int cnt;
+		Trie[] next;
+
+		Trie() {
+			next = new Trie[ALPHABET_SIZE];
+		}
+
+		void insertForward(char[] word, int len) {
+			int i;
+			int idx;
+			Trie curr;
+
+			curr = this;
+			for (i = 0; i < len; i++) {
+				curr.cnt++;
+				idx = word[i] - DIFF;
+				if (curr.next[idx] == null) {
+					curr.next[idx] = new Trie();
+				}
+				curr = curr.next[idx];
+			}
+			curr.cnt++;
+		}
+
+		void insertBackward(char[] word, int len) {
+			int i;
+			int idx;
+			Trie curr;
+
+			curr = this;
+			for (i = len - 1; i >= 0; i--) {
+				curr.cnt++;
+				idx = word[i] - DIFF;
+				if (curr.next[idx] == null) {
+					curr.next[idx] = new Trie();
+				}
+				curr = curr.next[idx];
+			}
+			curr.cnt++;
+		}
+
+		int searchForward(char[] word, int len) {
+			int i;
+			int idx;
+			Trie curr;
+
+			curr = this;
+			for (i = 0; i < len; i++) {
+				if (word[i] == WILD_CARD) {
+					break;
+				}
+				idx = word[i] - DIFF;
+				if (curr.next[idx] == null) {
+					return FAIL;
+				}
+				curr = curr.next[idx];
+			}
+			return curr.cnt;
+		}
+
+		int searchBackward(char[] word, int len) {
+			int i;
+			int idx;
+			Trie curr;
+
+			curr = this;
+			for (i = len - 1; i >= 0; i--) {
+				if (word[i] == WILD_CARD) {
+					break;
+				}
+				idx = word[i] - DIFF;
+				if (curr.next[idx] == null) {
+					return FAIL;
+				}
+				curr = curr.next[idx];
+			}
+			return curr.cnt;
+		}
+	}
+
+	public int[] solution(String[] words, String[] queries) {
+		int i;
+		int len;
+		int size;
+		int[] ans;
+		char[] arr;
+		Trie[] forward;
+		Trie[] backward;
+
+		forward = new Trie[MAX_WORD_LEN];
+		backward = new Trie[MAX_WORD_LEN];
+		for (i = 1; i < MAX_WORD_LEN; i++) {
+			forward[i] = new Trie();
+			backward[i] = new Trie();
+		}
+		for (String word : words) {
+			arr = word.toCharArray();
+			len = arr.length;
+			if (len >= MAX_WORD_LEN) {
+				continue;
+			}
+			forward[len].insertForward(arr, len);
+			backward[len].insertBackward(arr, len);
+		}
+		size = queries.length;
+		ans = new int[size];
+		for (i = 0; i < size; i++) {
+			arr = queries[i].toCharArray();
+			len = arr.length;
+			if (arr[0] == WILD_CARD) {
+				ans[i] = backward[len].searchBackward(arr, len);
+			} else {
+				ans[i] = forward[len].searchForward(arr, len);
+			}
+		}
+		return ans;
+	}
+}
