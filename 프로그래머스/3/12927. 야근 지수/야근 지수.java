692b8e0dbccf2f3543fbe6d24f8b6b9a758598c6
cookingTorch <jhw7181@naver.com>
2025-05-12 13:12:42 +0900

    [level 3] Title: 야근 지수, Time: 1.23 ms, Memory: 53.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,55 @@
+class Solution {
+    private static final int MAX = 50_000;
+
+    public long solution(int n, int[] works) {
+        int i;
+        int l;
+        int r;
+        int len;
+        int mid;
+        int sum;
+        int work;
+        long ans;
+
+        len = works.length;
+        sum = 0;
+        for (i = 0; i < len; i++) {
+            sum += works[i];
+        }
+        if (n >= sum) {
+            return 0L;
+        }
+        work = 0;
+        l = 0;
+        r = MAX + 1;
+        while (l < r) {
+            mid = l + r >>> 1;
+            sum = 0;
+            for (i = 0; i < len; i++) {
+                if (works[i] > mid) {
+                    sum += works[i] - mid;
+                }
+            }
+            if (sum >= n) {
+                l = mid + 1;
+            } else {
+                r = mid;
+                work = sum;
+            }
+        }
+        ans = 0L;
+        for (i = 0; i < len; i++) {
+            if (works[i] >= r) {
+                if (work == n) {
+                    ans += (long) r * r;
+                } else {
+                    ans += (long) (r - 1) * (r - 1);
+                    work++;
+                }
+            } else {
+                ans += (long) works[i] * works[i];
+            }
+        }
+        return ans;
+    }
+}
