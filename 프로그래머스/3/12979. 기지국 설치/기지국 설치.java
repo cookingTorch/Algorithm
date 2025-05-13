2943f5cdc842189453cac7c101d963ccf773d1b1
cookingTorch <jhw7181@naver.com>
2025-05-13 09:59:38 +0900

    [level 3] Title: 기지국 설치, Time: 0.52 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -3,21 +3,22 @@ class Solution {
         int i;
         int len;
         int cnt;
+        int gap;
         int range;
 
         range = w << 1 | 1;
         len = stations.length;
         cnt = 0;
-        if (stations[0] - w > 1) {
-            cnt += (stations[0] - w - 2) / range + 1;
+        if ((gap = stations[0] - w - 1) > 0) {
+            cnt += (gap - 1) / range + 1;
         }
         for (i = 1; i < len; i++) {
-            if (stations[i - 1] + (w << 1) + 1 < stations[i]) {
-                cnt += (stations[i] - stations[i - 1] - range - 1) / range + 1;
+            if ((gap = stations[i] - stations[i - 1] - range) > 0) {
+                cnt += (gap - 1) / range + 1;
             }
         }
-        if (stations[len - 1] + w < n) {
-            cnt += (n - stations[len - 1] - w - 1) / range + 1;
+        if ((gap = n - stations[len - 1] - w) > 0) {
+            cnt += (gap - 1) / range + 1;
         }
         return cnt;
     }
