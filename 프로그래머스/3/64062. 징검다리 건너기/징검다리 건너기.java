6e1d8a3e1570832cf09f607061aa2d858c32ed50
cookingTorch <jhw7181@naver.com>
2025-05-21 21:00:31 +0900

    [level 3] Title: 징검다리 건너기, Time: 38.85 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -2,25 +2,25 @@ import java.util.ArrayDeque;
 
 class Solution {
 	private static final int INF = Integer.MAX_VALUE >>> 1;
-	
+
 	public int solution(int[] stones, int k) {
 		int i;
 		int len;
 		int min;
 		int[] rock;
 		ArrayDeque<int[]> stack;
-		
+
 		len = stones.length;
 		stack = new ArrayDeque<>(len);
 		stack.addFirst(new int[] {-1, INF});
 		min = INF;
 		for (i = 0; i < len; i++) {
-			while ((rock = stack.pollFirst())[1] <= stones[i]) {
+			while (stack.peekFirst()[1] <= stones[i]) {
+				rock = stack.pollFirst();
 				if (i - stack.peekFirst()[0] - 1 >= k) {
 					min = Math.min(min, rock[1]);
 				}
 			}
-			stack.addFirst(rock);
 			stack.addFirst(new int[] {i, stones[i]});
 		}
 		while (stack.size() > 1) {
