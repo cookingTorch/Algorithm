3b278ed3e240b3de9c6f776844765766ea456697
cookingTorch <jhw7181@naver.com>
2025-05-22 10:22:41 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 27.21 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -7,31 +7,33 @@ class Solution {
 		long sum2;
 		
 		len = sequence.length;
-		max = Math.max(sum1 = sequence[0], sum2 = -sequence[0]);
-		for (i = 1; i < len; i++) {
-			if (sum1 <= 0L) {
-				sum1 = -sequence[i];
-			} else {
+        sum1 = 0L;
+        sum2 = 0L;
+        max = 0L;
+		for (i = 0; i < len; i++) {
+			if (sum1 > 0L) {
 				sum1 -= sequence[i];
-			}
-			if (sum2 <= 0L) {
-				sum2 = sequence[i];
 			} else {
+				sum1 = -sequence[i];
+			}
+			if (sum2 > 0L) {
 				sum2 += sequence[i];
+			} else {
+				sum2 = sequence[i];
 			}
 			max = Math.max(max, Math.max(sum1, sum2));
 			if (++i == len) {
 				break;
 			}
-			if (sum1 <= 0L) {
-				sum1 = sequence[i];
-			} else {
+			if (sum1 > 0L) {
 				sum1 += sequence[i];
-			}
-			if (sum2 <= 0L) {
-				sum2 = -sequence[i];
 			} else {
+				sum1 = sequence[i];
+			}
+			if (sum2 > 0L) {
 				sum2 -= sequence[i];
+			} else {
+				sum2 = -sequence[i];
 			}
 			max = Math.max(max, Math.max(sum1, sum2));
 		}
