d896e946b3e775d265405d9680a8658497b93a68
cookingTorch <jhw7181@naver.com>
2025-05-22 10:06:57 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 15.78 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -3,37 +3,37 @@ class Solution {
 		int i;
 		int len;
 		long max;
-		long[] dp1;
-		long[] dp2;
-
+		long sum1;
+		long sum2;
+		
 		len = sequence.length;
-		dp1 = new long[len];
-		dp2 = new long[len];
-		max = Math.max(dp1[0] = sequence[0], dp2[0] = -sequence[0]);
+		max = Math.max(sum1 = sequence[0], sum2 = -sequence[0]);
 		for (i = 1; i < len; i++) {
-			if (dp1[i - 1] <= 0L) {
-				max = Math.max(max, dp1[i] = -sequence[i]);
+			if (sum1 <= 0L) {
+				sum1 = -sequence[i];
 			} else {
-				max = Math.max(max, dp1[i] = dp1[i - 1] - sequence[i]);
+				sum1 -= sequence[i];
 			}
-			if (dp2[i - 1] <= 0L) {
-				max = Math.max(max, dp2[i] = sequence[i]);
+			if (sum2 <= 0L) {
+				sum2 = sequence[i];
 			} else {
-				max = Math.max(max, dp2[i] = dp2[i - 1] + sequence[i]);
+				sum2 += sequence[i];
 			}
+			max = Math.max(max, Math.max(sum1, sum2));
 			if (++i == len) {
 				break;
 			}
-			if (dp1[i - 1] <= 0L) {
-				max = Math.max(max, dp1[i] = sequence[i]);
+			if (sum1 <= 0L) {
+				sum1 = sequence[i];
 			} else {
-				max = Math.max(max, dp1[i] = dp1[i - 1] + sequence[i]);
+				sum1 += sequence[i];
 			}
-			if (dp2[i - 1] <= 0L) {
-				max = Math.max(max, dp2[i] = -sequence[i]);
+			if (sum2 <= 0L) {
+				sum2 = -sequence[i];
 			} else {
-				max = Math.max(max, dp2[i] = dp2[i - 1] - sequence[i]);
+				sum2 -= sequence[i];
 			}
+			max = Math.max(max, Math.max(sum1, sum2));
 		}
 		return max;
 	}
