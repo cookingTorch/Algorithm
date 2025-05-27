00ecfe9a2ded69523518c38bd45055a06e83205c
cookingTorch <jhw7181@naver.com>
2025-05-27 13:55:05 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 0.89 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -1,28 +1,39 @@
 class Solution {
     public int solution(String s) {
         int i;
-        int l;
+        int p;
         int r;
+        int thr;
         int len;
         int max;
+        int[] manacher;
         char[] str;
 
-        str = s.toCharArray();
-        len = str.length;
-        max = 1;
-        for (i = 1; i < len; i++) {
-            for (l = i - 1, r = i; l >= 0 && r < len; l--, r++) {
-                if (str[l] != str[r]) {
-                    break;
+        len = s.length();
+        str = new char[len << 1 | 1];
+        for (i = 0; i < len; i++) {
+            str[i << 1 | 1] = s.charAt(i);
+        }
+        len = len << 1 | 1;
+        manacher = new int[len];
+        max = 0;
+        p = r = -1;
+        for (i = 0; i < len; i++) {
+            if (i > r) {
+                p = i;
+                thr = Math.min(len - 1, p << 1);
+                for (r = p; r <= thr && str[r] == str[(p << 1) - r]; r++);
+                max = Math.max(max, manacher[i] = --r - p);
+            } else {
+                if (manacher[(p << 1) - i] == r - i) {
+                    p = i;
+                    thr = Math.min(len - 1, p << 1);
+                    for (; r <= thr && str[r] == str[(p << 1) - r]; r++);
+                    max = Math.max(max, manacher[i] = --r - p);
+                } else {
+                    manacher[i] = Math.min(manacher[(p << 1) - i], r - i);
                 }
             }
-            max = Math.max(max, r - l - 1);
-            for (l = i - 1, r = i + 1; l >= 0 && r < len; l--, r++) {
-                if (str[l] != str[r]) {
-                    break;
-                }
-            }
-            max = Math.max(max, r - l - 1);
         }
         return max;
     }
