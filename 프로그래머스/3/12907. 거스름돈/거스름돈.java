f57cac4ad6c3dc26600bfad244f5f24e17465b34
cookingTorch <jhw7181@naver.com>
2025-05-27 15:26:29 +0900

    [level 3] Title: 거스름돈, Time: 37.53 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n, int[] money) {
+        int i;
+        int j;
+        int len;
+        int thr;
+        int cost;
+        int[] dp;
+
+        len = money.length;
+        dp = new int[n + 1];
+        dp[0] = 1;
+        for (i = 0; i < len; i++) {
+            cost = money[i];
+            thr = n - cost;
+            for (j = 0; j <= thr; j++) {
+                dp[j + cost] += dp[j];
+            }
+        }
+        return dp[n];
+    }
+}
