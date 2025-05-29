0573f71e0236e71cd83fe3921236c81643942bc1
cookingTorch <jhw7181@naver.com>
2025-05-29 14:12:34 +0900

    [level 3] Title: 인사고과, Time: 188.34 ms, Memory: 109 MB -BaekjoonHub

---
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+
+class Solution {
+    private static final int FAIL = -1;
+
+    private int upperBound(int[][] arr, int right, int val) {
+        int mid;
+        int left;
+
+        left = 0;
+        while (left < right) {
+            mid = left + right >>> 1;
+            if (arr[mid][0] + arr[mid][1] > val) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return right;
+    }
+
+    public int solution(int[][] scores) {
+        int len;
+        int peer;
+        int work;
+        int prev;
+        int[] wanho;
+
+        wanho = scores[0];
+        len = scores.length;
+        Arrays.sort(scores, 0, len, (o1, o2) -> o2[0] - o1[0]);
+        prev = 0;
+        peer = scores[0][0];
+        work = scores[0][1];
+        for (int[] score : scores) {
+            if (score[0] != peer) {
+                peer = score[0];
+                prev = work;
+            }
+            if (score[1] < prev) {
+                if (score == wanho) {
+                    return FAIL;
+                }
+                score[0] = 0;
+                score[1] = FAIL;
+            } else {
+                work = Math.max(work, score[1]);
+            }
+        }
+        Arrays.sort(scores, 0, len, (o1, o2) -> o2[0] + o2[1] - o1[0] - o1[1]);
+        return upperBound(scores, len, wanho[0] + wanho[1]) + 1;
+    }
+}
