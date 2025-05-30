72e8ea7596de363ab85235315eec20c3fe8584c5
cookingTorch <jhw7181@naver.com>
2025-05-30 13:22:46 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 0.55 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
@@ -1,9 +1,15 @@
 class Solution {
-	private boolean kmp(int[] pat, int patLen, int[] src, int srcLen, int[] lps) {
+	private int n;
+	private int len;
+	private int sum;
+	private int xor;
+	private int[] lps;
+
+	private boolean kmp(int[] pat, int patLen, int[] txt, int txtLen) {
 		int i;
 		int j;
 
-		for (i = 1, j = 0; i < patLen; ) {
+		for (i = 1, j = 0; i < patLen;) {
 			if (pat[i] == pat[j]) {
 				lps[i++] = ++j;
 			} else if (j != 0) {
@@ -12,8 +18,8 @@ class Solution {
 				lps[i++] = 0;
 			}
 		}
-		for (i = 0, j = 0; i < srcLen; ) {
-			if (src[i] == pat[j]) {
+		for (i = 0, j = 0; i < txtLen;) {
+			if (txt[i] == pat[j]) {
 				if (++j == patLen) {
 					return true;
 				}
@@ -27,30 +33,30 @@ class Solution {
 		return false;
 	}
 
-	private boolean find(int[][] lock, int xor, int thr, int[] curKey, long[] keyMap, int[] lps, int len, int[] lockMap, int n) {
+	private boolean find(long[] keys, int[][] lock, int[] k, int[] l) {
 		int i;
 		int j;
-		int lockRow;
+		int row;
 
 		for (i = 0; i < n; i++) {
-			lockRow = 0;
+			row = 0;
 			for (j = 0; j < n; j++) {
-				lockRow = lockRow << 1 | lock[i][j];
+				row = row << 1 | lock[i][j];
 			}
-			lockMap[i] = lockRow ^ xor;
+			l[i] = row ^ xor;
 		}
-		for (i = 0; i < thr; i++) {
-			for (j = n; j < thr; j++) {
-				curKey[j] = (int) (keyMap[j] >>> i) & xor;
+		for (i = 0; i < sum; i++) {
+			for (j = n; j < sum; j++) {
+				k[j] = (int) (keys[j] >>> i) & xor;
 			}
-			if (kmp(lockMap, n, curKey, len, lps)) {
+			if (kmp(l, n, k, len)) {
 				return true;
 			}
 		}
 		return false;
 	}
 
-	private void rotate(int[][] lock, int[][] temp, int n) {
+	private void rotate(int[][] lock, int[][] temp) {
 		int i;
 		int j;
 
@@ -66,42 +72,37 @@ class Solution {
 
 	public boolean solution(int[][] key, int[][] lock) {
 		int m;
-		int n;
 		int i;
 		int j;
-		int len;
-		int thr;
-		int xor;
-		int[] lps;
-		int[] curKey;
-		int[] lockMap;
+		int[] k;
+		int[] l;
 		int[][] temp;
-		long keyRow;
-		long[] keyMap;
+		long row;
+		long[] keys;
 
 		m = key.length;
 		n = lock.length;
 		len = m + (n << 1) - 1;
-		keyMap = new long[len];
+		keys = new long[len];
 		for (i = 0; i < m; i++) {
-			keyRow = 0L;
+			row = 0L;
 			for (j = 0; j < m; j++) {
-				keyRow = keyRow << 1 | key[i][j];
+				row = row << 1 | key[i][j];
 			}
-			keyMap[i + n] = keyRow << n;
+			keys[i + n] = row << n;
 		}
 		xor = (1 << n) - 1;
-		lockMap = new int[n];
-		curKey = new int[len];
+		l = new int[n];
+		k = new int[len];
 		lps = new int[len];
-		thr = m + n;
+		sum = m + n;
 		temp = new int[n][n];
-		if (find(lock, xor, thr, curKey, keyMap, lps, len, lockMap, n)) {
+		if (find(keys, lock, k, l)) {
 			return true;
 		}
 		for (i = 0; i < 3; i++) {
-			rotate(lock, temp, n);
-			if (find(lock, xor, thr, curKey, keyMap, lps, len, lockMap, n)) {
+			rotate(lock, temp);
+			if (find(keys, lock, k, l)) {
 				return true;
 			}
 		}
