c21520ef188a01b302ba3cfbfa415cb84a0bbbe3
cookingTorch <jhw7181@naver.com>
2025-06-08 23:11:36 +0900

    [level 3] Title: 양과 늑대, Time: 0.96 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,23 +1,13 @@
 class Solution {
-    private static final class Edge {
-        int to;
-        Edge next;
-
-        Edge(int to, Edge next) {
-            this.to = to;
-            this.next = next;
-        }
-    }
-
-    private int len;
     private int max;
-    private boolean[] next;
+    private int[] size;
+    private int[][] adj;
     private boolean[] isSheep;
-    private Edge[] adj;
 
-    private void dfs(int node, int state, int cnt) {
+    private void dfs(int node, int state, int cnt, int[] prev, int idx) {
         int i;
-        Edge edge;
+        int len;
+        int[] next;
 
         if (isSheep[node]) {
             state++;
@@ -25,37 +15,35 @@ class Solution {
         } else if (--state <= 0) {
             return;
         }
-        for (edge = adj[node]; edge != null; edge = edge.next) {
-            next[edge.to] = true;
+        len = prev.length;
+        next = new int[len + size[node] - 1];
+        System.arraycopy(prev, 0, next, 0, idx);
+        if (len > idx + 1) {
+            System.arraycopy(prev, idx + 1, next, idx, len - idx - 1);
         }
-        next[node] = false;
+        System.arraycopy(adj[node], 0, next, len - 1, size[node]);
+        len += size[node] - 1;
         for (i = 0; i < len; i++) {
-            if (next[i]) {
-                dfs(i, state, cnt);
-            }
-        }
-        next[node] = true;
-        for (edge = adj[node]; edge != null; edge = edge.next) {
-            next[edge.to] = false;
+            dfs(next[i], state, cnt, next, i);
         }
     }
 
     public int solution(int[] info, int[][] edges) {
         int i;
+        int len;
 
         len = info.length;
         isSheep = new boolean[len];
         for (i = 0; i < len; i++) {
             isSheep[i] = info[i] == 0;
         }
-        adj = new Edge[len];
+        adj = new int[len][len];
+        size = new int[len];
         for (int[] edge : edges) {
-            adj[edge[0]] = new Edge(edge[1], adj[edge[0]]);
+            adj[edge[0]][size[edge[0]]++] = edge[1];
         }
         max = 0;
-        next = new boolean[len];
-        next[0] = true;
-        dfs(0, 0, 0);
+        dfs(0, 0, 0, new int[] {0}, 0);
         return max;
     }
 }
