8165afb7486b11f911099753ba8b7714868fcb13
cookingTorch <jhw7181@naver.com>
2025-06-10 11:28:19 +0900

    [level 3] Title: N으로 표현, Time: 5.86 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -5,63 +5,48 @@ class Solution {
     private static final int THR = 8;
     private static final int FAIL = -1;
 
+    private int number;
+    private HashSet<Integer> visited;
+    private ArrayList<Integer> list;
+
+    private boolean addNumber(int res) {
+        if (!visited.contains(res)) {
+            if (res == number) {
+                return true;
+            }
+            list.add(res);
+            visited.add(res);
+        }
+        return false;
+    }
+
     public int solution(int N, int number) {
         int i;
         int j;
         int num;
         int thr;
-        int res;
-        HashSet<Integer> visited;
         ArrayList<Integer>[] lists;
 
+        this.number = number;
         lists = new ArrayList[THR + 1];
         visited = new HashSet<>();
         visited.add(0);
         num = 0;
         loop:
         for (i = 1; i <= THR; i++) {
-            lists[i] = new ArrayList<>();
-            num = num * 10 + N;
-            if (num == number) {
-                break;
+            lists[i] = list = new ArrayList<>();
+            if (addNumber(num = num * 10 + N)) {
+                break loop;
             }
-            lists[i].add(num);
-            visited.add(num);
             thr = i >>> 1;
             for (j = 1; j <= thr; j++) {
                 for (int num1 : lists[j]) {
                     for (int num2 : lists[i - j]) {
-                        res = num1 + num2;
-                        if (!visited.contains(res)) {
-                            if (res == number) {
-                                break loop;
-                            }
-                            lists[i].add(res);
-                            visited.add(res);
-                        }
-                        res = Math.abs(num1 - num2);
-                        if (!visited.contains(res)) {
-                            if (res == number) {
-                                break loop;
-                            }
-                            lists[i].add(res);
-                            visited.add(res);
-                        }
-                        res = num1 * num2;
-                        if (!visited.contains(res)) {
-                            if (res == number) {
-                                break loop;
-                            }
-                            lists[i].add(res);
-                            visited.add(res);
-                        }
-                        res = num1 > num2 ? num1 / num2 : num2 / num1;
-                        if (!visited.contains(res)) {
-                            if (res == number) {
-                                break loop;
-                            }
-                            lists[i].add(res);
-                            visited.add(res);
+                        if (addNumber(num1 + num2)
+                                || addNumber(Math.abs(num1 - num2))
+                                || addNumber(num1 * num2)
+                                || addNumber(num1 > num2 ? num1 / num2 : num2 / num1)) {
+                            break loop;
                         }
                     }
                 }
