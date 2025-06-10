9e7e1b38c4a81d83de8c80b50be52ed90adbd42c
cookingTorch <jhw7181@naver.com>
2025-06-10 13:49:05 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 35.88 ms, Memory: 95 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,37 +1,28 @@
 class Solution {
     private static final int PLUS = -1;
 
-    private static final class User {
-        int ratio;
-        int cost;
-
-        User(int ratio, int cost) {
-            this.ratio = ratio;
-            this.cost = cost;
-        }
-
-        int pay() {
-            int i;
-            int res;
-
-            res = 0;
-            for (i = 0; i < len; i++) {
-                if (discount[i] >= ratio) {
-                    res += emoticons[i] / 100 * (100 - discount[i]);
-                }
-            }
-            return res >= cost ? PLUS : res;
-        }
-    }
-
-    private static int len;
-    private static int[] discount;
-    private static int[] emoticons;
-
+    private int len;
     private int maxCnt;
     private int maxSum;
-    private User[] users;
-    
+    private int[] cost;
+    private int[] emoticons;
+    private int[][] users;
+
+    private int pay(int[] user) {
+        int i;
+        int res;
+        int thr;
+
+        res = 0;
+        thr = user[0];
+        for (i = 0; i < len; i++) {
+            if (cost[i] <= thr) {
+                res += emoticons[i] * cost[i];
+            }
+        }
+        return res >= user[1] ? PLUS : res;
+    }
+
     private void calc() {
         int res;
         int sum;
@@ -39,8 +30,8 @@ class Solution {
 
         sum = 0;
         cnt = 0;
-        for (User user : users) {
-            res = user.pay();
+        for (int[] user : users) {
+            res = pay(user);
             if (res == PLUS) {
                 cnt++;
             } else {
@@ -62,24 +53,25 @@ class Solution {
             calc();
             return;
         }
-        for (i = 10; i <= 40; i += 10) {
-            discount[depth] = i;
+        for (i = 90; i >= 60; i -= 10) {
+            cost[depth] = i;
             dfs(depth + 1);
         }
     }
 
     public int[] solution(int[][] users, int[] emoticons) {
         int i;
-        int size;
 
+        this.users = users;
         this.emoticons = emoticons;
         len = emoticons.length;
-        discount = new int[len];
-        size = users.length;
-        this.users = new User[size];
-        for (i = 0; i < size; i++) {
-            this.users[i] = new User(users[i][0], users[i][1]);
+        for (int[] user : users) {
+            user[0] = 100 - user[0];
         }
+        for (i = 0; i < len; i++) {
+            emoticons[i] /= 100;
+        }
+        cost = new int[len];
         maxCnt = 0;
         maxSum = 0;
         dfs(0);
