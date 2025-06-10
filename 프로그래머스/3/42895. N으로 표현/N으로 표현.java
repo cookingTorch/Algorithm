1b0f2aa1a9469fe44012716d037b8945865473d2
cookingTorch <jhw7181@naver.com>
2025-06-10 11:21:57 +0900

    [level 3] Title: N으로 표현, Time: 6.16 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashSet;
 
 class Solution {
@@ -11,48 +12,61 @@ class Solution {
         int thr;
         int res;
         HashSet<Integer> visited;
-        HashSet<Integer>[] sets;
-        
-        sets = new HashSet[THR + 1];
+        ArrayList<Integer>[] lists;
+
+        lists = new ArrayList[THR + 1];
         visited = new HashSet<>();
         visited.add(0);
         num = 0;
+        loop:
         for (i = 1; i <= THR; i++) {
-            sets[i] = new HashSet<>();
+            lists[i] = new ArrayList<>();
             num = num * 10 + N;
-            sets[i].add(num);
+            if (num == number) {
+                break;
+            }
+            lists[i].add(num);
             visited.add(num);
             thr = i >>> 1;
             for (j = 1; j <= thr; j++) {
-                for (int num1 : sets[j]) {
-                    for (int num2 : sets[i - j]) {
+                for (int num1 : lists[j]) {
+                    for (int num2 : lists[i - j]) {
                         res = num1 + num2;
                         if (!visited.contains(res)) {
-                            sets[i].add(res);
+                            if (res == number) {
+                                break loop;
+                            }
+                            lists[i].add(res);
                             visited.add(res);
                         }
                         res = Math.abs(num1 - num2);
                         if (!visited.contains(res)) {
-                            sets[i].add(res);
+                            if (res == number) {
+                                break loop;
+                            }
+                            lists[i].add(res);
                             visited.add(res);
                         }
                         res = num1 * num2;
                         if (!visited.contains(res)) {
-                            sets[i].add(res);
+                            if (res == number) {
+                                break loop;
+                            }
+                            lists[i].add(res);
                             visited.add(res);
                         }
                         res = num1 > num2 ? num1 / num2 : num2 / num1;
                         if (!visited.contains(res)) {
-                            sets[i].add(res);
+                            if (res == number) {
+                                break loop;
+                            }
+                            lists[i].add(res);
                             visited.add(res);
                         }
                     }
                 }
             }
-            if (sets[i].contains(number)) {
-                return i;
-            }
         }
-        return FAIL;
+        return i > THR ? FAIL : i;
     }
 }
