70afd5c52cf0b470ec046d93657c4398c1ab7901
cookingTorch <jhw7181@naver.com>
2025-06-11 13:31:22 +0900

    [level 3] Title: 스타 수열, Time: 61.35 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/3/70130. 스타 수열/스타 수열.java
+++ 프로그래머스/3/70130. 스타 수열/스타 수열.java
@@ -1,33 +1,42 @@
 class Solution {
-    private static int max;
+    private static final class Star {
+        static int max;
 
-    private void add(int[] star, int idx) {
-        if (idx - 1 == star[0]) {
-            return;
+        int cnt;
+        int last;
+
+        void add(int idx) {
+            if (idx - 1 == last) {
+                return;
+            }
+            last = idx;
+            if (++cnt > max) {
+                max = cnt;
+            }
         }
-        star[0] = idx;
-        max = Math.max(max, ++star[1]);
     }
 
     public int solution(int[] a) {
         int i;
         int len;
-        int[][] stars;
+        Star[] arr;
 
         len = a.length;
         if (len == 1) {
             return 0;
         }
-        max = 0;
-        stars = new int[len][2];
-        stars[a[0]][0]--;
-        stars[a[1]][0]--;
+        arr = new Star[len];
+        for (i = 0; i < len; i++) {
+            arr[i] = new Star();
+        }
+        arr[a[0]].last--;
+        arr[a[1]].last--;
         for (i = 1; i < len; i++) {
             if (a[i - 1] != a[i]) {
-                add(stars[a[i - 1]], i);
-                add(stars[a[i]], i);
+                arr[a[i - 1]].add(i);
+                arr[a[i]].add(i);
             }
         }
-        return max << 1;
+        return Star.max << 1;
     }
 }
