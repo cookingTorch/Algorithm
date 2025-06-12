4501461f83bae4d9684099a8adf97d3f29384970
cookingTorch <jhw7181@naver.com>
2025-06-12 14:08:21 +0900

    [level 3] Title: 카운트 다운, Time: 0.73 ms, Memory: 69.1 MB -BaekjoonHub

--- 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
+++ 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
@@ -1,4 +1,5 @@
 class Solution {
+    private static final int THR = 310;
     private static final int INF = Integer.MAX_VALUE >>> 1;
     private static final int SHIFT = 11;
     private static final int SINGLE = 1 << SHIFT;
@@ -7,32 +8,35 @@ class Solution {
     public int[] solution(int target) {
         int i;
         int j;
+        int dest;
         int[] dp;
 
-        dp = new int[target + 1];
-        for (i = 1; i <= target; i++) {
+        dest = target > THR ? target - ((target - THR + 59) / 60) * 60 : target;
+        dp = new int[dest + 1];
+        dp[0] = (target - dest) / 60 * MULTI;
+        for (i = 1; i <= dest; i++) {
             dp[i] = INF;
         }
         for (i = 1; i <= 20; i++) {
-            for (j = i; j <= target; j++) {
+            for (j = i; j <= dest; j++) {
                 dp[j] = Math.min(dp[j], dp[j - i] + SINGLE);
             }
         }
-        for (j = 50; j <= target; j++) {
+        for (j = 50; j <= dest; j++) {
             dp[j] = Math.min(dp[j], dp[j - 50] + SINGLE);
         }
         for (i = 21; i <= 40; i++) {
             if ((i & 1) == 0 || i % 3 == 0) {
-                for (j = i; j <= target; j++) {
+                for (j = i; j <= dest; j++) {
                     dp[j] = Math.min(dp[j], dp[j - i] + MULTI);
                 }
             }
         }
         for (i = 42; i <= 60; i += 3) {
-            for (j = i; j <= target; j++) {
+            for (j = i; j <= dest; j++) {
                 dp[j] = Math.min(dp[j], dp[j - i] + MULTI);
             }
         }
-        return new int[] {dp[target] >> SHIFT, (dp[target] >> SHIFT) - (SINGLE - 1 & dp[target])};
+        return new int[] {dp[dest] >> SHIFT, (dp[dest] >> SHIFT) - (SINGLE - 1 & dp[dest])};
     }
 }
