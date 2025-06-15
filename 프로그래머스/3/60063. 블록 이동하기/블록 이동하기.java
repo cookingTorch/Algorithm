0ac3a140ebe7789d773606a9107b112a7495fa65
cookingTorch <jhw7181@naver.com>
2025-06-15 22:26:27 +0900

    [level 3] Title: 블록 이동하기, Time: 11.45 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.java
+++ 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.java
@@ -4,10 +4,10 @@ class Solution {
     private static final int H = 0;
     private static final int V = 1;
     private static final int INF = Integer.MAX_VALUE >>> 1;
-    private static final int[][] DX = {{-1, -1, 0, 0, -1, 0, 1, 0}, {0, 0, 1, 1, -1, 0, 1, 0}};
-    private static final int[][] DY = {{0, 1, 0, 1, 0, 1, 0, -1}, {-1, 0, -1, 0, 0, 1, 0, -1}};
-    private static final int[][] RDX = {{-1, -1, 0, 0}, {0, 0, 0, 0}};
-    private static final int[][] RDY = {{0, 0, 0, 0}, {-1, 0, -1, 0}};
+    private static final int[][] dx = {{-1, -1, 0, 0, -1, 0, 1, 0}, {0, 0, 1, 1, -1, 0, 1, 0}};
+    private static final int[][] dy = {{0, 1, 0, 1, 0, 1, 0, -1}, {-1, 0, -1, 0, 0, 1, 0, -1}};
+    private static final int[][] rdx = {{-1, -1, 0, 0}, {0, 0, 0, 0}};
+    private static final int[][] rdy = {{0, 0, 0, 0}, {-1, 0, -1, 0}};
 
     private static final class Node implements Comparable<Node> {
         int x;
@@ -15,10 +15,10 @@ class Solution {
         int dir;
         int time;
 
-        Node(int x, int y, int dir, int time) {
+        Node(int dir, int x, int y, int time) {
+            this.dir = dir;
             this.x = x;
             this.y = y;
-            this.dir = dir;
             this.time = time;
         }
 
@@ -29,7 +29,7 @@ class Solution {
     }
 
     private static boolean[][] rotate;
-    private static boolean[][][] empty;
+    private static boolean[][][] map;
 
     private static int dijkstra(int len) {
         int i;
@@ -43,14 +43,7 @@ class Solution {
         int dir;
         int ndir;
         int time;
-        int[] dx;
-        int[] dy;
-        int[] rdx;
-        int[] rdy;
-        int[][] ndist;
         int[][][] dist;
-        boolean[][] nempty;
-        boolean[][] nvisited;
         boolean[][][] visited;
         Node cur;
         PriorityQueue<Node> pq;
@@ -66,7 +59,7 @@ class Solution {
             }
         }
         dist[H][1][1] = 0;
-        pq.add(new Node(1, 1, H, 0));
+        pq.add(new Node(H, 1, 1, 0));
         while (pq.size() != 0) {
             cur = pq.poll();
             x = cur.x;
@@ -81,32 +74,23 @@ class Solution {
                 break;
             }
             time++;
-            dx = DX[dir];
-            dy = DY[dir];
-            rdx = RDX[dir];
-            rdy = RDY[dir];
             ndir = dir ^ 1;
-            ndist = dist[ndir];
-            nvisited = visited[ndir];
             for (i = 0; i < 4; i++) {
-                nx = x + dx[i];
-                ny = y + dy[i];
-                rx = x + rdx[i];
-                ry = y + rdy[i];
-                if (rotate[rx][ry] && !nvisited[nx][ny] && time < ndist[nx][ny]) {
-                    ndist[nx][ny] = time;
-                    pq.offer(new Node(nx, ny, ndir, time));
+                nx = x + dx[dir][i];
+                ny = y + dy[dir][i];
+                rx = x + rdx[dir][i];
+                ry = y + rdy[dir][i];
+                if (rotate[rx][ry] && !visited[ndir][nx][ny] && time < dist[ndir][nx][ny]) {
+                    dist[ndir][nx][ny] = time;
+                    pq.offer(new Node(ndir, nx, ny, time));
                 }
             }
-            ndist = dist[dir];
-            nempty = empty[dir];
-            nvisited = visited[dir];
             for (; i < 8; i++) {
-                nx = x + dx[i];
-                ny = y + dy[i];
-                if (nempty[nx][ny] && !nvisited[nx][ny] && time < ndist[nx][ny]) {
-                    ndist[nx][ny] = time;
-                    pq.offer(new Node(nx, ny, dir, time));
+                nx = x + dx[dir][i];
+                ny = y + dy[dir][i];
+                if (map[dir][nx][ny] && !visited[dir][nx][ny] && time < dist[dir][nx][ny]) {
+                    dist[dir][nx][ny] = time;
+                    pq.offer(new Node(dir, nx, ny, time));
                 }
             }
         }
@@ -117,24 +101,24 @@ class Solution {
         int i;
         int j;
         int len;
-        boolean[][] map;
+        boolean[][] isEmpty;
 
         len = board.length;
-        map = new boolean[len + 2][len + 2];
+        isEmpty = new boolean[len + 2][len + 2];
         for (i = 0; i < len; i++) {
             for (j = 0; j < len; j++) {
                 if (board[i][j] == 0) {
-                    map[i + 1][j + 1] = true;
+                    isEmpty[i + 1][j + 1] = true;
                 }
             }
         }
-        empty = new boolean[2][len + 2][len + 2];
+        map = new boolean[2][len + 2][len + 2];
         rotate = new boolean[len + 2][len + 2];
         for (i = 1; i <= len; i++) {
             for (j = 1; j <= len; j++) {
-                empty[H][i][j] = map[i][j] && map[i][j + 1];
-                empty[V][i][j] = map[i][j] && map[i + 1][j];
-                rotate[i][j] = empty[H][i][j] && empty[V][i][j] && map[i + 1][j + 1];
+                map[H][i][j] = isEmpty[i][j] && isEmpty[i][j + 1];
+                map[V][i][j] = isEmpty[i][j] && isEmpty[i + 1][j];
+                rotate[i][j] = map[H][i][j] && map[V][i][j] && isEmpty[i + 1][j + 1];
             }
         }
         return dijkstra(len);
