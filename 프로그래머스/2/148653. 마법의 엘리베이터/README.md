5447a3e0b05d09dc8d67c4c1d6699cc7af0df2a8
cookingTorch <jhw7181@naver.com>
2025-06-19 10:57:35 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/README.md
+++ 프로그래머스/2/148653. 마법의 엘리베이터/README.md
@@ -1,10 +1,10 @@
 # [level 2] 마법의 엘리베이터 - 148653 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653) 
 
 ### 성능 요약
 
-메모리: 74.2 MB, 시간: 0.02 ms
+메모리: 76.2 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 11일 20:36:47
+2025년 06월 19일 10:57:32
 
 ### 문제 설명
 
--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -2,15 +2,14 @@ class Solution {
     public int solution(int storey) {
         int ans;
         int num;
-        
-        ans = 0;
-        for (; storey != 0; storey /= 10) {
+
+        for (ans = 0; storey != 0; storey /= 10) {
             if ((num = storey % 10) > 5 || (num == 5 && storey % 100 > 50)) {
                 ans += 10 - num;
                 storey += 10;
-                continue;
+            } else {
+                ans += num;
             }
-            ans += num;
         }
         return ans;
     }
