a6c790adfec90418315f4e868822f82935c4689b
cookingTorch <jhw7181@naver.com>
2025-06-20 15:09:22 +0900

    [level 2] Title: 숫자 블록, Time: 104.63 ms, Memory: 55.7 MB -BaekjoonHub

--- 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
@@ -3,36 +3,33 @@ class Solution {
 
     public int[] solution(long begin, long end) {
         int i;
+        int j;
+        int k;
         int len;
+        int sqrt;
+        int num;
+        int start;
         int[] ans;
-        long j;
-        long thr;
-        long num;
-        long start;
 
-        len = (int) (end - begin + 1);
+        start = (int) begin;
+        len = (int) end - start + 1;
         ans = new int[len];
-        for (i = 0; i < len; i++) {
-            num = begin + i;
-            thr = (long) Math.sqrt(num);
-            start = Math.max(2, (num + MAX - 1) / MAX);
-            for (j = start; j <= thr; j++) {
-                if (num % j == 0L) {
-                    ans[i] = (int) (num / j);
-                    break;
-                }
-            }
-            if (j > thr) {
-                for (j = start - 1; j > 0; j--) {
-                    if (num % j == 0L) {
-                        ans[i] = (int) j;
+        for (i = start == 1 ? 1 : 0; i < len; i++) {
+            num = start + i;
+            sqrt = (int) Math.sqrt(num);
+            for (j = 2; j <= sqrt; j++) {
+                if (num % j == 0) {
+                    if ((k = num / j) > MAX) {
+                        ans[i] = j;
+                    } else {
+                        ans[i] = k;
                         break;
                     }
                 }
             }
-        }
-        if (begin == 1) {
-            ans[0] = 0;
+            if (ans[i] == 0) {
+                ans[i]++;
+            }
         }
         return ans;
     }
