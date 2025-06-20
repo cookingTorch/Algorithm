143c306dfd8956730e11d761a955ccc4de9b2aac
cookingTorch <jhw7181@naver.com>
2025-06-20 15:31:16 +0900

    [level 2] Title: 숫자 블록, Time: 99.16 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
@@ -4,31 +4,33 @@ class Solution {
     public int[] solution(long begin, long end) {
         int i;
         int j;
-        int k;
         int len;
-        int sqrt;
         int num;
+        int thr;
+        int sqrt;
         int start;
         int[] ans;
 
         start = (int) begin;
         len = (int) end - start + 1;
         ans = new int[len];
+        loop:
         for (i = start == 1 ? 1 : 0; i < len; i++) {
+            ans[i] = 1;
             num = start + i;
             sqrt = (int) Math.sqrt(num);
-            for (j = 2; j <= sqrt; j++) {
+            thr = Math.min(sqrt, (num + MAX - 1) / MAX - 1);
+            for (j = Math.max(thr + 1, 2); j <= sqrt; j++) {
                 if (num % j == 0) {
-                    if ((k = num / j) > MAX) {
-                        ans[i] = j;
-                    } else {
-                        ans[i] = k;
-                        break;
-                    }
+                    ans[i] = num / j;
+                    continue loop;
                 }
             }
-            if (ans[i] == 0) {
-                ans[i]++;
+            for (j = thr; j > 1; j--) {
+                if (num % j == 0) {
+                    ans[i] = j;
+                    break;
+                }
             }
         }
         return ans;
