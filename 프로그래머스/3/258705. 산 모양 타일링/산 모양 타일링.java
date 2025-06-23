7908cf473de2a8b3b2b72b5c0b3cc8d6b96126c7
cookingTorch <jhw7181@naver.com>
2025-06-23 11:50:30 +0900

    [level 3] Title: 산 모양 타일링, Time: 7.68 ms, Memory: 96.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -0,0 +1,24 @@
+class Solution {
+    private static final int MOD = 10_007;
+
+    public int solution(int n, int[] tops) {
+        int i;
+        int len;
+        int[] dp;
+
+        len = tops.length + 1 << 1;
+        dp = new int[len];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (i = 2; i < len; i += 2) {
+            if (tops[(i >>> 1) - 1] == 0) {
+                dp[i] = (dp[i - 2] + dp[i - 1]) % MOD;
+                dp[i + 1] = (dp[i] + dp[i - 1]) % MOD;
+            } else {
+                dp[i] = (dp[i - 2] + (dp[i - 1] << 1)) % MOD;
+                dp[i + 1] = (dp[i] + dp[i - 1]) % MOD;
+            }
+        }
+        return dp[len - 1];
+    }
+}
