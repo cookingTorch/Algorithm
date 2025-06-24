3ce5f02290dfcbe582ea226524532b82eb5b1760
cookingTorch <jhw7181@naver.com>
2025-06-24 15:00:29 +0900

    [level 3] Title: 억억단을 외우자, Time: 239.18 ms, Memory: 193 MB -BaekjoonHub

---
+++ 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int[] solution(int e, int[] starts) {
+        int i;
+        int j;
+        int len;
+        int max;
+        int prev;
+        int[] spf;
+        int[] exp;
+        int[] cnt;
+        int[] num;
+        int[] ans;
+
+        spf = new int[e + 1];
+        exp = new int[e + 1];
+        cnt = new int[e + 1];
+        cnt[1] = 1;
+        for (i = 2; i <= e; i++) {
+            if (spf[i] == 0) {
+                spf[i] = i;
+                exp[i] = 2;
+                cnt[i] = 2;
+                for (j = i << 1; j <= e; j += i) {
+                    if (spf[j] == 0) {
+                        spf[j] = i;
+                    }
+                }
+            } else {
+                prev = i / spf[i];
+                if (spf[prev] == spf[i]) {
+                    exp[i] = exp[prev] + 1;
+                    cnt[i] = cnt[prev] / exp[prev] * exp[i];
+                } else {
+                    exp[i] = 2;
+                    cnt[i] = cnt[prev] << 1;
+                }
+            }
+        }
+        max = 0;
+        num = new int[e + 1];
+        for (i = e; i >= 1; i--) {
+            if (cnt[i] >= max) {
+                max = cnt[i];
+                num[i] = i;
+            } else {
+                num[i] = num[i + 1];
+            }
+        }
+        len = starts.length;
+        ans = new int[len];
+        for (i = 0; i < len; i++) {
+            ans[i] = num[starts[i]];
+        }
+        return ans;
+    }
+}
