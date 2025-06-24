fb9105e362090a0d68232452a26cf009a491d238
cookingTorch <jhw7181@naver.com>
2025-06-24 13:58:16 +0900

    [level 3] Title: 등대, Time: 37.43 ms, Memory: 125 MB -BaekjoonHub

---
+++ 프로그래머스/3/133500. 등대/등대.java
@@ -0,0 +1,41 @@
+class Solution {
+    private static final class Edge {
+        int to;
+        Edge next;
+
+        Edge(int to, Edge next) {
+            this.to = to;
+            this.next = next;
+        }
+    }
+
+    private static int cnt;
+    private static Edge[] adj;
+
+    private static boolean isOn(int parent, int node) {
+        boolean on;
+        Edge edge;
+
+        on = false;
+        for (edge = adj[node]; edge != null; edge = edge.next) {
+            if (edge.to != parent) {
+                on |= !isOn(node, edge.to);
+            }
+        }
+        if (on) {
+            cnt++;
+        }
+        return on;
+    }
+
+    public int solution(int n, int[][] lighthouse) {
+        adj = new Edge[n + 1];
+        for (int[] edge : lighthouse) {
+            adj[edge[0]] = new Edge(edge[1], adj[edge[0]]);
+            adj[edge[1]] = new Edge(edge[0], adj[edge[1]]);
+        }
+        cnt = 0;
+        isOn(0, 1);
+        return cnt;
+    }
+}
