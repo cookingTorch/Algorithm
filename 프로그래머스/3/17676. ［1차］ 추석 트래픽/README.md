5a371f6563730a476e957c5ba3aca36aa805546b
cookingTorch <jhw7181@naver.com>
2025-06-25 16:18:17 +0900

    [level 3] Title: [1차] 추석 트래픽, Time: 10.85 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/3/17676. ［1차］ 추석 트래픽/README.md
+++ 프로그래머스/3/17676. ［1차］ 추석 트래픽/README.md
@@ -1,10 +1,10 @@
 # [level 3] [1차] 추석 트래픽 - 17676 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17676?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17676) 
 
 ### 성능 요약
 
-메모리: 88.9 MB, 시간: 11.35 ms
+메모리: 91.2 MB, 시간: 10.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 24일 16:14:25
+2025년 06월 25일 16:18:16
 
 ### 문제 설명
 
--- 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.java
+++ 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.java
@@ -2,6 +2,10 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 class Solution {
+    private static final int HR = 60;
+    private static final int MIN = 60_000;
+    private static final int SEC = 1_000;
+    private static final double MS = 1_000.0;
     private static final String DELIM = " :s";
 
     public int solution(String[] lines) {
@@ -20,14 +24,14 @@ class Solution {
         for (i = 0; i < len; i++) {
             st = new StringTokenizer(lines[i], DELIM, false);
             st.nextToken();
-            end[i] = (Integer.parseInt(st.nextToken()) * 60 + Integer.parseInt(st.nextToken())) * 60000 + (int) (Double.parseDouble(st.nextToken()) * 1000.0);
-            start[i] = end[i] - (int) (Double.parseDouble(st.nextToken()) * 1000.0) + 1;
+            end[i] = (Integer.parseInt(st.nextToken()) * HR + Integer.parseInt(st.nextToken())) * MIN + (int) (Double.parseDouble(st.nextToken()) * MS);
+            start[i] = end[i] - (int) (Double.parseDouble(st.nextToken()) * MS) + 1;
         }
         Arrays.sort(end);
         Arrays.sort(start);
         max = 0;
         for (l = 0, r = 0; r < len; r++) {
-            for (; start[r] - end[l] >= 1000; l++);
+            for (; start[r] - end[l] >= SEC; l++);
             max = Math.max(max, r - l + 1);
         }
         return max;
