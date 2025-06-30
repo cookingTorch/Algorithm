c4a1a968da99a7e61e2f3fffee5f1e7619ff6283
cookingTorch <jhw7181@naver.com>
2025-07-01 00:17:44 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.09 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/README.md
+++ 프로그래머스/2/81302. 거리두기 확인하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 거리두기 확인하기 - 81302 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302#fn1) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302?language=java) 
 
 ### 성능 요약
 
-메모리: 90.8 MB, 시간: 0.07 ms
+메모리: 77.2 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 30일 19:59:09
+2025년 07월 01일 00:17:40
 
 ### 문제 설명
 
--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,77 +1,66 @@
 class Solution {
-	private static final int P = 'P';
-	private static final int X = 'X';
-	private static final int SIZE = 5;
-	private static final int[] dx1 = {-1, 0, 1, 0};
-	private static final int[] dy1 = {0, 1, 0, -1};
-	private static final int[][] dx2 = {{-1, -2, -1}, {-1, 0, 1}, {1, 2, 1}, {-1, 0, 1}};
-	private static final int[][] dy2 = {{-1, 0, 1}, {1, 2, 1}, {-1, 0, 1}, {-1, -2, -1}};
-	
-	private static int[][] map;
-	
-	private static boolean validate(int x, int y) {
-		int i;
-		int j;
-		int nx;
-		int ny;
-		
-		for (i = 0; i < 4; i++) {
-			nx = x + dx1[i];
-			ny = y + dy1[i];
-			if (nx < 0 || nx >= SIZE || ny < 0 || ny >= SIZE) {
-				continue;
-			}
-			if (map[nx][ny] == P) {
-				return false;
-			}
-			if (map[nx][ny] == X) {
-				continue;
-			}
-			for (j = 0; j < 3; j++) {
-				nx = x + dx2[i][j];
-				ny = y + dy2[i][j];
-				if (nx < 0 || nx >= SIZE || ny < 0 || ny >= SIZE) {
-					continue;
-				}
-				if (map[nx][ny] == P) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
-	
-	public int[] solution(String[][] places) {
-		int i;
-		int j;
-		int k;
-		int len;
-		int[] ans;
-		String[] place;
-		
-		len = places.length;
-		ans = new int[len];
-		map = new int[SIZE][SIZE];
-		loop:
-		for (i = 0; i < len; i++) {
-			place = places[i];
-			for (j = 0; j < SIZE; j++) {
-				for (k = 0; k < SIZE; k++) {
-					map[j][k] = place[j].charAt(k);
-				}
-			}
-			ans[i] = 1;
-			for (j = 0; j < SIZE; j++) {
-				for (k = 0; k < SIZE; k++) {
-					if (map[j][k] == P) {
-						if (!validate(j, k)) {
-							ans[i] = 0;
-							continue loop;
-						}
-					}
-				}
-			}
-		}
-		return ans;
-	}
+    private static final char P = 'P';
+    private static final char X = 'X';
+    private static final int SIZE = 5;
+    private static final int MAX = SIZE + 2;
+    private static final int[] dx1 = {-1, 0, 1, 0};
+    private static final int[] dy1 = {0, 1, 0, -1};
+    private static final int[][] dx2 = {{-1, -2, -1}, {-1, 0, 1}, {1, 2, 1}, {-1, 0, 1}};
+    private static final int[][] dy2 = {{-1, 0, 1}, {1, 2, 1}, {-1, 0, 1}, {-1, -2, -1}};
+
+    private static char[][] map;
+
+    private static boolean validate(int x, int y) {
+        int i;
+        int j;
+        char ch;
+
+        for (i = 0; i < 4; i++) {
+            ch = map[x + dx1[i]][y + dy1[i]];
+            if (ch == P) {
+                return false;
+            }
+            if (ch == X) {
+                continue;
+            }
+            for (j = 0; j < 3; j++) {
+                if (map[x + dx2[i][j]][y + dy2[i][j]] == P) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public int[] solution(String[][] places) {
+        int i;
+        int j;
+        int k;
+        int len;
+        int[] ans;
+        String[] place;
+
+        len = places.length;
+        ans = new int[len];
+        map = new char[SIZE + 4][SIZE + 4];
+        loop:
+        for (i = 0; i < len; i++) {
+            place = places[i];
+            for (j = 0; j < SIZE; j++) {
+                place[j].getChars(0, SIZE, map[j + 2], 2);
+            }
+            ans[i] = 1;
+            for (j = 2; j <= MAX; j++) {
+                for (k = 2; k <= MAX; k++) {
+                    if (map[j][k] == P) {
+                        if (!validate(j, k)) {
+                            ans[i] = 0;
+                            continue loop;
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
 }
