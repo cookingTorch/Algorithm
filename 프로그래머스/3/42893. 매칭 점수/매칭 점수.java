cdfea18e25f4da8fad2a0d9ba5aab82e3947bc23
cookingTorch <jhw7181@naver.com>
2025-06-30 11:09:26 +0900

    [level 3] Title: 매칭 점수, Time: 4.44 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/3/42893. 매칭 점수/매칭 점수.java
+++ 프로그래머스/3/42893. 매칭 점수/매칭 점수.java
@@ -2,180 +2,159 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 class Solution {
-	private static final class Page {
-		private static final char A = 'A';
-		private static final char Z = 'Z';
-		private static final char a = 'a';
-		private static final char z = 'z';
-		private static final char BRACE = '>';
-		private static final char QUOTE = '"';
-		private static final char[] META = "<meta ".toCharArray();
-		private static final char[] HREF = "<a href=\"".toCharArray();
-		private static final char[] CONTENT = "content=\"".toCharArray();
-		private static final int NULL = -1;
-		private static final int DIFF = a - A;
-		private static final int META_LEN = META.length;
-		private static final int HREF_LEN = HREF.length;
-		private static final int CONTENT_LEN = CONTENT.length;
+    private static final char A = 'A';
+    private static final char Z = 'Z';
+    private static final char a = 'a';
+    private static final char z = 'z';
+    private static final char NULL = '\0';
+    private static final char BRACE = '>';
+    private static final char QUOTE = '"';
+    private static final char[] META = "<meta ".toCharArray();
+    private static final char[] HREF = "<a href=\"".toCharArray();
+    private static final char[] CONTENT = "content=\"".toCharArray();
+    private static final int NONE = -1;
+    private static final int DIFF = a - A;
+    private static final int MAX_LEN = 15_001;
+    private static final int META_LEN = META.length;
+    private static final int HREF_LEN = HREF.length;
+    private static final int CONTENT_LEN = CONTENT.length;
 
-		private static int len;
-		private static char[] word;
-		private static HashMap<String, Page> map;
+    private static int strLen;
+    private static int pageLen;
+    private static int strStart;
+    private static char[] str;
+    private static char[] page;
 
-		int cnt;
-		int size;
-		int letter;
-		char[] txt;
-		double score;
-		ArrayList<String> links;
+    private static char lower(char ch) {
+        if (ch < a) {
+            return (char) (ch + DIFF);
+        }
+        return ch;
+    }
 
-		Page(String page) {
-			int i;
-			int j;
+    private static boolean isLetter(char ch) {
+        return (A <= ch && ch <= Z) || (a <= ch && ch <= z);
+    }
 
-			txt = page.toCharArray();
-			size = txt.length;
-			links = new ArrayList<>();
-			letter = NULL;
-			for (i = 0; i < size; i++) {
-				isWord(i);
-				if (isPattern(i, META, META_LEN)) {
-					for (j = i + META_LEN; j < size; j++) {
-						if (txt[j] == BRACE) {
-							break;
-						}
-						if (isPattern(j, CONTENT, CONTENT_LEN)) {
-							map.put(getUrl(j + CONTENT_LEN), this);
-							break;
-						}
-					}
-				}
-				if (isPattern(i, HREF, HREF_LEN)) {
-					links.add(getUrl(i + HREF_LEN));
-				}
-			}
-			if (letter != NULL && size - letter == len) {
-				for (i = 0; i < len; i++) {
-					if (lower(txt[letter + i]) != word[i]) {
-						return;
-					}
-				}
-				cnt++;
-			}
-		}
+    private static boolean isStr(int i) {
+        int j;
 
-		private static char lower(char ch) {
-			if (ch < a) {
-				return (char) (ch + DIFF);
-			}
-			return ch;
-		}
+        if (strStart == NONE) {
+            if (isLetter(page[i])) {
+                strStart = i;
+            }
+            return false;
+        } else if (!isLetter(page[i])) {
+            if (i - strStart != strLen) {
+                strStart = NONE;
+                return false;
+            }
+            i = strStart;
+            strStart = NONE;
+            for (j = 0; j < strLen; j++) {
+                if (lower(page[i + j]) != str[j]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
 
-		static void init(String str) {
-			int i;
+    private static boolean isPattern(int i, char[] pat, int patLen) {
+        int j;
 
-			word = str.toCharArray();
-			len = word.length;
-			for (i = 0; i < len; i++) {
-				word[i] = lower(word[i]);
-			}
-			map = new HashMap<>();
-		}
+        if (i + patLen > pageLen) {
+            return false;
+        }
+        for (j = 0; j < patLen; j++) {
+            if (page[i + j] != pat[j]) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-		private boolean isLetter(char ch) {
-			return (A <= ch && ch <= Z) || (a <= ch && ch <= z);
-		}
+    private static String getUrl(int i) {
+        StringBuilder sb;
 
-		private void isWord(int i) {
-			int j;
+        sb = new StringBuilder();
+        for (; i < pageLen; i++) {
+            if (page[i] == QUOTE) {
+                break;
+            }
+            sb.append(page[i]);
+        }
+        return sb.toString();
+    }
 
-			if (letter == NULL) {
-				if (isLetter(txt[i])) {
-					letter = i;
-				}
-			} else if (!isLetter(txt[i])) {
-				if (i - letter != len) {
-					letter = NULL;
-					return;
-				}
-				i = letter;
-				letter = NULL;
-				for (j = 0; j < len; j++) {
-					if (lower(txt[i + j]) != word[j]) {
-						return;
-					}
-				}
-				cnt++;
-			}
-		}
+    public int solution(String word, String[] pages) {
+        int i;
+        int j;
+        int idx;
+        int ans;
+        int pagesLen;
+        int[] strCnt;
+        double max;
+        double linkScore;
+        double[] scores;
+        ArrayList<String>[] links;
+        HashMap<String, Integer> map;
 
-		private boolean isPattern(int i, char[] str, int len) {
-			int j;
-
-			if (i + len > size) {
-				return false;
-			}
-			for (j = 0; j < len; j++) {
-				if (txt[i + j] != str[j]) {
-					return false;
-				}
-			}
-			return true;
-		}
-
-		private String getUrl(int i) {
-			StringBuilder sb;
-
-			sb = new StringBuilder();
-			for (; i < size; i++) {
-				if (txt[i] == QUOTE) {
-					break;
-				}
-				sb.append(txt[i]);
-			}
-			return sb.toString();
-		}
-
-		void calc() {
-			int len;
-			double linkScore;
-			Page page;
-
-			score += cnt;
-			len = links.size();
-			linkScore = (double) cnt / len;
-			for (String link : links) {
-				if ((page = map.get(link)) != null) {
-					page.score += linkScore;
-				}
-			}
-		}
-	}
-
-	public int solution(String word, String[] pages) {
-		int i;
-		int len;
-		int ans;
-		double max;
-		Page[] nodes;
-
-		Page.init(word);
-		len = pages.length;
-		nodes = new Page[len];
-		for (i = 0; i < len; i++) {
-			nodes[i] = new Page(pages[i]);
-		}
-		for (i = 0; i < len; i++) {
-			nodes[i].calc();
-		}
-		ans = 0;
-		max = 0.0;
-		for (i = 0; i < len; i++) {
-			if (nodes[i].score > max) {
-				max = nodes[i].score;
-				ans = i;
-			}
-		}
-		return ans;
-	}
+        str = word.toCharArray();
+        strLen = str.length;
+        for (i = 0; i < strLen; i++) {
+            str[i] = lower(str[i]);
+        }
+        pagesLen = pages.length;
+        strCnt = new int[pagesLen];
+        links = new ArrayList[pagesLen];
+        map = new HashMap<>();
+        page = new char[MAX_LEN];
+        for (idx = 0; idx < pagesLen; idx++) {
+            pageLen = pages[idx].length();
+            pages[idx].getChars(0, pageLen, page, 0);
+            page[pageLen++] = NULL;
+            links[idx] = new ArrayList<>();
+            strStart = NONE;
+            for (i = 0; i < pageLen; i++) {
+                if (isStr(i)) {
+                    strCnt[idx]++;
+                }
+                if (isPattern(i, META, META_LEN)) {
+                    for (j = i + META_LEN; j < pageLen; j++) {
+                        if (page[j] == BRACE) {
+                            break;
+                        }
+                        if (isPattern(j, CONTENT, CONTENT_LEN)) {
+                            map.put(getUrl(j + CONTENT_LEN), idx);
+                            break;
+                        }
+                    }
+                }
+                if (isPattern(i, HREF, HREF_LEN)) {
+                    links[idx].add(getUrl(i + HREF_LEN));
+                }
+            }
+        }
+        scores = new double[pagesLen];
+        for (idx = 0; idx < pagesLen; idx++) {
+            scores[idx] += strCnt[idx];
+            linkScore = (double) strCnt[idx] / links[idx].size();
+            for (String link : links[idx]) {
+                if (map.containsKey(link)) {
+                    scores[map.get(link)] += linkScore;
+                }
+            }
+        }
+        ans = 0;
+        max = 0.0;
+        for (idx = 0; idx < pagesLen; idx++) {
+            if (scores[idx] > max) {
+                ans = idx;
+                max = scores[idx];
+            }
+        }
+        return ans;
+    }
 }
