520187768c2590c2194503d3b674a77322c99322
cookingTorch <jhw7181@naver.com>
2025-07-04 17:17:56 +0900

    [level 2] Title: 점 찍기, Time: 9.18 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -5,11 +5,11 @@ class Solution {
         long sum;
         double rr;
 
-        sum = 0;
         r = d / k;
         rr = ((double) d / k) * ((double) d / k);
+        sum = r + 1L;
         for (i = 0L; i <= r; i++) {
-            sum += (long) Math.sqrt(rr - i * i) + 1L;
+            sum += (long) Math.sqrt(rr - i * i);
         }
         return sum;
     }
