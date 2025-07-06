f56b85a797194474eca3569603567b61c7d9f215
cookingTorch <jhw7181@naver.com>
2025-07-07 08:55:32 +0900

    [level 3] Title: 주사위 고르기, Time: 222.73 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
@@ -1,77 +1,84 @@
 import java.util.Arrays;
 
 class Solution {
-    int n, max, temp, r;
-    int[] dices1, dices2, result1, result2, cnt1, cnt2, ans;
-    int[][] nums;
+    private static final int NUMS = 6;
 
-    private void pi(int[] dices, int[] result, int[] cnt, int d) {
-        int sum, i;
+    private static int idx;
+    private static int[] arr;
+    private static int[][] dices;
 
-        if (d == r) {
-            sum = 0;
-            for (i = 0; i < r; i++) {
-                sum += nums[dices[i]][result[i]];
-            }
-            cnt[sum]++;
-        } else {
-            for (i = 0; i < 6; i++) {
-                result[d] = i;
-                pi(dices, result, cnt, d + 1);
-            }
+    private static void dfs(int mask, int sum) {
+        int i;
+        int[] dice;
+
+        if (mask == 0) {
+            arr[idx++] = sum;
+            return;
         }
-    }
-
-    private void combi(int start, int d) {
-        int i, j, i1, i2;
-
-        if (d == r) {
-            i1 = 0;
-            i2 = 0;
-            for (i = 0; i < n; i++) {
-                if (i1 >= r || dices1[i1] != i) {
-                    dices2[i2++] = i;
-                } else {
-                    i1++;
-                }
-            }
-            cnt1 = new int[501];
-            cnt2 = new int[501];
-            pi(dices1, result1, cnt1, 0);
-            pi(dices2, result2, cnt2, 0);
-            temp = 0;
-            for (i = 1; i <= 500; i++) {
-                if (cnt1[i] > 0) {
-                    for (j = 1; j < i; j++) {
-                        temp += cnt1[i] * cnt2[j];
-                    }
-                }
-            }
-            if (temp > max) {
-                ans = Arrays.copyOf(dices1, r);
-                for (i = 0; i < r; i++) {
-                    ans[i]++;
-                }
-                max = temp;
-            }
-        } else {
-            for (i = start; i < n; i++) {
-                dices1[d] = i;
-                combi(i + 1, d + 1);
-            }
+        dice = dices[Integer.numberOfTrailingZeros(mask)];
+        mask &= mask - 1;
+        for (i = 0; i < NUMS; i++) {
+            dfs(mask, sum + dice[i]);
         }
     }
 
     public int[] solution(int[][] dice) {
-        nums = dice;
-        n = nums.length;
-        r = n / 2;
-        dices1 = new int[r];
-        dices2 = new int[r];
-        result1 = new int[r];
-        result2 = new int[r];
-        max = -1;
-        combi(0, 0);
-        return ans;
+        int n;
+        int r;
+        int c;
+        int i;
+        int j;
+        int thr;
+        int cnt;
+        int max;
+        int ans;
+        int half;
+        int full;
+        int mask;
+        int size;
+        int[] a;
+        int[] b;
+        int[] res;
+
+        dices = dice;
+        n = dice.length;
+        half = n >>> 1;
+        size = (int) Math.pow(NUMS, half);
+        a = new int[size];
+        b = new int[size];
+        thr = 1 << n;
+        full = thr - 1;
+        max = 0;
+        ans = 0;
+        for (mask = (1 << half) - 1; mask < thr;) {
+            arr = a;
+            idx = 0;
+            dfs(mask, 0);
+            arr = b;
+            idx = 0;
+            dfs(~mask & full, 0);
+            Arrays.sort(a);
+            Arrays.sort(b);
+            cnt = 0;
+            for (i = 0, j = 0; i < size; i++) {
+                for (; j < size && a[i] > b[j]; j++);
+                cnt += j;
+            }
+            if (cnt > max) {
+                max = cnt;
+                ans = mask;
+            }
+            c = mask & -mask;
+            r = mask + c;
+            mask = ((r ^ mask) >>> 2) / c | r;
+        }
+        res = new int[half];
+        idx = 0;
+        for (i = 0; i < n; i++) {
+            if ((ans & 1 << i) != 0) {
+                res[idx++] = i + 1;
+            }
+        }
+        return res;
     }
 }
