92b89058faf89fd31881ce6b5901d10f75c28d6e
cookingTorch <jhw7181@naver.com>
2025-07-07 15:54:09 +0900

    [Gold V] Title: 집합의 표현, Time: 308 ms, Memory: 48732 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -4,62 +4,61 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static final char UNION = '0';
-	private static final String YES = "YES\n";
-	private static final String NO = "NO\n";
-	
-	private static int[] roots;
-	
-	private static final int find(int v) {
-		if (roots[v] <= 0) {
-			return v;
-		}
-		return roots[v] = find(roots[v]);
-	}
-	
-	private static final void union(int u, int v) {
-		int ru;
-		int rv;
-		
-		if ((ru = find(u)) == (rv = find(v))) {
-			return;
-		}
-		if (roots[ru] > roots[rv]) {
-			roots[ru] = rv;
-		} else {
-			if (roots[ru] == roots[rv]) {
-				roots[ru]--;
-			}
-			roots[rv] = ru;
-		}
-	}
-	
-	private static final boolean check(int u, int v) {
-		return find(u) == find(v);
-	}
-	
-	public static void main(String[] args) throws IOException {
-		int n;
-		int m;
-		int i;
-		StringBuilder sb;
-		BufferedReader br;
-		StringTokenizer st;
-		
-		br = new BufferedReader(new InputStreamReader(System.in));
-		st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		roots = new int[n + 2];
-		sb = new StringBuilder();
-		for (i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			if (st.nextToken().charAt(0) == UNION) {
-				union(Integer.parseInt(st.nextToken()) + 1, Integer.parseInt(st.nextToken()) + 1);
-			} else {
-				sb.append(check(Integer.parseInt(st.nextToken()) + 1, Integer.parseInt(st.nextToken()) + 1) ? YES : NO);
-			}
-		}
-		System.out.print(sb);
-	}
+    private static final int UNION = '0';
+    private static final String NO = "NO\n";
+    private static final String YES = "YES\n";
+    private static final String DELIM = " ";
+
+    private static int[] roots;
+
+    private static int find(int v) {
+        if (roots[v] <= 0) {
+            return v;
+        }
+        return roots[v] = find(roots[v]);
+    }
+
+    private static void union(int u, int v) {
+        if ((u = find(u)) == (v = find(v))) {
+            return;
+        }
+        if (roots[u] > roots[v]) {
+            roots[u] = v;
+        } else {
+            if (roots[u] == roots[v]) {
+                roots[u]--;
+            }
+            roots[v] = u;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        int n;
+        int m;
+        int u;
+        int v;
+        StringBuilder sb;
+        BufferedReader br;
+        StringTokenizer st;
+
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine(), DELIM, false);
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        roots = new int[n + 2];
+        sb = new StringBuilder();
+        while (m-- > 0) {
+            st = new StringTokenizer(br.readLine(), DELIM, false);
+            if (st.nextToken().charAt(0) == UNION) {
+                u = (u = Integer.parseInt(st.nextToken())) == 0 ? n + 1 : u;
+                v = (v = Integer.parseInt(st.nextToken())) == 0 ? n + 1 : v;
+                union(u, v);
+            } else {
+                u = (u = Integer.parseInt(st.nextToken())) == 0 ? n + 1 : u;
+                v = (v = Integer.parseInt(st.nextToken())) == 0 ? n + 1 : v;
+                sb.append(find(u) == find(v) ? YES : NO);
+            }
+        }
+        System.out.print(sb.toString());
+    }
 }
