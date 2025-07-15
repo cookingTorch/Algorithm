04399df1b2183ed70fd6f2459e3b752940f46574
cookingTorch <jhw7181@naver.com>
2025-07-15 15:52:13 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 12.76 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,48 +1,48 @@
-import java.util.ArrayDeque;
-
 class Solution {
-	private static final int FAIL = -1;
+    private static final int FAIL = -1;
 
-	public int solution(int[] queue1, int[] queue2) {
-		int i;
-		int len;
-		int num;
-		long sum;
-		long dest;
-		ArrayDeque<Integer> q1;
-		ArrayDeque<Integer> q2;
+    public int solution(int[] queue1, int[] queue2) {
+        int l;
+        int r;
+        int i;
+        int len;
+        int thr;
+        int[] arr;
+        long diff;
 
-		len = queue1.length;
-		q1 = new ArrayDeque<>();
-		dest = 0L;
-		for (i = 0; i < len; i++) {
-			dest += queue1[i];
-			q1.addLast(queue1[i]);
-		}
-		sum = dest;
-		q2 = new ArrayDeque<>();
-		for (i = 0; i < len; i++) {
-			dest += queue2[i];
-			q2.addLast(queue2[i]);
-		}
-		if ((dest & 1) != 0) {
-			return FAIL;
-		}
-		len *= 3;
-		dest >>>= 1;
-		for (i = 0; i < len; i++) {
-			if (sum > dest) {
-				num = q1.pollFirst();
-				q2.addLast(num);
-				sum -= num;
-			} else if (sum < dest) {
-				num = q2.pollFirst();
-				q1.addLast(num);
-				sum += num;
-			} else {
-				break;
-			}
-		}
-		return i == len ? FAIL : i;
-	}
+        len = queue1.length;
+        arr = new int[len << 1];
+        System.arraycopy(queue1, 0, arr, 0, len);
+        System.arraycopy(queue2, 0, arr, len, len);
+        diff = 0L;
+        for (i = 0; i < len; i++) {
+            diff += queue1[i];
+        }
+        for (i = 0; i < len; i++) {
+            diff -= queue2[i];
+        }
+        if ((diff & 1) != 0) {
+            return FAIL;
+        }
+        l = 0;
+        r = len;
+        thr = len * 3;
+        len <<= 1;
+        for (i = 0; i < thr; i++) {
+            if (diff > 0L) {
+                diff -= arr[l++] << 1;
+                if (l == len) {
+                    l = 0;
+                }
+            } else if (diff < 0L) {
+                diff += arr[r++] << 1;
+                if (r == len) {
+                    r = 0;
+                }
+            } else {
+                break;
+            }
+        }
+        return i == thr ? FAIL : i;
+    }
 }
