c8e6f4193f819665e9ca62942eec887fc03369a1
cookingTorch <jhw7181@naver.com>
2025-07-17 13:15:58 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.87 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -6,6 +6,7 @@ class Solution {
     private static final int ADD = 0;
     private static final int SUB = 1;
     private static final int MUL = 2;
+    private static final int MAX_LEN = 50;
     private static final int PLUS = '+';
     private static final int MINUS = '-';
     private static final int[][] PRIORITIES = {
@@ -31,18 +32,18 @@ class Solution {
         }
     }
 
-    private static long eval(int[] ops, long[] nums, int[] priority) {
+    private static long eval(ArrayList<Integer> ops, ArrayList<Long> nums, int[] priority) {
         int i;
         int len;
 
-        len = nums.length;
-        numStack.addFirst(nums[0]);
+        len = nums.size();
+        numStack.addFirst(nums.get(0));
         for (i = 1; i < len; i++) {
-            while (!opStack.isEmpty() && priority[opStack.peekFirst()] >= priority[ops[i]]) {
+            while (!opStack.isEmpty() && priority[opStack.peekFirst()] >= priority[ops.get(i)]) {
                 calc(opStack.pollFirst());
             }
-            numStack.addFirst(nums[i]);
-            opStack.addFirst(ops[i]);
+            numStack.addFirst(nums.get(i));
+            opStack.addFirst(ops.get(i));
         }
         while (!opStack.isEmpty()) {
             calc(opStack.pollFirst());
@@ -51,32 +52,21 @@ class Solution {
     }
 
     public long solution(String expression) {
-        int i;
         int op;
-        int len;
-        int[] ops;
         long max;
-        long[] nums;
-        ArrayList<Long> numList;
-        ArrayList<Integer> opList;
+        ArrayList<Long> nums;
+        ArrayList<Integer> ops;
         StringTokenizer st;
 
-        opList = new ArrayList<>();
-        numList = new ArrayList<>();
+        ops = new ArrayList<>();
+        nums = new ArrayList<>();
         st = new StringTokenizer(expression, DELIM, true);
-        opList.add(0);
-        numList.add(Long.parseLong(st.nextToken()));
+        ops.add(0);
+        nums.add(Long.parseLong(st.nextToken()));
         while (st.hasMoreTokens()) {
             op = st.nextToken().charAt(0);
-            opList.add(op == PLUS ? 0 : op == MINUS ? 1 : 2);
-            numList.add((long) Integer.parseInt(st.nextToken()));
-        }
-        len = numList.size();
-        ops = new int[len];
-        nums = new long[len];
-        for (i = 0; i < len; i++) {
-            ops[i] = opList.get(i);
-            nums[i] = numList.get(i);
+            ops.add(op == PLUS ? 0 : op == MINUS ? 1 : 2);
+            nums.add((long) Integer.parseInt(st.nextToken()));
         }
         max = 0L;
         opStack = new ArrayDeque<>();
