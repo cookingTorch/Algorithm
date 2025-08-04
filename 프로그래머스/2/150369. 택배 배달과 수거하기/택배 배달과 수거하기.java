1ef60307723a1fc828220f7f4a37a5f671e254be
cookingTorch <jhw7181@naver.com>
2025-08-04 13:46:23 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 6.81 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,30 +1,21 @@
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
-        int temp;
-        int pickupIdx;
-        int pickupCnt;
-        int deliveryIdx;
-        int deliveryCnt;
-        long totalDist;
+        int i;
+        int cnt;
+        int pickupCap;
+        int deliveryCap;
+        long dist;
         
-        for (deliveryIdx = n - 1; deliveryIdx >= 0 && deliveries[deliveryIdx] == 0; deliveryIdx--);
-        for (pickupIdx = n - 1; pickupIdx >= 0 && pickups[pickupIdx] == 0; pickupIdx--);
-        totalDist = 0;
-        while (deliveryIdx >= 0 || pickupIdx >= 0) {
-            totalDist += Math.max(deliveryIdx, pickupIdx) + 1 << 1;
-            for (deliveryCnt = 0; deliveryIdx >= 0 && deliveryCnt < cap;) {
-                temp = Math.min(deliveries[deliveryIdx], cap - deliveryCnt);
-                deliveryCnt += temp;
-                deliveries[deliveryIdx] -= temp;
-                for (; deliveryIdx >= 0 && deliveries[deliveryIdx] == 0; deliveryIdx--);
-            }
-            for (pickupCnt = 0; pickupIdx >= 0 && pickupCnt < cap;) {
-                temp = Math.min(pickups[pickupIdx], cap - pickupCnt);
-                pickupCnt += temp;
-                pickups[pickupIdx] -= temp;
-                for (; pickupIdx >= 0 && pickups[pickupIdx] == 0; pickupIdx--);
+        for (dist = 0L, deliveryCap = 0, pickupCap = 0, i = n - 1; i >= 0; i--) {
+            deliveryCap -= deliveries[i];
+            pickupCap -= pickups[i];
+            if (deliveryCap < 0 || pickupCap < 0) {
+                cnt = (-Math.min(deliveryCap, pickupCap) + cap - 1) / cap;
+                deliveryCap += cnt * cap;
+                pickupCap += cnt * cap;
+                dist += (i + 1) * cnt;
             }
         }
-        return totalDist;
+        return dist << 1;
     }
 }
