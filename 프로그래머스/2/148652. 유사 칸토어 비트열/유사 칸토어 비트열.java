f32ef961615fd10f28114be52e026ecc9c86f121
cookingTorch <jhw7181@naver.com>
2025-08-11 16:53:38 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 0.07 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.java
@@ -0,0 +1,24 @@
+class Solution {
+    private static long left;
+    private static long right;
+
+    private static int dnc(int n, long start, long end) {
+        long len;
+
+        if (end < left || right < start) {
+            return 0;
+        }
+        if (left <= start && end <= right) {
+            return (int) Math.pow(4, n);
+        }
+        len = (end - start) / 5;
+        n--;
+        return dnc(n, start, start += len) + dnc(n, start, start += len) + dnc(n, start += len, start += len) + dnc(n, start, start + len);
+    }
+
+    public int solution(int n, long l, long r) {
+        left = l - 1;
+        right = r;
+        return dnc(n, 0L, (long) Math.pow(5, n));
+    }
+}
