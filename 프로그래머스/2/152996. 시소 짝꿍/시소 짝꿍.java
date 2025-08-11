6f8059e5d12c85556a4af93f07ab17fa96f39ee9
cookingTorch <jhw7181@naver.com>
2025-08-11 17:31:41 +0900

    [level 2] Title: 시소 짝꿍, Time: 36.47 ms, Memory: 94.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+class Solution {
+    public long solution(int[] weights) {
+        int i;
+        int len;
+        long num;
+        long cnt;
+        HashMap<Integer, Integer> map;
+
+        map = new HashMap<>();
+        cnt = 0;
+        len = weights.length;
+        for (i = 0; i < len; i++) {
+            map.put(weights[i], map.getOrDefault(weights[i], 0) + 1);
+        }
+        for (int weight : map.keySet()) {
+            num = map.get(weight);
+            cnt += (num * (num - 1L) >> 1)
+                    + num * (
+                    ((weight & 1) == 0 ? map.getOrDefault((weight >> 1) * 3, 0) : 0)
+                    + (weight % 3 == 0 ? map.getOrDefault(weight / 3 << 2, 0) : 0)
+                    + map.getOrDefault(weight << 1, 0));
+        }
+        return cnt;
+    }
+}
