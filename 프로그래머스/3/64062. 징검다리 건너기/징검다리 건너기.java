4431bc1916cdf9c0abde8b65effb3ff2d8ff2fa7
cookingTorch <jhw7181@naver.com>
2025-09-08 10:12:24 +0900

    [level 3] Title: 징검다리 건너기, Time: 45.28 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,39 +1,39 @@
 import java.util.ArrayDeque;
 
 class Solution {
-	private static final int INF = Integer.MAX_VALUE >>> 1;
+    private static final int INF = Integer.MAX_VALUE >>> 1;
+    private static final int[] NIL = new int[] {-1, INF};
 
-	public int solution(int[] stones, int k) {
-		int i;
-		int len;
-		int min;
-		int[] rock;
-		ArrayDeque<int[]> stack;
+    public int solution(int[] stones, int k) {
+        int i;
+        int len;
+        int min;
+        int[] rock;
+        ArrayDeque<int[]> stack;
 
-		len = stones.length;
-		stack = new ArrayDeque<>(len << 1);
-		stack.addFirst(new int[] {-1, INF});
-		min = INF;
-		for (i = 0; i < len; i++) {
-			while (stack.peekFirst()[1] <= stones[i]) {
-				rock = stack.pollFirst();
-				if (i - stack.peekFirst()[0] - 1 >= k) {
-					min = Math.min(min, rock[1]);
-                    break;
-				}
-			}
+        len = stones.length;
+        stack = new ArrayDeque<>(len << 1);
+        stack.addFirst(NIL);
+        min = INF;
+        for (i = 0; i < len; i++) {
             while (stack.peekFirst()[1] <= stones[i]) {
-				rock = stack.pollFirst();
-			}
-			stack.addFirst(new int[] {i, stones[i]});
-		}
-		while (stack.size() > 1) {
-			rock = stack.pollFirst();
-			if (len - stack.peekFirst()[0] - 1 >= k) {
-				min = Math.min(min, rock[1]);
+                rock = stack.pollFirst();
+                if (i - stack.peekFirst()[0] - 1 >= k) {
+                    min = Math.min(min, rock[1]);
+                    break;
+                }
+            }
+            while (stack.peekFirst()[1] <= stones[i]) {
+                stack.pollFirst();
+            }
+            stack.addFirst(new int[] {i, stones[i]});
+        }
+        while ((rock = stack.pollFirst()) != NIL) {
+            if (len - stack.peekFirst()[0] - 1 >= k) {
+                min = Math.min(min, rock[1]);
                 break;
-			}
-		}
-		return min;
-	}
+            }
+        }
+        return min;
+    }
 }
