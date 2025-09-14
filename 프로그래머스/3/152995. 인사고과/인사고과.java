0f387f43db5a3b0c99e2c2847c7e01b785e0c0ee
cookingTorch <jhw7181@naver.com>
2025-09-14 19:57:02 +0900

    [level 3] Title: 인사고과, Time: 118.12 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.java
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -1,43 +1,41 @@
 import java.util.Arrays;
 
 class Solution {
-    private static final int FAIL = -1;
+	private static final int FAIL = -1;
 
-    public int solution(int[][] scores) {
-        int len;
-        int ans;
-        int sum;
-        int peer;
-        int work;
-        int prev;
-        int[] wanho;
+	public int solution(int[][] scores) {
+		int ans;
+		int sum;
+		int work;
+		int peer;
+		int prev;
+		int[] wanho;
 
-        wanho = scores[0];
-        sum = wanho[0] + wanho[1];
-        len = scores.length;
-        Arrays.sort(scores, 0, len, (o1, o2) -> o2[0] - o1[0]);
-        ans = 1;
-        prev = 0;
-        peer = 0;
-        work = 0;
-        for (int[] score : scores) {
-            if (score[0] != peer) {
-                peer = score[0];
-                prev = work;
-            }
-            if (score[1] < prev) {
-                if (score == wanho) {
-                    ans = FAIL;
-                    break;
-                }
-                continue;
-            } else {
-                work = Math.max(work, score[1]);
-            }
-            if (score[0] + score[1] > sum) {
-                ans++;
-            }
-        }
-        return ans;
-    }
+		wanho = scores[0];
+		sum = wanho[0] + wanho[1];
+		Arrays.sort(scores, 0, scores.length, (o1, o2) -> o2[0] - o1[0]);
+		ans = 1;
+		prev = 0;
+		work = 0;
+		peer = 0;
+		for (int[] score : scores) {
+			if (score[0] != work) {
+				work = score[0];
+				prev = peer;
+			}
+			if (score[1] < prev) {
+				if (score == wanho) {
+					ans = FAIL;
+					break;
+				}
+				continue;
+			} else {
+				peer = Math.max(peer, score[1]);
+			}
+			if (score[0] + score[1] > sum) {
+				ans++;
+			}
+		}
+		return ans;
+	}
 }
