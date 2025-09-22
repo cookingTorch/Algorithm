769b0cb339dea1335748ad2a7eb8f4a744912a3b
cookingTorch <jhw7181@naver.com>
2025-09-22 20:15:58 +0900

    [level 2] Title: 무인도 여행, Time: 7.10 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution {
+	private static final int DIFF = '0';
+	private static final int[] FAIL = {-1};
+	private static final int[] dx = {-1, 0, 1, 0};
+	private static final int[] dy = {0, 1, 0, -1};
+	private static final char SEA = 'X';
+
+	private static int r;
+	private static int c;
+	private static char[][] map;
+
+	private static int dfs(int x, int y) {
+		int i;
+		int nx;
+		int ny;
+		int res;
+
+		res = map[x][y] - DIFF;
+		map[x][y] = SEA;
+		for (i = 0; i < 4; i++) {
+			nx = x + dx[i];
+			ny = y + dy[i];
+			if (nx < 0 || nx >= r || ny < 0 || ny >= c || map[nx][ny] == SEA) {
+				continue;
+			}
+			res += dfs(nx, ny);
+		}
+		return res;
+	}
+
+	public int[] solution(String[] maps) {
+		int i;
+		int j;
+		ArrayList<Integer> list;
+
+		r = maps.length;
+		map = new char[r][];
+		for (i = 0; i < r; i++) {
+			map[i] = maps[i].toCharArray();
+		}
+		c = map[0].length;
+		list = new ArrayList<>();
+		for (i = 0; i < r; i++) {
+			for (j = 0; j < c; j++) {
+				if (map[i][j] != SEA) {
+					list.add(dfs(i, j));
+				}
+			}
+		}
+		if (list.isEmpty()) {
+			return FAIL;
+		}
+		Collections.sort(list);
+		return list.stream().mapToInt(x -> x).toArray();
+	}
+}
