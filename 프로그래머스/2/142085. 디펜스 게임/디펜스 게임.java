7e0a8893a42e5df3f75c19980a4200b96437aed3
cookingTorch <jhw7181@naver.com>
2025-09-29 19:59:22 +0900

    [level 2] Title: 디펜스 게임, Time: 154.30 ms, Memory: 148 MB -BaekjoonHub

---
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -0,0 +1,23 @@
+import java.util.PriorityQueue;
+
+class Solution {
+	public int solution(int n, int k, int[] enemy) {
+		int i;
+		int len;
+		PriorityQueue<Integer> pq;
+
+		len = enemy.length;
+		pq = new PriorityQueue<>(k, (o1, o2) -> o2 - o1);
+		for (i = 0; i < len; i++) {
+			pq.offer(enemy[i]);
+			if ((n -= enemy[i]) < 0) {
+				if (--k >= 0) {
+					n += pq.poll();
+				} else {
+					break;
+				}
+			}
+		}
+		return i;
+	}
+}
