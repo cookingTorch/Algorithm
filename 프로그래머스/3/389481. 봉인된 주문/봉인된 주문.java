f672598087bf9579194c5d6cc396a1f69e4b5cf5
cookingTorch <jhw7181@naver.com>
2025-10-14 23:42:15 +0900

    [level 3] Title: 봉인된 주문, Time: 23.67 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.java
+++ 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.java
@@ -1,59 +1,63 @@
-import java.util.Arrays;
-
 class Solution {
-    private static final long DIFF = 'a';
-    private static final long ALPHA = 26L;
+	private static final int DIFF = 'a';
+	private static final int RADIX = 26;
 
-    private static long toNum(String str) {
-        int i;
-        int len;
-        long res;
+	private static long toNum(String str) {
+		int i;
+		int len;
+		long res;
 
-        res = 0L;
-        len = str.length();
-        for (i = 0; i < len; i++) {
-            res = res * ALPHA + str.charAt(i) - DIFF + 1;
-        }
-        return res;
-    }
+		res = 0L;
+		len = str.length();
+		for (i = 0; i < len; i++) {
+			res = res * RADIX + str.charAt(i) - DIFF + 1;
+		}
+		return res;
+	}
 
-    private static String toStr(long n) {
-        int i;
-        int len;
-        long tmp;
-        char[] res;
+	private static String toStr(long num) {
+		int i;
+		int len;
+		long tmp;
+		char[] res;
 
-        len = 0;
-        tmp = n;
-        while (tmp > 0L) {
-            tmp = (tmp - 1L) / ALPHA;
-            len++;
-        }
-        res = new char[len];
-        for (i = len - 1; i >= 0; i--) {
-            res[i] = (char) (--n % ALPHA + DIFF);
-            n /= ALPHA;
-        }
-        return new String(res);
-    }
+		len = 0;
+		for  (tmp = num; tmp > 0L; tmp = (tmp - 1L) / RADIX) {
+			len++;
+		}
+		res = new char[len];
+		for (i = len - 1; i >= 0; i--) {
+			res[i] = (char) (--num % RADIX + DIFF);
+			num /= RADIX;
+		}
+		return new String(res);
+	}
 
-    public String solution(long n, String[] bans) {
-        int i;
-        int len;
-        long[] arr;
+	public String solution(long n, String[] bans) {
+		int i;
+		int len;
+		int cnt;
+		long num;
+		long thr;
+		boolean[] arr;
 
-        len = bans.length;
-        arr = new long[len];
-        for (i = 0; i < len; i++) {
-            arr[i] = toNum(bans[i]);
-        }
-        Arrays.sort(arr, 0, len);
-        for (i = 0; i < len; i++) {
-            if (arr[i] > n) {
-                break;
-            }
-            n++;
-        }
-        return toStr(n);
-    }
+		len = bans.length;
+		cnt = 0;
+		thr = n + len;
+		arr = new boolean[len + 1];
+		for (i = 0; i < len; i++) {
+			num = toNum(bans[i]);
+			if (num <= n) {
+				cnt++;
+			} else if (num < thr) {
+				arr[(int) (num - n)] = true;
+			}
+		}
+		for (i = 1; i <= cnt; i++) {
+			if (arr[i]) {
+				cnt++;
+			}
+		}
+		return toStr(n + cnt);
+	}
 }
