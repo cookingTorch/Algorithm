4ebecdb25dfe60aebff62ac2b77b86e91fc93adf
cookingTorch <jhw7181@naver.com>
2025-10-15 00:21:19 +0900

    [level 3] Title: 산 모양 타일링, Time: 3.56 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -1,23 +1,17 @@
 class Solution {
-    private static final int MOD = 10_007;
+	private static final int MOD = 10_007;
 
-    public int solution(int n, int[] tops) {
-        int i;
-        int len;
-        int[] dp;
+	public int solution(int n, int[] tops) {
+		int i;
+		int cnt1;
+		int cnt2;
 
-        len = n + 1 << 1;
-        dp = new int[len];
-        dp[0] = 1;
-        dp[1] = 1;
-        for (i = 2; i < len; i += 2) {
-            if (tops[(i >>> 1) - 1] == 0) {
-                dp[i] = (dp[i - 2] + dp[i - 1]) % MOD;
-            } else {
-                dp[i] = (dp[i - 2] + (dp[i - 1] << 1)) % MOD;
-            }
-            dp[i + 1] = (dp[i] + dp[i - 1]) % MOD;
-        }
-        return dp[len - 1];
-    }
+		cnt1 = 1;
+		cnt2 = 1;
+		for (i = 0; i < n; i++) {
+			cnt1 = (cnt1 + (cnt2 << tops[i])) % MOD;
+			cnt2 = (cnt1 + cnt2) % MOD;
+		}
+		return cnt2;
+	}
 }
