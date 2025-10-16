be8dd92b6be9553310ebd5551b1b913c1900ecd5
cookingTorch <jhw7181@naver.com>
2025-10-17 02:29:39 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 12.74 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
@@ -1,40 +1,50 @@
 class Solution {
-    public int solution(int alp, int cop, int[][] problems) {
-        int i;
-        int j;
-        int ni;
-        int nj;
-        int len;
-        int maxAlp;
-        int maxCop;
-        int[][] dp;
+	private static final int ALP_REQ = 0;
+	private static final int COP_REQ = 1;
+	private static final int ALP_RWD = 2;
+	private static final int COP_RWD = 3;
+	private static final int COST = 4;
 
-        len = problems.length;
-        maxAlp = 0;
-        maxCop = 0;
-        for (i = 0; i < len; i++) {
-            maxAlp = Math.max(maxAlp, problems[i][0]);
-            maxCop = Math.max(maxCop, problems[i][1]);
-        }
-        alp = Math.min(alp, maxAlp);
-        cop = Math.min(cop, maxCop);
-        dp = new int[maxAlp + 1][maxCop + 1];
-        for (i = alp; i <= maxAlp; i++) {
-            for (j = cop; j <= maxCop; j++) {
-                dp[i][j] = i - alp + j - cop;
-            }
-        }
-        for (i = alp; i <= maxAlp; i++) {
-            for (j = cop; j <= maxCop; j++) {
-                for (int[] problem : problems) {
-                    if (i >= problem[0] && j >= problem[1]) {
-                        ni = Math.min(maxAlp, i + problem[2]);
-                        nj = Math.min(maxCop, j + problem[3]);
-                        dp[ni][nj] = Math.min(dp[ni][nj], dp[i][j] + problem[4]);
-                    }
-                }
-            }
-        }
-        return dp[maxAlp][maxCop];
-    }
+	public int solution(int alp, int cop, int[][] problems) {
+		int i;
+		int j;
+		int k;
+		int ni;
+		int nj;
+		int len;
+		int maxAlp;
+		int maxCop;
+		int[] problem;
+		int[][] dp;
+
+		len = problems.length;
+		maxAlp = 0;
+		maxCop = 0;
+		for (i = 0; i < len; i++) {
+			problem = problems[i];
+			problem[ALP_REQ] = Math.max(0, problem[ALP_REQ] - alp);
+			problem[COP_REQ] = Math.max(0, problem[COP_REQ] - cop);
+			maxAlp = Math.max(maxAlp, problem[ALP_REQ]);
+			maxCop = Math.max(maxCop, problem[COP_REQ]);
+		}
+		dp = new int[maxAlp + 1][maxCop + 1];
+		for (i = 0; i <= maxAlp; i++) {
+			for (j = 0; j <= maxCop; j++) {
+				dp[i][j] = i + j;
+			}
+		}
+		for (i = 0; i <= maxAlp; i++) {
+			for (j = 0; j <= maxCop; j++) {
+				for (k = 0; k < len; k++) {
+					problem = problems[k];
+					if (i >= problem[ALP_REQ] && j >= problem[COP_REQ]) {
+						ni = Math.min(maxAlp, i + problem[ALP_RWD]);
+						nj = Math.min(maxCop, j + problem[COP_RWD]);
+						dp[ni][nj] = Math.min(dp[ni][nj], dp[i][j] + problem[COST]);
+					}
+				}
+			}
+		}
+		return dp[maxAlp][maxCop];
+	}
 }
