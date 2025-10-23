df24746f1088a38d3467e9bbcaafa23139549c63
cookingTorch <jhw7181@naver.com>
2025-10-23 21:48:59 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 76.64 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,56 +1,49 @@
 class Solution {
-    private static int len;
-    private static int[] diff;
-    private static int[] time;
-    private static int[] retry;
-    private static long lmt;
-    private static long tot;
+	private static int len;
+	private static int[] diff;
+	private static int[] retry;
+	private static long lmt;
+	private static long total;
 
-    private static boolean validate(int lv) {
-        int i;
-        long sum;
+	private static int lowerBound(int r) {
+		int i;
+		int l;
+		int mid;
+		long sum;
 
-        sum = tot;
-        for (i = 0; i < len; i++) {
-            if (diff[i] > lv && (sum += retry[i] * (diff[i] - lv)) > lmt) {
-                return false;
-            }
-        }
-        return true;
-    }
+		l = 1;
+		while (l < r) {
+			mid = l + r >>> 1;
+			sum = total;
+			for (i = 0; i < len; i++) {
+				if (diff[i] > mid && (sum += retry[i] * (diff[i] - mid)) > lmt) {
+					break;
+				}
+			}
+			if (i == len) {
+				r = mid;
+			} else {
+				l = mid + 1;
+			}
+		}
+		return r;
+	}
 
-    private static int lowerBound(int r) {
-        int l;
-        int mid;
+	public int solution(int[] diffs, int[] times, long limit) {
+		int i;
+		int max;
 
-        l = 1;
-        while (l < r) {
-            mid = l + r >>> 1;
-            if (validate(mid)) {
-                r = mid;
-            } else {
-                l = mid + 1;
-            }
-        }
-        return r;
-    }
-
-    public int solution(int[] diffs, int[] times, long limit) {
-        int i;
-        int max;
-
-        len = times.length;
-        tot = times[0];
-        max = diffs[0];
-        retry = new int[len];
-        for (i = 1; i < len; i++) {
-            tot += times[i];
-            max = Math.max(max, diffs[i]);
-            retry[i] = times[i - 1] + times[i];
-        }
-        lmt = limit;
-        diff = diffs;
-        time = times;
-        return lowerBound(max + 1);
-    }
+		len = times.length;
+		total = times[0];
+		max = diffs[0];
+		retry = new int[len];
+		for (i = 1; i < len; i++) {
+			total += times[i];
+			max = Math.max(max, diffs[i]);
+			retry[i] = times[i - 1] + times[i];
+		}
+		lmt = limit;
+		diff = diffs;
+		return lowerBound(max + 1);
+	}
 }
