ad3f3c05df8727e6a398ec8b82162f36aa70f47c
cookingTorch <jhw7181@naver.com>
2025-10-23 21:15:33 +0900

    [level 3] Title: [1차] 추석 트래픽, Time: 7.52 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.java
+++ 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.java
@@ -1,39 +1,41 @@
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 class Solution {
-    private static final int HR = 60;
-    private static final int MIN = 60_000;
-    private static final int SEC = 1_000;
-    private static final double MS = 1_000.0;
-    private static final String DELIM = " :s";
+	private static final int RADIX = 10;
 
-    public int solution(String[] lines) {
-        int l;
-        int r;
-        int i;
-        int len;
-        int max;
-        int[] end;
-        int[] start;
-        StringTokenizer st;
+	private static int getS(String line) {
+		return Integer.parseInt(line, 11, 13, RADIX) * 3_600_000
+				+ Integer.parseInt(line, 14, 16, RADIX) * 60_000
+				+ Integer.parseInt(line, 17, 19, RADIX) * 1_000
+				+ Integer.parseInt(line, 20, 23, RADIX);
+	}
 
-        len = lines.length;
-        end = new int[len];
-        start = new int[len];
-        for (i = 0; i < len; i++) {
-            st = new StringTokenizer(lines[i], DELIM, false);
-            st.nextToken();
-            end[i] = (Integer.parseInt(st.nextToken()) * HR + Integer.parseInt(st.nextToken())) * MIN + (int) (Double.parseDouble(st.nextToken()) * MS);
-            start[i] = end[i] - (int) (Double.parseDouble(st.nextToken()) * MS) + 1;
-        }
-        Arrays.sort(end);
-        Arrays.sort(start);
-        max = 0;
-        for (l = 0, r = 0; r < len; r++) {
-            for (; start[r] - end[l] >= SEC; l++);
-            max = Math.max(max, r - l + 1);
-        }
-        return max;
-    }
+	private static int getT(String line) {
+		return (int) (Double.parseDouble(line.substring(24, line.length() - 1)) * 1_000.0);
+	}
+
+	public int solution(String[] lines) {
+		int l;
+		int r;
+		int i;
+		int len;
+		int max;
+		int[] end;
+		int[] start;
+
+		len = lines.length;
+		end = new int[len];
+		start = new int[len];
+		for (i = 0; i < len; i++) {
+			end[i] = getS(lines[i]);
+			start[i] = end[i] - getT(lines[i]) + 1;
+		}
+		Arrays.sort(start);
+		max = 0;
+		for (l = 0, r = 0; r < len; r++) {
+			for (; end[l] < start[r] - 999; l++);
+			max = Math.max(max, r - l + 1);
+		}
+		return max;
+	}
 }
