676229534de1ff22ba60bb25764c775a18111919
cookingTorch <jhw7181@naver.com>
2025-10-27 15:22:45 +0900

    [level 3] Title: 매칭 점수, Time: 4.06 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/3/42893. 매칭 점수/매칭 점수.java
+++ 프로그래머스/3/42893. 매칭 점수/매칭 점수.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * 각 페이지의 검색어 개수, URL, 링크 저장
+ * 페이지별 매칭점수 계산
+ * 매칭점수가 가장 높은 페이지 반환
+ */
 class Solution {
     private static final char A = 'A';
     private static final char Z = 'Z';
@@ -14,7 +19,7 @@ class Solution {
     private static final char[] CONTENT = "content=\"".toCharArray();
     private static final int NONE = -1;
     private static final int DIFF = a - A;
-    private static final int MAX_LEN = 15_001;
+    private static final int MAX_LEN = 1_501;
     private static final int META_LEN = META.length;
     private static final int HREF_LEN = HREF.length;
     private static final int CONTENT_LEN = CONTENT.length;
@@ -25,67 +30,65 @@ class Solution {
     private static char[] str;
     private static char[] page;
 
-    private static char lower(char ch) {
-        if (ch < a) {
-            return (char) (ch + DIFF);
+    private static char lower(char ch) { // 소문자 변환
+        if (ch < a) { // 대문자일 경우
+            return (char) (ch + DIFF); // 소문자로 변환하여 반환
         }
-        return ch;
+        return ch; // 소문자이면 그대로 반환
     }
 
-    private static boolean isLetter(char ch) {
-        return (A <= ch && ch <= Z) || (a <= ch && ch <= z);
+    private static boolean isLetter(char ch) { // 알파벳인지
+        return (A <= ch && ch <= Z) || (a <= ch && ch <= z); // A ~ Z 혹은 a ~ z
     }
 
-    private static boolean isStr(int i) {
+    private static boolean isStr(int i) { // 검색어인지 확인
         int j;
 
-        if (strStart == NONE) {
-            if (isLetter(page[i])) {
-                strStart = i;
+        if (strStart == NONE) { // 저장된 단어 시작 위치 없음
+            if (isLetter(page[i])) { // 현재 글자가 알파벳
+                strStart = i; // 현재 위치에서 단어가 시작
             }
-            return false;
-        } else if (!isLetter(page[i])) {
-            if (i - strStart != strLen) {
-                strStart = NONE;
-                return false;
+            return false; // 검색어 불일치
+        } else if (!isLetter(page[i])) { // 현재 글자가 알파벳이 아님
+            if (i - strStart != strLen) { // 저장된 단어 시작부터 현재 위치 전까지가 검색어 길이와 다름
+                strStart = NONE; // 단어 시작 위치 삭제
+                return false; // 검색어 불일치
             }
-            i = strStart;
-            strStart = NONE;
-            for (j = 0; j < strLen; j++) {
-                if (lower(page[i + j]) != str[j]) {
-                    return false;
+            i = strStart; // 저장된 단어 시작부터 비교
+            strStart = NONE; // 단어 시작 위치 삭제
+            for (j = 0; j < strLen; j++) { // 검색어와 글자 비교
+                if (lower(page[i + j]) != str[j]) { // 검색어와 다름
+                    return false; // 검색어 불일치
                 }
             }
-            return true;
+            return true; // 검색어 일치
         }
-        return false;
+        return false; // 검색어 불일치
     }
 
-    private static boolean isPattern(int i, char[] pat, int patLen) {
+    private static boolean isPattern(int i, char[] pat, int patLen) { // 패턴과 일치하는지 확인
         int j;
 
-        if (i + patLen > pageLen) {
+        if (i + patLen > pageLen) { // 페이지를 벗어남
             return false;
         }
-        for (j = 0; j < patLen; j++) {
-            if (page[i + j] != pat[j]) {
+        for (j = 0; j < patLen; j++) { // 패턴과 글자 비교
+            if (page[i + j] != pat[j]) { // 패턴 불일치
                 return false;
             }
         }
-        return true;
+        return true; // 패턴 일치
     }
 
-    private static String getUrl(int i) {
-        StringBuilder sb;
+    private static String getUrl(int start) { // URL 추출
+        int i;
 
-        sb = new StringBuilder();
-        for (; i < pageLen; i++) {
-            if (page[i] == QUOTE) {
+        for (i = start; i < pageLen; i++) { // 다음 쌍따옴표 전까지 기록
+            if (page[i] == QUOTE) { // 쌍따옴표 등장
                 break;
             }
-            sb.append(page[i]);
         }
-        return sb.toString();
+        return new String(page, start, i - start);
     }
 
     public int solution(String word, String[] pages) {
@@ -101,60 +104,60 @@ class Solution {
         ArrayList<String>[] links;
         HashMap<String, Integer> map;
 
-        str = word.toCharArray();
-        strLen = str.length;
-        for (i = 0; i < strLen; i++) {
+        str = word.toCharArray(); // 검색어
+        strLen = str.length; // 검색어 길이
+        for (i = 0; i < strLen; i++) { // 검색어 소문자 변환
             str[i] = lower(str[i]);
         }
-        pagesLen = pages.length;
-        strCnt = new int[pagesLen];
-        links = new ArrayList[pagesLen];
-        map = new HashMap<>();
-        page = new char[MAX_LEN];
-        for (idx = 0; idx < pagesLen; idx++) {
-            pageLen = pages[idx].length();
-            pages[idx].getChars(0, pageLen, page, 0);
-            page[pageLen++] = NULL;
-            links[idx] = new ArrayList<>();
+        pagesLen = pages.length; // 페이지 개수
+        strCnt = new int[pagesLen]; // 각 페이지의 검색어 개수
+        links = new ArrayList[pagesLen]; // 각 페이지의 링크들
+        map = new HashMap<>(); // 링크별 페이지 번호
+        page = new char[MAX_LEN]; // 페이지 내용
+        for (idx = 0; idx < pagesLen; idx++) { // 각 페이지 순회
+            pageLen = pages[idx].length(); // 페이지 내용 길이
+            pages[idx].getChars(0, pageLen, page, 0); // 페이지 내용 복사
+            page[pageLen++] = NULL; // 마지막에 NULL 문자 삽입
+            links[idx] = new ArrayList<>(); // 페이지의 링크들
             strStart = NONE;
-            for (i = 0; i < pageLen; i++) {
-                if (isStr(i)) {
-                    strCnt[idx]++;
+            for (i = 0; i < pageLen; i++) { // 페이지 내용 읽기
+                if (isStr(i)) { // 검색어이면
+                    strCnt[idx]++; // 검색어 개수 추가
                 }
-                if (isPattern(i, META, META_LEN)) {
-                    for (j = i + META_LEN; j < pageLen; j++) {
-                        if (page[j] == BRACE) {
+                if (isPattern(i, META, META_LEN)) { // meta 태그이면
+                    for (j = i + META_LEN; j < pageLen; j++) { // meta 태그 이후부터 순회
+                        if (page[j] == BRACE) { // 태그 닫힘
                             break;
                         }
-                        if (isPattern(j, CONTENT, CONTENT_LEN)) {
-                            map.put(getUrl(j + CONTENT_LEN), idx);
+                        if (isPattern(j, CONTENT, CONTENT_LEN)) { // content가 존재하면
+                            map.put(getUrl(j + CONTENT_LEN), idx); // 현재 페이지의 URL 저장
                             break;
                         }
                     }
                 }
-                if (isPattern(i, HREF, HREF_LEN)) {
-                    links[idx].add(getUrl(i + HREF_LEN));
+                if (isPattern(i, HREF, HREF_LEN)) { // a 태그의 href 이면
+                    links[idx].add(getUrl(i + HREF_LEN)); // 링크 추가
                 }
             }
         }
-        scores = new double[pagesLen];
-        for (idx = 0; idx < pagesLen; idx++) {
-            scores[idx] += strCnt[idx];
-            linkScore = (double) strCnt[idx] / links[idx].size();
-            for (String link : links[idx]) {
-                if (map.containsKey(link)) {
-                    scores[map.get(link)] += linkScore;
+        scores = new double[pagesLen]; // 매칭점수
+        for (idx = 0; idx < pagesLen; idx++) { // 각 페이지
+            scores[idx] += strCnt[idx]; // 기본점수
+            linkScore = (double) strCnt[idx] / links[idx].size(); // (기본점수 / 링크 수)
+            for (String link : links[idx]) { // 링크 순회
+                if (map.containsKey(link)) { // 링크에 해당하는 웹 페이지 존재
+                    scores[map.get(link)] += linkScore; // 해당하는 웹 페이지의 링크점수 추가
                 }
             }
         }
-        ans = 0;
-        max = 0.0;
-        for (idx = 0; idx < pagesLen; idx++) {
-            if (scores[idx] > max) {
-                ans = idx;
-                max = scores[idx];
+        ans = 0; // 매칭 점수가 가장 높은 페이지
+        max = 0.0; // 최대 매칭 점수
+        for (idx = 0; idx < pagesLen; idx++) { // 매칭 점수가 가장 높은 페이지 번호 계산
+            if (scores[idx] > max) { // 최대 매칭 점수보다 높은 페이지
+                ans = idx; // 매칭 점수가 가장 높은 페이지 업데이트
+                max = scores[idx]; // 최대 매칭 점수 업데이트
             }
         }
-        return ans;
+        return ans; // 매칭 점수가 가장 높은 페이지 반환
     }
 }
