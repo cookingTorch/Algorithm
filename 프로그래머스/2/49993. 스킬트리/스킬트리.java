e269989ee8009447694b3cde43f75f8c9c158242
cookingTorch <jhw7181@naver.com>
2025-10-29 16:59:13 +0900

    [level 2] Title: 스킬트리, Time: 0.04 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,46 +1,46 @@
 class Solution {
-    private static final int ALPH = 26;
-    private static final int DIFF = 'A';
+    private static final int SIZE = 'Z' + 1;
+    private static final int OFFSET = 'A';
+    private static final int LENGTH = SIZE - OFFSET;
 
     public int solution(String skill, String[] skill_trees) {
         int i;
         int j;
         int ch;
+        int len;
         int idx;
         int cur;
         int cnt;
-        int len;
         int size;
-        int skillLen;
-        boolean[] arr;
+        int[] pat;
         boolean[] init;
-        String tree;
+        boolean[] check;
+        String str;
 
-        init = new boolean[ALPH];
-        arr = new boolean[ALPH];
-        skillLen = skill.length();
-        for (i = 1; i < skillLen; i++) {
-            init[skill.charAt(i) - DIFF] = true;
+        len = skill.length();
+        init = new boolean[SIZE];
+        check = new boolean[SIZE];
+        pat = new int[len + 1];
+        pat[0] = skill.charAt(0);
+        for (i = 1; i < len; i++) {
+            init[pat[i] = skill.charAt(i)] = true;
         }
         cnt = 0;
         size = skill_trees.length;
-        loop:
         for (i = 0; i < size; i++) {
-            tree = skill_trees[i];
+            str = skill_trees[i];
+            len = str.length();
             idx = 0;
-            cur = skill.charAt(idx) - DIFF;
-            System.arraycopy(init, 0, arr, 0, ALPH);
-            len = tree.length();
+            cur = pat[idx];
+            System.arraycopy(init, OFFSET, check, OFFSET, LENGTH);
             for (j = 0; j < len; j++) {
-                ch = tree.charAt(j) - DIFF;
-                if (arr[ch]) {
-                    continue loop;
+                ch = str.charAt(j);
+                if (check[ch]) {
+                    break;
                 }
                 if (ch == cur) {
-                    if (++idx < skillLen) {
-                        cur = skill.charAt(idx) - DIFF;
-                        arr[cur] = false;
-                    }
+                    cur = pat[++idx];
+                    check[cur] = false;
                 }
             }
             if (j == len) {
