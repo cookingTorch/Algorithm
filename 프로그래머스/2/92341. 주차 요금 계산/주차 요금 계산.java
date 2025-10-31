eb91e2b2f141662f06ad076dba1452760de1caed
cookingTorch <jhw7181@naver.com>
2025-10-31 09:29:34 +0900

    [level 2] Title: 주차 요금 계산, Time: 10.44 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,103 +1,91 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
 class Solution {
-	private static final int IN = 2;
-	private static final int DIFF = '0';
+    private static final String DELIM = " :";
 
-	private static final class Car implements Comparable<Car> {
-		private static final int NIL = -1;
-		private static final int MAX = 23 * 60 + 59;
+    private static final class Car implements Comparable<Car> {
+        private static final int IN = 2;
+        private static final int NIL = -1;
+        private static final int MAX = timeToInt("23", "59");
 
-		private static int time1;
-		private static int fee1;
-		private static int time2;
-		private static int fee2;
+        int in;
+        int sum;
+        int num;
 
-		int in;
-		int num;
-		int time;
+        Car(String h, String m, int num) {
+            in = timeToInt(h, m);
+            this.num = num;
+        }
 
-		Car(int num, int in) {
-			this.time = 0;
-			this.in = in;
-			this.num = num;
-		}
+        private static int timeToInt(String h, String m) {
+            return Integer.parseInt(h) * 60 + Integer.parseInt(m);
+        }
 
-		void in(int in) {
-			this.in = in;
-		}
+        private void in(String h, String m) {
+            in = timeToInt(h, m);
+        }
 
-		void out(int out) {
-			this.time += out - in;
-			in = NIL;
-		}
+        private void out(String h, String m) {
+            sum += timeToInt(h, m) - in;
+            in = NIL;
+        }
 
-		int calc() {
-			if (in != NIL) {
-				time += MAX - in;
-			}
-			if (time <= time1) {
-				return fee1;
-			}
-			return fee1 + ((time - time1 - 1) / time2 + 1) * fee2;
-		}
+        void inOut(String h, String m, int io) {
+            if (io == IN) {
+                in(h, m);
+            } else {
+                out(h, m);
+            }
+        }
 
-		@Override
-		public int compareTo(Car o) {
-			return num - o.num;
-		}
-	}
+        int getSum() {
+            if (in != NIL) {
+                sum += MAX - in;
+            }
+            return sum;
+        }
 
-	private static int toMin(String time) {
-		return ((time.charAt(0) - DIFF) * 10 + time.charAt(1) - DIFF) * 60 + (time.charAt(3) - DIFF) * 10 + time.charAt(4) - DIFF;
-	}
+        @Override
+        public int compareTo(Car o) {
+            return num - o.num;
+        }
+    }
+    public int[] solution(int[] fees, String[] records) {
+        int i;
+        int io;
+        int len;
+        int num;
+        int[] ans;
+        String h;
+        String m;
+        HashMap<Integer, Car> map;
+        ArrayList<Car> cars;
+        StringTokenizer st;
 
-	public int[] solution(int[] fees, String[] records) {
-		int i;
-		int len;
-		int num;
-		int time;
-		int size;
-		int inOut;
-		int[] ans;
-		Car[] arr;
-		HashMap<Integer, Car> map;
-		StringTokenizer st;
-
-		Car.time1 = fees[0];
-		Car.fee1 = fees[1];
-		Car.time2 = fees[2];
-		Car.fee2 = fees[3];
-		map = new HashMap<>();
-		len = records.length;
-		for (i = 0; i < len; i++) {
-			st = new StringTokenizer(records[i]);
-			time = toMin(st.nextToken());
-			num = Integer.parseInt(st.nextToken());
-			inOut = st.nextToken().length();
-			if (inOut == IN) {
-				if (map.containsKey(num)) {
-					map.get(num).in(time);
-				} else {
-					map.put(num, new Car(num, time));
-				}
-			} else {
-				map.get(num).out(time);
-			}
-		}
-		size = map.size();
-		arr = new Car[size];
-		i = 0;
-		for (Car car : map.values()) {
-			arr[i++] = car;
-		}
-		Arrays.sort(arr);
-		ans = new int[size];
-		for (i = 0; i < size; i++) {
-			ans[i] = arr[i].calc();
-		}
-		return ans;
-	}
+        map = new HashMap<>();
+        len = records.length;
+        for (i = 0; i < len; i++) {
+            st = new StringTokenizer(records[i], DELIM, false);
+            h = st.nextToken();
+            m = st.nextToken();
+            num = Integer.parseInt(st.nextToken());
+            io = st.nextToken().length();
+            if (map.containsKey(num)) {
+                map.get(num).inOut(h, m, io);
+            } else {
+                map.put(num, new Car(h, m, num));
+            }
+        }
+        cars = new ArrayList<>(map.values());
+        Collections.sort(cars);
+        len = cars.size();
+        ans = new int[len];
+        for (i = 0; i < len; i++) {
+            ans[i] = fees[1] + (int) Math.ceil((double) Math.max(0, (cars.get(i).getSum() - fees[0])) / fees[2]) * fees[3];
+        }
+        return ans;
+    }
 }
