967960a01242b7335039d98264fabbbda2cc796a
cookingTorch <jhw7181@naver.com>
2025-10-31 10:07:43 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 4.40 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,11 +1,15 @@
 class Solution {
+    private static long f(long x) {
+        return (x & 1L) == 0 ? x | 1L : x ^ 3L << Long.numberOfTrailingZeros(~x & x + 1L) - 1;
+    }
+
     public long[] solution(long[] numbers) {
         int i;
         int len;
 
         len = numbers.length;
         for (i = 0; i < len; i++) {
-            numbers[i] = (numbers[i] & 1L) == 0 ? numbers[i] | 1L : numbers[i] ^ 3L << Long.numberOfTrailingZeros(~numbers[i] & numbers[i] + 1L) - 1;
+            numbers[i] = f(numbers[i]);
         }
         return numbers;
     }
