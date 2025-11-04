65cb74820131743c1fc380c9623b75a3bf5608c5
cookingTorch <jhw7181@naver.com>
2025-11-04 17:52:52 +0900

    [level 3] Title: 금과 은 운반하기, Time: 82.18 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.java
+++ 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.java
@@ -1,47 +1,61 @@
 class Solution {
     private static final long MAX = 399_999_999_900_000L;
 
-    public long solution(int a, int b, int[] g, int[] s, int[] w, int[] t) {
-        int c;
+    private static long lowerBound(long ab, long a, long b, int len, long[] gs, long[] g, long[] s, long[] w, long[] t, long r) {
         int i;
-        int len;
-        int[] gs;
         long l;
-        long r;
         long mid;
         long sum;
         long gold;
         long silver;
         long weight;
 
-        len = t.length;
-        c = a + b;
-        gs = new int[len];
-        for (i = 0; i < len; i++) {
-            gs[i] = g[i] + s[i];
-        }
         l = 0L;
-        r = MAX;
         while (l < r) {
             mid = l + r >>> 1;
+            sum = 0L;
             gold = 0L;
             silver = 0L;
-            sum = 0L;
             for (i = 0; i < len; i++) {
-                weight = w[i] * (mid / t[i] + 1 >>> 1);
+                weight = w[i] * (mid / t[i] + 1L >>> 1);
+                sum += Math.min(gs[i], weight);
                 gold += Math.min(g[i], weight);
                 silver += Math.min(s[i], weight);
-                sum += Math.min(gs[i], weight);
-                if (gold >= a && silver >= b && sum >= c) {
+                if (sum >= ab && gold >= a && silver >= b) {
                     break;
                 }
             }
             if (i == len) {
-                l = mid + 1;
+                l = mid + 1L;
             } else {
                 r = mid;
             }
         }
         return r;
+    }
+
+    public long solution(int a, int b, int[] g, int[] s, int[] w, int[] t) {
+        int i;
+        int len;
+        long ab;
+        long[] time;
+        long[] gold;
+        long[] silver;
+        long[] weight;
+        long[] goldSilver;
+
+        ab = a + b;
+        len = w.length;
+        gold = new long[len];
+        silver = new long[len];
+        goldSilver = new long[len];
+        weight = new long[len];
+        time = new long[len];
+        for (i = 0; i < len; i++) {
+            goldSilver[i] = (gold[i] = g[i]) + (silver[i] = s[i]);
+            weight[i] = w[i];
+            time[i] = t[i];
+        }
+        return lowerBound(ab, a, b, len, goldSilver, gold, silver, weight, time, MAX);
     }
 }
