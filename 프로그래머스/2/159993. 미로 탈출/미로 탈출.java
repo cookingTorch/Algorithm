b8a3791e76bf855f7c3065f23a9a897fec2b3aa6
cookingTorch <jhw7181@naver.com>
2025-11-08 22:46:48 +0900

    [level 2] Title: 미로 탈출, Time: 7.20 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,86 +1,85 @@
 import java.util.ArrayDeque;
 
 class Solution {
-    private static final int S = 'S';
-    private static final int L = 'L';
-    private static final int E = 'E';
-    private static final int WALL = 'X';
-    private static final int FAIL = -1;
-    private static final int[] dx = {-1, 0, 1, 0};
-    private static final int[] dy = {0, 1, 0, -1};
+	private static final int S = 'S';
+	private static final int L = 'L';
+	private static final int E = 'E';
+	private static final int WALL = 'X';
+	private static final int NULL = -1;
+	private static final int[] dx = {-1, 0, 1, 0};
+	private static final int[] dy = {0, 1, 0, -1};
 
-    private static int r;
-    private static int c;
-    private static int ex;
-    private static int ey;
-    private static char[][] map;
+	private static int bfs(char[][] map, int r, int c, int x, int y) {
+		int i;
+		int nx;
+		int ny;
+		int dist;
+		int dest;
+		int[] cur;
+		boolean[][] visited;
+		ArrayDeque<int[]> q;
 
-    private static int bfs(int x, int y, int dest, ArrayDeque<int[]> q) {
-        int i;
-        int nx;
-        int ny;
-        int dist;
-        int[] cur;
-        boolean[][] visited;
+		dest = L;
+		q = new ArrayDeque<>();
+		q.addLast(new int[] {x, y, 0});
+		visited = new boolean[r][c];
+		visited[x][y] = true;
+		while (!q.isEmpty()) {
+			cur = q.pollFirst();
+			x = cur[0];
+			y = cur[1];
+			dist = cur[2] + 1;
+			for (i = 0; i < 4; i++) {
+				nx = x + dx[i];
+				ny = y + dy[i];
+				if (nx < 0 || nx >= r || ny < 0 || ny >= c || visited[nx][ny] || map[nx][ny] == WALL) {
+					continue;
+				}
+				if (map[nx][ny] == dest) {
+					if (dest == L) {
+						dest = E;
+						q.clear();
+						q.addLast(new int[] {nx, ny, dist});
+						visited = new boolean[r][c];
+						visited[nx][ny] = true;
+						break;
+					} else {
+						return dist;
+					}
+				}
+				q.addLast(new int[] {nx, ny, dist});
+				visited[nx][ny] = true;
+			}
+		}
+		return NULL;
+	}
 
-        visited = new boolean[r][c];
-        q.addLast(new int[] {x, y, 0});
-        visited[x][y] = true;
-        while (!q.isEmpty()) {
-            cur = q.pollFirst();
-            x = cur[0];
-            y = cur[1];
-            dist = cur[2] + 1;
-            for (i = 0; i < 4; i++) {
-                nx = x + dx[i];
-                ny = y + dy[i];
-                if (nx < 0 || nx >= r || ny < 0 || ny >= c || visited[nx][ny] || map[nx][ny] == WALL) {
-                    continue;
-                }
-                if (map[nx][ny] == dest) {
-                    ex = nx;
-                    ey = ny;
-                    return dist;
-                }
-                q.addLast(new int[] {nx, ny, dist});
-                visited[nx][ny] = true;
-            }
-        }
-        return FAIL;
-    }
+	public int solution(String[] maps) {
+		int i;
+		int j;
+		int r;
+		int c;
+		int x;
+		int y;
+		char[] row;
+		char[][] map;
 
-    public int solution(String[] maps) {
-        int i;
-        int j;
-        int x;
-        int y;
-        int dist1;
-        int dist2;
-        char[] row;
-        ArrayDeque<int[]> q;
-
-        x = 0;
-        y = 0;
-        r = maps.length;
-        c = maps[0].length();
-        map = new char[r][];
-        for (i = 0; i < r; i++) {
-            map[i] = row = maps[i].toCharArray();
-            for (j = 0; j < c; j++) {
-                if (row[j] == S) {
-                    x = i;
-                    y = j;
-                }
-            }
-        }
-        q = new ArrayDeque<>();
-        if ((dist1 = bfs(x, y, L, q)) == FAIL) {
-            return FAIL;
-        }
-        q.clear();
-        if ((dist2 = bfs(ex, ey, E, q)) == FAIL) {
-            return FAIL;
-        }
-        return dist1 + dist2;
-    }
+		x = NULL;
+		y = NULL;
+		r = maps.length;
+		c = maps[0].length();
+		map = new char[r][];
+		for (i = 0; i < r; i++) {
+			map[i] = row = maps[i].toCharArray();
+			if (x == NULL) {
+				for (j = 0; j < c; j++) {
+					if (row[j] == S) {
+						x = i;
+						y = j;
+					}
+				}
+			}
+		}
+		return bfs(map, r, c, x, y);
+	}
 }
