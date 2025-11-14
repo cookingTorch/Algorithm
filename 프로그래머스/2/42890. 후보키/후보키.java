57c58e1a87640d5504e2438ecd68abdc098880fb
cookingTorch <jhw7181@naver.com>
2025-11-14 09:37:27 +0900

    [level 2] Title: 후보키, Time: 1.46 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.java
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 class Solution {
-    private static int r;
+    private static int n;
     private static String[][] table;
 
     private static boolean isUnique(int mask) {
@@ -14,7 +14,7 @@ class Solution {
 
         set = new HashSet<>();
         size = Integer.bitCount(mask);
-        for (i = 0; i < r; i++) {
+        for (i = 0; i < n; i++) {
             list = new ArrayList<>(size);
             for (j = mask; j != 0; j &= j - 1) {
                 list.add(table[i][Integer.numberOfTrailingZeros(j)]);
@@ -27,32 +27,38 @@ class Solution {
     }
 
     public int solution(String[][] relation) {
+        int m;
+        int r;
         int c;
-        int i;
-        int j;
-        int cnt;
-        int full;
-        int rest;
-        boolean[] dp;
+        int ans;
+        int thr;
+        int len;
+        int sub;
+        int mask;
+        boolean[] visited;
 
-        r = relation.length;
-        c = relation[0].length;
         table = relation;
-        cnt = 0;
-        full = (1 << c) - 1;
-        dp = new boolean[full + 1];
-        for (i = 1; i <= full; i++) {
-            if (dp[i]) {
-                continue;
-            }
-            if (isUnique(i)) {
-                cnt++;
-                rest = full ^ i;
-                for (j = rest; j != 0; j = (j - 1) & rest) {
-                    dp[i | j] = true;
+        n = relation.length;
+        m = relation[0].length;
+        thr = 1 << m;
+        ans = 0;
+        visited = new boolean[thr];
+        for (len = 1; len <= m; len++) {
+            for (mask = (1 << len) - 1; mask < thr;) {
+                for (sub = mask; sub != 0; sub = (sub - 1) & mask) {
+                    if (visited[sub]) {
+                        break;
+                    }
                 }
+                if (sub == 0 && isUnique(mask)) {
+                    ans++;
+                    visited[mask] = true;
+                }
+                c = mask & -mask;
+                r = mask + c;
+                mask = ((r ^ mask) >>> 2) / c | r;
             }
         }
-        return cnt;
+        return ans;
     }
 }
