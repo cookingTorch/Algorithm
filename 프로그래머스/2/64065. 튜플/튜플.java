1962a51507bfadc9abf4dbdfcf3fcc70f3c989f0
cookingTorch <jhw7181@naver.com>
2025-11-24 13:11:26 +0900

    [level 2] Title: 튜플, Time: 27.68 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,46 +1,24 @@
+import java.util.StringTokenizer;
+
 class Solution {
-    private static final int COMMA = ',';
-    private static final int END = '}';
-    private static final int DIFF = '0';
     private static final int MAX = 100_000;
-
-    private static int size;
-
-    private static int[] count(String s) {
-        int i;
-        int ch;
-        int num;
-        int len;
-        int[] cnt;
-
-        size = 0;
-        cnt = new int[MAX + 1];
-        len = s.length() - 1;
-        num = 0;
-        for (i = 2; i < len; i++) {
-            ch = s.charAt(i);
-            switch (ch) {
-                case END:
-                    i += 2;
-                case COMMA:
-                    if (cnt[num]++ == 0) {
-                        size++;
-                    }
-                    num = 0;
-                    break;
-                default:
-                    num = num * 10 + ch - DIFF;
-            }
-        }
-        return cnt;
-    }
+    private static final String DELIM = "{,}";
 
     public int[] solution(String s) {
         int i;
+        int size;
         int[] cnt;
         int[] ans;
+        StringTokenizer st;
 
-        cnt = count(s);
+        size = 0;
+        cnt = new int[MAX + 1];
+        st = new StringTokenizer(s, DELIM, false);
+        while (st.hasMoreTokens()) {
+            if (cnt[Integer.parseInt(st.nextToken())]++ == 0) {
+                size++;
+            }
+        }
         ans = new int[size];
         for (i = 1; i <= MAX; i++) {
             if (cnt[i] != 0) {
