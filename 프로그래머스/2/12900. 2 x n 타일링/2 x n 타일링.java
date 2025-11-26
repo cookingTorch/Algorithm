c55387a9815358b04a4c6d9660bd1669f4869f40
cookingTorch <jhw7181@naver.com>
2025-11-26 10:05:26 +0900

    [level 2] Title: 2 x n 타일링, Time: 0.05 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -1,16 +1,32 @@
 class Solution {
-    private static final int MOD = 1_000_000_007;
+    private static final long MOD = 1_000_000_007L;
+    private static final long[][] FIRST = {{1L, 1L}, {1L, 0L}};
+
+    private static long[][] multiply(long[][] a, long[][] b) {
+        long[][] c;
+
+        c = new long[2][2];
+        c[0][0] = ((a[0][0] * b[0][0]) + (a[0][1] * b[1][0])) % MOD;
+        c[0][1] = ((a[0][0] * b[0][1]) + (a[0][1] * b[1][1])) % MOD;
+        c[1][0] = ((a[1][0] * b[0][0]) + (a[1][1] * b[1][0])) % MOD;
+        c[1][1] = ((a[1][0] * b[0][1]) + (a[1][1] * b[1][1])) % MOD;
+        return c;
+    }
+
+    private static long[][] power(long[][] matrix, long n) {
+        long[][] sqrt;
+
+        if (n == 1) {
+            return matrix;
+        }
+        if ((n & 1) == 0) {
+            sqrt = power(matrix, n >> 1);
+            return multiply(sqrt, sqrt);
+        }
+        return multiply(power(matrix, n - 1), FIRST);
+    }
 
     public int solution(int n) {
-        int i;
-        int[] dp;
-
-        dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
-        for (i = 2; i <= n; i++) {
-            dp[i] = (dp[i - 2] + dp[i - 1]) % MOD;
-        }
-        return dp[n];
+        return (int) power(FIRST, n + 1)[0][1];
     }
 }
