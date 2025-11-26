3e276d8b4a3c86db67c8a945bed6b430f39d79a1
cookingTorch <jhw7181@naver.com>
2025-11-26 09:14:53 +0900

    [level 2] Title: 완전범죄, Time: 0.17 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -1,26 +1,26 @@
 class Solution {
-    private static final int FAIL = -1;
-
     public int solution(int[][] info, int n, int m) {
         int i;
         int j;
         int len;
         int sum;
-        int val;
+        int value;
         int weight;
         int[] dp;
-
+        
+        m--;
         sum = 0;
-        dp = new int[m];
+        dp = new int[m + 1];
         len = info.length;
         for (i = 0; i < len; i++) {
-            val = info[i][0];
+            value = info[i][0];
             weight = info[i][1];
-            sum += val;
-            for (j = m - 1; j >= weight; j--) {
-                dp[j] = Math.max(dp[j], dp[j - weight] + val);
+            sum += value;
+            for (j = m; j >= weight; j--) {
+                dp[j] = Math.max(dp[j], dp[j - weight] + value);
             }
         }
-        return sum - dp[m - 1] < n ? sum - dp[m - 1] : FAIL;
+        sum -= dp[m];
+        return sum >= n ? -1 : sum;
     }
 }
