8c63ebb264ee2e39862e1a60136e9b9ad972d076
cookingTorch <jhw7181@naver.com>
2025-11-27 13:45:45 +0900

    [level 2] Title: 2 x n 타일링, Time: 0.05 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -1,26 +1,32 @@
 class Solution {
     private static final long MOD = 1_000_000_007;
+    private static final long[][] MATRIX = {{1L, 1L}, {1L, 0L}};
+
+    private static long[][] multiply(long[][] mtx1, long[][] mtx2) {
+        long[][] res;
+
+        res = new long[2][2];
+        res[0][0] = (mtx1[0][0] * mtx2[0][0] + mtx1[0][1] * mtx2[1][0]) % MOD;
+        res[0][1] = (mtx1[0][0] * mtx2[0][1] + mtx1[0][1] * mtx2[1][1]) % MOD;
+        res[1][0] = (mtx1[1][0] * mtx2[0][0] + mtx1[1][1] * mtx2[1][0]) % MOD;
+        res[1][1] = (mtx1[1][0] * mtx2[0][1] + mtx1[1][1] * mtx2[1][1]) % MOD;
+        return res;
+    }
+
+    private static long[][] power(long n) {
+        long[][] sqrt;
+
+        if (n == 1) {
+            return MATRIX;
+        }
+        sqrt = power(n >>> 1);
+        if ((n & 1) == 0) {
+            return multiply(sqrt, sqrt);
+        }
+        return multiply(multiply(sqrt, sqrt), MATRIX);
+    }
 
     public int solution(int n) {
-        int i;
-        long fk;
-        long fk1;
-        long tmp;
-        long tmp1;
-
-        fk = 0L;
-        fk1 = 1L;
-        for (i = Integer.highestOneBit(n); i != 0; i >>= 1) {
-            tmp = fk * ((fk1 << 1) - fk) % MOD;
-            tmp1 = (fk1 * fk1 + fk * fk) % MOD;
-            fk = tmp;
-            fk1 = tmp1;
-            if ((n & i) != 0) {
-                tmp = fk1 + fk;
-                fk = fk1;
-                fk1 = tmp;
-            }
-        }
-        return (int) (fk1 % MOD);
+        return (int) power(n)[0][0];
     }
 }
