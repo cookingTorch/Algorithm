9d90cce0750280238dc85e0d6eb1350a4ed36b45
cookingTorch <jhw7181@naver.com>
2025-12-02 13:00:59 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,38 +1,36 @@
 class Solution {
-	private static final int A = 'A';
-	private static final int LEN = A + 26;
+    private static final int A = 'A';
+    private static final int LEN = A + 26;
 
-	public int solution(String name) {
-		int i;
-		int min;
-		int len;
-		int last;
-		int move;
-		char[] str;
+    public int solution(String name) {
+        int i;
+        int h;
+        int v;
+        int ch;
+        int len;
+        int prev;
 
-		str = name.toCharArray();
-		len = str.length;
-		move = 0;
-		last = 0;
-		for (i = 0; i < len; i++) {
-			if (str[i] != A) {
-				move += Math.min(str[i] - A, LEN - str[i]);
-				last = i;
-				break;
-			}
-		}
-		if (i == len) {
-			return 0;
-		}
-		min = len - last;
-		for (++i; i < len; i++) {
-			if (str[i] != A) {
-				move += Math.min(str[i] - A, LEN - str[i]);
-				min = Math.min(min, Math.min((last << 1) + len - i, ((len - i) << 1) + last));
-				last = i;
-			}
-		}
-		min = Math.min(min, last);
-		return move + min;
-	}
+        len = name.length();
+        v = 0;
+        prev = 0;
+        for (i = 0; i < len; i++) {
+            if ((ch = name.charAt(i)) != A) {
+                v += Math.min(ch - A, LEN - ch);
+                prev = i;
+                break;
+            }
+        }
+        if (i == len) {
+            return 0;
+        }
+        h = len - prev;
+        for (++i; i < len; i++) {
+            if ((ch = name.charAt(i)) != A) {
+                v += Math.min(ch - A, LEN - ch);
+                h = Math.min(h, Math.min((prev << 1) + len - i, ((len - i) << 1) + prev));
+                prev = i;
+            }
+        }
+        return v + Math.min(h, prev);
+    }
 }
