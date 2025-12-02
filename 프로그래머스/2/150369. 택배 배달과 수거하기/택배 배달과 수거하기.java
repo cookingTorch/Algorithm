33497d9e554dc0df953f353f760602de67cddedd
cookingTorch <jhw7181@naver.com>
2025-12-02 14:46:28 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 7.33 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,27 +1,39 @@
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
-        int pIdx;
-        int dIdx;
-        int remain;
+        int i;
+        int cnt;
+        int max;
+        int diff;
+        int dCnt;
+        int pCnt;
+        int dTmp;
+        int pTmp;
         long dist;
 
-        for (dIdx = n - 1; dIdx >= 0 && deliveries[dIdx] == 0; dIdx--);
-        for (pIdx = n - 1; pIdx >= 0 && pickups[pIdx] == 0; pIdx--);
-        dist = 0;
-        while (dIdx >= 0 || pIdx >= 0) {
-            dist += Math.max(dIdx, pIdx) + 1 << 1;
-            for (remain = cap; dIdx >= 0; remain -= deliveries[dIdx--]) {
-                if (deliveries[dIdx] > remain) {
-                    deliveries[dIdx] -= remain;
-                    break;
-                }
+        dist = 0L;
+        cnt = 0;
+        dCnt = 0;
+        dTmp = 0;
+        pCnt = 0;
+        pTmp = 0;
+        for (i = n - 1; i >= 0; i--) {
+            dTmp -= deliveries[i];
+            if (dTmp < 0) {
+                dTmp = -dTmp;
+                diff = (dTmp + cap - 1) / cap;
+                dCnt += diff;
+                dTmp = diff * cap - dTmp;
             }
-            for (remain = cap; pIdx >= 0; remain -= pickups[pIdx--]) {
-                if (pickups[pIdx] > remain) {
-                    pickups[pIdx] -= remain;
-                    break;
-                }
+            pTmp -= pickups[i];
+            if (pTmp < 0) {
+                pTmp = -pTmp;
+                diff = (pTmp + cap - 1) / cap;
+                pCnt += diff;
+                pTmp = diff * cap - pTmp;
             }
+            max = Math.max(dCnt, pCnt);
+            dist += (long) (max - cnt) * (i + 1 << 1);
+            cnt = max;
         }
         return dist;
     }
