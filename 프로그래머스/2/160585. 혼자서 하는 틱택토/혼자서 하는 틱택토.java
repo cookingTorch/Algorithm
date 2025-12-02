fa416abf15ac683c6ee300e2fa6b19c83715bd39
cookingTorch <jhw7181@naver.com>
2025-12-02 17:54:12 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.04 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
@@ -1,65 +1,60 @@
 class Solution {
-    private static final char O = 'O';
-    private static final char X = 'X';
+    private static final int O = 'O';
+    private static final int X = 'X';
+    private static final int SIZE = 3;
 
     public int solution(String[] board) {
         int i;
         int j;
-        int lO;
-        int lX;
-        int rO;
-        int rX;
-        int rowO;
-        int rowX;
-        int colO;
-        int colX;
-        int cntO;
-        int cntX;
+        int ch;
+        int cnt;
+        boolean lO;
+        boolean lX;
+        boolean rO;
+        boolean rX;
+        boolean rowO;
+        boolean rowX;
+        boolean colO;
+        boolean colX;
         boolean winO;
         boolean winX;
 
-        lO = 0;
-        lX = 0;
-        rO = 0;
-        rX = 0;
-        cntO = 0;
-        cntX = 0;
+        cnt = 0;
         winO = false;
         winX = false;
-        for (i = 0; i < 3; i++) {
-            rowO = 0;
-            rowX = 0;
-            colO = 0;
-            colX = 0;
-            for (j = 0; j < 3; j++) {
-                if (board[i].charAt(j) == O) {
-                    rowO++;
-                } else if (board[i].charAt(j) == X) {
-                    rowX++;
+        lO = true;
+        lX = true;
+        rO = true;
+        rX = true;
+        for (i = 0; i < SIZE; i++) {
+            rowO = true;
+            rowX = true;
+            colO = true;
+            colX = true;
+            for (j = 0; j < SIZE; j++) {
+                ch = board[i].charAt(j);
+                rowO &= ch == O;
+                rowX &= ch == X;
+                ch = board[j].charAt(i);
+                colO &= ch == O;
+                colX &= ch == X;
+                if (ch == O) {
+                    cnt++;
+                } else if (ch == X) {
+                    cnt--;
                 }
-                if (board[j].charAt(i) == O) {
-                    colO++;
-                } else if (board[j].charAt(i) == X) {
-                    colX++;
-                }
             }
-            if (board[i].charAt(i) == O) {
-                lO++;
-            } else if (board[i].charAt(i) == X) {
-                lX++;
-            }
-            if (board[i].charAt(2 - i) == O) {
-                rO++;
-            } else if (board[i].charAt(2 - i) == X) {
-                rX++;
-            }
-            cntO += rowO;
-            cntX += rowX;
-            winO |= rowO == 3 || colO == 3;
-            winX |= rowX == 3 || colX == 3;
+            ch = board[i].charAt(i);
+            lO &= ch == O;
+            lX &= ch == X;
+            ch = board[i].charAt(2 - i);
+            rO &= ch == O;
+            rX &= ch == X;
+            winO |= rowO || colO;
+            winX |= rowX || colX;
         }
-        winO |= lO == 3 || rO == 3;
-        winX |= lX == 3 || rX == 3;
-        return (cntO == cntX && !winO) || (cntO - cntX == 1 && !winX) ? 1 : 0;
+        winO |= lO || rO;
+        winX |= lX || rX;
+        return (cnt == 0 && !winO) || (cnt == 1 && !winX) ? 1 : 0;
     }
 }
