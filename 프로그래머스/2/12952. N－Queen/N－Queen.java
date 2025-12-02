f4a874d208f345e63281b592fde8f198f46b8cf3
cookingTorch <jhw7181@naver.com>
2025-12-02 10:36:25 +0900

    [level 2] Title: N-Queen, Time: 6.00 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -4,26 +4,34 @@ class Solution {
     private static int range;
 
     private static void dfs(int l, int m, int r, int depth) {
-        int i;
         int e;
+        int i;
 
         if (depth == size) {
             cnt++;
             return;
         }
-        l = l << 1 & range;
-        r >>= 1;
-        e = range ^ (l | m | r);
+        e = range & ~(l | m | r);
         for (i = e & -e; e != 0; e ^= i, i = e & -e) {
-            dfs(l | i, m | i, r | i, depth + 1);
+            dfs((l | i) << 1, m | i, (r | i) >> 1, depth + 1);
         }
     }
 
     public int solution(int n) {
+        int i;
+        int mid;
+
         cnt = 0;
         size = n;
         range = (1 << n) - 1;
-        dfs(0, 0, 0, 0);
+        mid = 1 << (n >> 1);
+        for (i = 1; i < mid; i++) {
+            dfs(i << 1, i, i >> 1, 1);
+        }
+        cnt <<= 1;
+        if ((n & 1) != 0) {
+            dfs(mid << 1, mid, mid >> 1, 1);
+        }
         return cnt;
     }
 }
