a724dfb63ec52b6e9bc6797e6926ed1990092934
cookingTorch <jhw7181@naver.com>
2025-12-03 14:59:06 +0900

    [level 2] Title: 숫자 블록, Time: 107.92 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
@@ -1,39 +1,37 @@
 class Solution {
     private static final int MAX = 10_000_000;
-    
-    private static int calc(int n) {
-        int i;
-        int res;
-        
-        res = 1;
-        for (i = 2; i * i <= n; i++) {
-            if (n % i == 0) {
-                if (n / i <= MAX) {
-                    return n / i;
-                } else {
-                    res = i;
-                }
-            }
-        }
-        return res;
-    }
 
     public int[] solution(long begin, long end) {
-        int s;
-        int e;
         int i;
-        int size;
+        int j;
+        int len;
+        int num;
+        int thr;
+        int sqrt;
+        int start;
         int[] ans;
 
-        s = (int) begin;
-        e = (int) end;
-        size = e - s + 1;
-        ans = new int[size];
-        for (i = 0; i < size; i++) {
-            ans[i] = calc(s + i);
-        }
-        if (s == 1) {
-            ans[0] = 0;
+        start = (int) begin;
+        len = (int) end - start + 1;
+        ans = new int[len];
+        loop:
+        for (i = start == 1 ? 1 : 0; i < len; i++) {
+            ans[i] = 1;
+            num = start + i;
+            sqrt = (int) Math.sqrt(num);
+            thr = Math.min(sqrt, (num + MAX - 1) / MAX);
+            for (j = Math.max(thr, 2); j <= sqrt; j++) {
+                if (num % j == 0) {
+                    ans[i] = num / j;
+                    continue loop;
+                }
+            }
+            for (j = thr - 1; j > 1; j--) {
+                if (num % j == 0) {
+                    ans[i] = j;
+                    break;
+                }
+            }
         }
         return ans;
     }
