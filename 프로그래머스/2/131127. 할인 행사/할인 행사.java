5886933c0e76bfdff2bc4262271c5025b2d6bde5
cookingTorch <jhw7181@naver.com>
2025-12-13 15:54:46 +0900

    [level 2] Title: 할인 행사, Time: 20.84 ms, Memory: 110 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,51 @@
+import java.util.HashMap;
+
+class Solution {
+	private static final int RANGE = 10;
+
+	public int solution(String[] want, int[] number, String[] discount) {
+		int i;
+		int ans;
+		int len;
+		int size;
+		int item;
+		int match;
+		int[] arr;
+		int[] num;
+		int[] cnt;
+		HashMap<String, Integer> map;
+
+		map = new HashMap<>();
+		len = want.length;
+		num = new int[len + 1];
+		for (i = 0; i < len; i++) {
+			num[i] = number[i];
+			map.put(want[i], i);
+		}
+		num[len] = -1;
+		arr = new int[RANGE];
+		cnt = new int[len + 1];
+		match = 0;
+		for (i = 0; i < RANGE; i++) {
+			item = arr[i] = map.getOrDefault(discount[i], len);
+			if (++cnt[item] == num[item]) {
+				match++;
+			}
+		}
+		ans = match == len ? 1 : 0;
+		size = discount.length;
+		for (; i < size; i++) {
+			item = arr[i % RANGE];
+			if (cnt[item]-- == num[item]) {
+				match--;
+			}
+			item = arr[i % RANGE] = map.getOrDefault(discount[i], len);
+			if (++cnt[item] == num[item]) {
+				if (++match == len) {
+					ans++;
+				}
+			}
+		}
+		return ans;
+	}
+}
