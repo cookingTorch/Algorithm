b2e80a7ca480bdf9652de7d02e4bddc94a166318
cookingTorch <jhw7181@naver.com>
2025-12-17 19:11:20 +0900

    [level 2] Title: [1차] 캐시, Time: 33.72 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,41 +1,41 @@
 import java.util.HashMap;
 
 class Solution {
-	private static final int MISS = 5;
+    private static final int MISS = 5;
 
-	public int solution(int cacheSize, String[] cities) {
-		int l;
-		int r;
-		int len;
-		int idx;
-		int cnt;
-		int time;
-		boolean[] out;
-		HashMap<String, Integer> map;
+    public int solution(int cacheSize, String[] cities) {
+        int l;
+        int r;
+        int len;
+        int idx;
+        int cnt;
+        int time;
+        boolean[] out;
+        HashMap<String, Integer> map;
 
-		map = new HashMap<>();
-		len = cities.length;
-		out = new boolean[len + 1];
-		out[len] = true;
-		l = 0;
-		cnt = 0;
-		time = 0;
-		for (r = 0; r < len; r++) {
-			idx = map.getOrDefault(cities[r] = cities[r].toLowerCase(), len);
-			map.put(cities[r], r);
-			if (out[idx]) {
-				if (cnt == cacheSize) {
-					for (; out[l]; l++);
-					out[l++] = true;
-				} else {
-					cnt++;
-				}
-				time += MISS;
-			} else {
-				out[idx] = true;
-				time++;
-			}
-		}
-		return time;
-	}
+        map = new HashMap<>();
+        len = cities.length;
+        out = new boolean[len + 1];
+        out[len] = true;
+        l = 0;
+        cnt = 0;
+        time = 0;
+        for (r = 0; r < len; r++) {
+            idx = map.getOrDefault(cities[r] = cities[r].toLowerCase(), len);
+            map.put(cities[r], r);
+            if (out[idx]) {
+                if (cnt == cacheSize) {
+                    for (; out[l]; l++);
+                    out[l++] = true;
+                } else {
+                    cnt++;
+                }
+                time += MISS;
+            } else {
+                out[idx] = true;
+                time++;
+            }
+        }
+        return time;
+    }
 }
