f6569648cef13173163d63b96184c0c1fcd31842
cookingTorch <jhw7181@naver.com>
2025-12-24 18:16:26 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 5.59 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,46 +1,46 @@
 class Solution {
-    private static boolean isPrime(long n) {
-        long i;
+	private static boolean isPrime(long n) {
+		long i;
 
-        if (n == 3L) {
-            return true;
-        }
-        if (n < 2L || n % 3L == 0L) {
-            return false;
-        }
-        for (i = 5L; i * i <= n; i += 6L) {
-            if (n % i == 0L || n % (i + 2L) == 0L) {
-                return false;
-            }
-        }
-        return true;
-    }
+		if (n == 2L || n == 3L) {
+			return true;
+		}
+		if ((n & 1L) == 0L || n % 3L == 0L || n == 1L) {
+			return false;
+		}
+		for (i = 5L; i * i <= n; i += 6L) {
+			if (n % i == 0L || n % (i + 2L) == 0L) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-    public int solution(int n, int k) {
-        int r;
-        int pow;
-        int cnt;
-        long num;
+	public int solution(int n, int k) {
+		int r;
+		int idx;
+		int cnt;
+		long num;
 
-        cnt = 0;
-        pow = 0;
-        num = 0L;
-        for (; n != 0;) {
-            r = n % k;
-            if (r == 0) {
-                if (isPrime(num)) {
-                    cnt++;
-                }
-                pow = 0;
-                num = 0L;
-            } else {
-                num += r * (long) Math.pow(10, pow++);
-            }
-            n /= k;
-        }
-        if (isPrime(num)) {
-            cnt++;
-        }
-        return cnt;
-    }
+		num = 0L;
+		idx = 0;
+		cnt = 0;
+		while (n != 0) {
+			r = n % k;
+			if (r == 0) {
+				if (isPrime(num)) {
+					cnt++;
+				}
+				num = 0L;
+				idx = 0;
+			} else {
+				num += r * (long) Math.pow(10, idx++);
+			}
+			n /= k;
+		}
+		if (isPrime(num)) {
+			cnt++;
+		}
+		return cnt;
+	}
 }
