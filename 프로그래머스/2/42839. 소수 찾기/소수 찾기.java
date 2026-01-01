ae96750007e6922e62cd2b013ae10bfb7c71ae52
cookingTorch <jhw7181@naver.com>
2026-01-02 08:49:51 +0900

    [level 2] Title: 소수 찾기, Time: 3.32 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,87 +1,69 @@
+import java.util.HashSet;
+
 class Solution {
-    private static final int DIFF = '0';
+	private static final int DIFF = '0';
 
-    private static int cnt;
-    private static int len;
-    private static int last;
-    private static int[] nums;
-    private static boolean[] used;
+	private static int cnt;
+	private static int len;
+	private static int[] nums;
+	private static boolean[] visited;
+	private static HashSet<Integer> set;
 
-    private static boolean isPrime(int n) {
-        int i;
+	private static boolean isPrime(int n) {
+		int i;
 
-        if (n % 3 == 0) {
-            return false;
-        }
-        for (i = 5; i * i <= n; i += 6) {
-            if (n % i == 0 || n % (i + 2) == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
+		if (n == 2 || n == 3) {
+			return true;
+		}
+		if (n < 2 || (n & 1) == 0 || n % 3 == 0) {
+			return false;
+		}
+		for (i = 5; i * i <= n; i += 6) {
+			if (n % i == 0 || n % (i + 2) == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-    private static void dfs(int cur, int depth) {
-        int i;
-        int num;
-        int visited;
+	private static void dfs(int num, int depth) {
+		int i;
 
-        cur *= 10;
-        if (isPrime(cur + last)) {
-            cnt++;
-        }
-        if (depth == len) {
-            return;
-        }
-        visited = 0;
-        for (i = 0; i < len; i++) {
-            num = nums[i];
-            if (!used[i] && (visited & 1 << num) == 0) {
-                visited |= 1 << num;
-                used[i] = true;
-                dfs(cur + num, depth + 1);
-                used[i] = false;
-            }
-        }
-    }
+		if (!set.contains(num)) {
+			if (isPrime(num)) {
+				cnt++;
+			}
+			set.add(num);
+		}
+		if (depth++ == len) {
+			return;
+		}
+		num *= 10;
+		for (i = 0; i < len; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				dfs(num + nums[i], depth);
+				visited[i] = false;
+			}
+		}
+	}
 
-    public int solution(String numbers) {
-        int i;
-        int j;
-        int first;
-        int visLast;
-        int visFirst;
+	public int solution(String numbers) {
+		int i;
 
-        len = numbers.length();
-        nums = new int[len];
-        for (i = 0; i < len; i++) {
-            nums[i] = numbers.charAt(i) - DIFF;
-        }
-        visLast = 0;
-        used = new boolean[len];
-        for (i = 0; i < len; i++) {
-            last = nums[i];
-            if ((visLast & 1 << last) != 0) {
-                continue;
-            }
-            visLast |= 1 << last;
-            if ((last & 1) == 0) {
-                continue;
-            }
-            used[i] = true;
-            visFirst = 1;
-            for (j = 0; j < len; j++) {
-                first = nums[j];
-                if (j == i || (visFirst & 1 << first) != 0) {
-                    continue;
-                }
-                visFirst |= 1 << first;
-                used[j] = true;
-                dfs(first, 2);
-                used[j] = false;
-            }
-            used[i] = false;
-        }
-        return cnt + Integer.bitCount(visLast & (1 << 2 | 1 << 3 | 1 << 5 | 1 << 7));
-    }
+		len = numbers.length();
+		nums = new int[len];
+		for (i = 0; i < len; i++) {
+			nums[i] = numbers.charAt(i) - DIFF;
+		}
+		visited = new boolean[len];
+		set = new HashSet<>();
+		cnt = 0;
+		for (i = 0; i < len; i++) {
+			visited[i] = true;
+			dfs(nums[i], 1);
+			visited[i] = false;
+		}
+		return cnt;
+	}
 }
