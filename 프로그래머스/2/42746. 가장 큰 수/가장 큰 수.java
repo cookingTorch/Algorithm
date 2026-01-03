57f0fc91e1407460fd771bb11f8be57b4e2e20b2
cookingTorch <jhw7181@naver.com>
2026-01-03 17:46:07 +0900

    [level 2] Title: 가장 큰 수, Time: 179.40 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -14,26 +14,7 @@ class Solution {
         for (i = 0; i < len; i++) {
             arr[i] = Integer.toString(numbers[i]);
         }
-        Arrays.sort(arr, 0, len, (o1, o2) -> {
-            int i1;
-            int len1;
-            int len2;
-            int total;
-            char c1;
-            char c2;
-
-            len1 = o1.length();
-            len2 = o2.length();
-            total = len1 + len2;
-            for (i1 = 0; i1 < total; i1++) {
-                c1 = i1 < len1 ? o1.charAt(i1) : o2.charAt(i1 - len1);
-                c2 = i1 < len2 ? o2.charAt(i1) : o1.charAt(i1 - len2);
-                if (c1 != c2) {
-                    return c2 - c1;
-                }
-            }
-            return 0;
-        });
+        Arrays.sort(arr, 0, len, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
         sb = new StringBuilder();
         if (arr[0].charAt(0) == ZERO) {
             sb.append(ZERO);
