f70ad45d696e71fa801485f42acfccfa9fb885ae
cookingTorch <jhw7181@naver.com>
2026-01-03 21:04:36 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 23.01 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,35 +1,31 @@
 class Solution {
-    private static final int ONE = (1 << 4) - 1;
-    private static final int ZERO = 0;
-    private static final int FAIL = -1;
+	private static final int FAIL = -1;
 
-    private static int[] ans;
-    private static int[][] map;
+	public int[] solution(int[][] arr) {
+		int i;
+		int j;
+		int len;
+		int[] ans;
 
-    private static int dfs(int x, int y, int size) {
-        int res;
-
-        if (size == 1) {
-            ans[map[x][y]]++;
-            return map[x][y];
-        }
-        size >>= 1;
-        res = ((dfs(x, y, size) << 1 | dfs(x, y + size, size)) << 1 | dfs(x + size, y, size)) << 1 | dfs(x + size, y + size, size);
-        if (res == ZERO) {
-            ans[0] -= 3;
-            return 0;
-        } else if (res == ONE) {
-            ans[1] -= 3;
-            return 1;
-        } else {
-            return FAIL;
-        }
-    }
-
-    public int[] solution(int[][] arr) {
-        map = arr;
-        ans = new int[2];
-        dfs(0, 0, arr.length);
-        return ans;
-    }
+		ans = new int[2];
+		len = arr.length;
+		for (i = 0; i < len; i++) {
+			for (j = 0; j < len; j++) {
+				ans[arr[i][j]]++;
+			}
+		}
+		for (; len > 1; len >>>= 1) {
+			for (i = 0; i < len; i += 2) {
+				for (j = 0; j < len; j += 2) {
+					if (arr[i][j] != FAIL && arr[i][j] == arr[i][j + 1] && arr[i][j] == arr[i + 1][j] && arr[i][j] == arr[i + 1][j + 1]) {
+						arr[i >>> 1][j >>> 1] = arr[i][j];
+						ans[arr[i][j]] -= 3;
+					} else {
+						arr[i >>> 1][j >>> 1] = FAIL;
+					}
+				}
+			}
+		}
+		return ans;
+	}
 }
