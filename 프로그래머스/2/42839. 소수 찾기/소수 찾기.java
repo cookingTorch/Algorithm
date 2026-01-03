97238eb2cebeb10b12dc11468404105e0d0d2182
cookingTorch <jhw7181@naver.com>
2026-01-03 23:03:01 +0900

    [level 2] Title: 소수 찾기, Time: 0.53 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,11 +1,12 @@
 class Solution {
     private static final int DIFF = '0';
     private static final int RADIX = 10;
-    private static final int[] PRIMES = new int[] {2, 3, 5, 7};
 
     private static int ans;
     private static int len;
+    private static int size;
     private static int last;
+    private static int[] nums;
     private static int[] cnts;
 
     private static boolean isPrime(int n) {
@@ -32,10 +33,10 @@ class Solution {
         if (depth++ == len) {
             return;
         }
-        for (i = 0; i < RADIX; i++) {
+        for (i = 0; i < size; i++) {
             if (cnts[i] > 0) {
                 cnts[i]--;
-                dfs(num + i, depth);
+                dfs(num + nums[i], depth);
                 cnts[i]++;
             }
         }
@@ -45,25 +46,31 @@ class Solution {
         int i;
         int j;
 
-        ans = 0;
         cnts = new int[RADIX];
         len = numbers.length();
         for (i = 0; i < len; i++) {
             cnts[numbers.charAt(i) - DIFF]++;
         }
-        for (int k : PRIMES) {
-            if (cnts[k] > 0) {
-                ans++;
+        ans = 0;
+        size = 0;
+        nums = new int[len];
+        for (i = 0; i < RADIX; i++) {
+            if (cnts[i] > 0) {
+                nums[size] = i;
+                cnts[size++] = cnts[i];
+                if (i == 2 || i == 3 || i == 5 || i == 7) {
+                    ans++;
+                }
             }
         }
-        for (i = 1; i < RADIX; i++) {
+        for (i = nums[0] == 0 ? 1 : 0; i < size; i++) {
             if (cnts[i] > 0) {
                 cnts[i]--;
-                for (j = 1; j < RADIX; j += 2) {
-                    if (cnts[j] > 0) {
+                for (j = 0; j < size; j++) {
+                    if ((nums[j] & 1) != 0 && cnts[j] > 0) {
                         cnts[j]--;
-                        last = j;
-                        dfs(i, 2);
+                        last = nums[j];
+                        dfs(nums[i], 2);
                         cnts[j]++;
                     }
                 }
