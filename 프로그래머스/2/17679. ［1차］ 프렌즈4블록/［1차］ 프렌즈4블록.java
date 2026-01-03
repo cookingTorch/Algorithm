10510880de1c760d66e3d0bacdf07a2ee9b304a1
cookingTorch <jhw7181@naver.com>
2026-01-03 17:27:28 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 1.66 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -1,91 +1,75 @@
-import java.util.ArrayDeque;
-
 class Solution {
-    private static final int EMPTY = 0;
-    
-    private static int c;
-    private static int cnt;
-    private static int[][] map;
-    private static boolean end;
-    private static boolean[][] delete;
-    private static ArrayDeque<Integer> q;
-    
-    private static void init(int n, int m, String[] board) {
-        int i;
-        int j;
+	private static final char EMPTY = '\0';
 
-        cnt = 0;
-        map = new int[n][m];
-        delete = new boolean[n][m];
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < m; j++) {
-                map[i][j] = board[c - j].charAt(i);
-            }
-        }
-        q = new ArrayDeque<>(m);
-    }
-    
-    private static boolean find(int i, int j) {
-        int ch;
+	private static boolean find(char[] prevRow, char[] row, boolean[] prevDel, boolean[] del) {
+		int i;
+		boolean match;
 
-        ch = map[i][j];
-        if (ch == EMPTY) {
-            return true;
-        }
-        if (map[i][j + 1] == ch && map[i + 1][j] == ch && map[i + 1][j + 1] == ch) {
-            delete[i][j] = true;
-            delete[i][j + 1] = true;
-            delete[i + 1][j] = true;
-            delete[i + 1][j + 1] = true;
-            end = false;
-        }
-        return false;
-    }
-    
-    private static void move(int[] map, boolean[] delete) {
-        int i;
-        
-        for (i = 0; i <= c; i++) {
-            if (map[i] == EMPTY) {
-                break;
-            }
-            if (delete[i]) {
-                cnt++;
-                delete[i] = false;
-            } else {
-                q.addLast(map[i]);
-            }
-        }
-        for (i = 0; !q.isEmpty(); i++) {
-            map[i] = q.pollFirst();
-        }
-        for (; i <= c; i++) {
-            map[i] = EMPTY;
-        }
-    }
+		match = false;
+		for (i = 1; row[i] != EMPTY; i++) {
+			if (row[i] == prevRow[i] && row[i] == row[i - 1] && row[i] == prevRow[i - 1]) {
+				prevDel[i - 1] = true;
+				prevDel[i] = true;
+				del[i - 1] = true;
+				del[i] = true;
+				match = true;
+			}
+		}
+		return match;
+	}
 
-    public int solution(int m, int n, String[] board) {
-        int r;
-        int i;
-        int j;
+	private static int fall(char[] row, boolean[] del) {
+		int l;
+		int r;
+		int cnt;
 
-        r = n - 1;
-        c = m - 1;
-        init(n, m, board);
-        end = false;
-        while (!end) {
-            end = true;
-            for (i = 0; i < r; i++) {
-                for (j = 0; j < c; j++) {
-                    if (find(i, j)) {
-                        break;
-                    }
-                }
-            }
-            for (i = 0; i < n; i++) {
-                move(map[i], delete[i]);
-            }
-        }
-        return cnt;
-    }
+		for (r = 0; row[r] != EMPTY && !del[r]; r++);
+		l = r;
+		cnt = 0;
+		for (; row[r] != EMPTY; r++) {
+			if (del[r]) {
+				cnt++;
+				del[r] = false;
+			} else {
+				row[l++] = row[r];
+			}
+		}
+		while (l < r) {
+			row[l++] = EMPTY;
+		}
+		return cnt;
+	}
+
+	public int solution(int m, int n, String[] board) {
+		int i;
+		int j;
+		int cnt;
+		char[][] map;
+		boolean end;
+		boolean[][] del;
+
+		map = new char[n][m + 1];
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < m; j++) {
+				map[i][j] = board[m - 1 - j].charAt(i);
+			}
+		}
+		cnt = 0;
+		del = new boolean[n][m];
+		for (;;) {
+			end = true;
+			for (i = 1; i < n; i++) {
+				if (find(map[i - 1], map[i], del[i - 1], del[i])) {
+					end = false;
+				}
+			}
+			if (end) {
+				break;
+			}
+			for (i = 0; i < n; i++) {
+				cnt += fall(map[i], del[i]);
+			}
+		}
+		return cnt;
+	}
 }
