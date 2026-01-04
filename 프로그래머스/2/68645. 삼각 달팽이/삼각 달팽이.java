1459a703df64e72fa57a38a80912d342dcecebc7
cookingTorch <jhw7181@naver.com>
2026-01-05 00:49:33 +0900

    [level 2] Title: 삼각 달팽이, Time: 5.56 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,69 +1,32 @@
 class Solution {
-    private static final int EMPTY = 0;
-
-    private static void fill(int[][] arr, int n) {
-        int x;
-        int y;
-        int i;
-        int j;
-        int num;
-
-        num = 1;
-        for (i = 0; i < n; i++) {
-            arr[i][0] = num++;
-        }
-        for (i = 1; i < n; i++) {
-            arr[n - 1][i] = num++;
-        }
-        for (i = n - 2; i > 0; i--) {
-            arr[i][i] = num++;
-        }
-        if (n <= 3) {
-            return;
-        }
-        for (x = 1, y = 1;;) {
-            for (i = x + 1; arr[i][y] == EMPTY; i++) {
-                arr[i][y] = num++;
-            }
-            if (--i == x) {
-                break;
-            }
-            x = i;
-            for (j = y + 1; arr[x][j] == EMPTY; j++) {
-                arr[x][j] = num++;
-            }
-            if (--j == y) {
-                break;
-            }
-            y = j;
-            for (i = x - 1, j = y - 1; arr[i][j] == EMPTY; i--, j--) {
-                arr[i][j] = num++;
-            }
-            if (++i == x) {
-                break;
-            }
-            x = i;
-            y = ++j;
-        }
-    }
-
     public int[] solution(int n) {
         int i;
-        int j;
         int idx;
+        int num;
+        int len;
+        int level;
         int[] ans;
-        int[][] arr;
 
-        arr = new int[n][];
-        for (i = 0; i < n; i++) {
-            arr[i] = new int[i + 1];
-        }
-        fill(arr, n);
-        ans = new int[n * (n + 1) >>> 1];
+        num = 1;
         idx = 0;
-        for (i = 0; i < n; i++) {
-            System.arraycopy(arr[i], 0, ans, idx, i + 1);
-            idx += i + 1;
+        level = 0;
+        ans = new int[n * (n + 1) >>> 1];
+        for (len = n; len != 0; len--) {
+            for (i = 0; i < len; i++) {
+                ans[idx += level++] = num++;
+            }
+            if (--len == 0) {
+                break;
+            }
+            for (i = 0; i < len; i++) {
+                ans[++idx] = num++;
+            }
+            if (--len == 0) {
+                break;
+            }
+            for (i = len; i > 0; i--) {
+                ans[idx -= level--] = num++;
+            }
         }
         return ans;
     }
