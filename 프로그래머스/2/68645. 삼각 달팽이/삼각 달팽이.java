3c24873992332f4c0b453d0758b6787cc227ed86
cookingTorch <jhw7181@naver.com>
2026-01-05 00:51:40 +0900

    [level 2] Title: 삼각 달팽이, Time: 6.09 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -7,25 +7,25 @@ class Solution {
         int level;
         int[] ans;
 
-        num = 1;
+        num = 0;
         idx = 0;
         level = 0;
         ans = new int[n * (n + 1) >>> 1];
         for (len = n; len != 0; len--) {
             for (i = 0; i < len; i++) {
-                ans[idx += level++] = num++;
+                ans[idx += level++] = ++num;
             }
             if (--len == 0) {
                 break;
             }
             for (i = 0; i < len; i++) {
-                ans[++idx] = num++;
+                ans[++idx] = ++num;
             }
             if (--len == 0) {
                 break;
             }
             for (i = len; i > 0; i--) {
-                ans[idx -= level--] = num++;
+                ans[idx -= level--] = ++num;
             }
         }
         return ans;
