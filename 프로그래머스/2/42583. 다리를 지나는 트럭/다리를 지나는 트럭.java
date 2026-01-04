035ae778ff9a0e6b3602dc46a3b96629e23b94fa
cookingTorch <jhw7181@naver.com>
2026-01-04 12:09:28 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.04 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,27 +1,30 @@
 class Solution {
-    public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int i;
-        int len;
-        int head;
-        int time;
-        int[] start;
-
-        len = truck_weights.length;
-        start = new int[len];
-        start[0] = time = 1;
-        weight -= truck_weights[0];
-        head = 0;
-        for (i = 1; i < len; i++) {
-            if (++time - start[head] == bridge_length) {
-                weight += truck_weights[head++];
-            }
-            while (weight < truck_weights[i]) {
-                time = start[head] + bridge_length;
-                weight += truck_weights[head++];
-            }
-            start[i] = time;
-            weight -= truck_weights[i];
-        }
-        return start[len - 1] + bridge_length;
-    }
+	public int solution(int bridge_length, int weight, int[] truck_weights) {
+		int l;
+		int r;
+		int len;
+		int time;
+		int truck;
+		int[] end;
+		
+		len = truck_weights.length;
+		end = new int[len];
+		l = 0;
+		time = 1;
+		end[l] = time + bridge_length;
+		weight -= truck_weights[l];
+		for (r = 1; r < len; r++) {
+			if (++time == end[l]) {
+				weight += truck_weights[l++];
+			}
+			truck = truck_weights[r];
+			while (weight < truck) {
+				time = end[l];
+				weight += truck_weights[l++];
+			}
+			end[r] = time + bridge_length;
+			weight -= truck_weights[r];
+		}
+		return end[len - 1];
+	}
 }
