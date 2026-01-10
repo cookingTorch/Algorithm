b5a6811e794c0b551ea37a7880c868b3399670f6
cookingTorch <jhw7181@naver.com>
2026-01-10 19:03:11 +0900

    [level 2] Title: 호텔 대실, Time: 0.68 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,7 +1,7 @@
 class Solution {
     private static final int DIFF = '0' * 671;
     private static final int SIZE = timeToInt("23:59");
-    
+
     private static int timeToInt(String time) {
         return time.charAt(0) * 600 + time.charAt(1) * 60 + time.charAt(3) * 10 + time.charAt(4) - DIFF;
     }
@@ -10,17 +10,18 @@ class Solution {
         int i;
         int len;
         int max;
-        int[] cnt;
-        
-        cnt = new int[SIZE + 11];
+        int cnt;
+        int[] diff;
+
+        diff = new int[SIZE + 11];
         len = book_time.length;
         for (i = 0; i < len; i++) {
-            cnt[timeToInt(book_time[i][0])]++;
-            cnt[timeToInt(book_time[i][1]) + 10]--;
+            diff[timeToInt(book_time[i][0])]++;
+            diff[timeToInt(book_time[i][1]) + 10]--;
         }
-        max = cnt[0];
-        for (i = 1; i < SIZE; i++) {
-            max = Math.max(max, cnt[i] += cnt[i - 1]);
+        max = cnt = 0;
+        for (i = 0; i < SIZE; i++) {
+            max = Math.max(max, cnt += diff[i]);
         }
         return max;
     }
