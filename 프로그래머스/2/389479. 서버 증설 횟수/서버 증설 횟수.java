b2c51b5b9cc7d6e8f7d9e143faf7913884d79ca2
cookingTorch <jhw7181@naver.com>
2026-01-10 20:20:36 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.09 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,18 +1,18 @@
 class Solution {
-    private static final int DAY = 24;
-
     public int solution(int[] players, int m, int k) {
         int i;
+        int on;
+        int len;
         int cnt;
-        int cur;
         int[] off;
 
+        on = 0;
         cnt = 0;
-        cur = 0;
-        off = new int[DAY << 1];
-        for (i = 0; i < DAY; i++) {
-            cur -= off[i];
-            cur += off[i + k] = Math.max(0, players[i] / m - cur);
+        len = players.length;
+        off = new int[len + k];
+        for (i = 0; i < len; i++) {
+            on -= off[i];
+            on += off[i + k] = Math.max(0, players[i] / m - on);
             cnt += off[i + k];
         }
         return cnt;
