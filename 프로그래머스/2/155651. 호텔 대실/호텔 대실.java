41b2963be104a43dba783d5ab5287a9d3eae9a6d
cookingTorch <jhw7181@naver.com>
2026-01-10 17:21:01 +0900

    [level 2] Title: 호텔 대실, Time: 0.88 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,36 +1,26 @@
-import java.util.Arrays;
-
 class Solution {
-    private static final int DIFF = '0';
-
-    private static int timeToInt(String str) {
-        return (str.charAt(0) - DIFF) * 600 + (str.charAt(1) - DIFF) * 60 + (str.charAt(3) - DIFF) * 10 + (str.charAt(4) - DIFF);
+    private static final int DIFF = '0' * 671;
+    private static final int SIZE = timeToInt("23:59");
+    
+    private static int timeToInt(String time) {
+        return time.charAt(0) * 600 + time.charAt(1) * 60 + time.charAt(3) * 10 + time.charAt(4) - DIFF;
     }
 
     public int solution(String[][] book_time) {
         int i;
         int len;
-        int cur;
         int max;
-        int size;
-        int[] arr;
-
+        int[] cnt;
+        
+        cnt = new int[SIZE + 11];
         len = book_time.length;
-        size = len << 1;
-        arr = new int[size];
         for (i = 0; i < len; i++) {
-            arr[i] = timeToInt(book_time[i][0]) << 1 | 1;
-            arr[len + i] = timeToInt(book_time[i][1]) + 10 << 1;
+            cnt[timeToInt(book_time[i][0])]++;
+            cnt[timeToInt(book_time[i][1]) + 10]--;
         }
-        Arrays.sort(arr, 0, size);
-        cur = 0;
-        max = 0;
-        for (i = 0; i < size; i++) {
-            if ((arr[i] & 1) == 0) {
-                max = Math.max(max, cur--);
-            } else {
-                cur++;
-            }
+        max = cnt[0];
+        for (i = 1; i < SIZE; i++) {
+            max = Math.max(max, cnt[i] += cnt[i - 1]);
         }
         return max;
     }
