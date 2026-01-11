2a7bbaffe848d7e24cb8a672c40617ee7f3dcf5a
cookingTorch <jhw7181@naver.com>
2026-01-11 15:43:38 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 7.05 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -4,77 +4,66 @@ import java.util.HashMap;
 import java.util.Map.Entry;
 
 class Solution {
-    private static final int DIFF = 'A';
-    private static final int SIZE = 11;
+	private static final int SIZE = 11;
+	private static final int DIFF = 'A';
 
-    public String[] solution(String[] orders, int[] course) {
-        int i;
-        int j;
-        int len;
-        int bit;
-        int sub;
-        int cnt;
-        int size;
-        int[] max;
-        int[] order;
-        boolean[] lens;
-        String str;
-        String[] ans;
-        StringBuilder sb;
-        HashMap<Integer, Integer> map;
-        ArrayList<Integer> courses;
+	public String[] solution(String[] orders, int[] course) {
+		int i;
+		int j;
+		int len;
+		int sub;
+		int cnt;
+		int size;
+		int order;
+		int[] max;
+		char[] res;
+		boolean[] courses;
+		String str;
+		String[] ans;
+		HashMap<Integer, Integer> map;
+		ArrayList<Integer> list;
 
-        lens = new boolean[SIZE];
-        size = course.length;
-        for (i = 0; i < size; i++) {
-            lens[course[i]] = true;
-        }
-        size = orders.length;
-        order = new int[size];
-        for (i = 0; i < size; i++) {
-            bit = 0;
-            str = orders[i];
-            len = str.length();
-            for (j = 0; j < len; j++) {
-                bit |= 1 << str.charAt(j) - DIFF;
-            }
-            order[i] = bit;
-        }
-        max = new int[SIZE];
-        map = new HashMap<>();
-        for (i = 0; i < size; i++) {
-            for (j = 0; j < i; j++) {
-                bit = order[i] & order[j];
-                for (sub = bit; sub != 0; sub = sub - 1 & bit) {
-                    len = Integer.bitCount(sub);
-                    if (!lens[len]) {
-                        continue;
-                    }
-                    cnt = map.getOrDefault(sub, 0) + 1;
-                    map.put(sub, cnt);
-                    max[len] = Math.max(max[len], cnt);
-                }
-            }
-        }
-        courses = new ArrayList<>();
-        for (Entry<Integer, Integer> entry : map.entrySet()) {
-            bit = entry.getKey();
-            if (entry.getValue() == max[Integer.bitCount(bit)]) {
-                courses.add(bit);
-            }
-        }
-        size = courses.size();
-        ans = new String[size];
-        for (i = 0; i < size; i++) {
-            sb = new StringBuilder();
-            bit = courses.get(i);
-            for (; bit != 0; bit ^= 1 << cnt) {
-                cnt = Integer.numberOfTrailingZeros(bit);
-                sb.append((char) (cnt + DIFF));
-            }
-            ans[i] = sb.toString();
-        }
-        Arrays.sort(ans);
-        return ans;
-    }
+		courses = new boolean[SIZE];
+		size = course.length;
+		for (i = 0; i < size; i++) {
+			courses[course[i]] = true;
+		}
+		map = new HashMap<>();
+		max = new int[SIZE];
+		size = orders.length;
+		for (i = 0; i < size; i++) {
+			order = 0;
+			str = orders[i];
+			len = str.length();
+			for (j = 0; j < len; j++) {
+				order |= 1 << str.charAt(j) - DIFF;
+			}
+			for (sub = order; sub != 0; sub = sub - 1 & order) {
+				if (courses[len = Integer.bitCount(sub)]) {
+					max[len] = Math.max(max[len], map.merge(sub, 1, (v, nv) -> v + 1));
+				}
+			}
+		}
+		list = new ArrayList<>();
+		for (Entry entry : map.entrySet()) {
+			sub = (int) entry.getKey();
+			cnt = (int) entry.getValue();
+			if (cnt > 1 && cnt == max[Integer.bitCount(sub)]) {
+				list.add(sub);
+			}
+		}
+		size = list.size();
+		ans = new String[size];
+		res = new char[SIZE];
+		for (i = 0; i < size; i++) {
+			sub = list.get(i);
+			len = Integer.bitCount(sub);
+			for (j = 0; sub != 0; sub ^= sub & -sub, j++) {
+				res[j] = (char) (DIFF + Integer.numberOfTrailingZeros(sub));
+			}
+			ans[i] = new String(res, 0, len);
+		}
+		Arrays.sort(ans, 0, size);
+		return ans;
+	}
 }
