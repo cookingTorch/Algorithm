c5153b534b5e928d0bfd713d0e7b2e776568b8e3
cookingTorch <jhw7181@naver.com>
2026-01-27 22:23:15 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.09 ms, Memory: 98.1 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -1,7 +1,7 @@
 class Solution {
     private static int getGcd(int a, int b) {
         int r;
-        
+
         while (b != 0) {
             r = a % b;
             a = b;
@@ -9,8 +9,8 @@ class Solution {
         }
         return a;
     }
-    
+
     public long solution(int w, int h) {
-        return (long) w * h - (w + h - getGcd(w, h));
+        return (long) w * h - w - h + getGcd(w, h);
     }
 }
