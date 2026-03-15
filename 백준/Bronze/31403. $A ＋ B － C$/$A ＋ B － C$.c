eaad900b00abbddbeee42ab718227728f28c8ce6
cookingTorch <jhw7181@naver.com>
2026-03-15 13:33:29 +0900

    [Bronze IV] Title: $A + B - C$, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.c
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.c
@@ -10,10 +10,10 @@ int add(int n, char s[]) {
 }
 
 int main() {
-    int a, b, c;
+    int a, c;
     char sa[5], sb[5], sc[5];
 
     scanf("%s %s %s", sa, sb, sc);
-    printf("%d\n", (a = add(0, sa)) + (b = add(0, sb)) - (c = add(0, sc)));
+    printf("%d\n", (a = add(0, sa)) + add(0, sb) - (c = add(0, sc)));
     printf("%d", add(a, sb) - c);
 }
