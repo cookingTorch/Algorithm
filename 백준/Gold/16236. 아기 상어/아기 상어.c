745ddef85348c8b7033767897e97a2630317fa12
cookingTorch <jhw7181@naver.com>
2026-03-15 16:46:12 +0900

    [Gold III] Title: 아기 상어, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.c
+++ 백준/Gold/16236. 아기 상어/아기 상어.c
@@ -3,30 +3,22 @@
 int n, l, r, sx, sy, size = 2, cnt = 0, time = 0;
 int dx[] = {-1, 0, 0, 1};
 int dy[] = {0, -1, 1, 0};
-int map[22][22], visited[22][22], q[400][2];
-
-void add(int x, int y) {
-    q[++r][0] = x;
-    q[r][1] = y;
-}
-
-void poll(int* x, int* y) {
-    *x = q[l][0];
-    *y = q[l++][1];
-}
+int map[22][22], visited[22][22], qx[400], qy[400];
 
 int bfs() {
     int x, y, nx, ny, ex = 21, ey = 21, i, dist, len;
 
     time++;
     l = 0;
-    r = -1;
-    add(sx, sy);
+    r = 0;
+    qx[0] = sx;
+    qy[0] = sy;
     visited[sx][sy] = time;
     for (dist = 1; l <= r; dist++) {
         len = r - l + 1;
         while (len-- > 0) {
-            poll(&x, &y);
+            x = qx[l];
+            y = qy[l++];
             for (i = 0; i < 4; i++) {
                 nx = x + dx[i];
                 ny = y + dy[i];
@@ -34,7 +26,8 @@ int bfs() {
                     continue;
                 }
                 if (map[nx][ny] == 0 || map[nx][ny] == size) {
-                    add(nx, ny);
+                    qx[++r] = nx;
+                    qy[r] = ny;
                     visited[nx][ny] = time;
                 } else if (map[nx][ny] < size) {
                     if (nx < ex) {
